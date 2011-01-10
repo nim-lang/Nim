d7952244a5acc198e8349120f880aad880d2eb56
Araq <rumpf_a@web.de>
2011-01-10 21:28:48 +0100

    SPEC change: ^ is right-assoc and of highest priority

---
+++ tests/accept/run/toprprec.nim
@@ -0,0 +1,12 @@
+# Test operator precedence: 
+
+assert 3+5*5-2 == 28- -26-28
+
+proc `^-` (x, y: int): int =  
+  # now right-associative!
+  result = x - y
+  
+assert 34 ^- 6 ^- 2 == 30
+assert 34 - 6 - 2 == 26
+echo "done"
+
