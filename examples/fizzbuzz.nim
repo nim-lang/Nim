d0e4d5f00903111a42fa3b1d930317f385326db0
takano32 <mitsuhiro.takano@gree.co.jp>
2011-11-29 16:36:05 +0900

    sorry, i didnt test and commit...

--- examples/fizzbuzz.nim
+++ examples/fizzbuzz.nim
@@ -3,12 +3,12 @@
 const f = "Fizz"
 const b = "Buzz"
 for i in 1..100:
-  if i mod 3 == 0:
-    echo f
+  if i mod 15 == 0:
+    echo f, b
   elif i mod 5 == 0:
     echo b
-  elif i mod 15 == 0:
-    echo f, b
+  elif i mod 3 == 0:
+    echo f
   else:
     echo i
 
