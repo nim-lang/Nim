f8721ed20e93fc09ae95469f981cfa7fd0a807f4
Araq <rumpf_a@web.de>
2012-01-08 10:51:45 +0100

    adapted titer4 test because of new implicit items/pairs iterators

--- tests/reject/titer4.nim
+++ tests/reject/titer4.nim
@@ -3,8 +3,8 @@ discard """
   line: 7
   errormsg: "iterator within for loop context expected"
 """
-
-for x in {'a'..'z'}: #ERROR_MSG iterator within for loop context expected
+# implicit items/pairs, but not if we have 3 for loop vars:
+for x, y, z in {'a'..'z'}: #ERROR_MSG iterator within for loop context expected
   nil
 
 
