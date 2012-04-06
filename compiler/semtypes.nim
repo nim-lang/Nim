e941a147670442e706d1777f4a1bc368d0cbcdba
Zahary Karadjov <zahary@gmail.com>
2012-04-06 18:14:12 +0300

    completing the "types as first class values" reform

--- compiler/semtypes.nim
+++ compiler/semtypes.nim
@@ -693,7 +693,7 @@ proc semTypeNode(c: PContext, n: PNode, prev: PType): PType =
   of nkTypeOfExpr:
     # for ``type(countup(1,3))``, see ``tests/ttoseq``.
     checkSonsLen(n, 1)
-    result = semExprWithType(c, n.sons[0], {efInTypeof, efAllowType}).typ
+    result = semExprWithType(c, n.sons[0], {efInTypeof}).typ
   of nkPar: 
     if sonsLen(n) == 1: result = semTypeNode(c, n.sons[0], prev)
     else: GlobalError(n.info, errTypeExpected)
