97ab16d46b55aa9a4b2acbc72777fb6a8d5a162f
Zahary Karadjov <zahary@gmail.com>
2012-04-10 22:32:23 +0300

    typetraits module and tests

---
+++ lib/pure/typetraits.nim
@@ -0,0 +1,15 @@
+#
+#
+#            Nimrod's Runtime Library
+#        (c) Copyright 2012 Nimrod Contributors
+#
+#    See the file "copying.txt", included in this
+#    distribution, for details about the copyright.
+#
+
+## This module defines compile-time reflection procs for
+## working with types
+
+proc name*(t: typedesc): string {.magic: "TypeTrait".}
+  ## Returns the name of the given type
+
