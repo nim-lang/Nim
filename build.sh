90517b28088be2062034fa37471019a3575a76f0
Zahary Karadjov <zahary@gmail.com>
2012-06-14 19:58:47 +0300

    don't take into account the user config file when building sources

--- build.sh
+++ build.sh
@@ -28,7 +28,7 @@ done
 
 CC="gcc"
 LINKER="gcc"
-COMP_FLAGS="-w -g3 -O0 -O3 -fno-strict-aliasing$extraBuildArgs"
+COMP_FLAGS="-w -O3 -fno-strict-aliasing$extraBuildArgs"
 LINK_FLAGS=""
 # platform detection
 ucpu=`uname -m`
