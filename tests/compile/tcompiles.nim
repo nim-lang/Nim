49a0c0cb7bbc0cef85a151dbca4521e8654b3c3b
Araq <rumpf_a@web.de>
2012-07-20 16:53:01 +0200

    better test for 'system.compiles'

--- tests/compile/tcompiles.nim
+++ tests/compile/tcompiles.nim
@@ -1,12 +1,31 @@
 discard """
-  output: '''no'''
+  output: '''obj has '==': false
+int has '==': true
+false
+true
+true
+no'''
 """
 
 # test the new 'compiles' feature:
+
+template supports(opr, x: expr): bool {.immediate.} =
+  compiles(opr(x)) or compiles(opr(x, x))
+
+type
+  TObj = object
+
+var
+  myObj {.compileTime.}: TObj
+
+echo "obj has '==': ", supports(`==`, myObj)
+echo "int has '==': ", supports(`==`, 45)
+
+echo supports(`++`, 34)
+echo supports(`not`, true)
+echo supports(`+`, 34)
 
 when compiles(4+5.0 * "hallo"):
   echo "yes"
 else:
   echo "no"
-  
-
