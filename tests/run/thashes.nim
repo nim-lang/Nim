664bd55e852f237dec03913accedcc5291863048
Simon Hafner <hafnersimon@gmail.com>
2012-09-08 15:54:00 -0500

    real test now

--- tests/run/thashes.nim
+++ tests/run/thashes.nim
@@ -4,4 +4,5 @@ import hashes
 suite "hashes":
   suite "hashing":
     test "0.0 and -0.0 should have the same hash value":
-      check hash(0.0) == hash(-0.0)
+      var dummy = 0.0
+      check hash(dummy) == hash(-dummy)
