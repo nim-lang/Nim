92b449562e4815b69b07e74a4f37ff38e27c6a3d
Araq <rumpf_a@web.de>
2012-09-10 19:34:16 +0200

    tr macros now documented

--- tests/patterns/tor.nim
+++ tests/patterns/tor.nim
@@ -7,3 +7,14 @@ template testOr{ (arithOps{f})(a, b) }(a, b, f: expr): expr = f(a+1, b)
 
 let xx = 10
 echo 10*xx
+
+template t{x = (~x){y} and (~x){z}}(x, y, z: bool): stmt =
+  x = y
+  if x: x = z
+
+var
+  a = true
+  b = true
+  c = false
+a = b and a
+echo a
