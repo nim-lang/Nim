7c08d14cfd864ca66a2fc5aa64a4d3f8c94cd01a
Araq <rumpf_a@web.de>
2012-09-23 21:35:12 +0200

    preparations for 0.9.0

--- compiler/nversion.nim
+++ compiler/nversion.nim
@@ -14,9 +14,9 @@ const
   MaxSetElements* = 1 shl 16  # (2^16) to support unicode character sets?
   defaultAsmMarkerSymbol* = '!'
   VersionMajor* = 0
-  VersionMinor* = 8
-  VersionPatch* = 15
+  VersionMinor* = 9
+  VersionPatch* = 0
   VersionAsString* = $VersionMajor & "." & $VersionMinor & "." & $VersionPatch
 
-  RodFileVersion* = "1209"       # modify this if the rod-format changes!
+  RodFileVersion* = "1210"       # modify this if the rod-format changes!
 
