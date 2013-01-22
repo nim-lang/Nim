e273d406252a26d08f2d1128f4925bcf5f2edcac
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2013-01-22 20:29:07 +0100

    Renames each proc to map, each is left deprecated.

--- tests/compile/toverprc.nim
+++ tests/compile/toverprc.nim
@@ -21,7 +21,7 @@ proc takeParseInt(x: proc (y: string): int {.noSideEffect.}): int =
   result = x("123")
   
 echo "Give a list of numbers (separated by spaces): "
-var x = stdin.readline.split.each(parseInt).max
+var x = stdin.readline.split.map(parseInt).max
 echo x, " is the maximum!"
 echo "another number: ", takeParseInt(parseInt)
 
