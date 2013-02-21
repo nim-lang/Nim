5bf71d12c547f413b992e8704f85b2fab2938534
Simon Hafner <hafnersimon@gmail.com>
2013-02-21 14:29:39 -0600

    added tests for == and $

---
+++ tests/run/tobject.nim
@@ -0,0 +1,20 @@
+import unittest
+
+type Obj = object
+  foo: int
+
+proc makeObj(x: int): ref Obj = 
+  new(result)
+  result.foo = x
+
+proc initObject(x: int): Obj = 
+  result.foo = x
+
+suite "object basic methods":
+  test "it should convert an objcet to a string":
+    var obj = makeObj(1)
+    discard $obj
+  test "it should test equality based on fields":
+    check(initObj(1) == initObj(1))
+  test "it should test equality based on fields for refs too":
+    check(makeObj(1) == makeObj(1))
