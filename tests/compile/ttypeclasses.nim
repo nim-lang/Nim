de0c911e3efb3c643edd3bba7af5d381bdcf5537
Zahary Karadjov <zahary@gmail.com>
2013-05-26 22:15:30 +0300

    test cases for the new features

--- tests/compile/ttypeclasses.nim
+++ tests/compile/ttypeclasses.nim
@@ -5,6 +5,8 @@ type
   T1 = expr
   T2 = expr
 
+  Numeric = int|float
+
 proc takesExpr(x, y) =
   echo x, y
 
@@ -31,4 +33,23 @@ takesFoo(f, f)
 
 takes2Types(1, 1, "string")
 takes2Types[string, int]("test", "test", 1)
+
+proc takesSeq(x: seq) =
+  echo "seq"
+
+takesSeq(@[1, 2, 3])
+takesSeq(@["x", "y", "z"])
+
+proc takesSeqOfFoos(x: seq[TFoo]) =
+  echo "foo seq"
+
+var sf = newSeq[TFoo[int]](3)
+
+takesSeq(sf)
+takesSeqOfFoos(sf)
+
+proc takesFooOfNumeric(x: TFoo[Numeric]) =
+  echo "foo of numeric"
+
+takesFooOfNumeric(sf[0])
 
