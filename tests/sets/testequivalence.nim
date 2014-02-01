9f29bb8d9ee01ead64e8af89d471f6fcb67b9712
Simon Hafner <hafnersimon@gmail.com>
2014-02-01 16:07:44 -0600

    corrected docs and tests

--- tests/sets/testequivalence.nim
+++ tests/sets/testequivalence.nim
@@ -1,16 +1,12 @@
 import unittest
 import sets
 
-suite "sets":
-  test "equivalent or subset":
-    check toSet(@[1,2,3]) <= toSet(@[1,2,3,4])
-    check toSet(@[1,2,3]) <= toSet(@[1,2,3])
-    check(not(toSet(@[1,2,3]) <= toSet(@[1,2])))
-  test "strict subset":
-    check toSet(@[1,2,3]) <= toSet(@[1,2,3,4])
-    check(not(toSet(@[1,2,3]) < toSet(@[1,2,3])))
-    check(not(toSet(@[1,2,3]) < toSet(@[1,2])))
-  test "==":
-    check(not(toSet(@[1,2,3]) == toSet(@[1,2,3,4])))
-    check toSet(@[1,2,3]) == toSet(@[1,2,3])
-    check(not(toSet(@[1,2,3]) == toSet(@[1,2])))
+doAssert(toSet(@[1,2,3]) <= toSet(@[1,2,3,4]), "equivalent or subset")
+doAssert(toSet(@[1,2,3]) <= toSet(@[1,2,3]), "equivalent or subset")
+doAssert((not(toSet(@[1,2,3]) <= toSet(@[1,2]))), "equivalent or subset")
+doAssert(toSet(@[1,2,3]) <= toSet(@[1,2,3,4]), "strict subset")
+doAssert((not(toSet(@[1,2,3]) < toSet(@[1,2,3]))), "strict subset")
+doAssert((not(toSet(@[1,2,3]) < toSet(@[1,2]))), "strict subset")
+doAssert((not(toSet(@[1,2,3]) == toSet(@[1,2,3,4]))), "==")
+doAssert(toSet(@[1,2,3]) == toSet(@[1,2,3]), "==")
+doAssert((not(toSet(@[1,2,3]) == toSet(@[1,2]))), "==")
