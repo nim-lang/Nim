5643b847b0b04428eb6aef6b032d1859b3a8e1c1
Araq <rumpf_a@web.de>
2014-04-06 19:53:24 +0200

    fixes #297

---
+++ tests/typerel/texplicitcmp.nim
@@ -0,0 +1,32 @@
+discard """
+  output: '''[1 2 3 ]
+[1 2 3 ]
+[1 2 3 ]'''
+"""
+
+# bug #297
+
+import json, tables, algorithm
+
+proc outp(a: openarray[int]) =
+  stdout.write "["
+  for i in a: stdout.write($i & " ")
+  stdout.write "]\n"
+
+proc works() =
+  var f = @[3, 2, 1]
+  sort(f, system.cmp[int])
+  outp(f)
+
+proc weird(json_params: TTable) =
+  var f = @[3, 2, 1]
+  # The following line doesn't compile: type mismatch. Why?
+  sort(f, system.cmp[int])
+  outp(f)
+
+when isMainModule:
+  var t = @[3, 2, 1]
+  sort(t, system.cmp[int])
+  outp(t)
+  works()
+  weird(initTable[string, TJsonNode]())
