20e3ab70b28b738364d5535c59e16d221237d5e5
Dominik Picheta <dominikpicheta@googlemail.com>
2014-04-18 23:31:45 +0100

    Improves tobjasgn test.

--- tests/assign/tobjasgn.nim
+++ tests/assign/tobjasgn.nim
@@ -1,5 +1,5 @@
 discard """
-  output: '''0 0
+  output: '''8 5 0 0
 pre test a:test b:1 c:2 haha:3
 assignment test a:test b:1 c:2 haha:3
 '''
@@ -9,13 +9,13 @@ assignment test a:test b:1 c:2 haha:3
 
 type
   TSomeObj = object of TObject
-    a: int
+    a, b: int
   PSomeObj = ref object
-    a: int
+    a, b: int
  
-var a = TSomeObj()
-var b = PSomeObj()
-echo a.a, " ", b.a
+var a = TSomeObj(a: 8)
+var b = PSomeObj(a: 5)
+echo a.a, " ", b.a, " ", a.b, " ", b.b
 
 # bug #575
 
