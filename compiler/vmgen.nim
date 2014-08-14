65587f726e39af1c8e84c927db8ac26403f02d27
Araq <rumpf_a@web.de>
2014-08-14 03:02:36 +0200

    fixes #1433

--- compiler/vmgen.nim
+++ compiler/vmgen.nim
@@ -1093,6 +1093,8 @@ proc setSlot(c: PCtx; v: PSym) =
   # XXX generate type initialization here?
   if v.position == 0:
     if c.prc.maxSlots == 0: c.prc.maxSlots = 1
+    if c.prc.maxSlots >= high(TRegister):
+      internalError(v.info, "cannot generate code; too many registers required")
     v.position = c.prc.maxSlots
     c.prc.slots[v.position] = (inUse: true,
         kind: if v.kind == skLet: slotFixedLet else: slotFixedVar)
