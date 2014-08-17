710cbe3f1f5947ec76e517fa9a4f679ec653eefd
Araq <rumpf_a@web.de>
2014-08-17 21:23:46 +0200

    fixes #1492

--- compiler/condsyms.nim
+++ compiler/condsyms.nim
@@ -57,7 +57,7 @@ const
     endb profiler
     executable guiapp consoleapp library dll staticlib
 
-    quick nimbabel
+    quick
     release debug
     useWinAnsi useFork useNimRtl useMalloc useRealtimeGC ssl memProfiler
     nodejs kwin
