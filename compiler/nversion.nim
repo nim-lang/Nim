5272213da4b0701890d8a42fb292962704f0b99d
Araq <rumpf_a@web.de>
2014-10-11 01:30:17 +0200

    documentation updates

--- compiler/nversion.nim
+++ compiler/nversion.nim
@@ -13,8 +13,8 @@
 const 
   MaxSetElements* = 1 shl 16  # (2^16) to support unicode character sets?
   VersionMajor* = 0
-  VersionMinor* = 9
-  VersionPatch* = 6
+  VersionMinor* = 10
+  VersionPatch* = 0
   VersionAsString* = $VersionMajor & "." & $VersionMinor & "." & $VersionPatch
 
   RodFileVersion* = "1215"       # modify this if the rod-format changes!
