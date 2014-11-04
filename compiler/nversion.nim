e8a0b45f2adf94215bea025d237bb50ec6a4d915
Araq <rumpf_a@web.de>
2014-11-04 07:34:05 +0100

    proper development version number

--- compiler/nversion.nim
+++ compiler/nversion.nim
@@ -14,7 +14,7 @@ const
   MaxSetElements* = 1 shl 16  # (2^16) to support unicode character sets?
   VersionMajor* = 0
   VersionMinor* = 9
-  VersionPatch* = 6
+  VersionPatch* = 7
   VersionAsString* = $VersionMajor & "." & $VersionMinor & "." & $VersionPatch
 
   RodFileVersion* = "1215"       # modify this if the rod-format changes!
