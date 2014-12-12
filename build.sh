6b088938f70220f7e2702e4ec8829a10646a0db4
Philip Wernersbach <philip.wernersbach@gmail.com>
2014-12-11 23:19:47 -0500

    Add build scripts.

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+set -x
+
+if [ ! -d "csources" ]; then
+	git clone --depth 1 https://github.com/nim-lang/csources.git
+fi
+
+cd "csources"
+sh build.sh
+cd ".."
+
+./bin/nim c koch
+./koch boot -d:release
+
+cp -f install.sh.template install.sh
+chmod +x install.sh
+
+exit 0
