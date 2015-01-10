d2c20a32edd8ecb0d2ddaa3cfe39774caedceceb
Flaviu Tamas <tamasflaviu@gmail.com>
2015-01-10 16:21:11 -0500

    captureCount doesn't need to be exposed that way

--- src/nre.nim
+++ src/nre.nim
@@ -96,7 +96,7 @@ proc getinfo[T](self: Regex, opt: cint): T =
     # XXX Error message that doesn't expose implementation details
     raise newException(FieldError, "Invalid getinfo for $1, errno $2" % [$opt, $retcode])
 
-proc captureCount*(self: Regex): int =
+proc captureCount(self: Regex): int =
   ## get the maximum number of captures
   ##
   ## Does not return the number of captured captures
