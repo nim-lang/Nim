c0c0920d198381060c643af53a14e4b0751f5c39
Flaviu Tamas <tamasflaviu@gmail.com>
2015-01-15 17:39:20 -0500

    Add another split testcase
    Thanks Oleh Prypin for the suggestion!

--- test/split.nim
+++ test/split.nim
@@ -7,3 +7,4 @@ suite "string splitting":
     check("1 2 3 4 5 6 ".split(initRegex(" ", "S")) == @["1", "2", "3", "4", "5", "6", ""])
     check("1  2  ".split(initRegex(" ", "S")) == @["1", "", "2", "", ""])
     check("1 2".split(initRegex(" ", "S")) == @["1", "2"])
+    check("foo".split(initRegex("foo")) == @["", ""])
