ca299504d1dc3a23968c4ddf33fd64aa3f1a9422
Flaviu Tamas <tamasflaviu@gmail.com>
2015-01-18 12:49:22 -0500

    Make study a negatable option instead

--- test/split.nim
+++ test/split.nim
@@ -4,9 +4,9 @@ include nre
 suite "string splitting":
   test "splitting strings":
     check("12345".split(initRegex("")) == @["1", "2", "3", "4", "5"])
-    check("1 2 3 4 5 6 ".split(initRegex(" ", "S")) == @["1", "2", "3", "4", "5", "6", ""])
-    check("1  2  ".split(initRegex(" ", "S")) == @["1", "", "2", "", ""])
-    check("1 2".split(initRegex(" ", "S")) == @["1", "2"])
+    check("1 2 3 4 5 6 ".split(re" ") == @["1", "2", "3", "4", "5", "6", ""])
+    check("1  2  ".split(initRegex(" ")) == @["1", "", "2", "", ""])
+    check("1 2".split(initRegex(" ")) == @["1", "2"])
     check("foo".split(initRegex("foo")) == @["", ""])
 
   test "captured patterns":
