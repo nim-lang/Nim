6143393adbfc03b2de873c070fd53214696ca94c
Dominik Picheta <dominikpicheta@googlemail.com>
2015-01-28 14:58:31 +0000

    Move lib/pure/nodejs to compiler/nodejs. Ref #2017.

--- tests/testament/tester.nim
+++ tests/testament/tester.nim
@@ -12,7 +12,7 @@
 import
   parseutils, strutils, pegs, os, osproc, streams, parsecfg, json,
   marshal, backend, parseopt, specs, htmlgen, browsers, terminal,
-  algorithm, nodejs
+  algorithm, compiler/nodejs
 
 const
   resultsFile = "testresults.html"
