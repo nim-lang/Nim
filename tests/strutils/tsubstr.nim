2865425c6662f34e6e6a5b6782ec1b866b04638a
Simon Hafner <hafnersimon@gmail.com>
2015-02-01 22:19:51 -0600

    added strutils/split tests

---
+++ tests/strutils/tsubstr.nim
@@ -0,0 +1,11 @@
+import strutils
+
+let expected = @["this", "is", "an", "example"]
+assert(split("  this is an  example  ") == expected)
+assert(split(";;this;is;an;;example;;;", {';'}) == expected)
+assert(split(";;this;is;an;;example;;;", ';') == expected)
+
+let dateExpected = @["2012", "11", "20", "22", "08", "08.398990"]
+let date = "2012-11-20T22:08:08.398990"
+let separators = {' ', '-', ':', 'T'}
+assert(split(date, separators) == dateExpected)
