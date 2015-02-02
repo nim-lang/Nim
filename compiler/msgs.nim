fc5700619bfcab56fcfe7f62cf87906d1d012e8a
Simon Hafner <hafnersimon@gmail.com>
2015-02-01 23:24:43 -0600

    report how to create a compiler stacktrace #1280

--- compiler/msgs.nim
+++ compiler/msgs.nim
@@ -715,7 +715,10 @@ type
 proc handleError(msg: TMsgKind, eh: TErrorHandling, s: string) =
   template quit =
     if defined(debug) or gVerbosity >= 3 or msg == errInternal:
-      writeStackTrace()
+      if stackTraceAvailable():
+        writeStackTrace()
+      else:
+        stderr.writeln("No stack traceback available\nTo create a stacktrace, rerun compilation with ./koch temp c <file>")
     quit 1
 
   if msg >= fatalMin and msg <= fatalMax:
