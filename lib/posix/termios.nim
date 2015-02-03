3b68d9e93c603117a301aa5048bfd61774434eb2
def <dennis@felsin9.de>
2015-02-03 17:29:09 +0100

    Add copyright header

--- lib/posix/termios.nim
+++ lib/posix/termios.nim
@@ -1,3 +1,12 @@
+#
+#
+#            Nim's Runtime Library
+#        (c) Copyright 2015 Andreas Rumpf
+#
+#    See the file "copying.txt", included in this
+#    distribution, for details about the copyright.
+#
+
 {.deadCodeElim: on.}
 import posix
 
