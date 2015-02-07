7191869d0943466441f975cef0a841d0e68ec195
Flaviu Tamas <tamasflaviu@gmail.com>
2015-02-07 09:46:03 -0500

    Use /bin/sh
    Apparently /usr/bin/sh only works in Arch Linux

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/bin/sh
 nim c --path:src -r --verbosity:0 --hints:off --linedir:on --debuginfo \
   --stacktrace:on --linetrace:on "$@" ./test/testall.nim \
   | grep -vE 'ProveInit|instantiation from here'
