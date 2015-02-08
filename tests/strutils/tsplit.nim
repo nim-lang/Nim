ef0e5c17ef37d40699ab6a489eacdc2a8bb739a3
Simon Hafner <hafnersimon@gmail.com>
2015-02-08 10:26:45 -0600

    added additional test

--- tests/strutils/tsplit.nim
+++ tests/strutils/tsplit.nim
@@ -4,6 +4,7 @@ let expected = @["this", "is", "an", "example"]
 assert(split("  this is an  example  ") == expected)
 assert(split(";;this;is;an;;example;;;", {';'}) == expected)
 assert(split(";;this;is;an;;example;;;", ';') == expected)
+assert(split("foo", '') == @["foo"])
 
 let dateExpected = @["2012", "11", "20", "22", "08", "08.398990"]
 let date = "2012-11-20T22:08:08.398990"
