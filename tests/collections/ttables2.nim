1c88d26ebf72b64077f4a600c28012dba48cfc1c
Araq <rumpf_a@web.de>
2015-02-20 16:18:14 +0100

    added test case for #2107

--- tests/collections/ttables2.nim
+++ tests/collections/ttables2.nim
@@ -16,5 +16,15 @@ proc run1() =         # occupied Memory stays constant, but
   for i in 1 .. 50:   # aborts at run: 44 on win32 with 3.2GB with out of memory
     TestHashIntInt()
 
+# bug #2107
+
+var delTab = initTable[int,int](4)
+
+for i in 1..4:
+  delTab[i] = i
+  delTab.del(i)
+delTab[5] = 5
+
+
 run1()
 echo "true"
