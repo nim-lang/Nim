07351d5f37f9ff5fd78dd15fb5c24fd7d0a15eea
def <dennis@felsin9.de>
2015-03-28 15:07:55 +0100

    Initialize c compiler variables later ...
    ... so that symbols such as "cpp" are defined
    
    Fixes #1460

--- compiler/cgen.nim
+++ compiler/cgen.nim
@@ -486,7 +486,7 @@ proc lenField(p: BProc): PRope =
 include ccgcalls, "ccgstmts.nim", "ccgexprs.nim"
 
 # ----------------------------- dynamic library handling -----------------
-# We don't finalize dynamic libs as this does the OS for us.
+# We don't finalize dynamic libs as the OS does this for us.
 
 proc isGetProcAddr(lib: PLib): bool =
   let n = lib.path
