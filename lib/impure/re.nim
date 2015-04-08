7ee6c72b51e8e0877bc734a30c0498e42f0ca99a
Joseph Turner <turner.d.joseph@gmail.com>
2015-04-08 21:58:31 +0100

    Adds test case to is isMainModule block
    Adds the test case that originally caused the bug, issue #2418.

--- lib/impure/re.nim
+++ lib/impure/re.nim
@@ -474,7 +474,12 @@ when isMainModule:
   var accum: seq[string] = @[]
   for word in split("00232this02939is39an22example111", re"\d+"):
     accum.add(word)
-  assert(accum == @["this", "is", "an", "example"])
+  assert(accum == @["", "this", "is", "an", "example", ""])
+
+  accum = @[]
+  for word in split("AAA :   : BBB", re"\s*:\s*"):
+    accum.add(word)
+  assert(accum == @["AAA", "", "BBB"])
 
   for x in findAll("abcdef", re"^{.}", 3):
     assert x == "d"
