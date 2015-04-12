8b8224ecaf7ed8cfbe1a205b43c3402aabc1653f
Oleh Prypin <blaxpirit@gmail.com>
2015-04-12 17:31:43 +0300

    Add tests for empty or non-empty match

--- test/misc.nim
+++ test/misc.nim
@@ -1,6 +1,16 @@
-import unittest, nre, optional_t.nonstrict
+import unittest, nre, strutils, optional_t.nonstrict
 
 suite "Misc tests":
   test "unicode":
     check("".find(re("", "8")).match == "")
     check("перевірка".replace(re(r"\w", "uW"), "") == "")
+
+  test "empty or non-empty match":
+    check("abc".findall(re"|.").join(":") == ":a::b::c:")
+    check("abc".findall(re".|").join(":") == "a:b:c:")
+
+    check("abc".replace(re"|.", "x") == "xxxxxxx")
+    check("abc".replace(re".|", "x") == "xxxx")
+
+    check("abc".split(re"|.").join(":") == ":::::")
+    check("abc".split(re".|").join(":") == ":::")
