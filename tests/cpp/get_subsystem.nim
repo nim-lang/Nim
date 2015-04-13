6fb372d96bade748aa6cb3484fef2118b4585f26
Zahary Karadjov <zahary@gmail.com>
2015-04-14 00:03:31 +0300

    The getSubsystem<T> example in the manual currently fails with a codegen error
    A faulty proc declaration is generated:
    N_NIMCALL(System::Input*, SystemManager::getSubsystem<'*0>())(void);
    The manual has been edited to add a nodecl pragma, which alleviates the issue
    
    Fix a typo in the vector_iterator example from the previous commit.

---
+++ tests/cpp/get_subsystem.nim
@@ -0,0 +1,22 @@
+discard """
+  cmd: "nim cpp $file"
+"""
+
+{.emit: """
+
+namespace System {
+  struct Input {};
+}
+
+struct SystemManager {
+  template <class T>
+  static T* getSubsystem() { return new T; }
+};
+
+""".}
+
+type Input {.importcpp: "System::Input".} = object
+proc getSubsystem*[T](): ptr T {.importcpp: "SystemManager::getSubsystem<'*0>()".}
+
+let input: ptr Input = getSubsystem[Input]()
+
--- tests/cpp/vector_iterator.nim
+++ tests/cpp/vector_iterator.nim
@@ -6,16 +6,14 @@ discard """
 
 template <class T>
 struct Vector {
-  struct Iterator {
-
-  };
+  struct Iterator {};
 };
 
 """.}
 
 type
   Vector {.importcpp: "Vector".} [T] = object
-  VectorIterator {.importcpp: "Vector<'2>::Iterator".} [T] = object
+  VectorIterator {.importcpp: "Vector<'0>::Iterator".} [T] = object
 
 var x: VectorIterator[void]
 
