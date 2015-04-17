5146624f0d47e04ed44bc3930946594f6777248f
Araq <rumpf_a@web.de>
2015-04-18 01:18:23 +0200

    disabled JS test

--- tests/js/tunittests.nim
+++ tests/js/tunittests.nim
@@ -1,3 +1,10 @@
+discard """
+  disabled: "true"
+"""
+
+# Unittest uses lambdalifting at compile-time which we disable for the JS
+# codegen! So this cannot and will not work for quite some time.
+
 import unittest
 
 suite "Bacon":
