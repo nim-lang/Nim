c6605d3d507d13bb6e2996d4eadfb5681cfdc193
Araq <rumpf_a@web.de>
2015-05-02 23:52:24 +0200

    development version is 0.11.1

--- lib/system.nim
+++ lib/system.nim
@@ -1535,7 +1535,7 @@ const
   NimMinor*: int = 11
     ## is the minor number of Nim's version.
 
-  NimPatch*: int = 0
+  NimPatch*: int = 1
     ## is the patch number of Nim's version.
 
   NimVersion*: string = $NimMajor & "." & $NimMinor & "." & $NimPatch
