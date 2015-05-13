1d47617d1b72c642fac5543f38aa24330b0f9a19
Adam Strzelecki <ono@java.pl>
2015-05-13 22:32:10 +0200

    Macros: Introduce getType(t: typedesc): NimNode
    Since typedesc are exception in macros and they are not implicitly converted to
    NimNode on macro call, we need some means to perform such conversion on demand.
    
    Fortunately it is as simple as declaring new magic "NGetType" proc with
    typedesc parameter.
    
    NOTE: Keeping actual macro exceptional behavior for typedesc is important,
    since it allows passing typedesc macro parameter to other procs or macros
    expecting type parameter. If typedesc parameter was implicitly converted, then
    we would lost this ability.

--- lib/core/macros.nim
+++ lib/core/macros.nim
@@ -177,6 +177,12 @@ proc getType*(n: NimNode): NimNode {.magic: "NGetType", noSideEffect.}
   ## resolve recursive types, you have to call 'getType' again. To see what
   ## kind of type it is, call `typeKind` on getType's result.
 
+proc getType*(n: typedesc): NimNode {.magic: "NGetType", noSideEffect.}
+  ## Returns the Nim type node for given type. This can be used to turn macro
+  ## typedesc parameter into proper NimNode representing type, since typedesc
+  ## are an exception in macro calls - they are not mapped implicitly to
+  ## NimNode like any other arguments.
+
 proc typeKind*(n: NimNode): NimTypeKind {.magic: "NGetType", noSideEffect.}
   ## Returns the type kind of the node 'n' that should represent a type, that
   ## means the node should have been obtained via `getType`.
