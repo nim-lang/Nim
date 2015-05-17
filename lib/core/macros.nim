3d80fcb8c6ff1e03922f517681efad65d79563fb
transfuturist <timothy.schmid@gmail.com>
2015-05-16 17:50:01 -0700

    Add value check for NimNode items

--- lib/core/macros.nim
+++ lib/core/macros.nim
@@ -708,9 +708,10 @@ proc `$`*(node: NimNode): string {.compileTime.} =
 proc ident*(name: string): NimNode {.compileTime,inline.} = newIdentNode(name)
   ## Create a new ident node from a string
 
-iterator items*(n: NimNode): NimNode {.inline.}=
-  for i in 0 .. high(n):
-    yield n[i]
+iterator items*(n: NimNode): NimNode {.inline.} =
+  if n[0] != nil:
+    for i in 0 .. high(n):
+      yield n[i]
 
 {.deprecated: [children: items].}
 
