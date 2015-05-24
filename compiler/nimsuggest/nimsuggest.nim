4cc722a41420e24f53a3a66be08e3b4a41b76d69
Simon Hafner <hafnersimon@gmail.com>
2015-05-24 15:22:22 -0500

    now nimsuggest works on the first sug

--- compiler/nimsuggest/nimsuggest.nim
+++ compiler/nimsuggest/nimsuggest.nim
@@ -118,10 +118,9 @@ proc execute(cmd: IdeCmd, file, dirtyfile: string, line, col: int) =
 
   gTrackPos = newLineInfo(dirtyIdx, line, col)
   gErrorCounter = 0
-  if isKnownFile:
-    compileProject(dirtyIdx)
-  else:
+  if not isKnownFile:
     compileProject()
+  compileProject(dirtyIdx)
 
 proc executeEPC(cmd: IdeCmd, args: SexpNode) =
   let
@@ -192,8 +191,6 @@ proc parseCmdLine(cmd: string) =
   execute(gIdeCmd, orig, dirtyfile, line, col-1)
 
 proc serve() =
-  # do not stop after the first error:
-  msgs.gErrorMax = high(int)
   case gMode:
   of mstdin:
     echo Help
@@ -278,6 +275,9 @@ proc mainCommand =
     # current path is always looked first for modules
     prependStr(searchPaths, gProjectPath)
 
+  # do not stop after the first error:
+  msgs.gErrorMax = high(int)
+  compileProject()
   serve()
 
 proc processCmdLine*(pass: TCmdLinePass, cmd: string) =
