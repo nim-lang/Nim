2353fe343a11ba3744a11f8a61ec7c224c2f9490
def <dennis@felsin9.de>
2015-07-02 12:26:04 +0200

    Update deprecation notice about unsigned module

--- lib/core/unsigned.nim
+++ lib/core/unsigned.nim
@@ -7,7 +7,7 @@
 #    distribution, for details about the copyright.
 #
 
-## **Warning:** Since version 0.10.4 this module is deprecated.
+## **Warning:** Since version 0.11.4 this module is deprecated.
 ##
 ## This module implemented basic arithmetic operators for unsigned integers.
 ## These operators are now available in the ``system`` module directly.
