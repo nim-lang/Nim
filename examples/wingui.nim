5313ceaafd1d92a4d8337cdffaac4f834553a952
Araq <rumpf_a@web.de>
2015-07-12 13:32:58 +0200

    deleted wingui example

--- examples/wingui.nim
+++
@@ -1,9 +0,0 @@
-# test a Windows GUI application
-
-import
-  windows, shellapi, nb30, mmsystem, shfolder
-
-#proc MessageBox(hWnd: int, lpText, lpCaption: CString, uType: uint): int
-#  {stdcall, import: "MessageBox", header: "<windows.h>"}
-
-discard MessageBox(0, "Hello World!", "Nimrod GUI Application", 0)
