c6d609f88bf45371f99a1ed924dd4c4dc46ba181
Charles Blake <cblake@csail.mit.edu>
2015-08-03 12:25:30 -0400

    Fix accidentally deleted inp argument.

--- tests/misc/tmemlinesBuf.nim
+++ tests/misc/tmemlinesBuf.nim
@@ -1,6 +1,6 @@
 import memfiles
 var inp = memfiles.open("readme.txt")
 var buffer: TaintedString = ""
-for line in lines(, buffer):
+for line in lines(inp, buffer):
   echo("#" & line & "#")
 close(inp)
