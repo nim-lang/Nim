d7e1f514c40afc87d19a0205201947c9d14d3597
JamesP <jlp765@gmail.com>
2015-09-17 13:48:30 +1000

    fix setupQuery() for SqlQuery to produce a unique identiying query name

--- lib/impure/db_postgres.nim
+++ lib/impure/db_postgres.nim
@@ -117,8 +117,10 @@ proc newRow(L: int): Row =
 
 proc setupQuery(db: DbConn, query: SqlQuery,
                 args: varargs[string]): PPGresult =
-  var res = pqprepare(db, "setupQuery_Query", dbFormat(query, args), 0, nil)
-  result = pqexecPrepared(db, "setupQuery_Query", 0, nil,
+  # s is a dummy unique id str for each setupQuery query
+  let s = "setupQuery_Query_" & string(query)
+  var res = pqprepare(db, s, dbFormat(query, args), 0, nil)
+  result = pqexecPrepared(db, s, 0, nil,
                         nil, nil, 0)
   if pqResultStatus(result) != PGRES_TUPLES_OK: dbError(db)
 
