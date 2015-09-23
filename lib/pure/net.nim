4a715b8e085319c671d86638c326fd1bee14b9a6
Aman Gupta <aman@tmm1.net>
2015-09-23 12:03:14 -0700

    disable SSLv2

--- lib/pure/net.nim
+++ lib/pure/net.nim
@@ -223,10 +223,7 @@ when defined(ssl):
     of protSSLv23:
       newCTX = SSL_CTX_new(SSLv23_method()) # SSlv2,3 and TLS1 support.
     of protSSLv2:
-      when not defined(linux) and not defined(macosx):
-        newCTX = SSL_CTX_new(SSLv2_method())
-      else:
-        raiseSslError()
+      raiseSslError()
     of protSSLv3:
       newCTX = SSL_CTX_new(SSLv3_method())
     of protTLSv1:
