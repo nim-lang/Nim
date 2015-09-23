5f95c4e055be953acabac2f430491fc8c225264c
Aman Gupta <aman@tmm1.net>
2015-09-22 19:54:15 -0700

    avoid referencing undefined SSLv2_method on mac osx

--- lib/pure/net.nim
+++ lib/pure/net.nim
@@ -223,7 +223,7 @@ when defined(ssl):
     of protSSLv23:
       newCTX = SSL_CTX_new(SSLv23_method()) # SSlv2,3 and TLS1 support.
     of protSSLv2:
-      when not defined(linux):
+      when not defined(linux) and not defined(macosx):
         newCTX = SSL_CTX_new(SSLv2_method())
       else:
         raiseSslError()
