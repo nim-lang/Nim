30cc353831a9727cfc6b4e7c379d9920b7041059
Dominik Picheta <dominikpicheta@gmail.com>
2015-10-27 22:28:42 +0100

    Prep for v0.12.0

--- lib/system.nim
+++ lib/system.nim
@@ -1808,10 +1808,10 @@ const
   NimMajor*: int = 0
     ## is the major number of Nim's version.
 
-  NimMinor*: int = 11
+  NimMinor*: int = 12
     ## is the minor number of Nim's version.
 
-  NimPatch*: int = 3
+  NimPatch*: int = 0
     ## is the patch number of Nim's version.
 
   NimVersion*: string = $NimMajor & "." & $NimMinor & "." & $NimPatch
