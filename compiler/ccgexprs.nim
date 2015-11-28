6ddf5a0b7bf614a7cb34005ef976f172eaf8379e
Erik Johansson Andersson <ephja@yandex.com>
2015-11-28 20:03:17 +0100

    flush after printing with echo

--- compiler/ccgexprs.nim
+++ compiler/ccgexprs.nim
@@ -959,6 +959,8 @@ proc genEcho(p: BProc, n: PNode) =
       addf(args, ", $1? ($1)->data:\"nil\"", [rdLoc(a)])
   linefmt(p, cpsStmts, "printf($1$2);$n",
           makeCString(repeat("%s", n.len) & tnl), args)
+  linefmt(p, cpsStmts, "fflush(stdout);$n",
+          makeCString(repeat("%s", n.len) & tnl))
 
 proc gcUsage(n: PNode) =
   if gSelectedGC == gcNone: message(n.info, warnGcMem, n.renderTree)
