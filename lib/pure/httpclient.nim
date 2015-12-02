17e07212b66162db774aae6d3a82ccaae711168f
Christoph Herzog <chris@theduke.at>
2015-12-03 00:43:29 +0100

    AsyncHttpClient extends RootObj to be extendable

--- lib/pure/httpclient.nim
+++ lib/pure/httpclient.nim
@@ -598,7 +598,7 @@ proc generateHeaders(r: Uri, httpMethod: string,
   add(result, "\c\L")
 
 type
-  AsyncHttpClient* = ref object
+  AsyncHttpClient* = ref object of RootObj
     socket: AsyncSocket
     connected: bool
     currentURL: Uri ## Where we are currently connected.
