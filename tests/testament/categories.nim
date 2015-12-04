a8ba1d330a526481c0e9583de64509022a31ad79
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2015-12-04 11:12:54 -0800

    Disabled nakefile test

--- tests/testament/categories.nim
+++ tests/testament/categories.nim
@@ -247,10 +247,11 @@ proc findMainFile(dir: string): string =
   if nimFiles != 1: result.setlen(0)
 
 proc manyLoc(r: var TResults, cat: Category, options: string) =
+  const testsDisabledOnWindows = ["tests/manyloc\\nake\\nakefile.nim"]
   for kind, dir in os.walkDir("tests/manyloc"):
     if kind == pcDir:
       let mainfile = findMainFile(dir)
-      if mainfile != "":
+      if mainfile != "" and mainfile notin testsDisabledOnWindows:
         testNoSpec r, makeTest(mainfile, options, cat)
 
 proc compileExample(r: var TResults, pattern, options: string, cat: Category) =
