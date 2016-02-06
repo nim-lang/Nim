bf8e371d7ee31b8d8573b4bed0a426022330a631
def <dennis@felsin9.de>
2016-02-06 01:29:12 +0100

    Revert raiseOSError change

--- lib/pure/osproc.nim
+++ lib/pure/osproc.nim
@@ -875,8 +875,7 @@ elif not defined(useNimRtl):
       var error: cint
       let sizeRead = read(data.pErrorPipe[readIdx], addr error, sizeof(error))
       if sizeRead == sizeof(error):
-        raise newException(OSError,
-          "Could not find command: '$1'. OS error: $2" %
+        raiseOSError("Could not find command: '$1'. OS error: $2" %
           [$data.sysCommand, $strerror(error)])
 
       return pid
