6d675814091b20312cbe51b69261453e04e27214
Andreas Rumpf <rumpf_a@web.de>
2016-04-04 10:19:20 +0200

    fixes #3859

--- tests/vm/tmitems.nim
+++ tests/vm/tmitems.nim
@@ -1,5 +1,8 @@
 discard """
   msg: '''13'''
+  output: '''3
+3
+3'''
 """
 # bug #3731
 var list {.compileTime.} = newSeq[int]()
@@ -13,3 +16,16 @@ macro calc*(): stmt {.immediate.} =
     echo c
 
 calc()
+
+# bug #3859
+import macros
+macro m: stmt =
+  var s = newseq[NimNode](3)
+  # var s: array[3,NimNode]                 # not working either
+  for i in 0..<s.len: s[i] = newLit(3)    # works
+  #for x in s.mitems: x = newLit(3)
+  result = newStmtList()
+  for i in s:
+    result.add newCall(bindsym"echo", i)
+
+m()
