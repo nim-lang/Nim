299351a909ee4827984eb9390f8ed55437bd60a9
Andreas Rumpf <rumpf_a@web.de>
2016-05-11 18:16:25 +0200

    fixes #4070

---
+++ tests/closure/tdeeplynested.nim
@@ -0,0 +1,20 @@
+discard """
+  output: '''108'''
+"""
+
+# bug #4070
+
+proc id(f: (proc())): auto =
+  return f
+
+proc foo(myinteger: int): (iterator(): int) =
+  return iterator(): int {.closure.} =
+           proc bar() =
+             proc kk() =
+               echo "int: ", myinteger
+
+             kk()
+
+           id(bar)()
+
+discard foo(108)()
