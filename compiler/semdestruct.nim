6dcaafc70c218a059f6fa11efa59c91a81522e04
Clay Sweetser <clay.sweetser@gmail.com>
2016-06-18 23:53:41 -0400

    Fix #4393

--- compiler/semdestruct.nim
+++ compiler/semdestruct.nim
@@ -139,6 +139,7 @@ proc instantiateDestructor(c: PContext, typ: PType): PType =
   t = t.skipTypes({tyGenericInst})
   case t.kind
   of tySequence, tyArray, tyArrayConstr, tyOpenArray, tyVarargs:
+    t.destructor = analyzingDestructor
     if instantiateDestructor(c, t.sons[0]) != nil:
       t.destructor = getCompilerProc"nimDestroyRange"
       return t
