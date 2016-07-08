857b0c8d4c79d44fc0580df4658d1b17121dceed
Andreas Rumpf <rumpf_a@web.de>
2016-07-08 10:52:04 +0200

    fixes #4462

--- tests/vm/tvmmisc.nim
+++ tests/vm/tvmmisc.nim
@@ -1,5 +1,15 @@
 
-# 4412
+
+# bug #4462
+import macros
+
+proc foo(t: typedesc) {.compileTime.} =
+  echo getType(t).treeRepr
+
+static:
+  foo(int)
+
+# #4412
 proc default[T](t: typedesc[T]): T {.inline.} = discard
 
 static:
