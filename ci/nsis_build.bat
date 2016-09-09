ea76b2320b08ff8294daf8124e37f9939eb816dc
Dominik Picheta <dominikpicheta@gmail.com>
2016-09-09 23:52:44 +0100

    Improvements to GitLab CI NSIS script.
    Squashed commit of the following:
    
    commit a17ffc5cfe63b25f1866c57bf22730790b1ec86e
    Author: Dominik Picheta <dominikpicheta@gmail.com>
    Date:   Fri Sep 9 23:01:48 2016 +0100
    
        Debugging
    
    commit 6522890c1afea95616ed667b99ad6e9b5f3ac1bc
    Author: Dominik Picheta <dominikpicheta@gmail.com>
    Date:   Fri Sep 9 22:40:45 2016 +0100
    
        Another fix
    
    commit bfd0a623b7b9282209ad1639ed0a5850d517ea00
    Author: Dominik Picheta <dominikpicheta@gmail.com>
    Date:   Fri Sep 9 22:10:44 2016 +0100
    
        Grab nimsuggest
    
    commit 7173335682d87d4b1bde55dea0dcf17a7dad6d7a
    Author: Dominik Picheta <dominikpicheta@gmail.com>
    Date:   Fri Sep 9 21:56:04 2016 +0100
    
        Fix again.

--- ci/nsis_build.bat
+++ ci/nsis_build.bat
@@ -27,8 +27,11 @@ rem koch xz -d:release
 rem move /y build\nim-%NIMVER%.zip web\upload\download
 
 
-rem Grab C sources
+rem Grab C sources and nimsuggest
 git clone --depth 1 https://github.com/nim-lang/csources.git
+git clone --depth 1 https://github.com/nim-lang/nimsuggest.git
+
+set PATH=%CD%\bin;%PATH%
 
 ReM Build Win32 version:
 
@@ -38,11 +41,12 @@ call build.bat
 cd ..
 nim c koch || exit /b
 koch boot -d:release || exit /b
-cd ..\nimsuggest
-nim c -d:release --noNimblePath --path:..\nim  nimsuggest || exit /b
-copy /y nimsuggest.exe ..\nim\bin || exit /b
-cd ..\nim
+cd nimsuggest
+nim c -d:release --noNimblePath --path:.. nimsuggest || exit /b
+copy /y nimsuggest.exe ..\bin || exit /b
+cd ..
 koch nsis -d:release || exit /b
+dir build
 move /y build\nim_%NIMVER%.exe web\upload\download\nim-%NIMVER%_x32.exe || exit /b
 
 
@@ -53,9 +57,9 @@ call build64.bat
 cd ..
 nim c koch || exit /b
 koch boot -d:release || exit /b
-cd ..\nimsuggest
-nim c -d:release --noNimblePath --path:..\nim  nimsuggest || exit /b
-copy /y nimsuggest.exe ..\nim\bin || exit /b
-cd ..\nim
+cd nimsuggest
+nim c -d:release --noNimblePath --path:.. nimsuggest || exit /b
+copy /y nimsuggest.exe ..\bin || exit /b
+cd ..
 koch nsis -d:release || exit /b
 move /y build\nim_%NIMVER%.exe web\upload\download\nim-%NIMVER%_x64.exe || exit /b
