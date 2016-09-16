dec5707318602794eb537ab712ce3434720fed28
cheatfate <ka@hardcore.kiev.ua>
2016-09-16 04:15:37 +0300

    First attempt to fix #4777

--- tests/async/tnewasyncudp.nim
+++ tests/async/tnewasyncudp.nim
@@ -40,7 +40,7 @@ proc launchSwarm(name: ptr SockAddr) {.async.} =
   var i = 0
   var k = 0
   while i < swarmSize:
-    var peeraddr = prepareAddress(INADDR_ANY, 0)
+    var peeraddr = prepareAddress(INADDR_LOOPBACK, 0)
     var sock = newAsyncNativeSocket(nativesockets.AF_INET,
                                     nativesockets.SOCK_DGRAM,
                                     Protocol.IPPROTO_UDP)
@@ -81,7 +81,7 @@ proc readMessages(server: AsyncFD) {.async.} =
     inc(i)
 
 proc createServer() {.async.} =
-  var name = prepareAddress(INADDR_ANY, serverPort)
+  var name = prepareAddress(INADDR_LOOPBACK, serverPort)
   var server = newAsyncNativeSocket(nativesockets.AF_INET,
                                     nativesockets.SOCK_DGRAM,
                                     Protocol.IPPROTO_UDP)
@@ -90,7 +90,7 @@ proc createServer() {.async.} =
     raiseOSError(osLastError())
   asyncCheck readMessages(server)
 
-var name = prepareAddress(0x7F000001, serverPort) # 127.0.0.1
+var name = prepareAddress(INADDR_LOOPBACK, serverPort) # 127.0.0.1
 asyncCheck createServer()
 asyncCheck launchSwarm(cast[ptr SockAddr](name))
 while true:
