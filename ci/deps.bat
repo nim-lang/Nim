6e7d50310e2fdb101fa803cd99d3f041ea3b912c
Dominik Picheta <dominikpicheta@gmail.com>
2016-09-25 11:51:57 +0200

    CI: Install a specific Jester commit.

--- ci/deps.bat
+++ ci/deps.bat
@@ -1,4 +1,4 @@
 nim e install_nimble.nims
 nim e tests/test_nimscript.nims
 nimble update
-nimble install -y zip opengl sdl1 jester niminst
+nimble install -y zip opengl sdl1 jester@#d5ad84fc9 niminst
