9753fb064f28e35325fbf5b032561c086925c94b
Dominik Picheta <dominikpicheta@gmail.com>
2016-09-25 11:48:10 +0200

    Add PATCH HTTP Method.

--- lib/pure/httpcore.nim
+++ lib/pure/httpcore.nim
@@ -43,9 +43,8 @@ type
                       ## for specified address.
     HttpConnect,      ## Converts the request connection to a transparent
                       ## TCP/IP tunnel, usually used for proxies.
-    HttpPatch         ## Added in RFC 5789. Can be used to update partial
-                      ## resources. The set of changes is represented in a
-                      ## format called a "patch document".
+    HttpPatch         ## Applies partial modifications to a resource.
+
 {.deprecated: [httpGet: HttpGet, httpHead: HttpHead, httpPost: HttpPost,
                httpPut: HttpPut, httpDelete: HttpDelete, httpTrace: HttpTrace,
                httpOptions: HttpOptions, httpConnect: HttpConnect].}
