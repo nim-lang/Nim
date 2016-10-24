e700e3f205f8f206a6e24797b18154bd94edfc78
Araq <rumpf_a@web.de>
2016-10-24 12:49:15 +0200

    remove undocumented build_tools.sh; refs #4934

--- build_tools.sh
+++
@@ -1,3 +0,0 @@
-./bin/nim c --noNimblePath -p:compiler -o:./bin/nimble dist/nimble/src/nimble.nim
-./bin/nim c --noNimblePath -p:compiler -o:./bin/nimsuggest dist/nimsuggest/nimsuggest.nim
-./bin/nim c -o:./bin/nimgrep tools/nimgrep.nim
