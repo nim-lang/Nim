38f4fe5904c627395cdc3276f9b4eba21e104357
Felix Krause <contact@flyx.org>
2016-11-14 20:24:58 +0100

    Added gorgeEx test

--- tests/vm/tgorge.nim
+++ tests/vm/tgorge.nim
@@ -3,10 +3,19 @@ import os
 template getScriptDir(): string =
   parentDir(instantiationInfo(-1, true).filename)
 
-const
-  execName = when defined(windows): "tgorge.bat" else: "./tgorge.sh"
-  relOutput = gorge(execName)
-  absOutput = gorge(getScriptDir() / execName)
 
-doAssert relOutput == "gorge test"
-doAssert absOutput == "gorge test"
+block gorge:
+  const
+    execName = when defined(windows): "tgorge.bat" else: "./tgorge.sh"
+    relOutput = gorge(execName)
+    absOutput = gorge(getScriptDir() / execName)
+
+  doAssert relOutput == "gorge test"
+  doAssert absOutput == "gorge test"
+
+block gorgeEx:
+  const
+    execName = when defined(windows): "tgorgeex.bat" else: "./tgorgeex.sh"
+    res = gorgeEx(execName)
+  doAssert res.output == "gorgeex test"
+  doAssert res.resultCode == 1
