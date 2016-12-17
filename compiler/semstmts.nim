d4c33df91996e30ded5f7f2014e7cd49176b5b05
Araq <rumpf_a@web.de>
2016-12-17 23:02:53 +0100

    fixes #5129

--- compiler/semstmts.nim
+++ compiler/semstmts.nim
@@ -759,8 +759,10 @@ proc typeSectionRightSidePass(c: PContext, n: PNode) =
       if st.kind == tyGenericBody: st = st.lastSon
       internalAssert st.kind in {tyPtr, tyRef}
       internalAssert st.lastSon.sym == nil
-      st.lastSon.sym = newSym(skType, getIdent(s.name.s & ":ObjectType"),
+      let obj = newSym(skType, getIdent(s.name.s & ":ObjectType"),
                               getCurrOwner(), s.info)
+      obj.typ = st.lastSon
+      st.lastSon.sym = obj
 
 proc checkForMetaFields(n: PNode) =
   template checkMeta(t) =
