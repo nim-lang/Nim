671693eb4b6fd7eaeb581dbe5dbb560b6ce759c7
Fabian Keller <github.100.fkeller@spamgourmet.com>
2017-02-23 23:51:46 +0100

    add failing test for double newline issue

---
+++ tests/osproc/tb.nim
@@ -0,0 +1,14 @@
+stdout.writeLine("to stdout")
+stdout.flushFile()
+stdout.writeLine("to stdout")
+stdout.flushFile()
+
+stderr.writeLine("to stderr")
+stderr.flushFile()
+stderr.writeLine("to stderr")
+stderr.flushFile()
+
+stdout.writeLine("to stdout")
+stdout.flushFile()
+stdout.writeLine("to stdout")
+stdout.flushFile()
