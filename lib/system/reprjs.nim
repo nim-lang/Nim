77c3473386287cce72be2274d62417e01717692f
stisa <stisa4@gmail.com>
2017-03-16 23:58:47 +0100

    fix float ".0" , repr array begins to work

--- lib/system/reprjs.nim
+++ lib/system/reprjs.nim
@@ -8,7 +8,11 @@
 #
 
 proc reprInt(x: int64): string {.compilerproc.} = return $x
-proc reprFloat(x: float): string {.compilerproc.} = return $x
+proc reprFloat(x: float): string {.compilerproc.} = 
+  # Js toString doesn't differentiate between 1.0 and 1,
+  # but we do.
+  if $x == $(x.int): $x & ".0"
+  else: $x
 
 proc reprBool(x: bool): string {.compilerRtl.} =
   if x: result = "true"
@@ -138,3 +142,130 @@ proc reprSetAux(result: var string, s:int, typ: PNimType) {.asmNoStackFrame.}=
 proc reprSet(e: int, typ: PNimType): string {.compilerRtl.} =
   result = ""
   reprSetAux(result, e, typ)
+
+type
+  ReprClosure {.final.} = object
+    recdepth: int       # do not recurse endlessly
+    indent: int         # indentation
+
+proc initReprClosure(cl: var ReprClosure) =
+  cl.recdepth = -1      # default is to display everything!
+  cl.indent = 0
+
+proc reprBreak(result: var string, cl: ReprClosure) =
+  add result, "\n"
+  for i in 0..cl.indent-1: add result, ' '
+
+proc reprAux(result: var string, p: pointer, typ: PNimType, cl: var ReprClosure) 
+
+proc reprArray(p: pointer, typ: PNimType, 
+              cl: var ReprClosure):string {.compilerRtl.} =
+  result = "["
+  for i in 0 .. < typ.size:
+    if i > 0: add result, ", "
+    reprAux(result, p, typ.base, cl )
+    # We know the actual pointer in js has a `_Idx` suffix, 
+    # so we cheat and advance the pointer. 
+    # If c can do pointer math we are allowd this, right?
+    asm "`p`_Idx++;"
+
+  add result, "]"
+
+proc reprAux(result: var string, p: pointer, typ: PNimType, 
+            cl: var ReprClosure) =
+  if cl.recdepth == 0:
+    add result, "..."
+    return
+  dec(cl.recdepth)
+  case typ.kind
+  of tyInt..tyInt64,tyUInt..tyUInt64:
+    add result, reprInt(cast[ptr int](p)[])
+  of tyChar:
+    add result, reprChar(cast[ptr char](p)[])
+  of tyBool:
+    add result, reprBool(cast[ptr bool](p)[])
+  of tyFloat..tyFloat128:
+    add result, reprFloat(cast[ptr float](p)[])
+  of tyString:
+    add result, reprStr(cast[ptr string](p)[])
+  of tyEnum, tyOrdinal:
+    add result, reprEnum(cast[ptr int](p)[],typ)
+  of tySet:
+    add result, reprSet(cast[ptr int](p)[],typ)
+  of tyArray:
+    add result, reprArray(p,typ,cl)
+  else:
+    add result, "(invalid data!)"
+  inc(cl.recdepth)
+
+proc reprAny(p: pointer, typ: PNimType): string {.compilerRtl.}=
+  var
+    cl: ReprClosure
+  initReprClosure(cl)
+  result = ""
+  if typ.kind in {tyObject, tyTuple, tyArray, tyArrayConstr, tySet}:
+    reprAux(result, p, typ, cl)
+  else:
+    var p = p
+    reprAux(result, addr(p), typ, cl)
+  add result, "\n"
+
+#[]
+
+proc reprAux(result: var string, p: pointer, typ: PNimType,
+              cl: var ReprClosure) {.benign.}
+
+  proc reprAux(result: var string, p: pointer, typ: PNimType,
+               cl: var ReprClosure) =
+    if cl.recdepth == 0:
+      add result, "..."
+      return
+    dec(cl.recdepth)
+    case typ.kind
+    of tyArray, tyArrayConstr: reprArray(result, p, typ, cl)
+    #[
+    of tySet: reprSetAux(result, p, typ)
+    of tyArray, tyArrayConstr: reprArray(result, p, typ, cl)
+    of tyTuple: reprRecord(result, p, typ, cl)
+    of tyObject:
+      var t = cast[ptr PNimType](p)[]
+      reprRecord(result, p, t, cl)
+    of tyRef, tyPtr:
+      sysAssert(p != nil, "reprAux")
+      if cast[PPointer](p)[] == nil: add result, "nil"
+      else: reprRef(result, cast[PPointer](p)[], typ, cl)
+    of tySequence:
+      reprSequence(result, cast[PPointer](p)[], typ, cl)
+    of tyInt: add result, $(cast[ptr int](p)[])
+    of tyInt8: add result, $int(cast[ptr int8](p)[])
+    of tyInt16: add result, $int(cast[ptr int16](p)[])
+    of tyInt32: add result, $int(cast[ptr int32](p)[])
+    of tyInt64: add result, $(cast[ptr int64](p)[])
+    of tyUInt: add result, $(cast[ptr uint](p)[])
+    of tyUInt8: add result, $(cast[ptr uint8](p)[])
+    of tyUInt16: add result, $(cast[ptr uint16](p)[])
+    of tyUInt32: add result, $(cast[ptr uint32](p)[])
+    of tyUInt64: add result, $(cast[ptr uint64](p)[])
+
+    of tyFloat: add result, $(cast[ptr float](p)[])
+    of tyFloat32: add result, $(cast[ptr float32](p)[])
+    of tyFloat64: add result, $(cast[ptr float64](p)[])
+    of tyEnum: add result, reprEnum(getInt(p, typ.size), typ)
+    of tyBool: add result, reprBool(cast[ptr bool](p)[])
+    of tyChar: add result, reprChar(cast[ptr char](p)[])
+    of tyString:
+      let sp = cast[ptr string](p)
+      reprStrAux(result, if sp[].isNil: nil else: sp[].cstring, sp[].len)
+    of tyCString:
+      let cs = cast[ptr cstring](p)[]
+      if cs.isNil: add result, "nil"
+      else: reprStrAux(result, cs, cs.len)
+    of tyRange: reprAux(result, p, typ.base, cl)
+    of tyProc, tyPointer:
+      if cast[PPointer](p)[] == nil: add result, "nil"
+      else: add result, reprPointer(cast[PPointer](p)[])
+    ]#
+    else:
+      add result, "(invalid data!)"
+    inc(cl.recdepth)
+]#
