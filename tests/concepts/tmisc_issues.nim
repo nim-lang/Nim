52377740b6fe85b03f02552117ac92b82c2809a8
Zahary Karadjov <zahary@gmail.com>
2017-03-24 15:29:40 +0200

    close #1570

--- tests/concepts/tmisc_issues.nim
+++ tests/concepts/tmisc_issues.nim
@@ -1,5 +1,8 @@
 discard """
-output: '''true'''
+output: '''true
+true
+true
+true'''
 """
 
 # https://github.com/nim-lang/Nim/issues/1147
@@ -13,4 +16,21 @@ type CAddable = concept x
   x[].add(int)
 
 echo((ref TTest) is CAddable)
+
+# https://github.com/nim-lang/Nim/issues/1570
+type ConcretePointOfFloat = object
+  x, y: float
+
+type ConcretePoint[Value] = object
+  x, y: Value
+
+type AbstractPointOfFloat = generic p
+  p.x is float and p.y is float
+
+let p1 = ConcretePointOfFloat(x: 0, y: 0)
+let p2 = ConcretePoint[float](x: 0, y: 0)
+
+echo p1 is AbstractPointOfFloat      # true
+echo p2 is AbstractPointOfFloat      # true
+echo p2.x is float and p2.y is float # true
 
