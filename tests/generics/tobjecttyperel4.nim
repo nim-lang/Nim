a40160440e78efbe41242b499a4ef53b000f5340
andri lim <jangko128@gmail.com>
2017-04-04 18:28:52 +0700

    fixes #5570,  fixes #5602 generic parameter subtitution

---
+++ tests/generics/tobjecttyperel4.nim
@@ -0,0 +1,67 @@
+
+# bug 5570
+import macros
+
+type
+  BaseFruit[T] = object of RootObj
+    color: T
+
+  Banana[T] = object of BaseFruit[uint32]
+    weight: T
+
+macro getTypeName(typ: typed): untyped =
+  echo getType(typ).repr
+
+proc setColor[K](self: var BaseFruit[K], c: uint32) =
+  getTypeName(self.color)
+  self.color = c
+
+var x: Banana[float64]
+x.weight = 17
+getTypeName(x.color)
+x.setColor(11)
+echo x
+
+discard """
+  msg: '''uint32
+uint32
+float
+uint32
+uint32
+float'''
+  output: '''(weight: 17.0, color: 11)
+(weight: 0.0, color: 11.0, width: 17)
+0'''
+"""
+type
+  BaseCar[T, K] = object of RootObj
+    color: T
+    width: K
+
+  Wagon[T] = object of BaseCar[T, uint32]
+    weight: T
+
+proc setColor[T, K](self: var BaseCar[T, K], c: float) =
+  getTypeName(self.color)
+  self.color = c
+
+proc setWidth[T, K](self: var BaseCar[T, K], w: uint32) =
+  getTypeName(self.width)
+  self.width = w
+
+var y: Wagon[float64]
+getTypeName(y.color)
+getTypeName(y.width)
+y.setWidth(17)
+y.setColor(11)
+echo y
+
+# bug 5602
+type
+  Foo[T] = object of RootObj
+  Bar[T] = object of Foo[seq[T]]
+
+proc p[T](f: Foo[T]): T = discard
+
+var s: Bar[float]
+echo p(s).len
