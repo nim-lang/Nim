e442648362e5c9e13eb998ba110e63dc8013da0f
gogolxdong <lxdong.007@gmail.com>
2017-06-04 12:18:07 +0800

    add power operation

--- lib/pure/matutils.nim
+++ lib/pure/matutils.nim
@@ -1,4 +1,4 @@
-import sequtils
+import sequtils,math
 
 type Matrix*[M,N: static[int];T:SomeNumber] = array[M, array[N, T]]
  
@@ -51,10 +51,15 @@ proc `-`*[M,N,T](m: Matrix[M,N,T]): Matrix[M,N,T]=
     for j in result[0].low .. result[0].high:
         result[i][j] = -m[i][j]
 
-proc `*`*[M,P,N,T](a: Matrix[M,P,T]; k:T): Matrix[M,N,T]=
+proc `*`*[M,N,T](a: Matrix[M,N,T]; k:T): Matrix[M,N,T]=
   for i in result.low .. result.high:
     for j in result[0].low .. result[0].high:
         result[i][j] = k*a[i][j]
+
+proc `**`*[M,N,T](a: Matrix[M,N,T]; k:float64): Matrix[M,N,float64]=
+  for i in result.low .. result.high:
+    for j in result[0].low .. result[0].high:
+        result[i][j] = pow(k,a[i][j].float64)        
 
 when isMainModule:
   let a = [[1.0,  1.0,  1.0,   1.0],
@@ -62,7 +67,7 @@ when isMainModule:
          [3.0,  9.0, 27.0,  81.0],
          [4.0, 16.0, 64.0, 256.0]]
  
-  let b = [[  4.0  , -3.0  ,  4/3.0,  -1/4.0 ],
+  let b = [[4.0  , -3.0  ,  4/3.0,  -1/4.0 ],
          [-13/3.0, 19/4.0, -7/3.0,  11/24.0],
          [  3/2.0, -2.0  ,  7/6.0,  -1/4.0 ],
          [ -1/6.0,  1/4.0, -1/6.0,   1/24.0]]
@@ -79,3 +84,4 @@ when isMainModule:
   echo x*y
   echo x-x
   echo -x
+  echo x**2
