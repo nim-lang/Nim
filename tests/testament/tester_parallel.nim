2fe535e1054b5535443f5127d225d043e320509d
singularperturbation <sloanes.k@gmail.com>
2017-08-05 16:22:59 -0500

    Make tests parallel - first attempt
    If Travis or other CI have multiple cores, then we should be able to use
    those to run the tests (by category) in separate processes.
    
    This is an experiment.

---
+++ tests/testament/tester_parallel.nim
@@ -0,0 +1,46 @@
+import os, osproc, ospaths, strutils, threadpool
+from math import nextPowerOfTwo
+
+proc main() =
+  ## Run testament in parallel, spawning off a new process for each category
+  let workerCount = 
+    if (let numCpus = countProcessors(); numCpus != 0):
+      numCpus
+    else:
+      1
+  
+  # Additional categories added when running testament with "all"
+  const AdditionalCategories = ["debugger", "examples", "lib"]
+  
+  let testDir = "tests" & DirSep
+  let cmdTemplate = "tests/testament/tester --pedantic c $# -d:nimCoroutines"
+
+  # Sequence of commands 
+  var cmds = newSeqOfCap[string](nextPowerOfTwo(110 + AdditionalCategories.len))
+
+  for additional_cat in AdditionalCategories:
+    cmds.add(cmdTemplate.format(additional_cat))
+
+  for kind, dir in walkDir(testDir):
+    assert testDir.startsWith(testDir)
+    let cat = dir[testDir.len .. ^1]
+    
+    if kind == pcDir and cat notin ["testament", "testdata", "nimcache"]:
+      # Spawn worker process for this category
+      cmds.add(cmdTemplate.format(cat))
+  
+  # Now launch all commands with set # of processes
+  var cmdResults: seq[FlowVar[TaintedString]] = newSeq[FlowVar[TaintedString]](len(cmds))
+
+  setMaxPoolSize(workerCount)
+
+  for i, cmd in cmds:
+    cmdResults[i] = spawn execProcess(command = cmd)
+
+  sync()
+
+  for res in cmdResults:
+    echo ^res
+
+when isMainModule:
+  main()
