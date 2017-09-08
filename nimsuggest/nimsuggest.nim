e159ee32a940bee71b17e8b332036a7ebabd8dde
Federico Ceratto <federico.ceratto@suse.com>
2017-09-08 11:55:50 +0100

    Fix spelling error

--- nimsuggest/nimsuggest.nim
+++ nimsuggest/nimsuggest.nim
@@ -365,7 +365,7 @@ proc replEpc(x: ThreadParams) {.thread.} =
     of "epc-error":
       # an unhandled exception forces down the whole process anyway, so we
       # use 'quit' here instead of 'raise'
-      quit("recieved epc error: " & $messageBuffer)
+      quit("received epc error: " & $messageBuffer)
     else:
       let errMessage = case epcApi
                        of "return", "return-error":
