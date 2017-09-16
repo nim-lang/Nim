e79a1f3459492d42fb85898fa8109a57b731f761
Erik Johansson Andersson <ephja@yandex.com>
2017-09-16 22:31:47 +0200

    the output spec is too long; use 'assert' instead

--- tests/array/tarraylen.nim
+++ tests/array/tarraylen.nim
@@ -1,33 +1,15 @@
-discard """
-  file: "tarraylen.nim"
-  output: '''0
-1
-1
-1
-1
-2
-2
-3
-3
-3
-3
-1
-3
-1
-'''
-"""
 var a: array[0, int]
-echo a.len
-echo array[0..0, int].len
-echo(array[0..0, int]([1]).len)
-echo array[1..1, int].len
-echo(array[1..1, int]([1]).len)
-echo array[2, int].len
-echo(array[2, int]([1, 2]).len)
-echo array[1..3, int].len
-echo(array[1..3, int]([1, 2, 3]).len)
-echo array[0..2, int].len
-echo(array[0..2, int]([1, 2, 3]).len)
-echo array[-2 .. -2, int].len
-echo([1, 2, 3].len)
-echo([42].len)
+assert a.len == 0
+assert array[0..0, int].len == 1
+assert array[0..0, int]([1]).len == 1
+assert array[1..1, int].len == 1
+assert array[1..1, int]([1]).len == 1
+assert array[2, int].len == 2
+assert array[2, int]([1, 2]).len == 2
+assert array[1..3, int].len == 3
+assert array[1..3, int]([1, 2, 3]).len == 3
+assert array[0..2, int].len == 3
+assert array[0..2, int]([1, 2, 3]).len == 3
+assert array[-2 .. -2, int].len == 1
+assert([1, 2, 3].len == 3)
+assert([42].len == 1)
