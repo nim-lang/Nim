b402968a20401aa471b17739deba54961617927c
Erik Johansson Andersson <ephja@yandex.com>
2017-09-17 13:45:22 +0200

    assert->doAssert, add test spec

--- tests/array/tarraylen.nim
+++ tests/array/tarraylen.nim
@@ -1,15 +1,18 @@
+discard """
+  output: ""
+"""
 var a: array[0, int]
-assert a.len == 0
-assert array[0..0, int].len == 1
-assert array[0..0, int]([1]).len == 1
-assert array[1..1, int].len == 1
-assert array[1..1, int]([1]).len == 1
-assert array[2, int].len == 2
-assert array[2, int]([1, 2]).len == 2
-assert array[1..3, int].len == 3
-assert array[1..3, int]([1, 2, 3]).len == 3
-assert array[0..2, int].len == 3
-assert array[0..2, int]([1, 2, 3]).len == 3
-assert array[-2 .. -2, int].len == 1
-assert([1, 2, 3].len == 3)
-assert([42].len == 1)
+doAssert a.len == 0
+doAssert array[0..0, int].len == 1
+doAssert array[0..0, int]([1]).len == 1
+doAssert array[1..1, int].len == 1
+doAssert array[1..1, int]([1]).len == 1
+doAssert array[2, int].len == 2
+doAssert array[2, int]([1, 2]).len == 2
+doAssert array[1..3, int].len == 3
+doAssert array[1..3, int]([1, 2, 3]).len == 3
+doAssert array[0..2, int].len == 3
+doAssert array[0..2, int]([1, 2, 3]).len == 3
+doAssert array[-2 .. -2, int].len == 1
+doAssert([1, 2, 3].len == 3)
+doAssert([42].len == 1)
