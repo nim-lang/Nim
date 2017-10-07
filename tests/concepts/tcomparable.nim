c9edcf958325abc038f63ba6e7153ccd4b5ffe62
Zahary Karadjov <zahary@gmail.com>
2017-10-07 21:52:32 +0300

    close #6464

--- tests/concepts/tcomparable.nim
+++ tests/concepts/tcomparable.nim
@@ -11,3 +11,11 @@ proc myMax(a, b: Comparable): Comparable =
 doAssert myMax(5, 10) == 10
 doAssert myMax(31.3, 1.23124) == 31.3
 
+# issue 6464
+type T = concept x
+  x.q
+
+proc p(x, y: T): T = x
+proc q(x: int) = discard
+let x = p(5, 7)
+
