8cdd89f45647eb134316f3d4bd6668ce69e01875
Andreas Rumpf <rumpf_a@web.de>
2017-10-10 09:34:03 +0200

    make tests green again

--- tests/misc/treadx.nim
+++ tests/misc/treadx.nim
@@ -6,7 +6,7 @@ when not defined(windows):
   var buf: array[0..10, char]
   while true:
     var r = read(0, addr(buf), sizeof(buf)-1)
-    add inp, $buf.cstring
+    add inp, $cstring(addr buf)
     if r != sizeof(buf)-1: break
 
   echo inp
