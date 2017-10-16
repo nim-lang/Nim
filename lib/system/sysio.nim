527e20fd3a0f2cc13afcfb0d402b77087c94c3ee
Andreas Rumpf <rumpf_a@web.de>
2017-10-16 19:52:22 +0200

    fixes the Windows build

--- lib/system/sysio.nim
+++ lib/system/sysio.nim
@@ -406,14 +406,16 @@ proc setStdIoUnbuffered() =
 
 when declared(stdout):
   proc echoBinSafe(args: openArray[string]) {.compilerProc.} =
-    proc flockfile(f: File) {.importc, noDecl.}
-    proc funlockfile(f: File) {.importc, noDecl.}
-    flockfile(stdout)
+    when not defined(windows):
+      proc flockfile(f: File) {.importc, noDecl.}
+      proc funlockfile(f: File) {.importc, noDecl.}
+      flockfile(stdout)
     for s in args:
       discard c_fwrite(s.cstring, s.len, 1, stdout)
     const linefeed = "\n" # can be 1 or more chars
     discard c_fwrite(linefeed.cstring, linefeed.len, 1, stdout)
     discard c_fflush(stdout)
-    funlockfile(stdout)
+    when not defined(windows):
+      funlockfile(stdout)
 
 {.pop.}
