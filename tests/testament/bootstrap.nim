a77ca88b1c8f3fd9a0f893a66fd58fe7cadc938a
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2017-10-20 23:41:27 +0200

    Removed superfluous testament bootstrap and jQuery modules
    Now part of testamenthtml.templ

--- tests/testament/bootstrap.nim
+++
@@ -1,9 +0,0 @@
-const
-  # Integrity checked CDN resources
-  # https://ajax.aspnetcdn.com/ajax/bootstrap/3.3.7/bootstrap.min.js
-  # https://ajax.aspnetcdn.com/ajax/bootstrap/3.3.7/css/bootstrap.min.css
-  # https://ajax.aspnetcdn.com/ajax/bootstrap/3.3.7/css/bootstrap-theme.min.css
-  # using https://www.srihash.org/
-  bootstrap_script_cdn_tag* = """<script src="https://ajax.aspnetcdn.com/ajax/bootstrap/3.3.7/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>"""
-  bootstrap_style_cdn_tag* = """<link rel="stylesheet" href="https://ajax.aspnetcdn.com/ajax/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />"""
-  bootstrap_theme_style_cdn_tag* = """<link rel="stylesheet" href="https://ajax.aspnetcdn.com/ajax/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous" />"""
--- tests/testament/jquery.nim
+++
@@ -1,4 +0,0 @@
-const
-  # Integrity checked CDN resource https://ajax.aspnetcdn.com/ajax/jquery/jquery-2.2.0.min.js
-  # using https://www.srihash.org/
-  jquery_script_cdn_tag* = """<script src="https://ajax.aspnetcdn.com/ajax/jquery/jquery-2.2.0.min.js" integrity="sha384-K+ctZQ+LL8q6tP7I94W+qzQsfRV2a+AfHIi9k8z8l9ggpc8X+Ytst4yBo/hH+8Fk" crossorigin="anonymous"></script>"""
