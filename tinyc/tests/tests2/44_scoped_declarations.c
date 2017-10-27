02bdd257ac62d0e04d0609f4bb1fc0127e80b5c8
data-man <datamanrb@gmail.com>
2017-10-27 18:55:13 +0300

    add missed files

---
+++ tinyc/tests/tests2/44_scoped_declarations.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+   int a;
+
+   for (a = 0; a < 2; a++)
+   {
+      int b = a;
+   }
+
+   printf("it's all good\n");
+
+   return 0;
+}
+
+/* vim: set expandtab ts=4 sw=3 sts=3 tw=80 :*/
