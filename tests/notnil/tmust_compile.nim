96ddf6972d088e0e389840e718da7ca498129142
Araq <rumpf_a@web.de>
2017-11-23 03:12:09 +0100

    fixes #3993

--- tests/notnil/tmust_compile.nim
+++ tests/notnil/tmust_compile.nim
@@ -62,3 +62,17 @@ proc parse(cts: CTS, jn: JsonNode) =
 
 proc p(x: proc(){.closure.} not nil) = discard
 p(proc(){.closure.} = discard)
+
+# bug #3993
+
+type
+  List[T] = seq[T] not nil
+
+proc `^^`[T](v: T, lst: List[T]): List[T] =
+  result = @[v]
+  result.add(lst)
+
+proc Nil[T](): List[T] = @[]
+
+when isMainModule:
+  let lst = 1 ^^ 2 ^^ Nil[int]()
