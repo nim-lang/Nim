54b02533e955c88fb55b70ba183b7b2d66e1e01b
Fabian Keller <github.100.fkeller@spamgourmet.com>
2017-12-10 15:44:33 +0100

    adjusted `$` operator for remaining collections

--- tests/collections/tcollections_to_string.nim
+++ tests/collections/tcollections_to_string.nim
@@ -2,22 +2,47 @@ discard """
   exitcode: 0
   output: ""
 """
+import sets
 import tables
+import deques
 import lists
+import critbits
 
 # Tests for tuples
 doAssert $(1, 2, 3) == "(Field0: 1, Field1: 2, Field2: 3)"
-doAssert $("1", "2", "3") == "(Field0: \"1\", Field1: \"2\", Field2: \"3\")"
-doAssert $('1', '2', '3') == "(Field0: '1', Field1: '2', Field2: '3')"
+doAssert $("1", "2", "3") == """(Field0: "1", Field1: "2", Field2: "3")"""
+doAssert $('1', '2', '3') == """(Field0: '1', Field1: '2', Field2: '3')"""
 
 # Tests for seqs
 doAssert $(@[1, 2, 3]) == "@[1, 2, 3]"
-doAssert $(@["1", "2", "3"]) == "@[\"1\", \"2\", \"3\"]"
-doAssert $(@['1', '2', '3']) == "@['1', '2', '3']"
+doAssert $(@["1", "2", "3"]) == """@["1", "2", "3"]"""
+doAssert $(@['1', '2', '3']) == """@['1', '2', '3']"""
+
+# Tests for sets
+doAssert $(toSet([1])) == "{1}"
+doAssert $(toSet(["1"])) == """{"1"}"""
+doAssert $(toSet(['1'])) == """{'1'}"""
+doAssert $(toOrderedSet([1, 2, 3])) == "{1, 2, 3}"
+doAssert $(toOrderedSet(["1", "2", "3"])) == """{"1", "2", "3"}"""
+doAssert $(toOrderedSet(['1', '2', '3'])) == """{'1', '2', '3'}"""
 
 # Tests for tables
 doAssert $({1: "1", 2: "2"}.toTable) == """{1: "1", 2: "2"}"""
 doAssert $({"1": 1, "2": 2}.toTable) == """{"1": 1, "2": 2}"""
+
+# Tests for deques
+block:
+  var d = initDeque[int]()
+  d.addLast(1)
+  doAssert $d == "[1]"
+block:
+  var d = initDeque[string]()
+  d.addLast("1")
+  doAssert $d == """["1"]"""
+block:
+  var d = initDeque[char]()
+  d.addLast('1')
+  doAssert $d == "['1']"
 
 # Tests for lists
 block:
@@ -31,13 +56,28 @@ block:
   l.append("1")
   l.append("2")
   l.append("3")
-  doAssert $l == "[\"1\", \"2\", \"3\"]"
+  doAssert $l == """["1", "2", "3"]"""
 block:
   var l = initDoublyLinkedList[char]()
   l.append('1')
   l.append('2')
   l.append('3')
-  doAssert $l == "['1', '2', '3']"
+  doAssert $l == """['1', '2', '3']"""
+
+# Tests for critbits
+block:
+  var t: CritBitTree[int]
+  t["a"] = 1
+  doAssert $t == "{a: 1}"
+block:
+  var t: CritBitTree[string]
+  t["a"] = "1"
+  doAssert $t == """{a: "1"}"""
+block:
+  var t: CritBitTree[char]
+  t["a"] = '1'
+  doAssert $t == "{a: '1'}"
+
 
 # Test escaping behavior
 block:
