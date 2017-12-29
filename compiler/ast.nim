4d92d7f6d0a5c497419e00ab18692d0657c38361
Andrii Riabushenko <ariabushenko@gmail.com>
2017-12-29 12:14:03 +0300

    Add comma

--- compiler/ast.nim
+++ compiler/ast.nim
@@ -269,7 +269,7 @@ type
                       # language; for interfacing with Objective C
     sfDiscardable,    # returned value may be discarded implicitly
     sfOverriden,      # proc is overriden
-    sfGenSym          # symbol is 'gensym'ed; do not add to symbol table,
+    sfGenSym,         # symbol is 'gensym'ed; do not add to symbol table,
     sfCustomPragma    # symbol is custom pragma template
 
   TSymFlags* = set[TSymFlag]
