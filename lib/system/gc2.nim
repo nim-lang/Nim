d1e10f9aa3e033414fb924e4f90736e46fde8256
Yuriy Glukhov <yglukhov@users.noreply.github.com>
2017-12-31 11:28:51 +0300

    Fixed mutex usage in SharedList and SharedTable. Closes #6988 (#6990)

--- lib/system/gc2.nim
+++ lib/system/gc2.nim
@@ -133,7 +133,7 @@ proc initGC() =
     init(gch.additionalRoots)
     init(gch.greyStack)
     when hasThreadSupport:
-      gch.toDispose = initSharedList[pointer]()
+      init(gch.toDispose)
 
 # Which color to use for new objects is tricky: When we're marking,
 # they have to be *white* so that everything is marked that is only
