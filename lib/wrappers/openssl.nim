7b2b0ebf7fc7c6276f973f82424f7328823e221c
Araq <rumpf_a@web.de>
2018-02-09 16:47:30 +0100

    OpenSSL: 32bit DLL names on Windows lack the -x32 suffix

--- lib/wrappers/openssl.nim
+++ lib/wrappers/openssl.nim
@@ -33,8 +33,8 @@ when useWinVersion:
       DLLUtilName* = "(libcrypto-1_1-x64|libeay64).dll"
   else:
     const
-      DLLSSLName* = "(libssl-1_1-x32|ssleay32|libssl32).dll"
-      DLLUtilName* =  "(libcrypto-1_1-x32|libeay32).dll"
+      DLLSSLName* = "(libssl-1_1|ssleay32|libssl32).dll"
+      DLLUtilName* =  "(libcrypto-1_1|libeay32).dll"
 
   from winlean import SocketHandle
 else:
