14a0b5297914cbaf3d13f72a06287c5b2c887c68
Imran Hendley <imran.hendley@gmail.com>
2018-02-11 13:23:23 -0500

    remove shortcuts from < and ==

--- lib/pure/collections/intsets.nim
+++ lib/pure/collections/intsets.nim
@@ -328,14 +328,10 @@ proc `<=`*(s1, s2: IntSet): bool =
 
 proc `<`*(s1, s2: IntSet): bool =
   ## Returns true iff `s1` is proper subset of `s2`.
-  if s2.elems < s2.a.len and s1.elems > s2.elems:
-    return false
   return s1 <= s2 and not (s2 <= s1)
 
 proc `==`*(s1, s2: IntSet): bool =
   ## Returns true if both `s` and `t` have the same members and set size.
-  if s2.elems < s2.a.len and s1.elems != s2.elems:
-    return false
   return s1 <= s2 and s2 <= s1
 
 template dollarImpl(): untyped =
