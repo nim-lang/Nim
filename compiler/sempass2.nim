8ed38c1b25baeda8752a47f5f29e365316aa303b
cooldome <ariabushenko@bk.ru>
2018-02-13 21:17:29 +0000

    User pragmas in effect specs bug fix

--- compiler/sempass2.nim
+++ compiler/sempass2.nim
@@ -401,7 +401,10 @@ proc trackPragmaStmt(tracked: PEffects, n: PNode) =
 
 proc effectSpec(n: PNode, effectType: TSpecialWord): PNode =
   for i in countup(0, sonsLen(n) - 1):
-    var it = n.sons[i]
+    let it = n.sons[i]
+    if it.kind == nkPragma:
+      result = effectSpec(it, effectType)
+      if result != nil: return
     if it.kind == nkExprColonExpr and whichPragma(it) == effectType:
       result = it.sons[1]
       if result.kind notin {nkCurly, nkBracket}:
--- tests/pragmas/tuserpragma.nim
+++ tests/pragmas/tuserpragma.nim
@@ -1,7 +1,19 @@
+discard """
+output: 
+"""
 
 {.pragma: rtl, cdecl, exportc.}
 
 proc myproc(x, y: int): int {.rtl} =
-  nil
+  discard
 
+#####################
 
+# bug #7216
+{.pragma: my_pragma, raises: [].}
+
+doAssert(not compiles(
+  block:
+    proc test1 {.my_pragma.} =
+      raise newException(Exception, "msg")
+))
