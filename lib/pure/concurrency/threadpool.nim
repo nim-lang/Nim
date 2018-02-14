cb4921d37fe3a6d37ea4047a4593e63eff822682
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2018-02-14 17:07:10 +0200

    Simplify more, prevent extra stuff from getting into closure env

--- lib/pure/concurrency/threadpool.nim
+++ lib/pure/concurrency/threadpool.nim
@@ -32,13 +32,8 @@ type
     flowVar: pointer
     complete: bool
 
-  MsgFrom = ref object {.inheritable, pure.}
-    writeResult: proc(m: MsgFrom) {.nimcall.}
-    flowVar: pointer
-
-  ConcreteMsgFrom[T] = ref object of MsgFrom
-    when T isnot void:
-      v: T
+  MsgFrom = object
+    writeResult: proc(): int
 
   ChannelTo = Channel[MsgTo]
   ChannelFrom = Channel[MsgFrom]
@@ -126,18 +121,14 @@ proc newFlowVar[T](tp: ThreadPool): FlowVar[T] =
 
 proc sendBack[T](v: T, c: ChannelFromPtr, flowVar: pointer) {.gcsafe.} =
   if not flowVar.isNil:
-    var msg: ConcreteMsgFrom[T]
-    msg.new()
-    when T isnot void:
-      msg.v = v
-    msg.writeResult = proc(m: MsgFrom) {.nimcall.} =
-      let m = cast[ConcreteMsgFrom[T]](m)
-      let fv = cast[FlowVar[T]](m.flowVar)
+    var msg: MsgFrom
+    msg.writeResult = proc(): int =
+      let fv = cast[FlowVar[T]](flowVar)
       fv.tp = nil
       when T isnot void:
-        fv.v = m.v
+        fv.v = v
       GC_unref(fv)
-    msg.flowVar = flowVar
+      result = fv.idx
     c[].send(msg)
 
 macro partial(e: typed{nkCall}): untyped =
@@ -166,7 +157,17 @@ macro partial(e: typed{nkCall}): untyped =
     wrapperProc,
     newCall(wrapperIdent, par))
 
-  echo repr result
+  # echo repr result
+
+template setupAction(msg: MsgTo, e: untyped, body: untyped) =
+  block:
+    proc setup(m: var MsgTo, pe: proc) {.inline, nimcall.} =
+      m.action = proc(flowVar: pointer, chanFrom: ChannelFromPtr) =
+        let pe {.inject.} = pe
+        let chanFrom {.inject.} = chanFrom
+        let flowVar {.inject.} = flowVar
+        body
+    setup(msg, partial(e))
 
 template spawnFV*(tp: ThreadPool, e: typed{nkCall}): auto =
   when compiles(e isnot void):
@@ -175,8 +176,7 @@ template spawnFV*(tp: ThreadPool, e: typed{nkCall}): auto =
     type RetType = void
 
   var m: MsgTo
-  let pe = partial(e)
-  m.action = proc(flowVar: pointer, chanFrom: ChannelFromPtr) =
+  setupAction(m, e):
     sendBack(pe(), chanFrom, flowVar)
   let fv = newFlowVar[RetType](tp)
   m.flowVar = cast[pointer](fv)
@@ -189,16 +189,14 @@ template spawn*(tp: ThreadPool, e: typed{nkCall}): untyped =
     spawnFV(tp, e)
   else:
     var m: MsgTo
-    let pe = partial(e)
-    m.action = proc(flowVar: pointer, chanFrom: ChannelFromPtr) =
+    setupAction(m, e):
       pe()
     mixin dispatchMessage
     tp.dispatchMessage(m)
 
 template trySpawn*(tp: ThreadPool, e: typed{nkCall}): bool =
   var m: MsgTo
-  let pe = partial(e)
-  m.action = proc(flowVar: pointer, chanFrom: ChannelFromPtr) =
+  setupAction(m, e):
     pe()
   mixin tryDispatchMessage
   tp.tryDispatchMessage(m)
@@ -209,14 +207,13 @@ template spawnX*(tp: ThreadPool, call: typed) =
 
 proc nextMessage(tp: ThreadPool): int =
   let msg = tp.chanFrom.recv()
-  msg.writeResult(msg)
-  result = cast[FlowVarBase](msg.flowVar).idx
+  result = msg.writeResult()
 
 proc tryNextMessage(tp: ThreadPool): bool {.inline.} =
   let m = tp.chanFrom.tryRecv()
   result = m.dataAvailable
   if result:
-    m.msg.writeResult(m.msg)
+    discard m.msg.writeResult()
 
 proc await*(v: FlowVarBase) =
   while not v.isReadyAux:
