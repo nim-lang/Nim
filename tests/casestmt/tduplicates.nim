bcd1771919d9a44946be28117ed3e1f0495a7e52
Oscar Nihlgård <oscarnihlgard@gmail.com>
2018-02-23 22:13:49 +0100

    Improve test

--- tests/casestmt/tduplicates.nim
+++ tests/casestmt/tduplicates.nim
@@ -1,9 +1,13 @@
 discard """
-  action: run
+  output: '''
+OK
+OK
+OK
+  '''
 """
 
 type Kind = enum A, B
-var k: Kind
+var k = A
 
 template reject(b) =
   static: doAssert(not compiles(b))
@@ -31,17 +35,16 @@ reject:
     case k
     of [A, A]: discard
 
-reject:
-    case k
-    of {A, A..A}: discard
-
 var i = 2
 case i
-of { 1, 1 }: doAssert(false)
-of { 2, 2 }: discard
-else: doAssert(false)
+of { 1, 1 }: discard
+of { 2, 2 }: echo "OK"
+else: discard
 
 case i
 of { 10..30, 15..25, 5..15, 25..35 }: discard
-else: discard
+else: echo "OK"
 
+case k
+of {A, A..A}: echo "OK"
+of B: discard
