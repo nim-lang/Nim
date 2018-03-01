ef3afc490b95a16181dc018236bd0e45402adc64
konqoro <capoiosct@gmail.com>
2018-03-01 15:04:23 +0200

    Replace factorial function with a compile time one

--- lib/pure/math.nim
+++ lib/pure/math.nim
@@ -29,11 +29,21 @@ proc binom*(n, k: int): int {.noSideEffect.} =
   for i in countup(2, k):
     result = (result * (n + 1 - i)) div i
 
+proc createFactTable[n: static[int]]: array[0 .. n, int] =
+   result[0] = 1
+   for i in 1 .. n:
+      result[i] = result[i - 1] * i
+
+when sizeof(int) == 4:
+   const factTable = createFactTable[12]()
+else:
+   const factTable = createFactTable[20]()
+
 proc fac*(n: int): int {.noSideEffect.} =
-  ## Computes the faculty/factorial function.
-  result = 1
-  for i in countup(2, n):
-    result = result * i
+   ## Computes the faculty/factorial function.
+   assert(n > 0, $n & " must not be negative.")
+   assert(n < factTable.len, $n & " is too large to look up in the table")
+   factTable[n]
 
 {.push checks:off, line_dir:off, stack_trace:off.}
 
