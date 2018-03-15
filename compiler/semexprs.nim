89c10b1c24c67509a23e60d78fcc10d74062509a
Peter Munch-Ellingsen <peterme@peterme.net>
2018-03-15 22:48:31 +0100

    Fix result not being able to use in quote do
    This fixes the annoying issue of not be able to use result inside a
    quote do block. It works by a simple trick. The quote do mechanic is
    based on dynamically creating a template and immediately calling it with
    the arguments found within the quote do block. Since this is called in
    the scope of the macro the result variable is shadowed. This trick works
    by changing all occurences of result (which shouldn't cause any issues
    as result isn't used for anything else for the same reason) to another
    name and then passing in an IdentNode with result as a named parameter
    with that name.
    
    Note that currently this just replaces it with a fixed named variable
    "res" which should be changed to a non-colliding, dynamically created
    name.

--- compiler/semexprs.nim
+++ compiler/semexprs.nim
@@ -1812,6 +1812,7 @@ proc processQuotations(c: PContext; n: var PNode, op: string,
     ids.add n
     return
 
+
   if n.kind == nkPrefix:
     checkSonsLen(n, 2, c.config)
     if n[0].kind == nkIdent:
@@ -1822,6 +1823,10 @@ proc processQuotations(c: PContext; n: var PNode, op: string,
         n.sons[0] = newIdentNode(getIdent(c.cache, examinedOp.substr(op.len)), n.info)
   elif n.kind == nkAccQuoted and op == "``":
     returnQuote n[0]
+  elif n.kind == nkIdent:
+    if n.ident.s == "result":
+      # TODO: Create a non-colliding symbol
+      n = newIdentNode(getIdent("res"), n.info)
 
   for i in 0 ..< n.safeLen:
     processQuotations(c, n.sons[i], op, quotes, ids)
@@ -1833,15 +1838,19 @@ proc semQuoteAst(c: PContext, n: PNode): PNode =
   var
     quotedBlock = n[^1]
     op = if n.len == 3: expectString(c, n[1]) else: "``"
-    quotes = newSeq[PNode](1)
+    quotes = newSeq[PNode](2)
       # the quotes will be added to a nkCall statement
-      # leave some room for the callee symbol
-    ids = newSeq[PNode]()
+      # leave some room for the callee symbol and the result symbol
+    ids = newSeq[PNode](1)
       # this will store the generated param names
+      # leave some room for the result symbol
 
   if quotedBlock.kind != nkStmtList:
     localError(c.config, n.info, errXExpected, "block")
 
+  # TODO: Create a non-colliding symbol
+  # This adds a default first field to pass the result symbol
+  ids[0] = newIdentNode(getIdent("res"), n.info)
   processQuotations(c, quotedBlock, op, quotes, ids)
 
   var dummyTemplate = newProcNode(
@@ -1860,6 +1869,8 @@ proc semQuoteAst(c: PContext, n: PNode): PNode =
 
   var tmpl = semTemplateDef(c, dummyTemplate)
   quotes[0] = tmpl[namePos]
+  # This adds a call to newIdentNode("result") as the first argument to the template call
+  quotes[1] = newNode(nkCall, n.info, @[newIdentNode(getIdent("newIdentNode"), n.info), newStrNode(nkStrLit, "result")])
   result = newNode(nkCall, n.info, @[
      createMagic(c.graph, "getAst", mExpandToAst).newSymNode,
     newNode(nkCall, n.info, quotes)])
