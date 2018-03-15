ea8fc30b709b49514ec8cbce4788d2f49ca19094
cooldome <ariabushenko@bk.ru>
2018-03-15 23:45:49 +0000

    Add issue number

--- tests/ccgbugs/tforward_decl_only.nim
+++ tests/ccgbugs/tforward_decl_only.nim
@@ -3,6 +3,7 @@ ccodecheck: "\\i !@('struct tyObject_MyRefObject'[0-z]+' {')"
 output: "hello"
 """
 
+# issue #7339 
 # Test that MyRefObject is only forward declared as it used only by reference
 
 import mymodule
