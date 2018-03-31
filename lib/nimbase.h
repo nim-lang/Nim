87313ac7e47550b40f7c8da65c8070b554bbecea
Adrian Veith <adrian@veith-system.de>
2018-03-31 11:30:04 +0200

    put all typedefs into optional namespace Nim

--- lib/nimbase.h
+++ lib/nimbase.h
@@ -264,6 +264,11 @@ __clang__
 #  define HAVE_STDINT_H
 #endif
 
+/* wrap all Nim typedefs into namespace Nim */
+#ifdef USE_NIM_NAMESPACE
+namespace Nim {
+#endif
+
 /* bool types (C++ has it): */
 #ifdef __cplusplus
 #  ifndef NIM_TRUE
@@ -341,6 +346,7 @@ typedef NU8 NU;
 #  endif
 #endif
 
+
 extern NI nim_program_result;
 
 typedef float NF32;
@@ -413,10 +419,6 @@ typedef struct TStringDesc* string;
 #  endif
 #endif
 
-#ifdef USE_NIM_NAMESPACE
-namespace Nim {
-#endif
-
 typedef struct TFrame_ TFrame;
 struct TFrame_ {
   TFrame* prev;
@@ -451,10 +453,6 @@ struct TFrame_ {
 	FR.line = n; FR.filename = file;
 #endif
 
-#ifdef USE_NIM_NAMESPACE
-}
-#endif
-
 #define NIM_POSIX_INIT  __attribute__((constructor))
 
 #ifdef __GNUC__
@@ -483,6 +481,10 @@ static inline void GCGuard (void *ptr) { asm volatile ("" :: "X" (ptr)); }
    On disagreement, your C compiler will say something like:
    "error: 'Nim_and_C_compiler_disagree_on_target_architecture' declared as an array with a negative size" */
 typedef int Nim_and_C_compiler_disagree_on_target_architecture[sizeof(NI) == sizeof(void*) && NIM_INTBITS == sizeof(NI)*8 ? 1 : -1];
+
+#ifdef USE_NIM_NAMESPACE
+}
+#endif
 
 #ifdef  __cplusplus
 #  define NIM_EXTERNC extern "C"
