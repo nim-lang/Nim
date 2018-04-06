aa6353e4fa2115010f6217c99dfd140a3421dde0
Araq <rumpf_a@web.de>
2018-04-06 13:10:22 +0200

    fixes #6843

--- compiler/ccgexprs.nim
+++ compiler/ccgexprs.nim
@@ -271,7 +271,7 @@ proc genAssignment(p: BProc, dest, src: TLoc, flags: TAssignmentFlags) =
     # little HACK to support the new 'var T' as return type:
     linefmt(p, cpsStmts, "$1 = $2;$n", rdLoc(dest), rdLoc(src))
     return
-  let ty = skipTypes(dest.t, abstractRange + tyUserTypeClasses)
+  let ty = skipTypes(dest.t, abstractRange + tyUserTypeClasses + {tyStatic})
   case ty.kind
   of tyRef:
     genRefAssign(p, dest, src, flags)
