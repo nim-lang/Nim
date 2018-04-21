e17f3f37232a2a9f90a40c90e1e0a604d714874f
Dominik Picheta <dominikpicheta@gmail.com>
2018-04-21 11:16:53 +0100

    Added countup/countdown iterators for floats.

--- lib/system.nim
+++ lib/system.nim
@@ -2041,7 +2041,7 @@ iterator countdown*[T](a, b: T, step = 1): T {.inline.} =
     var res = a
     while res >= b:
       yield res
-      dec(res, step)
+      res -= step.T
 
 when defined(nimNewRoof):
   iterator countup*[T](a, b: T, step = 1): T {.inline.} =
@@ -2058,7 +2058,7 @@ when defined(nimNewRoof):
       var res: T = T(a)
       while res <= b:
         yield res
-        inc(res, step)
+        res += step.T
 
   iterator `..`*[T](a, b: T): T {.inline.} =
     ## An alias for `countup`.
@@ -2071,7 +2071,7 @@ when defined(nimNewRoof):
       var res: T = T(a)
       while res <= b:
         yield res
-        inc(res)
+        res += 1.T
 
   template dotdotImpl(t) {.dirty.} =
     iterator `..`*(a, b: t): t {.inline.} =
@@ -2102,7 +2102,7 @@ else:
       var res: T = T(a)
       while res <= b:
         yield res
-        inc(res, step)
+        res += step.T
 
   iterator `..`*[S, T](a: S, b: T): T {.inline.} =
     ## An alias for `countup`.
@@ -2115,7 +2115,8 @@ else:
       var res: T = T(a)
       while res <= b:
         yield res
-        inc(res)
+        res += 1.T
+
 
 
 iterator `||`*[S, T](a: S, b: T, annotation=""): T {.
