d01b1558a3573a07372103b09037992b5380903d
Dominik Picheta <dominikpicheta@googlemail.com>
2018-04-25 22:49:29 +0100

    Improve progress reporting example. Refs #7693

--- lib/pure/httpclient.nim
+++ lib/pure/httpclient.nim
@@ -74,12 +74,17 @@
 ##
 ## .. code-block:: Nim
 ##    import asyncdispatch, httpclient
-##    var client = newAsyncHttpClient()
+##    
 ##    proc onProgressChanged(total, progress, speed: BiggestInt) {.async.} =
 ##      echo("Downloaded ", progress, " of ", total)
 ##      echo("Current rate: ", speed div 1000, "kb/s")
-##    client.onProgressChanged = onProgressChanged
-##    discard waitFor client.getContent("http://speedtest-ams2.digitalocean.com/100mb.test")
+##
+##    proc asyncProc() {.async.} =
+##      var client = newAsyncHttpClient()
+##      client.onProgressChanged = onProgressChanged
+##      discard await client.getContent("http://speedtest-ams2.digitalocean.com/100mb.test")
+##
+##    waitFor asyncProc()
 ##
 ## If you would like to remove the callback simply set it to ``nil``.
 ##
