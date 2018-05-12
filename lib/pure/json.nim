ed4259883f399648353cebe9d316688557a1f539
Diego Guraieb <diegogub@gmail.com>
2018-05-12 22:52:50 +0400

    times module into json

--- lib/pure/json.nim
+++ lib/pure/json.nim
@@ -89,7 +89,7 @@
 ##    echo j2
 
 import
-  hashes, tables, strutils, lexbase, streams, unicode, macros, dates
+  hashes, tables, strutils, lexbase, streams, unicode, macros, times
 
 export
   tables.`$`
