784265adc1937acba0cdd7ce57ae36fb32e44538
Aearnus <tslimkemann42@gmail.com>
2018-05-22 16:48:31 -0700

    Added compiler check on transformFile

--- lib/pure/pegs.nim
+++ lib/pure/pegs.nim
@@ -1016,8 +1016,17 @@ proc transformFile*(infile, outfile: string,
   ## reads in the file `infile`, performs a parallel replacement (calls
   ## `parallelReplace`) and writes back to `outfile`. Raises ``EIO`` if an
   ## error occurs. This is supposed to be used for quick scripting.
-  var x = readFile(infile).string
-  writeFile(outfile, x.parallelReplace(subs))
+  ##
+  ## **Note**: this function warns at compile time if using the JS backend
+  ## because Javascript has no concept of a file system. It will compile as
+  ## a no-op in this situation.
+  when defined(js):
+    {.warning: "Since you are compiling with a JS backend, any file IO will be silently discarded.".}
+    discard
+  else:
+    var x = readFile(infile).string
+    writeFile(outfile, x.parallelReplace(subs))
+
 
 iterator split*(s: string, sep: Peg): string =
   ## Splits the string `s` into substrings.
