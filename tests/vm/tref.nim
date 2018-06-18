b168d493c728dd0f5cc2347b69b3305c022610ed
LemonBoy <thatlemon@gmail.com>
2018-06-18 23:48:19 +0200

    Copy the node if the assignment rhs is not a ref
    Fixes #7871

--- tests/vm/tref.nim
+++ tests/vm/tref.nim
@@ -10,3 +10,12 @@ static:
   b[5] = 'c'
   doAssert a[] == "Hellocworld"
   doAssert b[] == "Hellocworld"
+
+static:
+  type Obj = object
+    field: int
+  var s = newSeq[Obj](1)
+  var o = Obj()
+  s[0] = o
+  o.field = 2
+  doAssert s[0].field == 0
