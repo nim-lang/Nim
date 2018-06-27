b525bccc8878b3a66ea05f5fa692d6588637175b
Joey Yakimowich-Payne <jyapayne@gmail.com>
2018-06-27 10:02:58 +0900

    Remove Result type alias

--- lib/nintendoswitch/switch_memory.nim
+++ lib/nintendoswitch/switch_memory.nim
@@ -2,19 +2,16 @@ const virtMemHeader = "<switch/kernel/virtmem.h>"
 const svcHeader = "<switch/kernel/virtmem.h>"
 const mallocHeader = "<malloc.h>"
 
-type
-  Result* = uint32
-
 proc memalign*(bytes: csize, size: csize): pointer {.importc: "memalign",
     header: mallocHeader.}
 
 proc free*(address: pointer) {.importc: "free",
     header: mallocHeader.}
 
-proc svcMapMemory*(dst_addr: pointer; src_addr: pointer; size: uint64): Result {.
+proc svcMapMemory*(dst_addr: pointer; src_addr: pointer; size: uint64): uint32 {.
     importc: "svcMapMemory", header: svcHeader.}
 
-proc svcUnmapMemory*(dst_addr: pointer; src_addr: pointer; size: uint64): Result {.
+proc svcUnmapMemory*(dst_addr: pointer; src_addr: pointer; size: uint64): uint32 {.
     importc: "svcUnmapMemory", header: svcHeader.}
 
 proc virtmemReserveMap*(size: csize): pointer {.importc: "virtmemReserveMap",
