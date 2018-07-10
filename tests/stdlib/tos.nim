48b98cf701d486a33b78c92f4524d773fc19c605
Andreas Rumpf <rumpf_a@web.de>
2018-07-10 11:37:02 +0200

    fixes tos test for OSX

--- tests/stdlib/tos.nim
+++ tests/stdlib/tos.nim
@@ -145,7 +145,7 @@ else:
   echo getLastModificationTime("a") == tm
 removeFile("a")
 
-when defined(Linux) or defined(osx):
+when defined(Linux) or defined(macosx):
 
   block normalizedPath:
     block relative:
