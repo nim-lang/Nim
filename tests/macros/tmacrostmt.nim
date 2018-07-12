231a83a6b108dc676b53ba5f906d51ff14aa5b51
cooldome <cdome@bk.ru>
2018-07-12 11:03:08 +0200

    Fixes #8287 (#8288)

--- tests/macros/tmacrostmt.nim
+++ tests/macros/tmacrostmt.nim
@@ -24,3 +24,23 @@ macro foo: typed =
   else: echo "Does not compute! (test OK)"
 
 foo()
+
+#------------------------------------
+# bug #8287 
+type MyString = distinct string
+
+proc `$` (c: MyString): string {.borrow.}
+
+proc `!!` (c: cstring): int =
+  c.len
+
+proc f(name: MyString): int =
+  !! $ name
+
+macro repr_and_parse(fn: typed): typed =
+  let fn_impl = fn.getImpl
+  fn_impl.name = genSym(nskProc, $fn_impl.name)
+  echo fn_impl.repr
+  result = parseStmt(fn_impl.repr)
+
+repr_and_parse(f)
