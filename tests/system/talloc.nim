befc67f969e059d2b5b3da45e26492639572e56d
Arne Döring <arne.doering@gmx.net>
2018-07-16 14:21:10 +0200

    adapted talloc

--- tests/system/talloc.nim
+++ tests/system/talloc.nim
@@ -6,11 +6,11 @@ x = cast[ptr int](x.realloc(2))
 assert x != nil
 x.dealloc()
 
-x = createU(int, 3)
+x = cast[ptr int](createU(int, 3))
 assert x != nil
 x.dealloc()
 
-x = create(int, 4)
+x = cast[ptr int](create(int, 4))
 assert cast[ptr array[4, int]](x)[0] == 0
 assert cast[ptr array[4, int]](x)[1] == 0
 assert cast[ptr array[4, int]](x)[2] == 0
@@ -24,11 +24,11 @@ x = cast[ptr int](allocShared(100))
 assert x != nil
 deallocShared(x)
 
-x = createSharedU(int, 3)
+x = cast[ptr int](createSharedU(int, 3))
 assert x != nil
 x.deallocShared()
 
-x = createShared(int, 3)
+x = cast[ptr int](createShared(int, 3))
 assert x != nil
 assert cast[ptr array[3, int]](x)[0] == 0
 assert cast[ptr array[3, int]](x)[1] == 0
@@ -39,13 +39,13 @@ x = cast[ptr int](x.resizeShared(2))
 assert x != nil
 x.deallocShared()
 
-x = create(int, 10)
+x = cast[ptr int](create(int, 10))
 assert x != nil
 x = x.resize(12)
 assert x != nil
 x.dealloc()
 
-x = createShared(int, 1)
+x = cast[ptr int](createShared(int, 1))
 assert x != nil
 x = x.resizeShared(1)
 assert x != nil
