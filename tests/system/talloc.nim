731de0badce48f2c9d2688303a76b1eabd69b6ee
Arne Döring <arne.doering@gmx.net>
2018-07-25 15:08:40 +0200

    removed all type casting in talloc

--- tests/system/talloc.nim
+++ tests/system/talloc.nim
@@ -1,57 +1,66 @@
-var x: ptr int
 
-x = cast[ptr int](alloc(7))
-assert x != nil
-x = cast[ptr int](x.realloc(2))
-assert x != nil
-x.dealloc()
+block:
+  var x = alloc(7)
+  assert x != nil
+  x = x.realloc(2)
+  assert x != nil
+  x.dealloc()
 
-x = cast[ptr int](createU(int, 3))
-assert x != nil
-x.dealloc()
+block:
+  let x = createU(int, 3)
+  assert x != nil
+  x.dealloc()
 
-x = cast[ptr int](create(int, 4))
-assert cast[ptr array[4, int]](x)[0] == 0
-assert cast[ptr array[4, int]](x)[1] == 0
-assert cast[ptr array[4, int]](x)[2] == 0
-assert cast[ptr array[4, int]](x)[3] == 0
+block:
+  var x = create(int, 4)
+  assert x[0] == 0
+  assert x[1] == 0
+  assert x[2] == 0
+  assert x[3] == 0
 
-x = x.resize(4)
-assert x != nil
-x.dealloc()
+  x = x.resize(4)
+  assert x != nil
+  x.dealloc()
 
-x = cast[ptr int](allocShared(100))
-assert x != nil
-deallocShared(x)
+block:
+  let x = allocShared(100)
+  assert x != nil
+  deallocShared(x)
 
-x = cast[ptr int](createSharedU(int, 3))
-assert x != nil
-x.deallocShared()
+block:
+  let x = createSharedU(int, 3)
+  assert x != nil
+  x.deallocShared()
 
-x = cast[ptr int](createShared(int, 3))
-assert x != nil
-assert cast[ptr array[3, int]](x)[0] == 0
-assert cast[ptr array[3, int]](x)[1] == 0
-assert cast[ptr array[3, int]](x)[2] == 0
+block:
+  var x = createShared(int, 3)
+  assert x != nil
+  assert x[0] == 0
+  assert x[1] == 0
+  assert x[2] == 0
 
-assert x != nil
-x = cast[ptr int](x.resizeShared(2))
-assert x != nil
-x.deallocShared()
+  x = x.resizeShared(2)
+  assert x != nil
+  x.deallocShared()
 
-x = cast[ptr int](create(int, 10))
-assert x != nil
-x = x.resize(12)
-assert x != nil
-x.dealloc()
+block:
+  var x = create(int, 10)
+  assert x != nil
+  x = x.resize(12)
+  assert x != nil
+  x.dealloc()
 
-x = cast[ptr int](createShared(int, 1))
-assert x != nil
-x = x.resizeShared(1)
-assert x != nil
-x.deallocShared()
+block:
+  var x = createShared(int, 1)
+  assert x != nil
+  x = x.resizeShared(1)
+  assert x != nil
+  x.deallocShared()
 
-x = cast[ptr int](alloc0(125 shl 23))
-dealloc(x)
-x = cast[ptr int](alloc0(126 shl 23))
-dealloc(x)
+block:
+  let x = alloc0(125 shl 23)
+  dealloc(x)
+
+block:
+  let x = alloc0(126 shl 23)
+  dealloc(x)
