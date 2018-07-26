567a88e6072c9aeb7da20b032bee37788b21e7c0
bennyelg <benjaminelgazar@gmail.com>
2018-07-26 19:56:01 +0300

    fix ..

--- lib/pure/collections/sequtils.nim
+++ lib/pure/collections/sequtils.nim
@@ -680,7 +680,7 @@ template applyIt*(varSeq, op: untyped) =
   ##   var nums = @[1, 2, 3, 4]
   ##   nums.applyIt(it * 3)
   ##   assert nums[0] + nums[3] == 15
-  for i in low(varSeq) ..< high(varSeq):
+  for i in low(varSeq) .. high(varSeq):
     let it {.inject.} = varSeq[i]
     varSeq[i] = op
 
