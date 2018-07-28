8b0ddb7842f05429069e9bda841f73b10c490b23
andri lim <jangko128@gmail.com>
2018-07-28 21:45:31 +0700

    fix test

--- tests/stdlib/tencoding.nim
+++ tests/stdlib/tencoding.nim
@@ -6,14 +6,16 @@ discard """
 
 import encodings, strutils
 
-var utf16LEConverter = open(destEncoding = "utf-16", srcEncoding = "utf-8")
-var s = "some string"
-var c = utf16LEConverter.convert(s)
+when defined(windows):
+  var utf16to8 = open(destEncoding = "utf-16", srcEncoding = "utf-8")
+  var s = "some string"
+  var c = utf16to8.convert(s)
 
-var z = newStringOfCap(s.len * 2)
-for x in s:
-  z.add x
-  z.add chr(0)
+  var z = newStringOfCap(s.len * 2)
+  for x in s:
+    z.add x
+    z.add chr(0)
 
-doAssert z == c
+  doAssert z == c
+
 echo "OK"
