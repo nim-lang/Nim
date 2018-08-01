bd8e62f52a152a4bc9a051442642eb9c2326b1e7
James Osborn <osborn@alcf.anl.gov>
2018-07-31 23:33:28 -0500

    fixed #7894 make system tests run properly

---
+++ tests/system/t7894.nim
@@ -0,0 +1,18 @@
+discard """
+"""
+
+import os
+
+const size = 250000000
+var saved = newSeq[seq[int8]]()
+
+for i in 0..22:
+  # one of these is 0.25GB.
+  #echo i
+  var x = newSeq[int8](size)
+  sleep(10)
+  saved.add(x)
+
+for x in saved:
+  #echo x.len
+  doAssert x.len == size
