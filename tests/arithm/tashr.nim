c6474dde85bb6f933d2bd86c0d8e4cafab9720b1
andri lim <jangko128@gmail.com>
2018-08-06 22:02:20 +0700

    add targets to test

--- tests/arithm/tashr.nim
+++ tests/arithm/tashr.nim
@@ -1,5 +1,6 @@
 discard """
   output: ''''''
+  targets: '''c js'''
 """
 
 # issue #6255, feature request
@@ -11,7 +12,7 @@ var x3 = -123'i32
 var x4 = -123'i64
 var x5 = -123
 
-block ccgen_test:
+block codegen_test:
   doAssert ashr(x1, 1) == -62
   doAssert ashr(x2, 1) == -62
   doAssert ashr(x3, 1) == -62
@@ -31,3 +32,15 @@ static: # VM test
   doAssert ashr(-123'i32, 1) == -62
   doAssert ashr(-123'i64, 1) == -62
   doAssert ashr(-123    , 1) == -62
+
+  var y1 = -123'i8
+  var y2 = -123'i16
+  var y3 = -123'i32
+  var y4 = -123'i64
+  var y5 = -123
+
+  doAssert ashr(y1, 1) == -62
+  doAssert ashr(y2, 1) == -62
+  doAssert ashr(y3, 1) == -62
+  doAssert ashr(y4, 1) == -62
+  doAssert ashr(y5, 1) == -62
