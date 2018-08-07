b302fd3435e77e890d13466a659798810b2f9eb1
Arne Döring <arne.doering@gmx.net>
2018-08-07 19:08:42 +0200

    instance test size packed and unpacked

--- tests/misc/tsizeof.nim
+++ tests/misc/tsizeof.nim
@@ -2,11 +2,24 @@ discard """
   output: ""
 """
 
-# TODO test with inheritance
 # TODO test with small sized kind
-# TODO test padding byes in inheritance object
-# TODO test packed pragma (do a test cast that automatically tests with and without packed pragma)
-# TODO automatically test with packed and unpacked
+
+type
+  TMyEnum = enum
+    tmOne, tmTwo, tmThree, tmFour
+
+  TMyArray1 = array[3, uint8]
+  TMyArray2 = array[1..3, int32]
+  TMyArray3 = array[TMyEnum, float64]
+
+const
+  mysize1 = sizeof(TMyArray1)
+  mysize2 = sizeof(TMyArray2)
+  mysize3 = sizeof(TMyArray3)
+
+assert mysize1 == 3
+assert mysize2 == 12
+assert mysize3 == 32
 
 import macros, typetraits
 
@@ -49,13 +62,10 @@ proc strAlign(arg: string): string =
   for i in 0 ..< minLen - arg.len:
     result &= ' '
 
-proc intAlign(arg: int): string =
-  const minLen = 4
-  result = $arg
-  while result.len < minLen:
-    result.insert(" ")
-
 macro c_offsetof(a: typed, b: untyped): int32 =
+  ## Buffet proof implementation that works on actual offsetof operator
+  ## in the c backend. Assuming of course this implementation is
+  ## correct.
   let bliteral = newLit(repr(b))
   result = quote do:
     var res: int32
@@ -63,12 +73,17 @@ macro c_offsetof(a: typed, b: untyped): int32 =
     res
 
 macro c_sizeof(a: typed): int32 =
+  ## Buffet proof implementation that works using the sizeof operator
+  ## in the c backend. Assuming of course this implementation is
+  ## correct.
   result = quote do:
     var res: int32
     {.emit: [res, " = sizeof(", `a`, ");"] .}
     res
 
 macro c_alignof(arg: untyped): untyped =
+  ## Buffet proof implementation that works on actual alignment
+  ## behavior measured at runtime.
   let typeSym = genSym(nskType, "AlignTestType"&arg.repr)
   result = quote do:
     type
@@ -77,8 +92,28 @@ macro c_alignof(arg: untyped): untyped =
         member: `arg`
     c_offsetof(`typeSym`, member)
 
-{.pragma: objectconfig.}
-#  {.pragma: objectconfig, packed.}
+macro testAlign(arg:untyped):untyped =
+  let prefix = newLit(arg.lineinfo & "  alignof " & arg.repr & " ")
+  result = quote do:
+    let cAlign = c_alignof(`arg`)
+    let nimAlign = alignof(`arg`)
+    if cAlign != nimAlign:
+      echo `prefix`, cAlign, " != ", nimAlign
+
+testAlign(pointer)
+testAlign(int)
+testAlign(uint)
+testAlign(int8)
+testAlign(int16)
+testAlign(int32)
+testAlign(int64)
+testAlign(uint8)
+testAlign(uint16)
+testAlign(uint32)
+testAlign(uint64)
+testAlign(float)
+testAlign(float32)
+testAlign(float64)
 
 type
   MyEnum {.pure.} = enum
@@ -106,229 +141,210 @@ type
     ValueA
     ValueB
 
-  EnumObjectA  {.objectconfig.} = object
-    a : Enum1
-    b : Enum2
-    c : Enum4
-    d : Enum8
+testAlign(MyEnum)
+testAlign(OtherEnum)
+testAlign(Enum1)
+testAlign(Enum2)
+testAlign(Enum4)
+testAlign(Enum8)
 
-  EnumObjectB  {.objectconfig.} = object
-    a : Enum8
-    b : Enum4
-    c : Enum2
-    d : Enum1
 
-  TrivialType  {.objectconfig.} = object
-    x,y,z: int8
+template testinstance(body: untyped): untyped =
+  block:
+    {.pragma: objectconfig.}
+    body
 
-  SimpleAlignment {.objectconfig.} = object
-    # behaves differently on 32bit Windows and 32bit Linux
-    a,b: int8
-    c: int64
+  block:
+    {.pragma: objectconfig, packed.}
+    body
 
-  AlignAtEnd {.objectconfig.} = object
-    a: int64
-    b,c: int8
+testinstance:
+  type
 
-  SimpleBranch {.objectconfig.} = object
-    case kind: MyEnum
-    of MyEnum.ValueA:
-      a: int16
-    of MyEnum.ValueB:
-      b: int32
-    of MyEnum.ValueC:
+    EnumObjectA  {.objectconfig.} = object
+      a : Enum1
+      b : Enum2
+      c : Enum4
+      d : Enum8
+
+    EnumObjectB  {.objectconfig.} = object
+      a : Enum8
+      b : Enum4
+      c : Enum2
+      d : Enum1
+
+    TrivialType  {.objectconfig.} = object
+      x,y,z: int8
+
+    SimpleAlignment {.objectconfig.} = object
+      # behaves differently on 32bit Windows and 32bit Linux
+      a,b: int8
       c: int64
 
-  PaddingBeforeBranchA {.objectconfig.} = object
-    cause: int8
-    case kind: MyEnum
-    of MyEnum.ValueA:
-      a: int16
-    of MyEnum.ValueB:
-      b: int32
-    of MyEnum.ValueC:
-      c: int64
+    AlignAtEnd {.objectconfig.} = object
+      a: int64
+      b,c: int8
 
-  PaddingBeforeBranchB {.objectconfig.} = object
-    cause: int8
-    case kind: MyEnum
-    of MyEnum.ValueA:
-      a: int8
-    of MyEnum.ValueB:
-      b: int16
-    of MyEnum.ValueC:
-      c: int32
+    SimpleBranch {.objectconfig.} = object
+      case kind: MyEnum
+      of MyEnum.ValueA:
+        a: int16
+      of MyEnum.ValueB:
+        b: int32
+      of MyEnum.ValueC:
+        c: int64
 
-  PaddingAfterBranch {.objectconfig.} = object
-    case kind: MyEnum
-    of MyEnum.ValueA:
-      a: int8
-    of MyEnum.ValueB:
-      b: int16
-    of MyEnum.ValueC:
-      c: int32
-    cause: int64
+    PaddingBeforeBranchA {.objectconfig.} = object
+      cause: int8
+      case kind: MyEnum
+      of MyEnum.ValueA:
+        a: int16
+      of MyEnum.ValueB:
+        b: int32
+      of MyEnum.ValueC:
+        c: int64
 
-  RecursiveStuff {.objectconfig.} = object
-    case kind: MyEnum    # packedOffset:    0
-    of MyEnum.ValueA:    # packedOffset:
-      a: int16           # packedOffset:    1
-    of MyEnum.ValueB:    # packedOffset:
-      b: int32           # packedOffset:    1
-    of MyEnum.ValueC:    # packedOffset:
-      case kind2: MyEnum # packedOffset:    1
-      of MyEnum.ValueA:  # packedOffset:
-        ca1: int8
-        ca2: int32
-      of MyEnum.ValueB:  # packedOffset:
-        cb: int32        # packedOffset:    2
-      of MyEnum.ValueC:  # packedOffset:
-        cc: int64        # packedOffset:    2
-      d1: int8
-      d2: int64
+    PaddingBeforeBranchB {.objectconfig.} = object
+      cause: int8
+      case kind: MyEnum
+      of MyEnum.ValueA:
+        a: int8
+      of MyEnum.ValueB:
+        b: int16
+      of MyEnum.ValueC:
+        c: int32
 
-  Foobar {.objectconfig.} = object
-    case kind: OtherEnum
-    of OtherEnum.ValueA:
-      a: uint8
-    of OtherEnum.ValueB:
-      b: int8
-    c: int8
+    PaddingAfterBranch {.objectconfig.} = object
+      case kind: MyEnum
+      of MyEnum.ValueA:
+        a: int8
+      of MyEnum.ValueB:
+        b: int16
+      of MyEnum.ValueC:
+        c: int32
+      cause: int64
 
-  Bazing {.objectconfig.} = object of RootObj
-    a: int64
-    # TODO test on 32 bit system
-    # only there the object header is smaller than the first member
+    RecursiveStuff {.objectconfig.} = object
+      case kind: MyEnum    # packedOffset:    0
+      of MyEnum.ValueA:    # packedOffset:
+        a: int16           # packedOffset:    1
+      of MyEnum.ValueB:    # packedOffset:
+        b: int32           # packedOffset:    1
+      of MyEnum.ValueC:    # packedOffset:
+        case kind2: MyEnum # packedOffset:    1
+        of MyEnum.ValueA:  # packedOffset:
+          ca1: int8
+          ca2: int32
+        of MyEnum.ValueB:  # packedOffset:
+          cb: int32        # packedOffset:    2
+        of MyEnum.ValueC:  # packedOffset:
+          cc: int64        # packedOffset:    2
+        d1: int8
+        d2: int64
 
-  InheritanceA {.objectconfig.} = object of RootObj
-    a: char
+    Foobar {.objectconfig.} = object
+      case kind: OtherEnum
+      of OtherEnum.ValueA:
+        a: uint8
+      of OtherEnum.ValueB:
+        b: int8
+      c: int8
 
-  InheritanceB {.objectconfig.} = object of InheritanceA
-    b: char
+    Bazing {.objectconfig.} = object of RootObj
+      a: int64
+      # TODO test on 32 bit system
+      # only there the object header is smaller than the first member
 
-  InheritanceC {.objectconfig.} = object of InheritanceB
-    c: char
+    InheritanceA {.objectconfig.} = object of RootObj
+      a: char
 
-  #Float128Test = object
-  #  a: byte
-  #  b: float128
+    InheritanceB {.objectconfig.} = object of InheritanceA
+      b: char
 
-  #Bazang = object of RootObj
-  #  a: float128
+    InheritanceC {.objectconfig.} = object of InheritanceB
+      c: char
 
-const trivialSize = sizeof(TrivialType) # needs to be able to evaluate at compile time
+    #Float128Test = object
+    #  a: byte
+    #  b: float128
 
-macro testAlign(arg:untyped):untyped =
-  let prefix = newLit(arg.lineinfo & "  alignof " & arg.repr & " ")
-  result = quote do:
-    let cAlign = c_alignof(`arg`)
-    let nimAlign = alignof(`arg`)
-    if cAlign != nimAlign:
-      echo `prefix`, cAlign, " != ", nimAlign
+    #Bazang = object of RootObj
+    #  a: float128
 
-testAlign(pointer)
-testAlign(int)
-testAlign(uint)
-testAlign(int8)
-testAlign(int16)
-testAlign(int32)
-testAlign(int64)
-testAlign(uint8)
-testAlign(uint16)
-testAlign(uint32)
-testAlign(uint64)
-testAlign(float)
-testAlign(float32)
-testAlign(float64)
-testAlign(SimpleAlignment)
+  const trivialSize = sizeof(TrivialType) # needs to be able to evaluate at compile time
 
-proc main(): void =
-  var t : TrivialType
-  var a : SimpleAlignment
-  var b : AlignAtEnd
-  var c : SimpleBranch
-  var d : PaddingBeforeBranchA
-  var e : PaddingBeforeBranchB
-  var f : PaddingAfterBranch
-  var g : RecursiveStuff
-  var ro : RootObj
-  var
-    e1: Enum1
-    e2: Enum2
-    e4: Enum4
-    e8: Enum8
-  var
-    eoa: EnumObjectA
-    eob: EnumObjectB
+  testAlign(SimpleAlignment)
 
-  testSizeAlignOf(t,a,b,c,d,e,f,g,ro, e1, e2, e4, e8, eoa, eob)
+  proc main(): void =
+    var t : TrivialType
+    var a : SimpleAlignment
+    var b : AlignAtEnd
+    var c : SimpleBranch
+    var d : PaddingBeforeBranchA
+    var e : PaddingBeforeBranchB
+    var f : PaddingAfterBranch
+    var g : RecursiveStuff
+    var ro : RootObj
+    var
+      e1: Enum1
+      e2: Enum2
+      e4: Enum4
+      e8: Enum8
+    var
+      eoa: EnumObjectA
+      eob: EnumObjectB
 
-  testOffsetOf(TrivialType, x)
-  testOffsetOf(TrivialType, y)
-  testOffsetOf(TrivialType, z)
+    testSizeAlignOf(t,a,b,c,d,e,f,g,ro, e1, e2, e4, e8, eoa, eob)
 
-  testOffsetOf(SimpleAlignment, a)
-  testOffsetOf(SimpleAlignment, b)
-  testOffsetOf(SimpleAlignment, c)
-  testOffsetOf(AlignAtEnd, a)
-  testOffsetOf(AlignAtEnd, b)
-  testOffsetOf(AlignAtEnd, c)
+    testOffsetOf(TrivialType, x)
+    testOffsetOf(TrivialType, y)
+    testOffsetOf(TrivialType, z)
 
-  testOffsetOf(SimpleBranch, kindU, a)
-  testOffsetOf(SimpleBranch, kindU, b)
-  testOffsetOf(SimpleBranch, kindU, c)
+    testOffsetOf(SimpleAlignment, a)
+    testOffsetOf(SimpleAlignment, b)
+    testOffsetOf(SimpleAlignment, c)
+    testOffsetOf(AlignAtEnd, a)
+    testOffsetOf(AlignAtEnd, b)
+    testOffsetOf(AlignAtEnd, c)
 
-  testOffsetOf(PaddingBeforeBranchA, cause)
-  testOffsetOf(PaddingBeforeBranchA, kindU, a)
-  testOffsetOf(PaddingBeforeBranchB, cause)
-  testOffsetOf(PaddingBeforeBranchB, kindU, a)
+    testOffsetOf(SimpleBranch, kindU, a)
+    testOffsetOf(SimpleBranch, kindU, b)
+    testOffsetOf(SimpleBranch, kindU, c)
 
-  testOffsetOf(PaddingAfterBranch, kindU, a)
-  testOffsetOf(PaddingAfterBranch, cause)
+    testOffsetOf(PaddingBeforeBranchA, cause)
+    testOffsetOf(PaddingBeforeBranchA, kindU, a)
+    testOffsetOf(PaddingBeforeBranchB, cause)
+    testOffsetOf(PaddingBeforeBranchB, kindU, a)
 
-  testOffsetOf(Foobar, c)
+    testOffsetOf(PaddingAfterBranch, kindU, a)
+    testOffsetOf(PaddingAfterBranch, cause)
 
-  testOffsetOf(Bazing, a)
+    testOffsetOf(Foobar, c)
 
-  testOffsetOf(InheritanceA, a)
-  testOffsetOf(InheritanceB, b)
-  testOffsetOf(InheritanceC, c)
+    testOffsetOf(Bazing, a)
 
-  testOffsetOf(EnumObjectA, a)
-  testOffsetOf(EnumObjectA, b)
-  testOffsetOf(EnumObjectA, c)
-  testOffsetOf(EnumObjectA, d)
-  testOffsetOf(EnumObjectB, a)
-  testOffsetOf(EnumObjectB, b)
-  testOffsetOf(EnumObjectB, c)
-  testOffsetOf(EnumObjectB, d)
+    testOffsetOf(InheritanceA, a)
+    testOffsetOf(InheritanceB, b)
+    testOffsetOf(InheritanceC, c)
 
-  testOffsetOf(RecursiveStuff, kind)
-  testOffsetOf(RecursiveStuff, kindU.S1.a,             a)
-  testOffsetOf(RecursiveStuff, kindU.S2.b,             b)
-  testOffsetOf(RecursiveStuff, kindU.S3.kind2,         kind2)
-  testOffsetOf(RecursiveStuff, kindU.S3.kind2U.S1.ca1, ca1)
-  testOffsetOf(RecursiveStuff, kindU.S3.kind2U.S1.ca2, ca2)
-  testOffsetOf(RecursiveStuff, kindU.S3.kind2U.S2.cb,  cb)
-  testOffsetOf(RecursiveStuff, kindU.S3.kind2U.S3.cc,  cc)
-  testOffsetOf(RecursiveStuff, kindU.S3.d1,            d1)
-  testOffsetOf(RecursiveStuff, kindU.S3.d2,            d2)
+    testOffsetOf(EnumObjectA, a)
+    testOffsetOf(EnumObjectA, b)
+    testOffsetOf(EnumObjectA, c)
+    testOffsetOf(EnumObjectA, d)
+    testOffsetOf(EnumObjectB, a)
+    testOffsetOf(EnumObjectB, b)
+    testOffsetOf(EnumObjectB, c)
+    testOffsetOf(EnumObjectB, d)
 
-main()
+    testOffsetOf(RecursiveStuff, kind)
+    testOffsetOf(RecursiveStuff, kindU.S1.a,             a)
+    testOffsetOf(RecursiveStuff, kindU.S2.b,             b)
+    testOffsetOf(RecursiveStuff, kindU.S3.kind2,         kind2)
+    testOffsetOf(RecursiveStuff, kindU.S3.kind2U.S1.ca1, ca1)
+    testOffsetOf(RecursiveStuff, kindU.S3.kind2U.S1.ca2, ca2)
+    testOffsetOf(RecursiveStuff, kindU.S3.kind2U.S2.cb,  cb)
+    testOffsetOf(RecursiveStuff, kindU.S3.kind2U.S3.cc,  cc)
+    testOffsetOf(RecursiveStuff, kindU.S3.d1,            d1)
+    testOffsetOf(RecursiveStuff, kindU.S3.d2,            d2)
 
-type
-  TMyEnum = enum
-    tmOne, tmTwo, tmThree, tmFour
-
-  TMyArray1 = array[3, uint8]
-  TMyArray2 = array[1..3, int32]
-  TMyArray3 = array[TMyEnum, float64]
-
-const
-  mysize1 = sizeof(TMyArray1)
-  mysize2 = sizeof(TMyArray2)
-  mysize3 = sizeof(TMyArray3)
-
-assert mysize1 == 3
-assert mysize2 == 12
-assert mysize3 == 32
+  main()
