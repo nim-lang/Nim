547f1316f6858a4084334c6948d461c9904ed480
Timothee Cour <timothee.cour2@gmail.com>
2018-08-08 16:20:57 -0700

    save a backup of csources-built nim to bin/nim_csources to avoid recompiling from csources

--- build_all.sh
+++ build_all.sh
@@ -11,11 +11,20 @@ echo_run(){
 }
 
 [ -d csources ] || echo_run git clone --depth 1 https://github.com/nim-lang/csources.git
-(
+
+nim_csources=bin/nim_csources
+build_nim_csources(){
   ## avoid changing dir in case of failure
-  echo_run cd csources
-  echo_run sh build.sh
-)
+  (
+    echo_run cd csources
+    echo_run sh build.sh
+  )
+  # keep $nim_csources in case needed to investigate bootstrap issues
+  # without having to rebuild from csources
+  echo_run cp bin/nim $nim_csources
+}
+
+[ -f $nim_csources ] || echo_run build_nim_csources
 
 echo_run bin/nim c koch
 echo_run ./koch boot -d:release
