4a42fe1cb4abd7c0e21d6b0c833217ba18a87280
Matt Haggard <haggardii@gmail.com>
2018-08-08 09:41:19 -0600

    Add failing test for stderr redirection on macOS

---
+++ tests/osproc/tstderr.nim
@@ -0,0 +1,26 @@
+discard """
+  output: '''--------------------------------------
+to stderr
+to stderr
+--------------------------------------
+'''
+"""
+import osproc, os, streams
+
+const filename = when defined(Windows): "ta_out.exe" else: "ta_out"
+
+doAssert fileExists(getCurrentDir() / "tests" / "osproc" / filename)
+
+var p = startProcess(filename, getCurrentDir() / "tests" / "osproc",
+                     options={})
+
+let stdoutStream = p.outputStream
+let stderrStream = p.errorStream
+var x = newStringOfCap(120)
+var output = ""
+while stderrStream.readLine(x.TaintedString):
+  output.add(x & "\n")
+
+echo "--------------------------------------"
+stderr.write output
+echo "--------------------------------------"
