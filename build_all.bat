eadc298164d919daea26e18847af7ad4068591f3
Timothee Cour <timothee.cour2@gmail.com>
2018-08-10 20:44:51 -0700

    address comments

--- build_all.bat
+++ build_all.bat
@@ -7,38 +7,60 @@ rem note: this won't work if it's in a networked drive
 rem eg if using parallels on OSX, don't use a parallels shared folder, use C:\
 
 rem better to be verbose, for debugging purposes
+
 @echo on
+
+rem  lifetime of the environment will end with the termination of the batch
+setlocal
 
 echo "checks that gcc in PATH:"
 gcc -v
+IF ERRORLEVEL 1 (GOTO:END)
 
-IF EXIST csources\nul (
+IF EXIST csources\ (
   echo "csources already exists"
 ) ELSE (
   git clone --depth 1 https://github.com/nim-lang/csources.git
+  IF ERRORLEVEL 1 (GOTO:END)
 )
 
-rem nesting this inside an IF branch triggered a weird parsing error
-rem https://stackoverflow.com/questions/601089/detect-whether-current-windows-version-is-32-bit-or-64-bit
-if defined ProgramFiles(x86) (
-  echo detected 64 bit
+rem https://stackoverflow.com/questions/12322308/batch-file-to-check-64bit-or-32bit-os
+
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+IF ERRORLEVEL 1 (GOTO:END)
+
+echo %OS%
+if %OS%==64BIT (
   set buildfilename=.\build64.bat
 ) ELSE (
-  echo detected 32 bit
   set buildfilename=.\build.bat
+  echo "OS not recognized"
 )
 
 IF EXIST .\bin\nim.exe (
   echo skipping csources\build
 ) ELSE (
   cd csources
+  IF ERRORLEVEL 1 (GOTO:END)
   rem %buildfilename%
   %buildfilename%
+  IF ERRORLEVEL 1 (GOTO:END)
   cd ..
 )
 
 bin\nim c koch
+IF ERRORLEVEL 1 (GOTO:END)
 koch boot -d:release
+IF ERRORLEVEL 1 (GOTO:END)
 
 rem Compile Nimble and other tools
 koch tools
+IF ERRORLEVEL 1 (GOTO:END)
+
+:END
+IF ERRORLEVEL 1 (
+    ECHO FAILURE
+) ELSE (
+    ECHO SUCCESS
+)
+exit /b %ERRORLEVEL%
