aeca23dbc23c1aeb223460906ae2f31bf2c23649
Timothee Cour <timothee.cour2@gmail.com>
2018-08-14 14:05:43 -0700

    rename stdlib.nimble to std.nimble for consistency

--- tests/modules/import_styles.nim
+++ tests/modules/import_styles.nim
@@ -8,9 +8,6 @@ import std/strutils
 
 import std/sha1
 import std/sha1 as sha1_2
-# TODO: Error: cannot open file: stdlib/std/sha1; inconsistent with the fact we
-# generate `stdlib.std.sha1`
-# import stdlib/std/sha1 as sha1_3
 
 import ./mnotuniquename
 import ./tnotuniquename/mnotuniquename as mnotuniquename1
@@ -19,7 +16,7 @@ import pure/ospaths as ospaths1
 import std/ospaths as ospaths2
 import ospaths as ospaths3
 
-# Error: cannot open file: std/pure/ospaths
+# TODO: this is inconsistent: Error: cannot open file: std/pure/ospaths
 # import std/pure/ospaths as ospaths4
 
 echo "ok"
