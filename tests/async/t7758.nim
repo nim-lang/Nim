000b3ba14ef7049dede938e55657c0788e496f67
Dominik Picheta <dominikpicheta@gmail.com>
2018-08-22 17:11:00 +0100

    Don't skip poll() when no handles are present.
    Fixes #7886.
    
    Fixes #7758.
    
    Fixes #6929.
    
    Fixes #3909.
    
    Replaces #8209.

---
+++ tests/async/t7758.nim
@@ -0,0 +1,17 @@
+discard """
+  file: "t7758.nim"
+  exitcode: 0
+"""
+import asyncdispatch
+
+proc task() {.async.} =
+  await sleepAsync(1000)
+
+when isMainModule:
+  var counter = 0
+  var f = task()
+  while not f.finished:
+    inc(counter)
+    poll()
+
+doAssert counter == 2
