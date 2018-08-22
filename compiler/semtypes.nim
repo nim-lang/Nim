d94ee75d1cc4ce7caad014944f943cdfbae49715
LemonBoy <LemonBoy@users.noreply.github.com>
2018-08-22 15:49:43 +0200

    Pick skParam symbols when resolving type idents (#8720)
    Fixes #8357

--- compiler/semtypes.nim
+++ compiler/semtypes.nim
@@ -369,7 +369,7 @@ proc semTypeIdent(c: PContext, n: PNode): PSym =
   if n.kind == nkSym:
     result = getGenSym(c, n.sym)
   else:
-    result = pickSym(c, n, {skType, skGenericParam})
+    result = pickSym(c, n, {skType, skGenericParam, skParam})
     if result.isNil:
       result = qualifiedLookUp(c, n, {checkAmbiguity, checkUndeclared})
     if result != nil:
