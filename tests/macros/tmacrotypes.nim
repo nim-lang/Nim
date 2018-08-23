a7f7bdb08e858c57c48ce82fdbb907c6e80e7f88
cooldome <ariabushenko@bk.ru>
2018-08-23 20:29:07 +0100

    fixes 8754

--- tests/macros/tmacrotypes.nim
+++ tests/macros/tmacrotypes.nim
@@ -1,16 +1,19 @@
 discard """
-  nimout: '''void
-int'''
+  nimout: '''void; ntyVoid; void; void
+int; ntyInt; int; int
+proc (); ntyProc; proc[void]; proc ()
+proc (a: int; b: float): int; ntyProc; proc[int, int, float]; proc (a: int; b: float): int  
 """
 
 import macros
 
 macro checkType(ex: typed; expected: string): untyped =
-  var t = ex.getType()
-  echo t
+  echo ex.getTypeInst.repr, "; ", ex.typeKind, "; ", ex.getType.repr, "; ", ex.getTypeImpl.repr
 
 proc voidProc = echo "hello"
 proc intProc(a: int, b: float): int = 10
 
 checkType(voidProc(), "void")
 checkType(intProc(10, 20.0), "int")
+checkType(voidProc, "procTy")
+checkType(intProc, "procTy")
