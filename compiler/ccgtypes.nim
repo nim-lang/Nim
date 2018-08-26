f315dff41b6a8d925112521473718586596cb3f3
Vindaar <basti90@gmail.com>
2018-08-26 20:13:03 +0200

    fixes #8781 by appending "_U" instead of 'U'

--- compiler/ccgtypes.nim
+++ compiler/ccgtypes.nim
@@ -449,7 +449,7 @@ proc genRecordFieldsAux(m: BModule, n: PNode,
   of nkRecCase:
     if n.sons[0].kind != nkSym: internalError(m.config, n.info, "genRecordFieldsAux")
     add(result, genRecordFieldsAux(m, n.sons[0], accessExpr, rectype, check))
-    let uname = rope(mangle(n.sons[0].sym.name.s) & 'U')
+    let uname = rope(mangle(n.sons[0].sym.name.s) & "_U")
     let ae = if accessExpr != nil: "$1.$2" % [accessExpr, uname]
              else: uname
     var unionBody: Rope = nil
