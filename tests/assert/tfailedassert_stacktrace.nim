f4356d611585a49de7bce893378c1a997b868005
Ganesh Viswanathan <dev@genotrance.com>
2018-09-12 12:58:50 -0500

    Fix system.nim line number test case failure

--- tests/assert/tfailedassert_stacktrace.nim
+++ tests/assert/tfailedassert_stacktrace.nim
@@ -2,9 +2,9 @@ discard """
   output: '''
 tfailedassert_stacktrace.nim(16) tfailedassert_stacktrace
 tfailedassert_stacktrace.nim(15) foo
-system.nim(3777)         failedAssertImpl
-system.nim(3770)         raiseAssert
-system.nim(2817)         sysFatal
+system.nim(3778)         failedAssertImpl
+system.nim(3771)         raiseAssert
+system.nim(2818)         sysFatal
 '''
 """
 
