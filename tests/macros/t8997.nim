cb3d80450d8a6a1d2ec9c101164454cca6a4253c
LemonBoy <thatlemon@gmail.com>
2018-09-18 10:06:40 +0200

    Reject assignments with nkEmpty RHS
    Fixes #8997

---
+++ tests/macros/t8997.nim
@@ -0,0 +1,26 @@
+discard """
+  line: 24
+  errormsg: "illformed AST: "
+"""
+
+import macros
+
+type
+  Node* = ref object
+    children: seq[Node]
+
+proc newNode*(): Node =
+  Node(children: newSeq[Node]())
+
+macro build*(body: untyped): untyped =
+
+  template appendElement(tmp, childrenBlock) {.dirty.} =
+    bind newNode
+    let tmp = newNode()
+    tmp.children = childrenBlock  # this line seems to be the problem
+
+  let tmp = genSym(nskLet, "tmp")
+  let childrenBlock = newEmptyNode()
+  result = getAst(appendElement(tmp, childrenBlock))
+
+build(body)
