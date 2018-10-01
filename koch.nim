136e7d232e29828dffec6f3f5a5839a2ab3d9145
Arne Döring <arne.doering@gmx.net>
2018-10-01 19:16:42 +0200

    revert some tests to c only backend

--- koch.nim
+++ koch.nim
@@ -271,8 +271,8 @@ proc thVersion(i: int): string =
 proc boot(args: string) =
   var output = "compiler" / "nim".exe
   var finalDest = "bin" / "nim".exe
-  # default to use the 'c' command:
-  let bootOptions = if args.len == 0 or args.startsWith("-"): "c" else: ""
+  # default to use the 'cpp' command:
+  let bootOptions = if args.len == 0 or args.startsWith("-"): "cpp" else: ""
   let smartNimcache = (if "release" in args: "nimcache/r_" else: "nimcache/d_") &
                       hostOs & "_" & hostCpu
 
