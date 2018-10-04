64ae47e6f0fa065947fc31dbf6328d7be1456f2d
Timothee Cour <timothee.cour2@gmail.com>
2018-10-03 20:33:57 -0700

    Update macros.nim (#9182)
    * Update macros.nim
    
    * Update macros.nim

--- lib/core/macros.nim
+++ lib/core/macros.nim
@@ -1432,5 +1432,7 @@ macro unpackVarargs*(callee: untyped; args: varargs[untyped]): untyped =
     result.add args[i]
 
 proc getProjectPath*(): string = discard
-  ## Returns the path to the currently compiling project
+  ## Returns the path to the currently compiling project, not to
+  ## be confused with ``system.currentSourcePath`` which returns
+  ## the path of the current module.
 
