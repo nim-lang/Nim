efeb4f82a20eb1874f40bbc1af23f2cc87ad33d1
Timothee Cour <timothee.cour2@gmail.com>
2018-10-09 21:21:44 -0700

    fix tests

--- tests/errmsgs/undeclared_routime2.nim
+++ tests/errmsgs/undeclared_routime2.nim
@@ -2,8 +2,8 @@ discard """
 cmd: '''nim c --hints:off $file'''
 errormsg: "attempting to call routine: 'myPragma'"
 nimout: '''undeclared_routime2.nim(12, 26) Error: attempting to call routine: 'myPragma'
-  * found 'undeclared_routime2.myPragma()[declared in undeclared_routime2.nim(10, 5)]' of kind 'proc'
-  * found 'undeclared_routime2.myPragma()[declared in undeclared_routime2.nim(11, 9)]' of kind 'iterator'
+  found 'undeclared_routime2.myPragma()[declared in undeclared_routime2.nim(10, 5)]' of kind 'proc'
+  found 'undeclared_routime2.myPragma()[declared in undeclared_routime2.nim(11, 9)]' of kind 'iterator'
 '''
 """
 
