5cb6da0deba40e0e88d8dd782a1c9a994618bec8
Vindaar <basti90@gmail.com>
2018-10-13 00:19:20 +0200

    remove reference to string, sequence in `isNil` doc string

--- lib/core/typeinfo.nim
+++ lib/core/typeinfo.nim
@@ -246,8 +246,8 @@ proc base*(x: Any): Any =
 
 
 proc isNil*(x: Any): bool =
-  ## `isNil` for an any `x` that represents a sequence, string, cstring,
-  ## proc or some pointer type.
+  ## `isNil` for an any `x` that represents a cstring, proc or
+  ## some pointer type.
   assert x.rawType.kind in {tyCString, tyRef, tyPtr, tyPointer, tyProc}
   result = isNil(cast[ppointer](x.value)[])
 
