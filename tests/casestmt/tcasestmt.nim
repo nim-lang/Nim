4efbc250adb50e495b87f53ac6941865ae4d46e2
narimiran <narimiran@disroot.org>
2018-10-12 13:21:31 +0200

    split `tcomputedgoto`

--- tests/casestmt/tcasestmt.nim
+++ tests/casestmt/tcasestmt.nim
@@ -2,7 +2,6 @@ discard """
   file: "tcasestmt.nim"
   output:
 '''
-Not found!
 Found!
 1
 compiles for 1
@@ -12,18 +11,6 @@ set is 4 not 5
 array is 6 not 7
 default for 8
 an identifier
-yeah A enumB
-yeah A enumB
-yeah CD enumD
-yeah CD enumE
-yeah A enumB
-yeah CD enumE
-yeah CD enumD
-yeah A enumB
-yeah B enumC
-yeah A enumB
-yeah A enumB
-yeah A enumB
 OK
 OK
 OK
@@ -80,50 +67,15 @@ block emptyset_when:
 block setconstr:
   const
     SymChars: set[char] = {'a'..'z', 'A'..'Z', '\x80'..'\xFF'}
-  
+
   proc classify(s: string) =
     case s[0]
     of SymChars, '_': echo "an identifier"
     of {'0'..'9'}: echo "a number"
     else: echo "other"
-  
+
   classify("Hurra")
 
-
-block computedgoto:
-  type
-    MyEnum = enum
-      enumA, enumB, enumC, enumD, enumE, enumLast
-  
-  proc vm() =
-    var instructions: array[0..100, MyEnum]
-    instructions[2] = enumC
-    instructions[3] = enumD
-    instructions[4] = enumA
-    instructions[5] = enumD
-    instructions[6] = enumC
-    instructions[7] = enumA
-    instructions[8] = enumB
-  
-    instructions[12] = enumE
-    var pc = 0
-    while true:
-      {.computedGoto.}
-      let instr = instructions[pc]
-      let ra = instr.succ # instr.regA
-      case instr
-      of enumA:
-        echo "yeah A ", ra
-      of enumC, enumD:
-        echo "yeah CD ", ra
-      of enumB:
-        echo "yeah B ", ra
-      of enumE:
-        break
-      of enumLast: discard
-      inc(pc)
-  
-  vm()
 
 
 block tduplicates:
@@ -174,22 +126,22 @@ block tduplicates:
 block tcasestm:
   type
     Tenum = enum eA, eB, eC
-  
+
   var
     x: string = "yyy"
     y: Tenum = eA
     i: int
-  
+
   case y
   of eA: write(stdout, "a")
   of eB, eC: write(stdout, "b or c")
-  
+
   case x
   of "Andreas", "Rumpf": write(stdout, "Hallo Meister!")
   of "aa", "bb": write(stdout, "Du bist nicht mein Meister")
   of "cc", "hash", "when": discard
   of "will", "it", "finally", "be", "generated": discard
-  
+
   var z = case i
     of 1..5, 8, 9: "aa"
     of 6, 7: "bb"
@@ -200,10 +152,10 @@ block tcasestm:
       "dd"
     else:
       "zz"
-  
+
   echo z
   #OUT ayyy
-  
+
   let str1 = "Y"
   let str2 = "NN"
   let a = case str1:
@@ -212,25 +164,25 @@ block tcasestm:
     else:
       echo "no good"
       quit("quiting")
-  
+
   proc toBool(s: string): bool =
     case s:
     of "": raise newException(ValueError, "Invalid boolean")
     elif s[0] == 'Y': true
     elif s[0] == 'N': false
     else: "error".quit(2)
-  
-  
+
+
   let b = "NN".toBool()
-  
+
   doAssert(a == true)
   doAssert(b == false)
-  
+
   static:
     #bug #7407
     let bstatic = "N".toBool()
     doAssert(bstatic == false)
-  
+
   var bb: bool
   doassert(not compiles(
     bb = case str2:
@@ -238,26 +190,26 @@ block tcasestm:
       elif str.startsWith("Y"): true
       elif str.startsWith("N"): false
   ))
-  
+
   doassert(not compiles(
     bb = case str2:
       of "Y": true
       of "N": false
   ))
-  
+
   doassert(not compiles(
     bb = case str2:
       of "Y": true
       of "N": raise newException(ValueError, "N not allowed")
   ))
-  
+
   doassert(not compiles(
     bb = case str2:
       of "Y": raise newException(ValueError, "Invalid Y")
       else: raise newException(ValueError, "Invalid N")
   ))
-  
-  
+
+
   doassert(not compiles(
     bb = case str2:
       of "Y":
@@ -265,8 +217,8 @@ block tcasestm:
         true
       else: raise newException(ValueError, "Invalid")
   ))
-  
-  
+
+
   doassert(not compiles(
     bb = case str2:
       of "Y":
