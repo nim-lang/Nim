63e3f856a50b19221df00d7a9b7d3e5717af39f2
narimiran <narimiran@disroot.org>
2018-10-13 09:42:46 +0200

    merge tcan tests

---
+++ tests/generics/tcan.nim
@@ -0,0 +1,35 @@
+discard """
+  output: '''
+'''
+"""
+
+# Created by Eric Doughty-Papassideris on 2011-02-16.
+
+block talias_generic:
+  type
+    TGen[T] = object
+    TGen2[T] = TGen[T]
+
+
+block talias_specialised:
+  type
+    TGen[T] = object
+    TSpef = TGen[string]
+  var s: TSpef
+
+
+block tinherit:
+  type
+    TGen[T] = object of RootObj
+      x, y: T
+    TSpef[T] = object of TGen[T]
+
+  var s: TSpef[float]
+  s.x = 0.4
+  s.y = 0.6
+
+
+block tspecialise:
+  type
+    TGen[T] = object {.inheritable.}
+    TSpef = object of TGen[string]
