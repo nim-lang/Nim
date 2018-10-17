9041ef95809e61e1fdaf7eb380c1d5c6604f3b60
recloser <44084068+recloser@users.noreply.github.com>
2018-10-17 14:07:50 +0200

    Add a test for issue #9410

---
+++ tests/js/t9410.nim
@@ -0,0 +1,35 @@
+discard """
+  output: '''654 654
+256 256 256
+543 543 543 543
+'''
+"""
+
+block:
+    var someint = 500
+
+    let p: ptr int = someint.addr
+    let tup = (f: p)
+    p[] = 654
+    echo p[], " ", tup.f[]
+
+block:
+    var someint = 500
+
+    let p: ptr int = someint.addr
+    let arr = [p]
+    p[] = 256
+    echo someint, " ", p[], " ", arr[0][]
+
+block:
+    var someref: ref int
+    new(someref)
+    var someref2 = someref
+
+    let tup1 = (f: someref)
+    var tup2 = (f: someref)
+    tup2.f = someref
+
+    someref[] = 543
+
+    echo someref[], " ", tup1.f[], " ", tup2.f[], " ", someref2[]
