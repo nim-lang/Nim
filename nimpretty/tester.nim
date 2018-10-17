64f5c966851bbff3f7bf822d2143233797a98ae6
Araq <rumpf_a@web.de>
2018-10-17 12:52:25 +0200

    nimpretty: fixes #9384
    (cherry picked from commit bd9d5787f06a1f3e259055268824a852cd160798)

--- nimpretty/tester.nim
+++ nimpretty/tester.nim
@@ -9,7 +9,7 @@ var
   failures = 0
 
 when defined(develop):
-  const nimp = "bin/nimpretty".addFileExt(ExeExt)
+  const nimp = "bin" / "nimpretty".addFileExt(ExeExt)
   if execShellCmd("nim c -o:$# nimpretty/nimpretty.nim" % [nimp]) != 0:
     quit("FAILURE: compilation of nimpretty failed")
 else:
