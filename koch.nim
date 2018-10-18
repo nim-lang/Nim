d2ca8081060c0de9ab56d38f17a1a315661bdff8
Araq <rumpf_a@web.de>
2018-10-18 10:58:28 +0200

    koch: bootstrap with C++ for NIM_COMPILE_TO_CPP env var

--- koch.nim
+++ koch.nim
@@ -272,7 +272,8 @@ proc boot(args: string) =
   var output = "compiler" / "nim".exe
   var finalDest = "bin" / "nim".exe
   # default to use the 'c' command:
-  let bootOptions = if args.len == 0 or args.startsWith("-"): "c" else: ""
+  let defaultCommand = if getEnv("NIM_COMPILE_TO_CPP", "false") == "true": "cpp" else: "c"
+  let bootOptions = if args.len == 0 or args.startsWith("-"): defaultCommand else: ""
   let smartNimcache = (if "release" in args: "nimcache/r_" else: "nimcache/d_") &
                       hostOs & "_" & hostCpu
 
