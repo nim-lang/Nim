2440b2fdb65c8b70542e057001602df640055ea6
narimiran <narimiran@disroot.org>
2018-10-19 10:44:45 +0200

    merge stdlib tests

--- tests/stdlib/t9394.nim
+++
@@ -1,7 +0,0 @@
-import terminal, colors
-
-let codeFg = ansiForegroundColorCode(colAliceBlue)
-let codeBg = ansiBackgroundColorCode(colAliceBlue)
-
-doAssert codeFg == "\27[38;2;240;248;255m"
-doAssert codeBg == "\27[48;2;240;248;255m"
