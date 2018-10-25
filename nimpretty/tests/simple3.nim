03005fde99f800cd900e7740f6fd44d4ee54a1a1
Andreas Rumpf <rumpf_a@web.de>
2018-10-26 01:53:10 +0200

    nimpretty: fixes #9506

--- nimpretty/tests/simple3.nim
+++ nimpretty/tests/simple3.nim
@@ -7,3 +7,8 @@ foo bar
 
 proc fun() =
   echo "ok1"
+
+proc fun2(a = "fooo" & "bar" & "bar" & "bar" & "bar" & ("bar" & "bar" & "bar") & "bar" & "bar" & "bar" & "bar" & "bar" & "bar" & "bar" & "bar" & "bar"): auto =
+  discard
+
+fun2()
