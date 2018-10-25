bab9e0020a49ab9e5aa00d4d8761e3465075241d
Arne Döring <arne.doering@gmx.net>
2018-10-25 13:42:04 +0200

    fixes #5549

--- compiler/ccgstmts.nim
+++ compiler/ccgstmts.nim
@@ -509,7 +509,7 @@ proc genWhileStmt(p: BProc, t: PNode) =
          # for closure support weird loop bodies are generated:
       if loopBody.len == 2 and loopBody.sons[0].kind == nkEmpty:
         loopBody = loopBody.sons[1]
-      genComputedGoto(p, loopBody) # TODO foobar
+      genComputedGoto(p, loopBody)
     else:
       p.breakIdx = startBlock(p, "while (1) {$n")
       p.blocks[p.breakIdx].isLoop = true
@@ -874,15 +874,15 @@ proc genTryCpp(p: BProc, t: PNode, d: var TLoc) =
 
   discard pop(p.nestedTryStmts)
 
-  if not catchAllPresent and t[^1].kind == nkFinally:
-    # finally requires catch all presence
-    startBlock(p, "catch (...) {$n")
-    genSimpleBlock(p, t[^1][0])
-    line(p, cpsStmts, ~"throw;$n")
-    endBlock(p)
-
   if t[^1].kind == nkFinally:
-    genSimpleBlock(p, t[^1][0])
+    if catchAllPresent:
+      genSimpleBlock(p, t[^1][0])
+    else:
+      # finally requires catch all presence
+      startBlock(p, "catch (...) {$n")
+      genSimpleBlock(p, t[^1][0])
+      line(p, cpsStmts, ~"throw;$n")
+      endBlock(p)
 
 proc genTry(p: BProc, t: PNode, d: var TLoc) =
   # code to generate:
