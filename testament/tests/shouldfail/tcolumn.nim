fb1f5c62b5e74b1a532ddfe9d6c373fc61eff878
Arne Döring <arne.doering@gmx.net>
2018-11-30 14:28:18 +0100

    tshouldfail is stricter

--- testament/tests/shouldfail/tcolumn.nim
+++ testament/tests/shouldfail/tcolumn.nim
@@ -1,7 +1,7 @@
 discard """
+errormsg: "undeclared identifier: 'undeclared'"
 line: 8
 column: 7
-errormsg: "undeclared identifier: 'undeclared'"
 """
 
 # test should fail because the line directive is wrong
--- tests/testament/tshouldfail.nim
+++ tests/testament/tshouldfail.nim
@@ -3,15 +3,29 @@ cmd: "testament/tester --directory:testament --colors:off --backendLogging:off -
 action: compile
 nimout: '''
 FAIL: tccodecheck.nim C
+Failure: reCodegenFailure
+Expected:
+baz
 FAIL: tcolumn.nim C
+Failure: reLinesDiffer
 FAIL: terrormsg.nim C
+Failure: reMsgsDiffer
 FAIL: texitcode1.nim C
+Failure: reExitcodesDiffer
 FAIL: tfile.nim C
+Failure: reFilesDiffer
 FAIL: tline.nim C
+Failure: reLinesDiffer
 FAIL: tmaxcodesize.nim C
+Failure: reCodegenFailure
+max allowed size: 1
 FAIL: tnimout.nim C
+Failure: reMsgsDiffer
 FAIL: toutput.nim C
+Failure: reOutputsDiffer
 FAIL: toutputsub.nim C
+Failure: reOutputsDiffer
 FAIL: tsortoutput.nim C
+Failure: reOutputsDiffer
 '''
 """
