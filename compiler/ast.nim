0cf6ae93a38601e890ac5963ddc87546935aef38
Neelesh Chandola <neelesh.chandola@outlook.com>
2018-12-14 10:06:40 +0530

    Fix Formatting

--- compiler/ast.nim
+++ compiler/ast.nim
@@ -222,8 +222,8 @@ type
     nkState,              # give a label to a code section (for iterators)
     nkBreakState,         # special break statement for easier code generation
     nkFuncDef,            # a func
-    nkTupleConstr         # a tuple constructor
-    nkConstTuple,         # a ``const (a, b) = expr`` construct
+    nkTupleConstr,        # a tuple constructor
+    nkConstTuple          # a ``const (a, b) = expr`` construct
 
   TNodeKinds* = set[TNodeKind]
 
