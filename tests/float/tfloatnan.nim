6d486c1697f9c9205447fcefa353a068c4762947
Timothee Cour <timothee.cour2@gmail.com>
2019-01-14 23:02:50 -0800

    fix #10305 nim cpp is now nan-correct at CT

--- tests/float/tfloatnan.nim
+++ tests/float/tfloatnan.nim
@@ -14,3 +14,19 @@ echo "Nim: ", f32, " (float)"
 
 let f64: float64 = NaN
 echo "Nim: ", f64, " (double)"
+
+
+proc fun() =
+  # issue #10305
+  # with `-O3 -ffast-math`, generated C/C++ code is not nan compliant
+  # user can pass `--passC:-ffast-math` if he doesn't care.
+  let a1 = 0.0
+  let a = 0.0/a1
+  let b1 = a == 0.0
+  let b2 = a == a
+  doAssert not b1
+  doAssert not b2
+
+static: fun()
+fun()
+
