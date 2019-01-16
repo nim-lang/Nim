3f77a41c6ad2ddd1722e164d528fec04ddfe85f7
Timothee Cour <timothee.cour2@gmail.com>
2019-01-15 21:33:52 -0800

    better fix for `nim cpp` bootstrap error: error: no member named raise_id

--- koch.nim
+++ koch.nim
@@ -282,18 +282,18 @@ proc boot(args: string) =
                       hostOs & "_" & hostCpu
 
   copyExe(findStartNim(), 0.thVersion)
-  for i in 0..2+ord(useCpp):
-    # do the first iteration in C mode in order to avoid problem #10315:
-    let defaultCommand = if useCpp and i > 0: "cpp" else: "c"
+  for i in 0..2:
+    let defaultCommand = if useCpp: "cpp" else: "c"
     let bootOptions = if args.len == 0 or args.startsWith("-"): defaultCommand else: ""
-
     echo "iteration: ", i+1
-    let extraOption = if i == 0:
-      "--skipUserCfg --skipParentCfg"
+    var extraOption = ""
+    if i == 0:
+      extraOption.add " --skipUserCfg --skipParentCfg"
         # Note(D20190115T162028:here): the configs are skipped for bootstrap
         # (1st iteration) to prevent newer flags from breaking bootstrap phase.
         # fixes #10030.
-    else: ""
+      extraOption.add " -d:nimBoostrapCsources0_19_0"
+        # remove this when csources get updated
     exec i.thVersion & " $# $# $# --nimcache:$# compiler" / "nim.nim" %
       [bootOptions, extraOption, args, smartNimcache]
     if sameFileContent(output, i.thVersion):
