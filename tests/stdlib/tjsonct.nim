c2cf2549c4a4ae1b0c13af11fc82ec8798f5d1c4
Vindaar <basti90@gmail.com>
2019-02-11 17:09:44 +0100

    extend test case

--- tests/stdlib/tjsonct.nim
+++ tests/stdlib/tjsonct.nim
@@ -2,8 +2,11 @@ import json
 
 const jj = """
 {
- "name" : "Peter",
- "age" : 35
+  "name" : "Peter",
+  "age" : 35,
+  "data" : [1, 2, 3, 4],
+  "nested" : { "low" : 0,
+               "high" : 1 }
 }
 """
 
@@ -14,3 +17,7 @@ static:
   echo jData
   doAssert jData["name"].getStr == "Peter"
   doAssert jData["age"].getInt == 35
+  doAssert jData["data"] == % [1, 2, 3, 4]
+  doAssert jData["nested"].kind == JObject
+  doAssert jData["nested"]["low"].getInt == 0
+  doAssert jData["nested"]["high"].getInt == 1
