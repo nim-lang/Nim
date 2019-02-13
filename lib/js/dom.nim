99e04294d8867b252b58cfea2cbd508ba1acc90c
root <root@OFFICE.localdomain>
2019-02-14 06:02:16 +0800

    adjust whitespace to conform to NEP-1

--- lib/js/dom.nim
+++ lib/js/dom.nim
@@ -1183,9 +1183,9 @@ proc contains*(c: ClassList, class: cstring): bool
 proc toggle*(c: ClassList, class: cstring)
 
 # Style "methods"
-proc getPropertyValue*(s: Style, property : cstring): cstring
-proc removeProperty*(s: Style, property : cstring)
-proc setProperty*(s: Style, property , value: cstring, priority="") #The following values are accepted:"important",keyword undefined, ""
+proc getPropertyValue*(s: Style, property: cstring): cstring
+proc removeProperty*(s: Style, property: cstring)
+proc setProperty*(s: Style, property, value: cstring, priority = "") #The following values are accepted:"important",keyword undefined, ""
 proc getPropertyPriority*(s: Style, property: cstring): cstring #"important" or ""
 
 # Event "methods"
