2fae94cdc597542478aa308d0ec0eb74bf61021c
Miran <narimiran@disroot.org>
2019-02-19 11:43:34 +0100

    add more nimble packages to the test (#10707)

--- testament/important_packages.nim
+++ testament/important_packages.nim
@@ -8,7 +8,11 @@ var packages*: seq[tuple[name, cmd, version: string; hasDeps: bool]] = @[]
 
 pkg "arraymancer", "nim c src/arraymancer.nim", "", true
 pkg "ast_pattern_matching", "nim c tests/test1.nim"
+pkg "blscurve", "", "", true
+pkg "bncurve", "nim c tests/tarith.nim", "", true
 pkg "c2nim"
+pkg "chronicles", "nim c -o:chr chronicles.nim"
+pkg "chronos", "nim c tests/teststream.nim"
 pkg "cligen", "nim c -o:cligenn cligen.nim"
 pkg "compactdict", "nim c tests/test1.nim"
 pkg "criterion"
@@ -25,20 +29,23 @@ pkg "loopfusion"
 pkg "nake", "nim c nakefile.nim"
 pkg "neo", "nim c -d:blas=openblas tests/all.nim", "", true
 pkg "nigui", "nim c -o:niguii src/nigui.nim"
+pkg "nimcrypto", "nim c tests/testapi.nim"
 pkg "NimData", "nim c -o:nimdataa src/nimdata.nim", "", true
 pkg "nimes", "nim c src/nimes.nim", "", true
 pkg "nimgame2", "nim c nimgame2/nimgame.nim", "", true
 pkg "nimongo", "nimble test_ci", "", true
 pkg "nimpy", "nim c -o:nimpyy nimpy.nim"
 pkg "nimsl", "nim c test.nim"
+pkg "nimsvg", "nim c src/nimsvg.nim"
 pkg "nimx", "nim c --threads:on test/main.nim", "", true
 pkg "parsetoml"
 pkg "patty"
 pkg "plotly", "nim c examples/all.nim", "", true
 pkg "protobuf", "nim c -o:protobuff src/protobuf.nim", "", true
 pkg "regex", "nim c src/regex"
-pkg "rosencrantz", "nim c -o:rsncntz rosencrantz.nim"
+pkg "rosencrantz"
 pkg "sdl1", "nim c src/sdl.nim"
 pkg "sdl2_nim", "nim c sdl2/sdl.nim"
 pkg "stint", "nim c -o:stintt stint.nim"
 pkg "zero_functional", "nim c test.nim"
+
