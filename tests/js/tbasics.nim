2d1dea9484a3e0080336d2aec806c2f42480b1fc
narimiran <narimiran@disroot.org>
2019-02-21 08:56:43 +0100

    keep only the relevant part of `tbasics` test

--- tests/js/tbasics.nim
+++ tests/js/tbasics.nim
@@ -1,41 +1,6 @@
 discard """
-  output: '''ABCDC
-1
-14
-ok
-1'''
+  output: '''1'''
 """
-
-type
-  MyEnum = enum
-    A,B,C,D
-# trick the optimizer with an seq:
-var x = @[A,B,C,D]
-echo x[0],x[1],x[2],x[3],MyEnum(2)
-
-# bug #10651
-
-var xa: seq[int]
-var ya = @[1,2]
-xa &= ya
-echo xa[0]
-
-proc test =
-  var yup: seq[int]
-  try:
-    yup.add 14
-    echo yup.pop
-  finally:
-    discard
-
-test()
-
-when true:
-  var a: seq[int]
-
-  a.setLen(0)
-
-  echo "ok"
 
 # bug #10697
 proc test2 =
