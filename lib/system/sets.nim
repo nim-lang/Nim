a1a642ad77c78c7a631109b00671ba4be85794a0
Arne Döring <arne.doering@gmx.net>
2019-03-05 16:47:18 +0100

    use unsigned in system/sets.nim

--- lib/system/sets.nim
+++ lib/system/sets.nim
@@ -12,17 +12,17 @@
 type
   NimSet = array[0..4*2048-1, uint8]
 
-proc countBits32(n: int32): int {.compilerproc.} =
+proc countBits32(n: uint32): int {.compilerproc.} =
   var v = n
-  v = v -% ((v shr 1'i32) and 0x55555555'i32)
-  v = (v and 0x33333333'i32) +% ((v shr 2'i32) and 0x33333333'i32)
-  result = ((v +% (v shr 4'i32) and 0xF0F0F0F'i32) *% 0x1010101'i32) shr 24'i32
+  v = v - ((v shr 1'u32) and 0x55555555'u32)
+  v = (v and 0x33333333'u32) + ((v shr 2'u32) and 0x33333333'u32)
+  result = int(((v + (v shr 4'u32) and 0xF0F0F0F'u32) * 0x1010101'u32) shr 24'u32)
 
-proc countBits64(n: int64): int {.compilerproc.} =
-  result = countBits32(toU32(n and 0xffffffff'i64)) +
-           countBits32(toU32(n shr 32'i64))
+proc countBits64(n: uint64): int {.compilerproc.} =
+  result = countBits32(uint32(n and 0xffffffff'u64)) +
+           countBits32(uint32(n shr 32))
 
 proc cardSet(s: NimSet, len: int): int {.compilerproc, inline.} =
   for i in 0..<len:
     if likely(s[i] == 0): continue
-    inc(result, countBits32(int32(s[i])))
+    inc(result, countBits32(s[i]))
