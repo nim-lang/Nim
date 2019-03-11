35aab357e2ad6ebe1d3b5196e592bba8a10bed83
Arne Döring <arne.doering@gmx.net>
2019-03-11 19:21:39 +0100

    Less warnings in macros (#10799)

--- lib/system/helpers.nim
+++ lib/system/helpers.nim
@@ -1,17 +1,9 @@
-# helpers used system.nim and other modules, avoids code duplication while
-# also minimizing symbols exposed in system.nim
-#
-# TODO: move other things here that should not be exposed in system.nim
-
-proc lineInfoToString(file: string, line, column: int): string =
-  file & "(" & $line & ", " & $column & ")"
+# TODO: remove this file. It causes "declared but not used" warnings everywhere.
 
 type InstantiationInfo = tuple[filename: string, line: int, column: int]
 
 proc `$`(info: InstantiationInfo): string =
-  # The +1 is needed here
-  # instead of overriding `$` (and changing its meaning), consider explicit name.
-  lineInfoToString(info.fileName, info.line, info.column+1)
+  info.fileName & "(" & $info.line & ", " & $(info.column+1) & ")"
 
 proc isNamedTuple(T: type): bool =
   ## return true for named tuples, false for any other type.
