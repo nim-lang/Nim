545dfb469651085b428a781eafdab07fae35aa0e
Andrii Riabushenko <cdome@bk.ru>
2019-03-11 22:49:24 +0000

    improve test

--- tests/vm/tsignaturehash.nim
+++ tests/vm/tsignaturehash.nim
@@ -1,11 +1,17 @@
 # test sym digest is computable at compile time
 
 import macros, algorithm
+import md5
 
 macro testmacro(s: typed{nkSym}): string =
   let s = signaturehash(s) & " - " & symBodyHash(s)
   result = newStrLitNode(s)
 
+macro testmacro(s: typed{nkOpenSymChoice|nkClosedSymChoice}): string =
+  var str = ""
+  for sym in s:
+    str &= symBodyHash(sym)
+  result = newStrLitNode(getMD5(str))
 
 # something recursive and/or generic
 discard testmacro(testmacro)
