a7efe535de22ce183ba0534feee4f970854d1f56
lihaifeng <10214028@qq.com>
2019-03-31 13:27:10 +0800

    Update tparscfg.nim

--- tests/stdlib/tparscfg.nim
+++ tests/stdlib/tparscfg.nim
@@ -7,45 +7,45 @@ lihf8515
 10214028
 lihaifeng@wxm.com
 ===
-charset=utf-8
+charset="utf-8"
 [Package]
-name=hello
---threads:on
+name="hello"
+--threads:"on"
 [Author]
-name=lhf
-qq=10214028
+name="lhf"
+qq="10214028"
 email="lihaifeng@wxm.com"
 ===
-charset=utf-8
+charset="utf-8"
 [Package]
-name=hello
---threads:on
+name="hello"
+--threads:"on"
 [Author]
-name=lihf8515
-qq=10214028
+name="lihf8515"
+qq="10214028"
 '''
 """
 import parsecfg, streams
 
 ## Creating a configuration file.
 var dict1=newConfig()
-dict1.setSectionKey("","charset","utf-8")
-dict1.setSectionKey("Package","name","hello")
-dict1.setSectionKey("Package","--threads","on")
-dict1.setSectionKey("Author","name","lihf8515")
-dict1.setSectionKey("Author","qq","10214028")
-dict1.setSectionKey("Author","email","lihaifeng@wxm.com")
+dict1.set("","charset","utf-8")
+dict1.set("Package","name","hello")
+dict1.set("Package","--threads","on")
+dict1.set("Author","name","lihf8515")
+dict1.set("Author","qq","10214028")
+dict1.set("Author","email","lihaifeng@wxm.com")
 var ss = newStringStream()
-dict1.writeConfig(ss)
+dict1.write(ss)
 
 ## Reading a configuration file.
 var dict2 = loadConfig(newStringStream(ss.data))
-var charset = dict2.getSectionValue("","charset")
-var threads = dict2.getSectionValue("Package","--threads")
-var pname = dict2.getSectionValue("Package","name")
-var name = dict2.getSectionValue("Author","name")
-var qq = dict2.getSectionValue("Author","qq")
-var email = dict2.getSectionValue("Author","email")
+var charset = dict2.get("","charset")
+var threads = dict2.get("Package","--threads")
+var pname = dict2.get("Package","name")
+var name = dict2.get("Author","name")
+var qq = dict2.get("Author","qq")
+var email = dict2.get("Author","email")
 echo charset
 echo threads
 echo pname
@@ -57,13 +57,13 @@ echo "==="
 
 ## Modifying a configuration file.
 var dict3 = loadConfig(newStringStream(ss.data))
-dict3.setSectionKey("Author","name","lhf")
+dict3.set("Author","name","lhf")
 write(stdout, $dict3)
 
 echo "==="
 
 ## Deleting a section key in a configuration file.
 var dict4 = loadConfig(newStringStream(ss.data))
-dict4.delSectionKey("Author","email")
+dict4.del("Author","email")
 write(stdout, $dict4)
 
