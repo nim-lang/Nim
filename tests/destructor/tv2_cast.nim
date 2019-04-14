faba2935f23a16e509d2b4edcde4c48b9191a3c9
Clyybber <darkmine956@gmail.com>
2019-04-14 18:49:06 +0200

    Better testcase

--- tests/destructor/tv2_cast.nim
+++ tests/destructor/tv2_cast.nim
@@ -1,8 +1,11 @@
 discard """
   cmd: '''nim c --newruntime $file'''
+  output: '''@[1]
+@[108, 111, 108]'''
 """
 
 # bug #11018
 discard cast[seq[uint8]](@[1])
 discard cast[seq[uint8]]("test")
-discard cast[seq[uint8]]([1])
+echo cast[seq[uint8]](@[1])
+echo cast[seq[uint8]]("test")
