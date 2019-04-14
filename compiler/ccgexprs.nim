d02d8d426fd4d7104df590587836a238953d294c
Clyybber <darkmine956@gmail.com>
2019-04-14 13:34:09 +0200

    Fix build w/o --newruntime

--- compiler/ccgexprs.nim
+++ compiler/ccgexprs.nim
@@ -1839,7 +1839,7 @@ proc genSomeCast(p: BProc, e: PNode, d: var TLoc) =
     if srcTyp.kind in {tyPtr, tyPointer} and etyp.kind in IntegralTypes:
       putIntoDest(p, d, e, "(($1) (ptrdiff_t) ($2))" %
           [getTypeDesc(p.module, e.typ), rdCharLoc(a)], a.storage)
-    elif etyp.kind == tySequence:
+    elif p.config.selectedGc == gcDestructors and etyp.kind == tySequence:
       putIntoDest(p, d, e, "(($1_Content*) ($2.p))" %
           [getTypeDesc(p.module, e.typ), rdCharLoc(a)], a.storage)
     else:
