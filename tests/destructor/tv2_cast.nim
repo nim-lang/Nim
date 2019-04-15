25ef5d114137d544037f5f87656100b897c4632b
Clyybber <darkmine956@gmail.com>
2019-04-15 10:34:35 +0200

    Extend the fix for #11018 to strings

--- tests/destructor/tv2_cast.nim
+++ tests/destructor/tv2_cast.nim
@@ -1,7 +1,9 @@
 discard """
   cmd: '''nim c --newruntime $file'''
   output: '''@[1]
-@[116, 101, 115, 116]'''
+@[116, 101, 115, 116]
+test
+@[1953719668, 875770417]'''
 """
 
 # bug #11018
@@ -9,3 +11,9 @@ discard cast[seq[uint8]](@[1])
 discard cast[seq[uint8]]("test")
 echo cast[seq[uint8]](@[1])
 echo cast[seq[uint8]]("test")
+
+discard cast[seq[string]](@[116'u8, 101, 115, 116])
+echo cast[seq[string]](@[116'u8, 101, 115, 116])
+var a = cast[seq[uint32]]("test1234")
+a.setLen(2)
+echo a
