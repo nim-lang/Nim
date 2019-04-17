a67e45e8b03fe1b3160e14ccbd3df2ce6d0d8517
jiro4989 <jiroron666@gmail.com>
2019-04-17 13:36:44 +0900

    :memo: Write runnableExamples and type descriptions.

--- lib/pure/streams.nim
+++ lib/pure/streams.nim
@@ -136,7 +136,9 @@ proc newEIO(msg: string): owned(ref IOError) =
 
 type
   Stream* = ref StreamObj
-    ## TODO
+    ## A stream.
+    ## All procedures of this module use this type.
+    ## Procedures don't use `StreamObj <#StreamObj>`_.
   StreamObj* = object of RootObj
     ## Stream interface that supports writing or reading.
     ## 
@@ -167,31 +169,45 @@ type
 
 proc flush*(s: Stream) =
   ## Flushes the buffers that the stream `s` might use.
+  runnableExamples:
+    discard ## TODO
   if not isNil(s.flushImpl): s.flushImpl(s)
 
 proc close*(s: Stream) =
   ## Closes the stream `s`.
+  runnableExamples:
+    discard ## TODO
   if not isNil(s.closeImpl): s.closeImpl(s)
 
 proc atEnd*(s: Stream): bool =
   ## Checks if more data can be read from `f`. Returns ``true`` if all data has
   ## been read.
+  runnableExamples:
+    discard ## TODO
   result = s.atEndImpl(s)
 
 proc setPosition*(s: Stream, pos: int) =
   ## Sets the position `pos` of the stream `s`.
+  runnableExamples:
+    discard ## TODO
   s.setPositionImpl(s, pos)
 
 proc getPosition*(s: Stream): int =
   ## Retrieves the current position in the stream `s`.
+  runnableExamples:
+    discard ## TODO
   result = s.getPositionImpl(s)
 
 proc readData*(s: Stream, buffer: pointer, bufLen: int): int =
   ## Low level proc that reads data into an untyped `buffer` of `bufLen` size.
+  runnableExamples:
+    discard ## TODO
   result = s.readDataImpl(s, buffer, bufLen)
 
 proc readDataStr*(s: Stream, buffer: var string, slice: Slice[int]): int =
   ## Low level proc that reads data into a string ``buffer`` at ``slice``.
+  runnableExamples:
+    discard ## TODO
   if s.readDataStrImpl != nil:
     result = s.readDataStrImpl(s, buffer, slice)
   else:
@@ -204,6 +220,8 @@ when not defined(js):
     ##
     ## **Note:**
     ## * Not available this when backend is js
+    runnableExamples:
+      discard ## TODO
     const bufferSize = 1024
     var buffer {.noinit.}: array[bufferSize, char]
     while true:
@@ -219,11 +237,15 @@ when not defined(js):
 proc peekData*(s: Stream, buffer: pointer, bufLen: int): int =
   ## Low level proc that reads data into an untyped `buffer` of `bufLen` size
   ## without moving stream position
+  runnableExamples:
+    discard ## TODO
   result = s.peekDataImpl(s, buffer, bufLen)
 
 proc writeData*(s: Stream, buffer: pointer, bufLen: int) =
   ## Low level proc that writes an untyped `buffer` of `bufLen` size
   ## to the stream `s`.
+  runnableExamples:
+    discard ## TODO
   s.writeDataImpl(s, buffer, bufLen)
 
 proc write*[T](s: Stream, x: T) =
@@ -232,6 +254,8 @@ proc write*[T](s: Stream, x: T) =
   ## .. code-block:: Nim
   ##
   ##     s.writeData(s, addr(x), sizeof(x))
+  runnableExamples:
+    discard ## TODO
   var y: T
   shallowCopy(y, x)
   writeData(s, addr(y), sizeof(y))
@@ -239,6 +263,8 @@ proc write*[T](s: Stream, x: T) =
 proc write*(s: Stream, x: string) =
   ## Writes the string `x` to the the stream `s`. No length field or
   ## terminating zero is written.
+  runnableExamples:
+    discard ## TODO
   when nimvm:
     writeData(s, cstring(x), x.len)
   else:
@@ -247,125 +273,183 @@ proc write*(s: Stream, x: string) =
 proc write*(s: Stream, args: varargs[string, `$`]) =
   ## Writes one or more strings to the the stream. No length fields or
   ## terminating zeros are written.
+  runnableExamples:
+    discard ## TODO
   for str in args: write(s, str)
 
 proc writeLine*(s: Stream, args: varargs[string, `$`]) =
   ## Writes one or more strings to the the stream `s` followed
   ## by a new line. No length field or terminating zero is written.
+  runnableExamples:
+    discard ## TODO
   for str in args: write(s, str)
   write(s, "\n")
 
 proc read*[T](s: Stream, result: var T) =
   ## Generic read procedure. Reads `result` from the stream `s`.
+  runnableExamples:
+    discard ## TODO
   if readData(s, addr(result), sizeof(T)) != sizeof(T):
     raise newEIO("cannot read from stream")
 
 proc peek*[T](s: Stream, result: var T) =
   ## Generic peek procedure. Peeks `result` from the stream `s`.
+  runnableExamples:
+    discard ## TODO
   if peekData(s, addr(result), sizeof(T)) != sizeof(T):
     raise newEIO("cannot read from stream")
 
 proc readChar*(s: Stream): char =
   ## Reads a char from the stream `s`. Raises `IOError` if an error occurred.
   ## Returns '\\0' as an EOF marker.
+  runnableExamples:
+    discard ## TODO
   if readData(s, addr(result), sizeof(result)) != 1: result = '\0'
 
 proc peekChar*(s: Stream): char =
   ## Peeks a char from the stream `s`. Raises `IOError` if an error occurred.
   ## Returns '\\0' as an EOF marker.
+  runnableExamples:
+    discard ## TODO
   if peekData(s, addr(result), sizeof(result)) != 1: result = '\0'
 
 proc readBool*(s: Stream): bool =
   ## Reads a bool from the stream `s`. Raises `IOError` if an error occurred.
+  runnableExamples:
+    discard ## TODO
   read(s, result)
 
 proc peekBool*(s: Stream): bool =
   ## Peeks a bool from the stream `s`. Raises `IOError` if an error occurred.
+  runnableExamples:
+    discard ## TODO
   peek(s, result)
 
 proc readInt8*(s: Stream): int8 =
   ## Reads an int8 from the stream `s`. Raises `IOError` if an error occurred.
+  runnableExamples:
+    discard ## TODO
   read(s, result)
 
 proc peekInt8*(s: Stream): int8 =
   ## Peeks an int8 from the stream `s`. Raises `IOError` if an error occurred.
+  runnableExamples:
+    discard ## TODO
   peek(s, result)
 
 proc readInt16*(s: Stream): int16 =
   ## Reads an int16 from the stream `s`. Raises `IOError` if an error occurred.
+  runnableExamples:
+    discard ## TODO
   read(s, result)
 
 proc peekInt16*(s: Stream): int16 =
   ## Peeks an int16 from the stream `s`. Raises `IOError` if an error occurred.
+  runnableExamples:
+    discard ## TODO
   peek(s, result)
 
 proc readInt32*(s: Stream): int32 =
   ## Reads an int32 from the stream `s`. Raises `IOError` if an error occurred.
+  runnableExamples:
+    discard ## TODO
   read(s, result)
 
 proc peekInt32*(s: Stream): int32 =
   ## Peeks an int32 from the stream `s`. Raises `IOError` if an error occurred.
+  runnableExamples:
+    discard ## TODO
   peek(s, result)
 
 proc readInt64*(s: Stream): int64 =
   ## Reads an int64 from the stream `s`. Raises `IOError` if an error occurred.
+  runnableExamples:
+    discard ## TODO
   read(s, result)
 
 proc peekInt64*(s: Stream): int64 =
   ## Peeks an int64 from the stream `s`. Raises `IOError` if an error occurred.
+  runnableExamples:
+    discard ## TODO
   peek(s, result)
 
 proc readUint8*(s: Stream): uint8 =
   ## Reads an uint8 from the stream `s`. Raises `IOError` if an error occurred.
+  runnableExamples:
+    discard ## TODO
   read(s, result)
 
 proc peekUint8*(s: Stream): uint8 =
   ## Peeks an uint8 from the stream `s`. Raises `IOError` if an error occurred.
+  runnableExamples:
+    discard ## TODO
   peek(s, result)
 
 proc readUint16*(s: Stream): uint16 =
   ## Reads an uint16 from the stream `s`. Raises `IOError` if an error occurred.
+  runnableExamples:
+    discard ## TODO
   read(s, result)
 
 proc peekUint16*(s: Stream): uint16 =
   ## Peeks an uint16 from the stream `s`. Raises `IOError` if an error occurred.
+  runnableExamples:
+    discard ## TODO
   peek(s, result)
 
 proc readUint32*(s: Stream): uint32 =
   ## Reads an uint32 from the stream `s`. Raises `IOError` if an error occurred.
+  runnableExamples:
+    discard ## TODO
   read(s, result)
 
 proc peekUint32*(s: Stream): uint32 =
   ## Peeks an uint32 from the stream `s`. Raises `IOError` if an error occurred.
+  runnableExamples:
+    discard ## TODO
   peek(s, result)
 
 proc readUint64*(s: Stream): uint64 =
   ## Reads an uint64 from the stream `s`. Raises `IOError` if an error occurred.
+  runnableExamples:
+    discard ## TODO
   read(s, result)
 
 proc peekUint64*(s: Stream): uint64 =
   ## Peeks an uint64 from the stream `s`. Raises `IOError` if an error occurred.
+  runnableExamples:
+    discard ## TODO
   peek(s, result)
 
 proc readFloat32*(s: Stream): float32 =
   ## Reads a float32 from the stream `s`. Raises `IOError` if an error occurred.
+  runnableExamples:
+    discard ## TODO
   read(s, result)
 
 proc peekFloat32*(s: Stream): float32 =
   ## Peeks a float32 from the stream `s`. Raises `IOError` if an error occurred.
+  runnableExamples:
+    discard ## TODO
   peek(s, result)
 
 proc readFloat64*(s: Stream): float64 =
   ## Reads a float64 from the stream `s`. Raises `IOError` if an error occurred.
+  runnableExamples:
+    discard ## TODO
   read(s, result)
 
 proc peekFloat64*(s: Stream): float64 =
   ## Peeks a float64 from the stream `s`. Raises `IOError` if an error occurred.
+  runnableExamples:
+    discard ## TODO
   peek(s, result)
 
 proc readStr*(s: Stream, length: int): TaintedString =
   ## Reads a string of length `length` from the stream `s`. Raises `IOError` if
   ## an error occurred.
+  runnableExamples:
+    discard ## TODO
   result = newString(length).TaintedString
   var L = readData(s, cstring(result), length)
   if L != length: setLen(result.string, L)
@@ -373,6 +457,8 @@ proc readStr*(s: Stream, length: int): TaintedString =
 proc peekStr*(s: Stream, length: int): TaintedString =
   ## Peeks a string of length `length` from the stream `s`. Raises `IOError` if
   ## an error occurred.
+  runnableExamples:
+    discard ## TODO
   result = newString(length).TaintedString
   var L = peekData(s, cstring(result), length)
   if L != length: setLen(result.string, L)
@@ -386,9 +472,9 @@ proc readLine*(s: Stream, line: var TaintedString): bool =
   ## otherwise. If ``false`` is returned `line` contains no new data.
   ##
   ## See also:
-  ## * `readLine proc <#readLine,Stream>`_
-  ## * `peekLine proc <#peekLine,Stream>`_
-  ## * `peekLine proc <#peekLine,Stream,TaintedString>`_
+  ## * `readLine(Stream) proc <#readLine,Stream>`_
+  ## * `peekLine(Stream) proc <#peekLine,Stream>`_
+  ## * `peekLine(Stream, TaintedString) proc <#peekLine,Stream,TaintedString>`_
   runnableExamples:
     var strm = newStringStream("The first line\nthe second line\nthe third line")
     var line = ""
@@ -428,9 +514,9 @@ proc peekLine*(s: Stream, line: var TaintedString): bool =
   ## otherwise. If ``false`` is returned `line` contains no new data.
   ##
   ## See also:
-  ## * `readLine proc <#readLine,Stream>`_
-  ## * `readLine proc <#readLine,Stream,TaintedString>`_
-  ## * `peekLine proc <#peekLine,Stream>`_
+  ## * `readLine(Stream) proc <#readLine,Stream>`_
+  ## * `readLine(Stream, TaintedString) proc <#readLine,Stream,TaintedString>`_
+  ## * `peekLine(Stream) proc <#peekLine,Stream>`_
   runnableExamples:
     var strm = newStringStream("The first line\nthe second line\nthe third line")
     var line = ""
@@ -461,9 +547,9 @@ proc readLine*(s: Stream): TaintedString =
   ##   Raises `IOError` if an error occurred.
   ##
   ## See also:
-  ## * `readLine proc <#readLine,Stream,TaintedString>`_
-  ## * `peekLine proc <#peekLine,Stream>`_
-  ## * `peekLine proc <#peekLine,Stream,TaintedString>`_
+  ## * `readLine(Stream, TaintedString) proc <#readLine,Stream,TaintedString>`_
+  ## * `peekLine(Stream) proc <#peekLine,Stream>`_
+  ## * `peekLine(Stream, TaintedString) proc <#peekLine,Stream,TaintedString>`_
   runnableExamples:
     var strm = newStringStream("The first line\nthe second line\nthe third line")
     
@@ -494,9 +580,9 @@ proc peekLine*(s: Stream): TaintedString =
   ##   Raises `IOError` if an error occurred.
   ##
   ## See also:
-  ## * `readLine proc <#readLine,Stream>`_
-  ## * `readLine proc <#readLine,Stream,TaintedString>`_
-  ## * `peekLine proc <#peekLine,Stream,TaintedString>`_
+  ## * `readLine(Stream) proc <#readLine,Stream>`_
+  ## * `readLine(Stream, TaintedString) proc <#readLine,Stream,TaintedString>`_
+  ## * `peekLine(Stream, TaintedString) proc <#peekLine,Stream,TaintedString>`_
   runnableExamples:
     var strm = newStringStream("The first line\nthe second line\nthe third line")
     
@@ -517,8 +603,8 @@ iterator lines*(s: Stream): TaintedString =
   ## The iteration is based on ``readLine``.
   ##
   ## See also:
-  ## * `readLine proc <#readLine,Stream>`_
-  ## * `readLine proc <#readLine,Stream,TaintedString>`_
+  ## * `readLine(Stream) proc <#readLine,Stream>`_
+  ## * `readLine(Stream, TaintedString) proc <#readLine,Stream,TaintedString>`_
   runnableExamples:
     var strm = newStringStream("The first line\nthe second line\nthe third line")
     var lines: seq[string]
@@ -542,11 +628,12 @@ when not defined(js):
       ## **Note:**
       ## * Not available this when backend is js
     StringStreamObj* = object of StreamObj
-      ## TODO
+      ## A string stream object.
       ##
       ## **Note:**
       ## * Not available this when backend is js
-      data*: string
+      data*: string ## A string data.
+                    ## This is updated when called `writeLine` etc.
       pos: int
 
   proc ssAtEnd(s: Stream): bool =
@@ -612,6 +699,8 @@ when not defined(js):
     ##
     ## **Note:**
     ## * Not available this when backend is js
+    runnableExamples:
+      discard ## TODO
     new(result)
     result.data = s
     result.pos = 0
@@ -631,6 +720,7 @@ when not defined(js):
       ## **Note:**
       ## * Not available this when backend is js
     FileStreamObj* = object of Stream
+      ## A file stream object.
       ##
       ## **Note:**
       ## * Not available this when backend is js
@@ -665,6 +755,8 @@ when not defined(js):
     ##
     ## **Note:**
     ## * Not available this when backend is js
+    runnableExamples:
+      discard ## TODO
     new(result)
     result.f = f
     result.closeImpl = fsClose
@@ -688,9 +780,22 @@ when not defined(js):
     ##   use `openFileStream proc <#openFileStream,string,FileMode,int>`_
     ##   instead.
     ## * Not available this when backend is js
-    ##
-    ## See also:
-    ## * `FileMode enum <io.html#FileMode>`_ is available FileMode
+    runnableExamples:
+      from os import removeFile
+      try:
+        var strm = newFileStream("somefile.txt", fmWrite)
+        if not isNil(strm):
+          strm.writeLine("The first line")
+          strm.writeLine("the second line")
+          strm.writeLine("the third line")
+          strm.close()
+          ## Output (somefile.txt)
+          ## The first line
+          ## the second line
+          ## the third line
+          removeFile("somefile.txt")
+      except:
+        stderr.write getCurrentExceptionMsg()
     var f: File
     if open(f, filename, mode, bufSize): result = newFileStream(f)
 
@@ -700,11 +805,13 @@ when not defined(js):
     ##
     ## **Note:**
     ## * Not available this when backend is js
-    ##
-    ## See also:
-    ## * `newFileStream proc <#newFileStream,string,FileMode,int>`_ 
     runnableExamples:
-      discard # TODO
+      try:
+        var strm = openFileStream("somefile.txt")
+        echo strm.readLine()
+        strm.close()
+      except:
+        stderr.write getCurrentExceptionMsg()
     var f: File
     if open(f, filename, mode, bufSize):
       return newFileStream(f)
