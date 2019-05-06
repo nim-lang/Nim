a85d3879282a15e85ce9dd18b9a5bf020096a76c
Araq <rumpf_a@web.de>
2019-05-06 14:56:49 +0200

    fixes #10978

--- lib/system/indexerrors.nim
+++ lib/system/indexerrors.nim
@@ -1,7 +1,10 @@
 # imported by other modules, unlike helpers.nim which is included
 
 template formatErrorIndexBound*[T](i, a, b: T): string =
-  "index " & $i & " not in " & $a & " .. " & $b
+  when defined(standalone):
+    "indexOutOfBounds"
+  else:
+    "index " & $i & " not in " & $a & " .. " & $b
 
 template formatErrorIndexBound*[T](i, n: T): string =
   formatErrorIndexBound(i, 0, n)
