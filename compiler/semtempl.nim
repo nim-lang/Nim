75553ca6310ab0ee72d8a9221d33230c9d1a23d7
Aditya Siram <aditya.siram@gmail.com>
2019-05-12 12:48:17 -0500

    Fixes #9365 : let with exportC pragma in template.

--- compiler/semtempl.nim
+++ compiler/semtempl.nim
@@ -197,6 +197,13 @@ proc addLocalDecl(c: var TemplCtx, n: var PNode, k: TSymKind) =
     else:
       replaceIdentBySym(c.c, n, ident)
   else:
+    if (n.kind == nkPragmaExpr and sonsLen(n) >= 2 and n.sons[1].kind == nkPragma):
+      let pragmaNode = n.sons[1]
+      let numPragmas = sonsLen(pragmaNode)
+      for i in 0 .. numPragmas-1:
+        openScope(c)
+        pragmaNode.sons[i] = semTemplBody(c,pragmaNode.sons[i])
+        closeScope(c)
     let ident = getIdentNode(c, n)
     if not isTemplParam(c, ident):
       # fix #2670, consider:
