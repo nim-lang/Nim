bc0f2f0dd2128805c82ea8bc96b5d757ef1e92f8
Oscar Nihlgård <oscarnihlgard@gmail.com>
2019-05-15 08:22:45 +0200

    Add test case for #3766 (#11252)

--- tests/misc/tconv.nim
+++ tests/misc/tconv.nim
@@ -41,3 +41,17 @@ type E = enum a, b, c
 
 reject:
     const e = E(4)
+
+block: # issue 3766
+
+  type R = range[0..2]
+
+  reject:
+    type
+      T[n: static[R]] = object
+      V = T[3.R]
+
+  reject:
+    proc r(x: static[R]) =
+      echo x
+    r 3.R
