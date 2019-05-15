09ccb5ef2f09a716401d1c765af0a9555c7a6d52
narimiran <narimiran@disroot.org>
2019-05-15 16:25:09 +0200

    fix #10952, UNC paths

--- lib/pure/pathnorm.nim
+++ lib/pure/pathnorm.nim
@@ -28,6 +28,9 @@ proc next*(it: var PathIter; x: string): (int, int) =
   if not it.notFirst and x[it.i] in {DirSep, AltSep}:
     # absolute path:
     inc it.i
+    when doslikeFileSystem: # UNC paths have leading `\\`
+      if hasNext(it, x) and x[it.i] in {DirSep, AltSep}:
+        inc it.i
   else:
     while it.i < x.len and x[it.i] notin {DirSep, AltSep}: inc it.i
   if it.i > it.prev:
