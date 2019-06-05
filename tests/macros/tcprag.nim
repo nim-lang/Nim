2e0396c71863989d627d6fa808b6ccff8cf27137
Sebastian Schmidt <basti90@gmail.com>
2019-06-06 01:08:26 +0200

    add test case for #11415

--- tests/macros/tcprag.nim
+++ tests/macros/tcprag.nim
@@ -2,6 +2,9 @@ discard """
   output: '''true
 true
 true
+true
+true
+true
 '''
 """
 
@@ -30,3 +33,56 @@ macro m2(T: typedesc): untyped =
   result = quote do:
     `T`.hasCustomPragma(table)
 echo m2(User)
+
+
+
+block:
+  template noserialize() {.pragma.}
+
+  type
+    Point[T] = object
+      x, y: T
+
+    ReplayEventKind = enum
+      FoodAppeared, FoodEaten, DirectionChanged
+
+  # ref #11415
+  # this works, since `foodPos` is inside of a variant kind with a
+  # single `of` element
+  block:
+    type
+      ReplayEvent = object
+        time: float
+        pos {.noserialize.}: Point[float] # works before fix
+        case kind: ReplayEventKind
+        of FoodEaten:
+          foodPos {.noserialize.}: Point[float] # also works, only in one branch
+        of DirectionChanged, FoodAppeared:
+          playerPos: float
+
+    let ev = ReplayEvent(
+        pos: Point[float](x: 5.0, y: 1.0),
+        time: 1.2345,
+        kind: FoodEaten,
+        foodPos: Point[float](x: 5.0, y: 1.0)
+      )
+    echo ev.pos.hasCustomPragma(noserialize)
+    echo ev.foodPos.hasCustomPragma(noserialize)
+
+  # ref 11415
+  # this did not work, since `foodPos` is inside of a variant kind with a
+  # two `of` elements
+  block:
+    type
+      ReplayEvent = object
+        case kind: ReplayEventKind
+        of FoodEaten, FoodAppeared:
+          foodPos {.noserialize.}: Point[float] # did not work, because in two branches
+        of DirectionChanged:
+          playerPos: float
+
+    let ev = ReplayEvent(
+        kind: FoodEaten,
+        foodPos: Point[float](x: 5.0, y: 1.0)
+      )
+    echo ev.foodPos.hasCustomPragma(noserialize)
