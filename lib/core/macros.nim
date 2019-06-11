29b0ea183f3ca1660efaa84786478d84b110e5b4
Arne Döring <arne.doering@gmx.net>
2019-06-11 15:00:09 +0200

    fix typo

--- lib/core/macros.nim
+++ lib/core/macros.nim
@@ -1424,7 +1424,7 @@ when defined(nimMacrosGetNodeId):
   proc nodeID*(n: NimNode): int {.magic: NodeId.}
     ## Returns the id of ``n``, when the compiler has been compiled
     ## with the flag ``-d:useNodeids``, otherwise returns ``-1``. This
-    ## proc is for for the purpose to debug the compiler only.
+    ## proc is for the purpose to debug the compiler only.
 
 macro expandMacros*(body: typed): untyped =
   ## Expands one level of macro - useful for debugging.
