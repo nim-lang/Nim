6530a4100a14e60534a3338e1225a9318ec55231
Jasper Jenkins <jasper.vs.jenkins@gmail.com>
2019-06-12 02:59:26 -0700

    [bugfix] fix nimpretty for absolute paths (#11480)
    (cherry picked from commit da035e9c8385be59449d13d1355aba4f9f97a6b4)

--- nimpretty/nimpretty.nim
+++ nimpretty/nimpretty.nim
@@ -49,7 +49,9 @@ type
 proc prettyPrint(infile, outfile: string, opt: PrettyOptions) =
   var conf = newConfigRef()
   let fileIdx = fileInfoIdx(conf, AbsoluteFile infile)
-  conf.outFile = RelativeFile outfile
+  let f = splitFile(outfile.expandTilde)
+  conf.outFile = RelativeFile f.name & f.ext
+  conf.outDir = toAbsoluteDir f.dir
   var p: TParsers
   p.parser.em.indWidth = opt.indWidth
   if setupParsers(p, fileIdx, newIdentCache(), conf):
