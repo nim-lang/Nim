4d18ee498a4b98c27000c8205f3f5266a60aa5f8
Andreas Rumpf <rumpf_a@web.de>
2019-06-21 20:45:00 +0200

    closes #11533
    (cherry picked from commit 0db3c670ac46d431b86275d245b3b4ec0dd6c372)

--- tests/destructor/tsimpleclosure.nim
+++ tests/destructor/tsimpleclosure.nim
@@ -2,6 +2,9 @@ discard """
   cmd: '''nim c --newruntime $file'''
   output: '''a b
 70
+hello
+hello
+hello
 2 2  alloc/dealloc pairs: 0'''
 """
 
@@ -26,5 +29,20 @@ proc wrap =
   foo fIt
 
 wrap()
+
+# bug #11533
+proc say = echo "hello"
+
+# Error: internal error: genAssignment: tyNil
+var err0: proc() = say
+err0()
+
+var ok0: proc()
+ok0 = say
+ok0()
+
+var ok1 = say
+ok1()
+
 let (a, d) = allocCounters()
 discard cprintf("%ld %ld  alloc/dealloc pairs: %ld\n", a, d, system.allocs)
