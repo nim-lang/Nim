5af23213392d216921612752101a547ac71a4d0a
Timothee Cour <timothee.cour2@gmail.com>
2019-07-03 00:32:23 -0700

    fixup

--- compiler/vm.nim
+++ compiler/vm.nim
@@ -1082,7 +1082,7 @@ proc rawExecute(c: PCtx, start: int, tos: PStackFrame): TFullReg =
           VmArgs(ra: ra, rb: rb, rc: rc, slots: cast[pointer](regs),
                  currentException: c.currentExceptionA,
                  currentLineInfo: c.debug[pc]))
-      elif shouldImportcSymbol(prc):
+      elif importcCond(prc):
         if compiletimeFFI notin c.config.features:
           globalError(c.config, c.debug[pc], "VM not allowed to do FFI, see `compiletimeFFI`")
         # we pass 'tos.slots' instead of 'regs' so that the compiler can keep
