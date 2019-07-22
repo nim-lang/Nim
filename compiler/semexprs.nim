101bbbcd5879068f0d7e93a6a465470077d9515c
Jasper Jenkins <jasper.vs.jenkins@gmail.com>
2019-07-21 22:35:23 -0700

    mark the module used instead for export except

--- compiler/semexprs.nim
+++ compiler/semexprs.nim
@@ -2453,8 +2453,8 @@ proc semExportExcept(c: PContext, n: PNode): PNode =
        s.name.id notin exceptSet and sfError notin s.flags:
       strTableAdd(c.module.tab, s)
       result.add newSymNode(s, n.info)
-      markUsed(c, n.info, s, c.graph.usageSym)
     s = nextIter(i, exported.tab)
+  markUsed(c, n.info, exported, c.graph.usageSym)
 
 proc semExport(c: PContext, n: PNode): PNode =
   result = newNodeI(nkExportStmt, n.info)
