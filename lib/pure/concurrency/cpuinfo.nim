8df6c894353ad6ff9cd6f6e296a361f1c14ddaa9
awr1 <41453959+awr1@users.noreply.github.com>
2019-07-22 16:13:19 -0500

    added x86 feature detect

--- lib/pure/concurrency/cpuinfo.nim
+++ lib/pure/concurrency/cpuinfo.nim
@@ -68,7 +68,8 @@ proc countProcessors*(): int {.rtl, extern: "ncpi$1".} =
         wProcessorLevel: int16
         wProcessorRevision: int16
 
-    proc GetSystemInfo(lpSystemInfo: var SYSTEM_INFO) {.stdcall, dynlib: "kernel32", importc: "GetSystemInfo".}
+    proc GetSystemInfo(lpSystemInfo: var SYSTEM_INFO)
+      {.stdcall, dynlib: "kernel32", importc: "GetSystemInfo".}
 
     var
       si: SYSTEM_INFO
@@ -77,16 +78,16 @@ proc countProcessors*(): int {.rtl, extern: "ncpi$1".} =
   elif defined(macosx) or defined(bsd):
     var
       mib: array[0..3, cint]
-      numCPU: int
+      numCpu: int
       len: csize
     mib[0] = CTL_HW
     mib[1] = HW_AVAILCPU
-    len = sizeof(numCPU)
-    discard sysctl(addr(mib), 2, addr(numCPU), len, nil, 0)
-    if numCPU < 1:
+    len = sizeof(numCpu)
+    discard sysctl(addr(mib), 2, addr(numCpu), len, nil, 0)
+    if numCpu < 1:
       mib[1] = HW_NCPU
-      discard sysctl(addr(mib), 2, addr(numCPU), len, nil, 0)
-    result = numCPU
+      discard sysctl(addr(mib), 2, addr(numCpu), len, nil, 0)
+    result = numCpu
   elif defined(hpux):
     result = mpctl(MPC_GETNUMSPUS, nil, nil)
   elif defined(irix):
@@ -102,21 +103,206 @@ proc countProcessors*(): int {.rtl, extern: "ncpi$1".} =
     result = sysconf(SC_NPROCESSORS_ONLN)
   if result <= 0: result = 0
 
-when defined(x86):
+# TODO(awr1): get cache line size
+# TODO(awr1): get processor name
+# TODO(awr1): ARM processor exts
+# TODO(awr1): on ARM, probe /proc/cpuinfo, but what about windows on arm?
+
+const
+  onX86 = defined(i386) or defined(amd64)
+  onArm = defined(arm) or defined(arm64)
+
+when onX86:
   proc cpuidX86(leaf: int32): tuple[eax, ebx, ecx, edx: int32] =
     when defined(vcc):
       # limited inline asm support in vcc, so intrinsics, here we go:
-      proc cpuidVcc(cpuInfo: ptr int32, functionID: int32)
+      proc cpuidVcc(cpuInfo: ptr int32; functionID: int32)
         {.cdecl, importc: "__cpuid", header: "intrin.h".}
-      var x: array[4, int32]
-      cpuidVcc(addr x[0], leaf)
-      (x[0], x[1], x[2], x[3])
+      cpuidVcc(addr result.eax, leaf)
     else:
       var (eaxr, ebxr, ecxr, edxr) = (0'i32, 0'i32, 0'i32, 0'i32)
-      # zero ecx register first!
+      # zero ecx first!
       asm """
-        xor %ecx, %ecx
+        xorl %%ecx, %%ecx
         cpuid
         :"=a"(`eaxr`), "=b"(`ebxr`), "=c"(`ecxr`), "=d"(`edxr`)
         :"a"(`leaf`) """
       (eaxr, ebxr, ecxr, edxr)
+
+when onX86 or defined(nimdoc):
+  type
+    X86Feature* {.pure.} = enum
+      HypervisorPresence, SimultaneousMultithreading, Vmx, X87fpu, Mmx, MmxExt,
+      F3DNow, F3DNowExt, F3DNowPf, Sse, Sse2, Sse3, Ssse3, Sse4a, Sse41, Sse42,
+      Avx, Avx2, Avx512f, Avx512dq, Avx512ifma, Avx512pf, Avx512er, Avx512cd,
+      Avx512bw, Avx512vl, Avx512vbmi, Avx512vbmi2, Avx512vpopcntdq, Avx512vnni,
+      Avx512vnniw4, Avx512fmaps4, Avx512bitalg, Rdrand, Rdseed, MovBigEndian,
+      Popcnt, Fma3, Fma4, Cas8B, Cas16B, Abm, Bmi1, Bmi2, TsxHle, TsxRtm, Adx,
+      Sgx, Gfni, Aes, Vaes, Vpclmulqdq, Pclmulqdq, NxBit, Float16c, Ssbd,
+      SpecCtrl, Stibp, Sha, Clflush, ClflushOpt, Clwriteback, PrefetchWT1, Mpx
+
+when onARM or defined(nimdoc):
+  type
+    ArmFeature* {.pure.} = enum
+      Neon, Vfp
+
+type CpuFeature* = (when onX86: X86Feature elif onArm: ArmFeature)
+
+proc checkCpuFeatures*(features: set[CpuFeature]): set[CpuFeature] =
+  when onX86:
+    let
+      leaf1 = cpuidX86(leaf = 1)
+      leaf7 = cpuidX86(leaf = 7)
+      leaf8 = cpuidX86(leaf = 0x80000001'i32)
+    for feature in features:
+      template test(input, bit: int): bool =
+        ((1 shl bit) and input) != 0
+      template checkoff(input, bit: int): untyped =
+        if test(input, bit): result.incl(feature)
+
+      # see: https://en.wikipedia.org/wiki/CPUID#Calling_CPUID
+
+      case feature
+
+      # leaf 1, edx
+      of X87fpu:
+        leaf1.edx.checkoff(0)
+      of Clflush:
+        leaf1.edx.checkoff(19)
+      of Mmx:
+        leaf1.edx.checkoff(23)
+      of Sse:
+        leaf1.edx.checkoff(25)
+      of Sse2:
+        leaf1.edx.checkoff(26)
+      of SimultaneousMultithreading:
+        if leaf1.edx.test(28) or not leaf8.edx.test(1):
+          result.incl(feature)
+
+      # leaf 1, ecx
+      of Sse3:
+        leaf1.ecx.checkoff(0)
+      of Pclmulqdq:
+        leaf1.ecx.checkoff(1)
+      of Vmx:
+        leaf1.ecx.checkoff(5)
+      of Ssse3:
+        leaf1.ecx.checkoff(9)
+      of Fma3:
+        leaf1.ecx.checkoff(12)
+      of Cas16B:
+        leaf1.ecx.checkoff(13)
+      of Sse41:
+        leaf1.ecx.checkoff(19)
+      of Sse42:
+        leaf1.ecx.checkoff(20)
+      of MovBigEndian:
+        leaf1.ecx.checkoff(22)
+      of Popcnt:
+        leaf1.ecx.checkoff(23)
+      of Aes:
+        leaf1.ecx.checkoff(25)
+      of Avx:
+        leaf1.ecx.checkoff(28)
+      of Float16c:
+        leaf1.ecx.checkoff(29)
+      of Rdrand:
+        leaf1.ecx.checkoff(30)
+      of HypervisorPresence:
+        leaf1.ecx.checkoff(31)
+
+      # leaf 7, ecx
+      of PrefetchWT1:
+        leaf7.ecx.checkoff(0)
+      of Avx512vbmi:
+        leaf7.ecx.checkoff(1)
+      of Avx512vbmi2:
+        leaf7.ecx.checkoff(6)
+      of Gfni:
+        leaf7.ecx.checkoff(8)
+      of Vaes:
+        leaf7.ecx.checkoff(9)
+      of Vpclmulqdq:
+        leaf7.ecx.checkoff(10)
+      of Avx512vnni:
+        leaf7.ecx.checkoff(10)
+      of Avx512bitalg:
+        leaf7.ecx.checkoff(10)
+      of Avx512vpopcntdq:
+        leaf7.ecx.checkoff(10)
+
+      # leaf 7, ebx
+      of Sgx:
+        leaf7.ebx.checkoff(2)
+      of Bmi1:
+        leaf7.ebx.checkoff(3)
+      of TsxHle:
+        leaf7.ebx.checkoff(4)
+      of Avx2:
+        leaf7.ebx.checkoff(5)
+      of Bmi2:
+        leaf7.ebx.checkoff(8)
+      of TsxRtm:
+        leaf7.ebx.checkoff(11)
+      of Mpx:
+        leaf7.ebx.checkoff(14)
+      of Avx512f:
+        leaf7.ebx.checkoff(16)
+      of Avx512dq:
+        leaf7.ebx.checkoff(17)
+      of Rdseed:
+        leaf7.ebx.checkoff(18)
+      of Adx:
+        leaf7.ebx.checkoff(19)
+      of Avx512ifma:
+        leaf7.ebx.checkoff(21)
+      of ClflushOpt:
+        leaf7.ebx.checkoff(23)
+      of Clwriteback:
+        leaf7.ebx.checkoff(24)
+      of Avx512pf:
+        leaf7.ebx.checkoff(26)
+      of Avx512er:
+        leaf7.ebx.checkoff(27)
+      of Avx512cd:
+        leaf7.ebx.checkoff(28)
+      of Sha:
+        leaf7.ebx.checkoff(29)
+      of Avx512bw:
+        leaf7.ebx.checkoff(30)
+      of Avx512vl:
+        leaf7.ebx.checkoff(31)
+
+      # leaf 7, edx
+      of Avx512vnniw4:
+        leaf7.edx.checkoff(2)
+      of Avx512fmaps4:
+        leaf7.edx.checkoff(3)
+      of SpecCtrl:
+        leaf7.edx.checkoff(26)
+      of Stibp:
+        leaf7.edx.checkoff(27)
+      of Ssbd:
+        leaf7.edx.checkoff(31)
+
+      # leaf 8, edx
+      of Cas8B:
+        leaf8.edx.checkoff(8)
+      of NxBit:
+        leaf8.edx.checkoff(20)
+      of MmxExt:
+        leaf8.edx.checkoff(22)
+      of F3DNowExt:
+        leaf8.edx.checkoff(30)
+      of F3DNow:
+        leaf8.edx.checkoff(31)
+
+      # leaf 8, ecx
+      of Abm:
+        leaf8.ecx.checkoff(5)
+      of Sse4a:
+        leaf8.ecx.checkoff(6)
+      of F3DNowPf:
+        leaf8.ecx.checkoff(8)
+      of Fma4:
+        leaf8.ecx.checkoff(16)
