221ab2791a457a3701d03762ae55e04c1ac569e8
Arne Döring <arne.doering@gmx.net>
2019-07-26 13:02:39 +0200

    closes #10899

--- tests/misc/tlambdadonotation.nim
+++ tests/misc/tlambdadonotation.nim
@@ -10,3 +10,16 @@ proc main() =
     test = 0
 
 main()
+
+
+# issue #10899
+
+proc foo(x: proc {.closure.}) =
+  x()
+
+proc bar =
+  var x = 123
+  # foo proc = echo x     #[ ok ]#
+  foo: echo x             #[ SIGSEGV: Illegal storage access. (Attempt to read from nil?) ]#
+
+bar()
