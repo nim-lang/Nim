1a770402784ed99099f1905e1e4c201020e03bca
Arne Döring <arne.doering@gmx.net>
2019-07-27 07:21:47 +0200

    fix #11812 (#11829)
    * fixes #11812
    * fixes #10899
    * fixes #11367

--- compiler/sigmatch.nim
+++ compiler/sigmatch.nim
@@ -2117,7 +2117,7 @@ proc paramTypesMatchAux(m: var TCandidate, f, a: PType,
       # lift do blocks without params to lambdas
       let p = c.graph
       let lifted = c.semExpr(c, newProcNode(nkDo, argOrig.info, body = argOrig,
-          params = p.emptyNode, name = p.emptyNode, pattern = p.emptyNode,
+          params = nkFormalParams.newTree(p.emptyNode), name = p.emptyNode, pattern = p.emptyNode,
           genericParams = p.emptyNode, pragmas = p.emptyNode, exceptions = p.emptyNode), {})
       if f.kind == tyBuiltInTypeClass:
         inc m.genericMatches
