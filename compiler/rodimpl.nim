36c90fa8fbc72b425d1ad9845df0c4c52d3c3db3
Andreas Rumpf <rumpf_a@web.de>
2019-08-08 16:51:41 +0200

    removed unused imports [refactoring]
    (cherry picked from commit 0d5bd7e8b5b265342fad62b581c430ebabc77aa3)

--- compiler/rodimpl.nim
+++ compiler/rodimpl.nim
@@ -9,7 +9,7 @@
 
 ## This module implements the new compilation cache.
 
-import strutils, os, intsets, tables, ropes, db_sqlite, msgs, options, types,
+import strutils, intsets, tables, ropes, db_sqlite, msgs, options,
   renderer, rodutils, idents, astalgo, btrees, magicsys, cgmeth, extccomp,
   btrees, trees, condsyms, nversion, pathutils
 
