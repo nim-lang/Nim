6681319b5159d006268e3c6b4218621737873c46
narimiran <narimiran@disroot.org>
2019-08-13 13:49:52 +0200

    fix failing tests

--- tests/concepts/t3330.nim
+++ tests/concepts/t3330.nim
@@ -1,7 +1,11 @@
 discard """
-errormsg: "type mismatch: got <Bar[system.int]>"
-disabled: "32bit"
-nimout: '''
+  errormsg: "type mismatch: got <Bar[system.int]>"
+  disabled: "32bit"
+  cmd: "nim c --verbosity:0 --colors:off $file"
+  nimout: '''
+Hint: system [Processing]
+Hint: widestrs [Processing]
+Hint: io [Processing]
 t3330.nim(78, 4) Error: type mismatch: got <Bar[system.int]>
 but expected one of:
 proc test(foo: Foo[int])
@@ -13,11 +17,11 @@ proc add[T](x: var seq[T]; y: openArray[T])
   first type mismatch at position: 1
   required type for x: var seq[T]
   but expression 'k' is of type: Alias
-proc add[T](x: var seq[T]; y: T)
+proc add(result: var string; x: float)
   first type mismatch at position: 1
   required type for result: var string
   but expression 'k' is of type: Alias
-proc add(result: var string; x: float)
+proc add(x: var string; y: string)
   first type mismatch at position: 1
   required type for x: var string
   but expression 'k' is of type: Alias
@@ -25,7 +29,7 @@ proc add(x: var string; y: cstring)
   first type mismatch at position: 1
   required type for x: var string
   but expression 'k' is of type: Alias
-proc add(x: var string; y: string)
+proc add[T](x: var seq[T]; y: T)
   first type mismatch at position: 1
   required type for x: var seq[T]
   but expression 'k' is of type: Alias
@@ -43,18 +47,14 @@ t3330.nim(70, 6) Foo: 'bar.value' cannot be assigned to
 t3330.nim(63, 8) template/generic instantiation of `add` from here
 t3330.nim(71, 6) Foo: 'bar.x' cannot be assigned to
 
-expression: test(bar)'''
+expression: test(bar)
+'''
 """
 
 # Note: currently disabled on 32bit because the candidates are presented in
 # different order on travis with `NIM_COMPILE_TO_CPP=false CPU=i386`;
 # a possible fix would be to sort the candidates by proc signature or
 # declaration location
-
-
-
-
-
 
 
 ## line 60
@@ -76,4 +76,3 @@ proc test(foo: Foo[int]) =
 
 var bar = Bar[int]()
 bar.test()
-
