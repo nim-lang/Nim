b06580a8396ad78d93d110b0fcabb8d0e07f541a
Clyybber <darkmine956@gmail.com>
2019-08-17 01:43:30 +0200

    Fix test properly

--- tests/errmsgs/twrong_at_operator.nim
+++ tests/errmsgs/twrong_at_operator.nim
@@ -1,15 +1,8 @@
 discard """
 errormsg: "type mismatch: got <array[0..0, type int]>"
-line: 29
+line: 22
 nimout: '''
-Hint: used config file '/home/travis/build/nim-lang/Nim/config/nim.cfg' [Conf]
-Hint: used config file '/home/travis/build/nim-lang/Nim/config/config.nims' [Conf]
-Hint: used config file '/home/travis/build/nim-lang/Nim/tests/config.nims' [Conf]
-Hint: system [Processing]
-Hint: widestrs [Processing]
-Hint: io [Processing]
-Hint: twrong_at_operator [Processing]
-twrong_at_operator.nim(29, 30) Error: type mismatch: got <array[0..0, type int]>
+twrong_at_operator.nim(22, 30) Error: type mismatch: got <array[0..0, type int]>
 but expected one of:
 proc `@`[T](a: openArray[T]): seq[T]
   first type mismatch at position: 1
