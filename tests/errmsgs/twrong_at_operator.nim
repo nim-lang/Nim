ae416b36573c371a3edea7b503a85e7e100318e1
Timothee Cour <timothee.cour2@gmail.com>
2019-08-21 19:46:34 -0700

    fix tests

--- tests/errmsgs/twrong_at_operator.nim
+++ tests/errmsgs/twrong_at_operator.nim
@@ -6,7 +6,7 @@ twrong_at_operator.nim(22, 30) Error: type mismatch: got <array[0..0, type int]>
 but expected one of:
 proc `@`[T](a: openArray[T]): seq[T]
   first type mismatch at position: 1
-  required type for a: openarray[T]
+  required type for a: openArray[T]
   but expression '[int]' is of type: array[0..0, type int]
 proc `@`[IDX, T](a: array[IDX, T]): seq[T]
   first type mismatch at position: 1
