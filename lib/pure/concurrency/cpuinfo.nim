39fb5420baca713332a422d8eaecd4ed3e752727
awr1 <41453959+awr1@users.noreply.github.com>
2019-08-28 10:17:35 -0500

    Use return in cpuName()

--- lib/pure/concurrency/cpuinfo.nim
+++ lib/pure/concurrency/cpuinfo.nim
@@ -115,11 +115,11 @@ const
 when onX86 or defined(nimdoc):
   include "./cpuinfo_x86"
 
-proc cpuName*() =
-  when onX86:
-    cpuNameX86()
-  else:
-    ""
+proc cpuName*(): string =
+  return when onX86:
+           cpuNameX86()
+         else:
+           ""
   ## The CPU's full name, for example: `"Intel(R) Core(TM) i3-8350K CPU @
   ## 4.00GHz"`.
   ##
