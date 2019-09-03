1f57cdcb79a901fa119b0c78e6b4a6f4685048a6
awr1 <41453959+awr1@users.noreply.github.com>
2019-09-03 14:01:31 -0500

    documentation, operator change, made procs into templates

--- lib/pure/ptrarith.nim
+++ lib/pure/ptrarith.nim
@@ -10,16 +10,23 @@
 ## This module offers operators for performing arithmetic on raw pointer types
 ## in Nim. Nim is *not* C: using pointer arithmetic in Nim should be avoided (if
 ## possible, use `array[T]`, `ptr UncheckedArray[T]`, etc.), however, pointer
-## arithmetic is sometimes desirable for low-level ops/FFI with C.
+## arithmetic is sometimes desirable for low-level ops/FFI with C. **Pointer
+## arithmetic is a very sharp tool. This module is unsafe, comes without
+## guarantees** and might cause you to spend *endless hours in a debugger, eat
+## your cat or cause the heat death of the universe.*
 
-func `+`*[T](x: ptr[T]; offset: SomeInteger): ptr[T] =
+template `+%`*[T](x: ptr[T]; offset: SomeInteger): ptr[T] =
+  ## **Unsafe.** Adds `offset * sizeof(T)` bytes from the pointer `x`.
   cast[ptr[T]](cast[ByteAddress](x) + cast[ByteAddress](offset * sizeof(T)))
 
-func `-`*[T](x: ptr[T]; offset: SomeInteger): ptr[T] =
+template `-%`*[T](x: ptr[T]; offset: SomeInteger): ptr[T] =
+  ## **Unsafe.** Subtracts `offset * sizeof(T)` bytes from the pointer `x`.
   cast[ptr[T]](cast[ByteAddress](x) - cast[ByteAddress](offset * sizeof(T)))
 
-func `+=`*[T](x: var ptr[T]; offset: SomeInteger) =
-  x = x + offset
+template `+%=`*[T](x: var ptr[T]; offset: SomeInteger) =
+  ## **Unsafe.** Incremeents `offset * sizeof(T)` bytes from the pointer `x`.
+  x = x +% offset
 
-func `-=`*[T](x: var ptr[T]; offset: SomeInteger) =
-  x = x - offset
+template `-%=`*[T](x: var ptr[T]; offset: SomeInteger) =
+  ## **Unsafe.** Decrements `offset * sizeof(T)` bytes from the pointer `x`.
+  x = x -% offset
--- tests/stdlib/tptrarith.nim
+++ tests/stdlib/tptrarith.nim
@@ -4,17 +4,17 @@ import ptrarith
 var x = [2'i32, 5'i32, 8'i32, 4'i32, 12'i32]
 var y = addr x[0]
 
-doAssert((y + 1)[] == 5'i32)
-doAssert((y + 2)[] == 8'i32)
-doAssert((y + 3)[] == 4'i32)
-doAssert((y + 4)[] == 12'i32)
+doAssert((y +% 1)[] == 5'i32)
+doAssert((y +% 2)[] == 8'i32)
+doAssert((y +% 3)[] == 4'i32)
+doAssert((y +% 4)[] == 12'i32)
 
-y += 1
+y +%= 1
 
 doAssert(y[] == 5'i32)
-doAssert((y - 1)[] == 2'i32)
+doAssert((y -% 1)[] == 2'i32)
 
-y -= 1
+y -%= 1
 
 doAssert(y[] == 2'i32)
 
