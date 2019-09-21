fcc699789e4c2ef5b9a1569ea81117089b592773
PMunch <peterme@peterme.net>
2019-09-21 17:57:17 +0200

    Use longer timeouts to be more lenient in checking

--- tests/async/t12221.nim
+++ tests/async/t12221.nim
@@ -1,37 +1,37 @@
 import asyncdispatch, os, times
 
 proc doubleSleep(hardSleep: int) {.async.} =
-  await sleepAsync(100)
+  await sleepAsync(200)
   sleep(hardSleep)
 
 template assertTime(target, timeTook: float): untyped {.dirty.} =
-  assert(timeTook*1000 > target - 5, "Took too short, should've taken " &
+  assert(timeTook*1000 > target - 10, "Took too short, should've taken " &
     $target & "ms, but took " & $(timeTook*1000) & "ms")
-  assert(timeTook*1000 < target + 5, "Took too long, should've taken " &
+  assert(timeTook*1000 < target + 10, "Took too long, should've taken " &
     $target & "ms, but took " & $(timeTook*1000) & "ms")
 
 var start: float
 
 start = epochTime()
-waitFor sleepAsync(50) and sleepAsync(150) and doubleSleep(40)
-assertTime(150, epochTime() - start)
+waitFor sleepAsync(100) and sleepAsync(300) and doubleSleep(80)
+assertTime(300, epochTime() - start)
 
 start = epochTime()
-waitFor sleepAsync(50) and sleepAsync(150) and doubleSleep(60)
-assertTime(160, epochTime() - start)
+waitFor sleepAsync(100) and sleepAsync(300) and doubleSleep(120)
+assertTime(320, epochTime() - start)
 
 start = epochTime()
-waitFor sleepAsync(50) and sleepAsync(150) and doubleSleep(40) and sleepAsync(200)
-assertTime(200, epochTime() - start)
+waitFor sleepAsync(100) and sleepAsync(300) and doubleSleep(80) and sleepAsync(400)
+assertTime(400, epochTime() - start)
 
 start = epochTime()
-waitFor sleepAsync(50) and sleepAsync(150) and doubleSleep(60) and sleepAsync(200)
-assertTime(200, epochTime() - start)
+waitFor sleepAsync(100) and sleepAsync(300) and doubleSleep(120) and sleepAsync(400)
+assertTime(400, epochTime() - start)
 
 start = epochTime()
-waitFor (sleepAsync(50) and sleepAsync(150) and doubleSleep(40)) or sleepAsync(700)
-assertTime(150, epochTime() - start)
+waitFor (sleepAsync(100) and sleepAsync(300) and doubleSleep(80)) or sleepAsync(1400)
+assertTime(300, epochTime() - start)
 
 start = epochTime()
-waitFor (sleepAsync(50) and sleepAsync(150) and doubleSleep(60)) or sleepAsync(700)
-assertTime(160, epochTime() - start)
+waitFor (sleepAsync(100) and sleepAsync(300) and doubleSleep(120)) or sleepAsync(1400)
+assertTime(320, epochTime() - start)
