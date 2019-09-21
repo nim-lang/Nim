666704ba71e0c82a7e59bb072745b688f4fd8383
PMunch <peterme@peterme.net>
2019-09-21 19:39:59 +0200

    Increase poll timeout to make it easier to detect mistakes

--- tests/async/t12221.nim
+++ tests/async/t12221.nim
@@ -5,33 +5,48 @@ proc doubleSleep(hardSleep: int) {.async.} =
   sleep(hardSleep)
 
 template assertTime(target, timeTook: float): untyped {.dirty.} =
-  assert(timeTook*1000 > target - 150, "Took too short, should've taken " &
+  assert(timeTook*1000 > target - 1000, "Took too short, should've taken " &
     $target & "ms, but took " & $(timeTook*1000) & "ms")
-  assert(timeTook*1000 < target + 150, "Took too long, should've taken " &
+  assert(timeTook*1000 < target + 1000, "Took too long, should've taken " &
     $target & "ms, but took " & $(timeTook*1000) & "ms")
 
-var start: float
+var
+  start: float
+  fut: Future[void]
 
+# NOTE: this uses poll(3000) to limit timing error potential.
 start = epochTime()
-waitFor sleepAsync(50) and sleepAsync(150) and doubleSleep(40)
+fut = sleepAsync(50) and sleepAsync(150) and doubleSleep(40)
+while not fut.finished:
+  poll(3000)
 assertTime(150, epochTime() - start)
 
 start = epochTime()
-waitFor sleepAsync(50) and sleepAsync(150) and doubleSleep(100)
+fut = sleepAsync(50) and sleepAsync(150) and doubleSleep(100)
+while not fut.finished:
+  poll(3000)
 assertTime(200, epochTime() - start)
 
 start = epochTime()
-waitFor sleepAsync(50) and sleepAsync(150) and doubleSleep(40) and sleepAsync(300)
+fut = sleepAsync(50) and sleepAsync(150) and doubleSleep(40) and sleepAsync(300)
+while not fut.finished:
+  poll(3000)
 assertTime(300, epochTime() - start)
 
 start = epochTime()
-waitFor sleepAsync(50) and sleepAsync(150) and doubleSleep(100) and sleepAsync(300)
+fut = sleepAsync(50) and sleepAsync(150) and doubleSleep(100) and sleepAsync(300)
+while not fut.finished:
+  poll(3000)
 assertTime(300, epochTime() - start)
 
 start = epochTime()
-waitFor (sleepAsync(50) and sleepAsync(150) and doubleSleep(40)) or sleepAsync(700)
+fut = (sleepAsync(50) and sleepAsync(150) and doubleSleep(40)) or sleepAsync(700)
+while not fut.finished:
+  poll(3000)
 assertTime(150, epochTime() - start)
 
 start = epochTime()
-waitFor (sleepAsync(50) and sleepAsync(150) and doubleSleep(100)) or sleepAsync(700)
+fut = (sleepAsync(50) and sleepAsync(150) and doubleSleep(100)) or sleepAsync(700)
+while not fut.finished:
+  poll(3000)
 assertTime(200, epochTime() - start)
