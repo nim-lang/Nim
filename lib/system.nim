6b4b329f45ce23fc7590c2c9fb3cc50715b266a1
narimiran <narimiran@disroot.org>
2019-09-25 09:14:38 +0200

    devel version is 1.0.99 [ci skip]

--- lib/system.nim
+++ lib/system.nim
@@ -3543,7 +3543,7 @@ const
   NimMinor* {.intdefine.}: int = 0
     ## is the minor number of Nim's version.
 
-  NimPatch* {.intdefine.}: int = 0
+  NimPatch* {.intdefine.}: int = 99
     ## is the patch number of Nim's version.
 
   NimVersion*: string = $NimMajor & "." & $NimMinor & "." & $NimPatch
