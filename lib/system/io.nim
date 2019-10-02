ad13e18c7cd008da418770b7f1b8eae696992ced
zah <zahary@gmail.com>
2019-10-02 21:34:03 +0300

    `system.writeFile` has been overloaded to also support `openarray[byte]` (#12313)

--- lib/system/io.nim
+++ lib/system/io.nim
@@ -679,6 +679,18 @@ proc writeFile*(filename, content: string) {.tags: [WriteIOEffect], benign.} =
   else:
     sysFatal(IOError, "cannot open: " & filename)
 
+proc writeFile*(filename: string, content: openArray[byte]) =
+  ## Opens a file named `filename` for writing. Then writes the
+  ## `content` completely to the file and closes the file afterwards.
+  ## Raises an IO exception in case of an error.
+  var f: File
+  if open(f, filename, fmWrite):
+    try:
+      f.writeBuffer(unsafeAddr content[0], content.len)
+    finally:
+      close(f)
+  else:
+    raise newException(IOError, "cannot open: " & filename)
 
 proc readLines*(filename: string, n = 1.Natural): seq[TaintedString] =
   ## read `n` lines from the file named `filename`. Raises an IO exception
