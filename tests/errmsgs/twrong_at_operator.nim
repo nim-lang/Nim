383852cb3b97629c64997e9fb6b3bcc4d130cdbb
Jasper Jenkins <jasper.vs.jenkins@gmail.com>
2019-10-05 06:13:18 -0700

    fix/remove tests

--- tests/errmsgs/twrong_at_operator.nim
+++ tests/errmsgs/twrong_at_operator.nim
@@ -4,13 +4,13 @@ line: 22
 nimout: '''
 twrong_at_operator.nim(22, 30) Error: type mismatch: got <array[0..0, type int]>
 but expected one of:
-proc `@`[T](a: openArray[T]): seq[T]
-  first type mismatch at position: 1
-  required type for a: openArray[T]
-  but expression '[int]' is of type: array[0..0, type int]
 proc `@`[IDX, T](a: sink array[IDX, T]): seq[T]
   first type mismatch at position: 1
   required type for a: sink array[IDX, T]
+  but expression '[int]' is of type: array[0..0, type int]
+proc `@`[T](a: openArray[T]): seq[T]
+  first type mismatch at position: 1
+  required type for a: openArray[T]
   but expression '[int]' is of type: array[0..0, type int]
 
 expression: @[int]
