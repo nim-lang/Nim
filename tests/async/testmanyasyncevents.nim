b347490f9160bf02dda5333cbc76817e0dcbce50
Ray Imber <rayimber@gmail.com>
2019-10-07 10:06:42 -0700

    lowered the number of events in the test because some CI's have an extremely low FD limit

--- tests/async/testmanyasyncevents.nim
+++ tests/async/testmanyasyncevents.nim
@@ -1,7 +1,7 @@
 discard """
 output: '''
 hasPendingOperations: false
-triggerCount: 512 
+triggerCount: 100 
 '''
 """
 
@@ -10,7 +10,7 @@ import asyncDispatch
 var triggerCount = 0
 var evs = newSeq[AsyncEvent]()
 
-for i in 0 ..< 512: # has to be lower than the typical physical fd limit
+for i in 0 ..< 100: # has to be lower than the typical physical fd limit
   var ev = newAsyncEvent()
   evs.add(ev)
   addEvent(ev, proc(fd: AsyncFD): bool {.gcsafe,closure.} = triggerCount += 1; true)
