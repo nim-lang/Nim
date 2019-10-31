634dbee20a533fc637baf7de2e00bc20930868b1
nim_lang_cn <lxdong.007@gmail.com>
2019-10-31 11:24:50 +0800

    change foldl to support default value in scene like while true:readLine(stdin).splitWhitespace.filterIt(it in 0..9).mapIt(parseFloat it).foldl(a+b).echo when user doesn't input anything

--- lib/pure/collections/sequtils.nim
+++ lib/pure/collections/sequtils.nim
@@ -720,7 +720,7 @@ template foldl*(sequence, operation: untyped): untyped =
   let s = sequence
   # assert s.len > 0, "Can't fold empty sequences"
   var result: type(s[0])
-  # result = s[0]
+  if s.len > 0: result = s[0]
   for i in 1..<s.len:
     let
       a {.inject.} = result
