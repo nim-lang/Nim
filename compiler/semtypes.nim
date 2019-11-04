91ff3421fbda0f5333084768b2c1e8fcfb915b6a
Clyybber <darkmine956@gmail.com>
2019-11-04 14:47:48 +0100

    Hide backend improvements :(

--- compiler/semtypes.nim
+++ compiler/semtypes.nim
@@ -635,12 +635,11 @@ proc semRecordCase(c: PContext, n: PNode, check: var IntSet, pos: var int,
     errorUndeclaredIdentifier(c, n.sons[0].info, typ.sym.name.s)
   elif not isOrdinalType(typ):
     localError(c.config, n.sons[0].info, "selector must be of an ordinal type, float or string")
-  if optTinyRtti notin c.config.globalOptions:
-    if firstOrd(c.config, typ) != 0:
-      localError(c.config, n.info, "low(" & $a.sons[0].sym.name.s &
-                                   ") must be 0 for discriminant")
-    elif lengthOrd(c.config, typ) > 0x00007FFF:
-      localError(c.config, n.info, "len($1) must be less than 32768" % a.sons[0].sym.name.s)
+  if firstOrd(c.config, typ) != 0:
+    localError(c.config, n.info, "low(" & $a.sons[0].sym.name.s &
+                                 ") must be 0 for discriminant")
+  elif lengthOrd(c.config, typ) > 0x00007FFF:
+    localError(c.config, n.info, "len($1) must be less than 32768" % a.sons[0].sym.name.s)
 
   for i in 1 ..< len(n):
     var b = copyTree(n.sons[i])
