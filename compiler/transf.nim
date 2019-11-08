e861adb81fb83b5585130d8c85985fbff14cdc39
Andreas Rumpf <rumpf_a@web.de>
2019-11-08 16:32:57 +0100

    do not cache the noDestructors transformation

--- compiler/transf.nim
+++ compiler/transf.nim
@@ -1117,7 +1117,7 @@ proc transformBody*(g: ModuleGraph, prc: PSym, cache = true;
                     noDestructors = false): PNode =
   assert prc.kind in routineKinds
 
-  if prc.transformedBody != nil:
+  if prc.transformedBody != nil and not noDestructors:
     result = prc.transformedBody
   elif nfTransf in prc.ast[bodyPos].flags or prc.kind in {skTemplate}:
     result = prc.ast[bodyPos]
@@ -1137,8 +1137,7 @@ proc transformBody*(g: ModuleGraph, prc: PSym, cache = true;
 
     incl(result.flags, nfTransf)
 
-    let cache = cache or prc.typ.callConv == ccInline
-    if cache:
+    if cache or prc.typ.callConv == ccInline:
       # genProc for inline procs will be called multiple times from different modules,
       # it is important to transform exactly once to get sym ids and locations right
       prc.transformedBody = result
