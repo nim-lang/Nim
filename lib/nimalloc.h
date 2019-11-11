c22c3db9adb851c3dab370b5178e0ae580157a99
Arne Döring <arne.doering@gmx.net>
2019-11-11 15:04:16 +0100

    use alignas

--- lib/nimalloc.h
+++ lib/nimalloc.h
@@ -1,4 +1,7 @@
+#include <stdalign.h>
 
 // this type is used in alloc.nim. It is intended to ensure 16 byte
 // alignments for all allocations.
-typedef struct {} __attribute__ ((aligned (16))) NimAlign16Type;
+typedef struct {
+  alignas(16) char unused;
+} NimAlign16Type;
