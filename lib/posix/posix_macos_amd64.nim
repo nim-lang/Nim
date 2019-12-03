fb29737fb2d64df2cd49832b1909e21150e75237
Timothee Cour <timothee.cour2@gmail.com>
2019-12-02 18:16:42 -0600

    osx: support nanosecond resolution for file stat (eg getLastModificationTime)

--- lib/posix/posix_macos_amd64.nim
+++ lib/posix/posix_macos_amd64.nim
@@ -217,14 +217,9 @@ type
                           ## For a typed memory object, the length in bytes.
                           ## For other file types, the use of this field is
                           ## unspecified.
-    when StatHasNanoseconds:
-      st_atim*: Timespec  ## Time of last access.
-      st_mtim*: Timespec  ## Time of last data modification.
-      st_ctim*: Timespec  ## Time of last status change.
-    else:
-      st_atime*: Time     ## Time of last access.
-      st_mtime*: Time     ## Time of last data modification.
-      st_ctime*: Time     ## Time of last status change.
+    st_atim* {.importc:"st_atimespec".}: Timespec  ## Time of last access.
+    st_mtim* {.importc:"st_mtimespec".}: Timespec  ## Time of last data modification.
+    st_ctim*  {.importc:"st_ctimespec".}: Timespec  ## Time of last status change.
     st_blksize*: Blksize  ## A file system-specific preferred I/O block size
                           ## for this object. In some file system types, this
                           ## may vary from file to file.
