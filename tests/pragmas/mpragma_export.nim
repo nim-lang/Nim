9c22cc897eb486d0e8b7ea794d4f837200ffdd94
Timothee Cour <timothee.cour2@gmail.com>
2020-01-03 19:05:01 -0800

    add tests

--- tests/pragmas/mpragma_export.nim
+++ tests/pragmas/mpragma_export.nim
@@ -1,18 +1,9 @@
-template myfoo2* = {.exportc.}
-template myfoo3* = {.exportc: "myfoo3_in_c", discardable.}
-template myfoo4* = {.discardable, myfoo2.}
-template myfoo5 = {. .}
+{.pragma: myfoo2, exportc.}
+{.pragma: myfoo3, exportc: "myfoo3_in_c", discardable.}
+{.pragma: myfoo4, discardable, myfoo2.}
+{.pragma: myfoo5.}
+{.pragma: myfoo0, exportc: "myfoo0_in_c".}
+{.pragma: myfoo6, myfoo0, discardable.}
 
-when false:
-  # can't use classical pragma inside template pragma
-  {.pragma: myfoo0, exportc: "myfoo0_in_c".}
-elif false:
-  # can't use non exported template pragma inside exported template pragma
-  template myfoo0 = {.exportc: "myfoo0_in_c".}
-else:
-  # works
-  template myfoo0* = {.exportc: "myfoo0_in_c".}
-
-template myfoo6* = {.myfoo0, discardable.}
-
-export myfoo5
+export myfoo0 # needed if appears in an exported pragma; ideally should not be needed
+export myfoo2, myfoo3, myfoo4, myfoo5, myfoo6
