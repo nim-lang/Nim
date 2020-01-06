4af2fa62deee02646ea41f551632e914765d69fc
Timothee Cour <timothee.cour2@gmail.com>
2020-01-05 18:36:43 -0800

    move doc comments to module level

--- lib/std/wrapnils.nim
+++ lib/std/wrapnils.nim
@@ -1,23 +1,27 @@
+## This module allows chains of field-access and indexing where the LHS can be nil.
+## This simplifies code by reducing need for if-else branches around intermediate values
+## that maybe be nil.
+##
+## Note: experimental module and relies on {.experimental: "dotOperators".}
+## Unstable API.
+
+runnableExamples:
+  type Foo = ref object
+    x1: string
+    x2: Foo
+  var f: Foo
+  assert f.wrapnil.x2.x1[] == ""
+  assert Foo(x1: "a").wrapnil.x1[] == "a"
+
 type Wrapnil*[T] = object
   valueImpl*: T
 
 proc wrapnil*[T](a: T): Wrapnil[T] =
-  ## Allows chains of field-access and indexing where the LHS can be nil.
-  ## This simplifies code by reducing need for if-else branches around intermediate
-  ## maybe nil values.
-  ## Note: experimental module and relies on {.experimental: "dotOperators".}
-  runnableExamples:
-    type Foo = ref object
-      x1: string
-      x2: Foo
-    var f: Foo
-    assert f.wrapnil.x2.x1[] == ""
   Wrapnil[T](valueImpl: a)
 
 {.push experimental: "dotOperators".}
 
 template `.`*(a: Wrapnil, b): untyped =
-  ## See `wrapnil`
   let a2 = a.valueImpl # to avoid double evaluations
   when type(a2) is ref|ptr:
     if a2 == nil:
@@ -30,11 +34,9 @@ template `.`*(a: Wrapnil, b): untyped =
 {.pop.}
 
 template `[]`*(a: Wrapnil): untyped =
-  ## See `wrapnil`
   a.valueImpl
 
 template `[]`*[I](a: Wrapnil, i: I): untyped =
-  ## See `wrapnil`
   let a2 = a.valueImpl # to avoid double evaluations
   if len(a2) == 0:
     wrapnil(default(type(a2[i])))
