8f7539e765e56c2b92d4e3a7a3aa3d1d704cec93
Timothee Cour <timothee.cour2@gmail.com>
2020-01-06 23:58:51 -0800

    workaround bug #3171

--- tests/pragmas/tsym_as_pragma.nim
+++ tests/pragmas/tsym_as_pragma.nim
@@ -1,8 +1,9 @@
 
 # bug #3171
+when false:
+  # now getting: Error: invalid pragma: closure`gensym272010
+  template newDataWindow(): untyped =
+      let eventClosure = proc (closure: pointer): bool {.closure, cdecl.} =
+          discard
 
-template newDataWindow(): untyped =
-    let eventClosure = proc (closure: pointer): bool {.closure, cdecl.} =
-        discard
-
-newDataWindow()
+  newDataWindow()
