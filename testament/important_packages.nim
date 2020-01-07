6a7a19eb15d3878bc97a7ea729d981124628f3ad
loloiccl <oxisccl@gmail.com>
2020-01-07 14:47:40 +0900

    Modify the test command for nimly (nimble-package) (#13053)

--- testament/important_packages.nim
+++ testament/important_packages.nim
@@ -54,7 +54,7 @@ pkg "nimfp", "nim c -o:nfp -r src/fp.nim", true
 pkg "nimgame2", "nim c nimgame2/nimgame.nim", true
 pkg "nimgen", "nim c -o:nimgenn -r src/nimgen/runcfg.nim", true
 # pkg "nimlsp", "", true
-pkg "nimly", "nim c -r tests/test_nimly", true
+pkg "nimly", "", true
 # pkg "nimongo", "nimble test_ci", true
 pkg "nimpy", "nim c -r tests/nimfrompy.nim"
 pkg "nimquery"
