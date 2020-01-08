beb3d11da53d6fda7d64c95ab3de831413c4ec9c
Timothee Cour <timothee.cour2@gmail.com>
2020-01-08 00:31:00 -0800

    fixup

--- lib/system/stats_types_new.nim
+++ lib/system/stats_types_new.nim
@@ -2,6 +2,10 @@ proc `$`(t: typedesc): string {.magic: "TypeTrait".}
 
 proc newImpl[T](a: var ref T) {.magic: "New", noSideEffect.}
 
+{.push profiler: off.}
+let nimvm {.magic: "Nimvm", compileTime.}: bool = false
+{.pop.}
+
 type SysCounter* = object
   name*: cstring
   size*: int
@@ -16,30 +20,35 @@ proc new*[T](a: var ref T) {.noSideEffect.} =
 
   # TODO: how do get a unique hash (in case of name collisions for different types T)
   const name = $T
-  var i2 = 0
-  {.noSideEffect.}:
-    while true:
-      if i2 == sysCountersLen:
-        sysCounters[sysCountersLen] = SysCounter(name: name.cstring, nameLen: name.len, size: T.sizeof)
-        if sysCountersLen < sysCounters.len - 1:
-          sysCountersLen.inc
-        break
 
-      var ok = true
-      # TODO: strncmp ?
-      if sysCounters[i2].nameLen == name.len:
-        var j = 0
-        while true:
-          if j >= name.len:
-            break
-          if name[j] != sysCounters[i2].name[j]:
-            ok = false
-            break
-          j.inc
-        if ok:
+  when nimvm:
+    # TODO: support
+    discard
+  else:
+    var i2 = 0
+    {.noSideEffect.}:
+      while true:
+        if i2 == sysCountersLen:
+          sysCounters[sysCountersLen] = SysCounter(name: name.cstring, nameLen: name.len, size: T.sizeof)
+          if sysCountersLen < sysCounters.len - 1:
+            sysCountersLen.inc
           break
-      i2.inc
-    if i2 < sysCounters.len:
-      sysCounters[i2].count.inc
+
+        var ok = true
+        # TODO: strncmp ?
+        if sysCounters[i2].nameLen == name.len:
+          var j = 0
+          while true:
+            if j >= name.len:
+              break
+            if name[j] != sysCounters[i2].name[j]:
+              ok = false
+              break
+            j.inc
+          if ok:
+            break
+        i2.inc
+      if i2 < sysCounters.len:
+        sysCounters[i2].count.inc
 
   newImpl(a)
