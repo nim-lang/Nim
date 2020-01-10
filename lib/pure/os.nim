04bed16fc4089f608d72cd868d6e437b4459c17a
Andy Davidoff <andy+github@disruptek.com>
2020-01-10 14:06:23 -0500

    do it different

--- lib/pure/os.nim
+++ lib/pure/os.nim
@@ -82,14 +82,7 @@ type
 
 include "includes/osseps"
 
-proc normalizePathEnd(path: var string, trailingSep = false) =
-  ## Ensures ``path`` has exactly 0 or 1 trailing `DirSep`, depending on
-  ## ``trailingSep``, and taking care of edge cases: it preserves whether
-  ## a path is absolute or relative, and makes sure trailing sep is `DirSep`,
-  ## not `AltSep`.
-  ## See also:
-  ## * `normalizePathEnd proc <#normalizePathEnd,string,bool>`_ for a version
-  ## which returns a new string.
+template normalizePathEndImpl(path: var string; trailingSep: bool) =
   if path.len == 0: return
   var i = path.len
   while i >= 1 and path[i-1] in {DirSep, AltSep}: dec(i)
@@ -105,24 +98,59 @@ proc normalizePathEnd(path: var string, trailingSep = false) =
     # // => / (empty case was already taken care of)
     path = $DirSep
 
-proc normalizePathEnd(path: string, trailingSep = false): string =
-  ## A version of ``normalizePathEnd()``
-  ## See also:
-  ## * `normalizePathEnd proc <#normalizePathEnd,string,bool>`_ for the
-  ## in-place version.
-  runnableExamples:
-    when defined(posix):
-      assert normalizePathEnd("/lib/", trailingSep = true) == "/lib/"
-      assert normalizePathEnd("", trailingSep = true) == "/"
-      assert normalizePathEnd("/lib", trailingSep = false) == "/lib"
-      assert normalizePathEnd("lib//", trailingSep = false) == "lib"
-      assert normalizePathEnd("lib", trailingSep = false) == "lib"
-      assert normalizePathEnd("/lib//", trailingSep = true) == "/lib/"
-  result = path
-  result.normalizePathEnd(trailingSep)
-
 when (NimMajor, NimMinor) >= (1, 1):
-  export normalizePathEnd
+  proc normalizePathEnd*(path: var string, trailingSep = false) =
+    ## Ensures ``path`` has exactly 0 or 1 trailing `DirSep`, depending on
+    ## ``trailingSep``, and taking care of edge cases: it preserves whether
+    ## a path is absolute or relative, and makes sure trailing sep is `DirSep`,
+    ## not `AltSep`.
+    ## See also:
+    ## * `normalizePathEnd proc <#normalizePathEnd,string,bool>`_ for a version
+    ## which returns a new string.
+    normalizePathEndImpl(path, trailingSep = trailingSep)
+
+  proc normalizePathEnd*(path: string, trailingSep = false): string =
+    ## A version of ``normalizePathEnd()``
+    ## See also:
+    ## * `normalizePathEnd proc <#normalizePathEnd,string,bool>`_ for the
+    ## in-place version.
+    runnableExamples:
+      when defined(posix):
+        assert normalizePathEnd("/lib/", trailingSep = true) == "/lib/"
+        assert normalizePathEnd("", trailingSep = true) == "/"
+        assert normalizePathEnd("/lib", trailingSep = false) == "/lib"
+        assert normalizePathEnd("lib//", trailingSep = false) == "lib"
+        assert normalizePathEnd("lib", trailingSep = false) == "lib"
+        assert normalizePathEnd("/lib//", trailingSep = true) == "/lib/"
+    result = path
+    result.normalizePathEnd(trailingSep)
+
+else:
+  proc normalizePathEnd(path: var string, trailingSep = false) =
+    ## Ensures ``path`` has exactly 0 or 1 trailing `DirSep`, depending on
+    ## ``trailingSep``, and taking care of edge cases: it preserves whether
+    ## a path is absolute or relative, and makes sure trailing sep is `DirSep`,
+    ## not `AltSep`.
+    ## See also:
+    ## * `normalizePathEnd proc <#normalizePathEnd,string,bool>`_ for a version
+    ## which returns a new string.
+    normalizePathEndImpl(path, trailingSep = trailingSep)
+
+  proc normalizePathEnd(path: string, trailingSep = false): string =
+    ## A version of ``normalizePathEnd()``
+    ## See also:
+    ## * `normalizePathEnd proc <#normalizePathEnd,string,bool>`_ for the
+    ## in-place version.
+    runnableExamples:
+      when defined(posix):
+        assert normalizePathEnd("/lib/", trailingSep = true) == "/lib/"
+        assert normalizePathEnd("", trailingSep = true) == "/"
+        assert normalizePathEnd("/lib", trailingSep = false) == "/lib"
+        assert normalizePathEnd("lib//", trailingSep = false) == "lib"
+        assert normalizePathEnd("lib", trailingSep = false) == "lib"
+        assert normalizePathEnd("/lib//", trailingSep = true) == "/lib/"
+    result = path
+    result.normalizePathEnd(trailingSep)
 
 proc joinPath*(head, tail: string): string {.
   noSideEffect, rtl, extern: "nos$1".} =
