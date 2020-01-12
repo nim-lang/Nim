cda97f1532849a85fcb891a510ac5632ee43e7a4
Timothee Cour <timothee.cour2@gmail.com>
2020-01-11 17:53:10 -0800

    fixup

--- tests/stdlib/tstring.nim
+++ tests/stdlib/tstring.nim
@@ -1,8 +1,3 @@
-discard """
-  output: '''OK
-@[@[], @[], @[], @[], @[]]
-'''
-"""
 const characters = "abcdefghijklmnopqrstuvwxyz"
 const numbers = "1234567890"
 
@@ -78,16 +73,4 @@ proc test_string_cmp() =
 
 test_string_slice()
 test_string_cmp()
-
-
-#--------------------------
-# bug #7816
-import sugar
-import sequtils
-
-proc tester[T](x: T) =
-  let test = toSeq(0..4).map(i => newSeq[int]())
-  echo test
-
-tester(1)
 
