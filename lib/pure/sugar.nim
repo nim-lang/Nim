6b48b9d554ced9d5aad8479833b70500455e3840
Timothee Cour <timothee.cour2@gmail.com>
2020-01-19 16:55:39 -0800

    _

--- lib/pure/sugar.nim
+++ lib/pure/sugar.nim
@@ -369,7 +369,7 @@ proc splitDefinition*(def: NimNode): tuple[lhs: NimNode, rhs: NimNode, exported:
   ## allows library constructs such as:
   ## `byAddr: a2=expr`
   ## `byAddr: a2*=expr` (to indicate `export`)
-  doAssert def.kind == nnkStmtList and def.len == 1
+  doAssert def.kind == nnkStmtList and def.len == 1, $(def.repr, def.treeRepr)
   let def2 = def[0]
   case def2.kind
   of nnkInfix:
