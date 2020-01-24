4cbeddddcc4cde2fb467bb7a3a30114474ae1565
Timothee Cour <timothee.cour2@gmail.com>
2020-01-24 11:06:54 -0800

    nim dump: add libpath (#13249)

--- compiler/main.nim
+++ compiler/main.nim
@@ -306,6 +306,7 @@ proc mainCommand*(graph: ModuleGraph) =
       var dumpdata = %[
         (key: "version", val: %VersionAsString),
         (key: "prefixdir", val: %conf.getPrefixDir().string),
+        (key: "libpath", val: %conf.libpath.string),
         (key: "project_path", val: %conf.projectFull.string),
         (key: "defined_symbols", val: definedSymbols),
         (key: "lib_paths", val: %libpaths),
