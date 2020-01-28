b1ca628a699153844c8806df5afaab42e0662815
Andrii Riabushenko <cdome@bk.ru>
2020-01-28 10:39:48 +0000

    fixes ##13281

--- tests/ccgbugs/tcgbug.nim
+++ tests/ccgbugs/tcgbug.nim
@@ -2,6 +2,7 @@ discard """
 output: '''
 success
 M1 M2
+ok
 '''
 """
 
@@ -58,6 +59,14 @@ type
       of M2: b:float
       of M3: c:cstring
 
+  Helper* {.exportc: "PublicHelper".} = object
+    case isKind: bool
+      of true:
+        formatted: string
+      of false:
+        parsed1: string
+        parsed2: string
+
 proc newMyObject(kind: MyKind, val: string): MyObject =
   result = MyObject(kind: kind)
 
@@ -75,3 +84,9 @@ proc newMyObjectRef(kind: MyKind, val: string): MyObjectRef =
 
 
 echo newMyObject(M1, "2").kind, " ", newMyObjectRef(M2, "3").kind
+
+
+proc test(c: Helper): string =
+  c.formatted
+
+echo test(Helper(isKind: true, formatted: "ok"))
