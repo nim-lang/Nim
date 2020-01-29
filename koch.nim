c351ce89de3b93e632f80902b177ea744dddc1b3
Timothee Cour <timothee.cour2@gmail.com>
2020-01-29 10:57:11 -0800

    fixup

--- koch.nim
+++ koch.nim
@@ -486,9 +486,7 @@ proc runCI(cmd: string) =
   kochExecFold("Build Nimble", "nimble")
 
   ## test -d:nimHasLibFFI
-  ## pending https://github.com/Araq/libffi/pull/4
-  # execFold("nimble install -y libffi", "nimble install -y libffi")
-  execFold("nimble install -y https://github.com/timotheecour/libffi@9617720caba60ebd458fc1470f7b49ba19d32006", "nimble install -y libffi")
+  execFold("nimble install -y libffi", "nimble install -y libffi")
   kochExecFold("boot -d:release -d:nimHasLibFFI", "boot -d:release -d:nimHasLibFFI")
 
   if getEnv("NIM_TEST_PACKAGES", "false") == "true":
