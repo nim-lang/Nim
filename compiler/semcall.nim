5fcade639230c1469c6c4b0c001747032fc60848
Andrii Riabushenko <cdome@bk.ru>
2020-01-30 23:13:41 +0000

    revert semcall changes

--- compiler/semcall.nim
+++ compiler/semcall.nim
@@ -657,8 +657,7 @@ proc searchForBorrowProc(c: PContext, startScope: PScope, fn: PSym): PSym =
       x = t.baseOfDistinct
     call.add(newNodeIT(nkEmpty, fn.info, x))
   if hasDistinct:
-    let kind = if fn.kind == skProc: {skProc, skFunc} else: {fn.kind}
-    var resolved = semOverloadedCall(c, call, call, kind, {})
+    var resolved = semOverloadedCall(c, call, {fn.kind}, kind, {})
     if resolved != nil:
       result = resolved[0].sym
       if not compareTypes(result.typ[0], fn.typ[0], dcEqIgnoreDistinct):
