d5e035241de95e6970daec6204719949191b4fe6
Andrii Riabushenko <cdome@bk.ru>
2020-02-08 11:15:24 +0000

    fix test

--- nimsuggest/tests/tgeneric_highlight.nim
+++ nimsuggest/tests/tgeneric_highlight.nim
@@ -6,12 +6,12 @@ discard """
 $nimsuggest --tester $file
 >highlight $1
 highlight;;skType;;1;;7;;3
-highlight;;skFunc;;1;;0;;6
+highlight;;skProc;;1;;0;;6
 highlight;;skFunc;;1;;0;;6
 highlight;;skType;;1;;7;;3
 highlight;;skProc;;1;;0;;6
 highlight;;skType;;2;;14;;3
-highlight;;skFunc;;2;;7;;6
+highlight;;skProc;;2;;7;;6
 highlight;;skFunc;;2;;7;;6
 highlight;;skProc;;2;;14;;3
 highlight;;skProc;;2;;7;;6
