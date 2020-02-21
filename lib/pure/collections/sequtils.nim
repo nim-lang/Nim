e05aca8734702f0ad4beb0625780d354d7168bc7
Timothee Cour <timothee.cour2@gmail.com>
2020-02-21 08:26:52 -0800

    make unzip faster: seq[i]=val can be 7X faster than seq.add(elem) (#13448)

--- lib/pure/collections/sequtils.nim
+++ lib/pure/collections/sequtils.nim
@@ -290,11 +290,11 @@ proc unzip*[S, T](s: openArray[(S, T)]): (seq[S], seq[T]) {.since: (1, 1).} =
       unzipped2 = @['a', 'b', 'c']
     assert zipped.unzip() == (unzipped1, unzipped2)
     assert zip(unzipped1, unzipped2).unzip() == (unzipped1, unzipped2)
-  result[0] = newSeqOfCap[S](s.len)
-  result[1] = newSeqOfCap[T](s.len)
-  for elem in s:
-    result[0].add(elem[0])
-    result[1].add(elem[1])
+  result[0] = newSeq[S](s.len)
+  result[1] = newSeq[T](s.len)
+  for i in 0..<s.len:
+    result[0][i] = s[i][0]
+    result[1][i] = s[i][1]
 
 proc distribute*[T](s: seq[T], num: Positive, spread = true): seq[seq[T]] =
   ## Splits and distributes a sequence `s` into `num` sub-sequences.
