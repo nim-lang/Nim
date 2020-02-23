160cf12233b11132fb93dce15c875e077bc3be5d
hlaaftana <10591326+hlaaftana@users.noreply.github.com>
2020-02-23 19:45:40 +0300

    Minor doc change in macros, future -> sugar

--- lib/core/macros.nim
+++ lib/core/macros.nim
@@ -1471,7 +1471,7 @@ macro expandMacros*(body: typed): untyped =
   ## For instance,
   ##
   ## .. code-block:: nim
-  ##   import future, macros
+  ##   import sugar, macros
   ##
   ##   let
   ##     x = 10
