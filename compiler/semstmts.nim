a3c18a8de68b31abb45dd7f51230ddb907c9eef5
Araq <rumpf_a@web.de>
2020-03-05 13:24:53 +0100

    fixes #5170

--- compiler/semstmts.nim
+++ compiler/semstmts.nim
@@ -1298,6 +1298,8 @@ proc typeSectionFinalPass(c: PContext, n: PNode) =
         checkConstructedType(c.config, s.info, s.typ)
         if s.typ.kind in {tyObject, tyTuple} and not s.typ.n.isNil:
           checkForMetaFields(c, s.typ.n)
+          # fix bug #5170: ensure locally scoped object types get a unique name:
+          if s.typ.kind == tyObject and not isTopLevel(c): incl(s.flags, sfGenSym)
   #instAllTypeBoundOp(c, n.info)
 
 
