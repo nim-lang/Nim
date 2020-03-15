cbd987b2f155c5d9a965898808a5c844b36300a8
Joey Yakimowich-Payne <jyapayne@gmail.com>
2020-03-15 15:40:09 -0600

    Fix nim-gdb and rename to nim-gdb.bash

--- bin/nim-gdb.bash
+++ bin/nim-gdb.bash
@@ -5,10 +5,16 @@ set -e
 
 which nim > /dev/null || (echo "nim not in PATH"; exit 1)
 which gdb > /dev/null || (echo "gdb not in PATH"; exit 1)
-which readlink > /dev/null || which greadlink > /dev/null || (echo "readlink not in PATH. Please install coreutils from brew if on Mac."; exit 1)
+which readlink > /dev/null || \
+    which greadline > /dev/null || \
+    ([[ "$OSTYPE" != "darwin"* ]] && \
+        (echo "readlink not in PATH. Please install coreutils from homebrew."; exit 1)) || \
+    (echo "readlink not in PATH."; exit 1)
 
 nreadlink () {
-    (which greadlink > /dev/null && greadlink "$@") || (which readlink > /dev/null && readlink "$@") || echo "Readlink could not be found"
+    (which greadlink > /dev/null && greadlink "$@") || \
+    (which readlink > /dev/null && readlink "$@") || \
+    echo "Readlink could not be found"
 }
 
 # Find out where the pretty printer Python module is
@@ -19,11 +25,6 @@ GDB_PYTHON_MODULE_PATH="$NIM_SYSROOT/tools/nim-gdb.py"
 # Set the environment variable `NIM_GDB` to overwrite the call to a
 # different/specific command (defaults to `gdb`).
 NIM_GDB="${NIM_GDB:-gdb}"
-
-# This is needed for some reason. I can't get -eval-command to work ever
-echo "source $GDB_PYTHON_MODULE_PATH" > gdbcommands.txt
 # exec replaces the new process of bash with gdb. It is always good to
 # have fewer processes.
-exec ${NIM_GDB} --command="gdbcommands.txt" "$@"
-
-rm gdbcommands.txt
+exec "${NIM_GDB}" -eval-command="source $GDB_PYTHON_MODULE_PATH" "$@"
