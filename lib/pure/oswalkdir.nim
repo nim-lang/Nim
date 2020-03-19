a17e87b516a075a04e3d27c55b9314090abf5bae
Timothee Cour <timothee.cour2@gmail.com>
2020-03-19 02:23:05 -0700

    fix #11458 oswalkdir (#13689)
    * fix #11458 oswalkdir
    
    * changelog

--- lib/pure/oswalkdir.nim
+++ lib/pure/oswalkdir.nim
@@ -7,30 +7,7 @@
 #    distribution, for details about the copyright.
 #
 
-## Compile-time only version for walkDir if you need it at compile-time
-## for JavaScript.
-
-type
-  PathComponent* = enum ## Enumeration specifying a path component.
-    pcFile,             ## path refers to a file
-    pcLinkToFile,       ## path refers to a symbolic link to a file
-    pcDir,              ## path refers to a directory
-    pcLinkToDir         ## path refers to a symbolic link to a directory
-
-proc staticWalkDir(dir: string; relative: bool): seq[
-                  tuple[kind: PathComponent; path: string]] =
-  discard
-
-iterator walkDir*(dir: string; relative = false): tuple[kind: PathComponent;
-    path: string] =
-  for k, v in items(staticWalkDir(dir, relative)):
-    yield (k, v)
-
-iterator walkDirRec*(dir: string; filter = {pcFile, pcDir}): string =
-  var stack = @[dir]
-  while stack.len > 0:
-    for k, p in walkDir(stack.pop()):
-      if k in filter:
-        case k
-        of pcFile, pcLinkToFile: yield p
-        of pcDir, pcLinkToDir: stack.add(p)
+## This module is deprecated, ``import os`` instead.
+{.deprecated: "import os.nim instead".}
+import os
+export PathComponent, walkDir, walkDirRec
