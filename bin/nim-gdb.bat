66924dfeacd939f49e90b016d45201bf8e1fd573
Joey Yakimowich-Payne <jyapayne@gmail.com>
2020-03-20 13:00:18 -0600

    Revert #13658 for nim-gdb.bat. Fixes #13705

--- bin/nim-gdb.bat
+++ bin/nim-gdb.bat
@@ -6,7 +6,9 @@ for %%i in ("%NIM_BIN%\..\") do (set NIM_ROOT=%%~fi)
 set @GDB_PYTHON_MODULE_PATH=%NIM_ROOT%\tools\nim-gdb.py
 set @NIM_GDB=gdb.exe
 
-%@NIM_GDB% -eval-command="source  %@GDB_PYTHON_MODULE_PATH%" %*
+@echo source %@GDB_PYTHON_MODULE_PATH%> wingdbcommand.txt
+%@NIM_GDB% --command="wingdbcommand.txt" %*
+del wingdbcommand.txt /f /q
 
 EXIT /B %ERRORLEVEL%
 @echo on
