4ee1d5186ea4fe3f589884fcceea056e77db139c
Timothee Cour <timothee.cour2@gmail.com>
2020-03-24 19:08:56 -0700

    _

---
+++ tests/stdlib/mstackframes.nim
@@ -0,0 +1,46 @@
+import std/stackframes
+
+const expected = """
+tstackframes.nim(50)     tstackframes
+tstackframes.nim(41)     main ("main",)
+tstackframes.nim(35)     main2 ("main2", 5, 1)
+tstackframes.nim(35)     main2 ("main2", 4, 2)
+tstackframes.nim(35)     main2 ("main2", 3, 3)
+tstackframes.nim(34)     main2 ("main2", 2, 4)
+tstackframes.nim(33)     bar ("bar ",)
+"""
+
+
+
+
+# line 20
+var count = 0
+
+proc main1(n: int) =
+  setFrameMsg $("main1", n)
+  if n > 0:
+    main1(n-1)
+
+proc main2(n: int) =
+  count.inc
+  setFrameMsg $("main2", n, count)
+  proc bar() =
+    setFrameMsg $("bar ",)
+    if n < 3: raise newException(CatchableError, "on purpose")
+  bar()
+  main2(n-1)
+
+import strutils
+proc main() =
+  setFrameMsg $("main", )
+  try:
+    main2(5)
+  except CatchableError:
+    main1(10) # goes deep and then unwinds; sanity check to ensure `setFrameMsg` from inside
+              # `main1` won't invalidate the stacktrace; if StackTraceEntry.frameMsg
+              # were a reference instead of a copy, this would fail.
+    let e = getCurrentException()
+    let trace = e.getStackTrace
+    doAssert trace.startsWith(expected), "\n" & trace
+
+main()
--- tests/stdlib/tstackframes.nim
+++ tests/stdlib/tstackframes.nim
@@ -2,49 +2,27 @@ discard """
   cmd: "nim $target $options --excessiveStackTrace:off $file"
 """
 
-import std/stackframes
+import std/[strformat,os,osproc]
 
-const expected = """
-tstackframes.nim(50)     tstackframes
-tstackframes.nim(41)     main ("main",)
-tstackframes.nim(35)     main2 ("main2", 5, 1)
-tstackframes.nim(35)     main2 ("main2", 4, 2)
-tstackframes.nim(35)     main2 ("main2", 3, 3)
-tstackframes.nim(34)     main2 ("main2", 2, 4)
-tstackframes.nim(33)     bar ("bar ",)
-"""
-
-
-
-
-# line 20
-var count = 0
-
-proc main1(n: int) =
-  setFrameMsg $("main1", n)
-  if n > 0:
-    main1(n-1)
-
-proc main2(n: int) =
-  count.inc
-  setFrameMsg $("main2", n, count)
-  proc bar() =
-    setFrameMsg $("bar ",)
-    if n < 3: raise newException(CatchableError, "on purpose")
-  bar()
-  main2(n-1)
-
-import strutils
 proc main() =
-  setFrameMsg $("main", )
-  try:
-    main2(5)
-  except CatchableError:
-    main1(10) # goes deep and then unwinds; sanity check to ensure `setFrameMsg` from inside
-              # `main1` won't invalidate the stacktrace; if StackTraceEntry.frameMsg
-              # were a reference instead of a copy, this would fail.
-    let e = getCurrentException()
-    let trace = e.getStackTrace
-    doAssert trace.startsWith(expected), "\n" & trace
+  const nim = getCurrentCompilerExe()
+  const file = currentSourcePath().parentDir / "mstackframes.nim"
+  # strangely, --hint:cc:off was needed
+  let cmd = fmt"{nim} c -f --experimental:compiletimeFFI --hints:off --hint:cc:off {file}"
+  let (output, exitCode) = execCmdEx(cmd)
+  let expected = """
+hello world stderr
+hi stderr
+foo
+foo:100
+foo:101
+foo:102:103
+foo:102:103:104
+foo:0.03:asdf:103:105
+ret={s1:foobar s2:foobar age:25 pi:3.14}
+"""
+  doAssert output == expected, output
+  doAssert exitCode == 0
 
-main()
+when defined(nimHasLibFFIEnabled):
+  main()
