5f96df0e72d10c35257709be30d5b064d4c170f9
Timothee Cour <timothee.cour2@gmail.com>
2020-03-27 22:07:56 -0700

    _

---
+++ tests/stdlib/tdollars.nim
@@ -0,0 +1,13 @@
+# `$` tests are scattered but should be here since we have `lib/system/dollars.nim`
+
+template main() =
+  var a = "abc"
+  var b = a[0].unsafeAddr
+  doAssert type(b) is ptr char
+  when nimvm: discard
+  else:
+    doAssert cast[cstring](b) == "abc"
+  echo $b
+
+static: main()
+main()
