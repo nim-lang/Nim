fa34d85e0749c194dae76a4ae11dedb9b0a77d8a
Timothee Cour <timothee.cour2@gmail.com>
2020-03-28 14:47:42 -0700

    fixup

--- lib/system/repr.nim
+++ lib/system/repr.nim
@@ -17,7 +17,7 @@ proc reprFloat(x: float): string {.compilerproc.} = return $x
 
 proc reprPointer(x: pointer): string {.compilerproc.} =
   # size: 0x + hexAddr + '\0'
-  var buf {.noinit.}: array[2 + pointer.sizeof * 2 + 1], char]
+  var buf {.noinit.}: array[2 + pointer.sizeof * 2 + 1, char]
   result.strAppend buf[0].addr, c_sprintf(buf.toCstring, "%p", x)
 
 proc reprStrAux(result: var string, s: cstring; len: int) =
