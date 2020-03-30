8088633250872de8777c7078e636b2379780e107
Miran <narimiran@disroot.org>
2020-03-30 13:18:12 +0200

    faster CIs (#13803)
    * ttables: smaller table, 5x speedup
    
    * thavlak: less iterations, less loops; 30% speedup
    
    * tasyncclosestall: shorter timeout; 35% speedup
    
    * gcleak4: less iterations, 2x speedup
    
    * ttimes: remove deprecated stuff
    
    * tdangerisrelease: remove cpp backend, 3x speedup
    
    * tfrexp1: smaller range, 2x speedup
    
    * trtree: fix warnings, less iterations, 6x speedup
    
    * tasyncawait_cyclebreaker: smaller swarm size; 2x speedup
    
    * trealloc: smaller number of iterations; 10x speedup
    
    * towned_binary_tree: less iterations, 4x speedup
    
    * tclosure: remove unused code, less iterations; 2x speedup
    
    * twaitany: less durations; 1.4x speedup
    
    * tasync_misc: less iterations, 2x speedup
    
    * t8535: smaller sleep, 1.5x speedup
    
    * tmanyjoin: smaller sleep, 2x speedup
    
    * t12221: shorter sleeps, removed two slower tests; 1.6x speedup
    
    * tfuturestream: smaller sleep; 1.5x speedup
    
    * growobjcrash: less iterations; 2x speedup
    
    * ttryrecv: smaller sleep; 1.5x speedup
    
    * treusetvar: less threads; 2x speedup
    
    * delete tthreadanalysis2, basically a duplicate of tthreadanalysis
    
    * t7758: less iterations, 1.5x speedup
    
    * tasyncawait: smaller swarm, less messages; 1.5x speedup
    
    * tjsandnativeasync: smaller sleep, 1.5x speedup
    
    * tpendingcheck: smaller sleep, 1.5x speedup
    
    * remove rodfiles test category
    
    * move tseq from its own category to 'collections' category
    
    * remove unneeded tests and helpers from 'assert' category
    
    * stdlib: merge tbitops2 into tbitops
    
    * remove 'trepr2' from 'stdlib' cat
    
    * merge 'tstreams' into one file
    
    * remove 'tinefficient_const_table' from 'ccbugs' cat
    
    * merge 'tcollections_to_string' into 'tcollections'
    
    * tblocking_channel: smaller sleep, small speedup
    
    * tconvexhull: less iterartions; 1.2x speedup
    
    * merge 'tdeepcopy2' into 'tdeepcopy'
    
    * merge 'tdisjoint_slice2' into 'tdisjoint_slice1'
    
    * tmissing_deepcopy: smaller sequence
    
    * tsendtwice: smaller arrays; 5x speedup
    
    * remove 'tindexerrorformatbounds'
    
    * disable multimethod tests
    
    * remove 'gc:none' and 'refc' without 'd:useRealtimeGC' from gc tests
    
    * koch.nim: bootstrap just with '-d:release', no need for 'csource'
    
    * add github workflow for documentation
    
    * testament: no need for 8 sub-second decimals

--- tests/stdlib/ttimes.nim
+++ tests/stdlib/ttimes.nim
@@ -309,7 +309,7 @@ suite "ttimes":
     check dt.nanosecond == convert(Milliseconds, Nanoseconds, 1)
     check d(seconds = 1, milliseconds = 500) * 2 == d(seconds = 3)
     check d(seconds = 3) div 2 == d(seconds = 1, milliseconds = 500)
-    check d(milliseconds = 1001).seconds == 1
+    check d(milliseconds = 1001).inSeconds == 1
     check d(seconds = 1, milliseconds = 500) - d(milliseconds = 1250) ==
       d(milliseconds = 250)
     check d(seconds = 1, milliseconds = 1) < d(seconds = 1, milliseconds = 2)
@@ -503,12 +503,6 @@ suite "ttimes":
     )
     check $parse("02 Fir 2019", "dd MMM yyyy", utc(), loc) == "2019-01-02T00:00:00Z"
     check $parse("Fourthy 6, 2017", "MMMM d, yyyy", utc(), loc) == "2017-04-06T00:00:00Z"
-
-  test "countLeapYears":
-    # 1920, 2004 and 2020 are leap years, and should be counted starting at the following year
-    check countLeapYears(1920) + 1 == countLeapYears(1921)
-    check countLeapYears(2004) + 1 == countLeapYears(2005)
-    check countLeapYears(2020) + 1 == countLeapYears(2021)
 
   test "timezoneConversion":
     var l = now()
