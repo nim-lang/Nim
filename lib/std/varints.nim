3e88cdfa373f44ee025d1c83308d4a7246709f8d
juancarlospaco <juancarlospaco@gmail.com>
2020-04-01 12:37:16 -0300

    Reduce coverage

--- lib/std/varints.nim
+++ lib/std/varints.nim
@@ -121,54 +121,6 @@ proc decodeZigzag*(x: uint64): int64 {.inline.} =
   result = (`shr`(casted, 1)) xor (-(casted and 1))
 
 
-runnableExamples:
-  block:
-    var hugeIntArray: array[50, byte]
-    var readedInt: uint64
-    doAssert writeVu64(hugeIntArray, 0.uint64) == readVu64(hugeIntArray, readedInt)
-    doAssert readedInt == 0.uint64
-    doAssert writeVu64(hugeIntArray, uint64.high) == readVu64(hugeIntArray, readedInt)
-    doAssert readedInt == uint64.high
-    doAssert writeVu64(hugeIntArray, uint64(int64.high)) == readVu64(hugeIntArray, readedInt)
-    doAssert readedInt == uint64(int64.high)
-    doAssert writeVu64(hugeIntArray, uint64(int32.high)) == readVu64(hugeIntArray, readedInt)
-    doAssert readedInt == uint64(int32.high)
-    doAssert writeVu64(hugeIntArray, uint64(int16.high)) == readVu64(hugeIntArray, readedInt)
-    doAssert readedInt == uint64(int16.high)
-    doAssert writeVu64(hugeIntArray, uint64(int8.high)) == readVu64(hugeIntArray, readedInt)
-    doAssert readedInt == uint64(int8.high)
-    doAssert writeVu64(hugeIntArray, cast[uint64](0.0)) == readVu64(hugeIntArray, readedInt)
-    doAssert readedInt == cast[uint64](0.0)
-    doAssert writeVu64(hugeIntArray, cast[uint64](-0.0)) == readVu64(hugeIntArray, readedInt)
-    doAssert readedInt == cast[uint64](-0.0)
-    doAssert writeVu64(hugeIntArray, cast[uint64](0.1)) == readVu64(hugeIntArray, readedInt)
-    doAssert readedInt == cast[uint64](0.1)
-    doAssert writeVu64(hugeIntArray, cast[uint64](0.9555555555555555555555501)) == readVu64(hugeIntArray, readedInt)
-    doAssert readedInt == cast[uint64](0.9555555555555555555555501)
-    doAssert writeVu64(hugeIntArray, cast[uint64](+Inf)) == readVu64(hugeIntArray, readedInt)
-    doAssert readedInt == cast[uint64](+Inf)
-    doAssert writeVu64(hugeIntArray, cast[uint64](NegInf)) == readVu64(hugeIntArray, readedInt)
-    doAssert readedInt == cast[uint64](NegInf)
-    doAssert writeVu64(hugeIntArray, cast[uint64](Nan)) == readVu64(hugeIntArray, readedInt)
-    doAssert readedInt == cast[uint64](Nan)
-    doAssert writeVu64(hugeIntArray, cast[uint64](3.1415926535897932384626433)) == readVu64(hugeIntArray, readedInt)
-    doAssert readedInt == cast[uint64](3.1415926535897932384626433)
-    doAssert writeVu64(hugeIntArray, cast[uint64](2.71828182845904523536028747)) == readVu64(hugeIntArray, readedInt)
-    doAssert readedInt == cast[uint64](2.71828182845904523536028747)
-  block:
-    doAssert encodeZigzag(decodeZigzag(0.uint64)) == 0.uint64
-    doAssert encodeZigzag(decodeZigzag(uint64(uint32.high))) == uint64(uint32.high)
-    doAssert encodeZigzag(decodeZigzag(uint64(int32.high))) == uint64(int32.high)
-    doAssert encodeZigzag(decodeZigzag(uint64(int16.high))) == uint64(int16.high)
-    doAssert encodeZigzag(decodeZigzag(uint64(int8.high))) == uint64(int8.high)
-    doAssert encodeZigzag(decodeZigzag(cast[uint64](0.0))) == cast[uint64](0.0)
-    doAssert encodeZigzag(decodeZigzag(cast[uint64](0.1))) == cast[uint64](0.1)
-    doAssert encodeZigzag(decodeZigzag(cast[uint64](0.9555555555555555555555501))) == cast[uint64](0.9555555555555555555555501)
-    doAssert encodeZigzag(decodeZigzag(cast[uint64](+Inf))) == cast[uint64](+Inf)
-    doAssert encodeZigzag(decodeZigzag(cast[uint64](3.1415926535897932384626433))) == cast[uint64](3.1415926535897932384626433)
-    doAssert encodeZigzag(decodeZigzag(cast[uint64](2.71828182845904523536028747))) == cast[uint64](2.71828182845904523536028747)
-
-
 when isMainModule:
   #import random
 
