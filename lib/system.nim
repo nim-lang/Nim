7e83adff84be5d0c401a213eccb61e321a3fb1ff
narimiran <narimiran@disroot.org>
2020-04-02 16:33:40 +0200

    bump Nim version to 1.2.0

--- lib/system.nim
+++ lib/system.nim
@@ -2049,10 +2049,10 @@ const
   NimMajor* {.intdefine.}: int = 1
     ## is the major number of Nim's version.
 
-  NimMinor* {.intdefine.}: int = 1
+  NimMinor* {.intdefine.}: int = 2
     ## is the minor number of Nim's version.
 
-  NimPatch* {.intdefine.}: int = 1
+  NimPatch* {.intdefine.}: int = 0
     ## is the patch number of Nim's version.
 
   NimVersion*: string = $NimMajor & "." & $NimMinor & "." & $NimPatch
