e96808b25e212f3bb6bb488a10997148166d602a
Timothee Cour <timothee.cour2@gmail.com>
2020-04-07 04:44:01 -0700

    fixup

--- tests/ccgbugs/t13902.nim
+++ tests/ccgbugs/t13902.nim
@@ -1,16 +1,25 @@
-discard """
-  cmd: "nim c -r --skipParentCfg --skipUserCfg --cpu:i386 --passC:-m32 --passL:-m32 --stacktrace:off --hints:off $file"
-  action: run
-"""
-
 #issue #13902
-when defined(linux):
+when defined case_t13902:
   block:
     type Slot = distinct uint64
     var s = Slot(1)
     proc `$`(x: Slot): string {.borrow.}
     proc `+=`(x: var Slot, y: uint64) {.borrow.}
-    echo "s = ", s
+    # test was failing with either 0 or 2 echos but not with 1 echo
+    # echo "s = ", s
     s += 1
-    echo "s = ", s
+    # echo "s = ", s
     doAssert s.uint64 == 2, $s # was failing, showing 18419607611339964418
+
+else:
+  import std/[strformat,os,osproc]
+  proc main() =
+    when defined(linux):
+      # osx doesn't support 32bit and windows doesn't have the required
+      # cross compilation libraries by default
+      const nim = getCurrentCompilerExe()
+      const file = currentSourcePath()
+      let cmd = fmt"{nim} c -r -d:case_t13902 --skipParentCfg --skipUserCfg --cpu:i386 --passC:-m32 --passL:-m32 --stacktrace:off --hints:off {file}"
+      let (output, exitCode) = execCmdEx(cmd)
+      doAssert exitCode == 0, $(cmd, output)
+  main()
