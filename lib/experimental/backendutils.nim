2c249d9aa840f5140e71b5fe436f99b4b889a139
Timothee Cour <timothee.cour2@gmail.com>
2020-04-10 23:59:15 -0700

    add runnableExamples

--- lib/experimental/backendutils.nim
+++ lib/experimental/backendutils.nim
@@ -1,15 +1,18 @@
 ##[
 ## experimental API!
 
-Utilities to interface with generated backend (for now C/C++, later js) code
+Utilities to interface with generated backend (for now C/C++, later js) code,
+abstracting away platform differences and taking care of needy greedy details.
 ]##
 
 import macros
 
-static: doAssert defined(c) or defined(cpp)
+static: doAssert defined(c) or defined(cpp) or defined(nimdoc)
 
 macro c_astToStr*(T: typedesc): string =
-  ## returns the backend
+  ## returns the backend analog of `astToStr`
+  runnableExamples:
+    doAssert cint.c_astToStr == "int"
   result = newStmtList()
   var done {.global.}: bool
   if not done:
@@ -23,21 +26,31 @@ macro c_astToStr*(T: typedesc): string =
     $s
 
 template c_currentSourcePath*(): string =
+  ## returns the generated backend file
   var s: cstring
   {.emit("here"): [s, "= __FILE__;"].}
   $s
 
 template c_currentFunction*(): string =
+  runnableExamples:
+    proc fun(){.exportc.} = doAssert c_currentFunction == "fun"
+    fun()
   var s: cstring
-  {.emit("here"): [s, "= __FUNCTION__;"].}
+  # cast needed for C++
+  {.emit("here"): [s, "= (char*) __FUNCTION__;"].}
   $s
 
 template c_sizeof*(T: typedesc): int =
+  runnableExamples:
+    doAssert c_sizeof(cint) == cint.sizeof
   var s: int
   {.emit("here"): [s," = sizeof(", T, ");"].}
   s
 
 template cstaticIf*(cond: string, body) =
+  runnableExamples:
+    cstaticIf "defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L":
+      {.emit("HERE"): """_Static_assert(sizeof(_Bool) == 1, "bad"); """.}
   {.emit("here"): ["""#if """, cond, "\n"].}
   body
   {.emit("here"): "#endif\n".}
