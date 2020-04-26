8e8eac18301d1c86cf09e24d8df6301b85de5681
hlaaftana <hlaaftana@gmail.com>
2020-04-26 09:12:27 +0300

    fixes #14112, tests for #12892, #12671, #11697

--- testament/specs.nim
+++ testament/specs.nim
@@ -237,6 +237,8 @@ proc parseSpec*(filename: string): TSpec =
           when defined(freebsd): result.err = reDisabled
         of "arm64":
           when defined(arm64): result.err = reDisabled
+        of "i386":
+          when defined(i386): result.err = reDisabled
         of "openbsd":
           when defined(openbsd): result.err = reDisabled
         of "netbsd":
