e63fc95bd474b662cf1f14515e9d6b8d515f1357
narimiran <narimiran@disroot.org>
2020-04-28 15:59:32 +0200

    fix tdistros test which was failing on Nightlies

--- tests/distros/tdistros_detect.nim
+++ tests/distros/tdistros_detect.nim
@@ -7,15 +7,15 @@ discard """
 
 when defined(windows):
     doAssert detectOs(Windows) == true
-    doAssert detectOs(Ubuntu) == false
+    doAssert detectOs(Linux) == false
     doAssert detectOs(MacOSX) == false
 
 when defined(linux):
-    doAssert detectOs(Ubuntu) == true
+    doAssert detectOs(Linux) == true
     doAssert detectOs(Windows) == false
     doAssert detectOs(MacOSX) == false
 
 when defined(macosx):
     doAssert detectOs(MacOSX) == true
     doAssert detectOs(Windows) == false
-    doAssert detectOs(Ubuntu) == false
+    doAssert detectOs(Linux) == false
