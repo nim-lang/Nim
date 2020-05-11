287dff200d46302b17c3cecdc5f05321366dbc89
Antonis <b3liever@yandex.com>
2020-05-11 14:51:50 +0300

    fix closure env check
    (cherry picked from commit 3616b0698baf230fec7a60cd90c72b979e582179)

--- compiler/injectdestructors.nim
+++ compiler/injectdestructors.nim
@@ -382,7 +382,8 @@ proc sinkParamIsLastReadCheck(c: var Con, s: PNode) =
     localError(c.graph.config, c.otherRead.info, "sink parameter `" & $s.sym.name.s &
         "` is already consumed at " & toFileLineCol(c. graph.config, s.info))
 
-proc isClosureEnv(n: PNode): bool = n.kind == nkSym and n.sym.name.s[0] == ':'
+proc isClosureEnv(n: PNode): bool =
+  n.kind == nkDotExpr and n[0].kind == nkHiddenDeref and n[0][0].typ.kind == tyRef
 
 proc passCopyToSink(n: PNode; c: var Con): PNode =
   result = newNodeIT(nkStmtListExpr, n.info, n.typ)
