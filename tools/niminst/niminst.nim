2a441146b9e236f48e9742313c6e593173c38aaa
alaviss <leorize+oss@disroot.org>
2020-05-26 21:34:05 +0000

    Revert "niminst: use threaded compression when supported (#14455)"
    This reverts commit b749d58ea1d77aab0ab758a17140ebc57e32f8c8.

--- tools/niminst/niminst.nim
+++ tools/niminst/niminst.nim
@@ -700,10 +700,8 @@ RunProgram="tools\downloader.exe"
           if execShellCmd("tar cf $1.tar $1" % proj) != 0:
             echo("External program failed")
 
-        if execShellCmd("xz -T0 -9f $1.tar" % proj) != 0:
-          # Maybe the xz version is too old and doesn't support threading
-          if execShellCmd("xz -9f $1.tar" % proj) != 0:
-            echo("External program failed")
+        if execShellCmd("xz -9f $1.tar" % proj) != 0:
+          echo("External program failed")
     finally:
       setCurrentDir(oldDir)
 
