4ce132f5be5753cb98f2f558d799a8795ea59c16
Timothee Cour <timothee.cour2@gmail.com>
2020-06-19 00:53:45 -0700

    fix #14685 tests/async/t7758.nim flaky (#14721)
    * fix #14685 tests/async/t7758.nim flaky
    
    * address comment
    
    * address comment

--- tests/async/t7758.nim
+++ tests/async/t7758.nim
@@ -2,7 +2,8 @@ import asyncdispatch
 import std/unittest
 
 proc task() {.async.} =
-  await sleepAsync(40)
+  const tSleep = 40
+  await sleepAsync(tSleep)
 
 proc main() =
   var counter = 0
@@ -11,6 +12,10 @@ proc main() =
     inc(counter)
     poll(10)
 
-  check counter <= 4
+  const slack = 1
+    # because there is overhead in `async` + `sleepAsync`
+    # as can be seen by increasing `tSleep` from 40 to 49, which increases the number
+    # of failures.
+  check counter <= 4 + slack
 
 for i in 0 .. 10: main()
