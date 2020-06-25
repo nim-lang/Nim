a5667218a9ce4d1ce8e07f9382507a27eb296233
narimiran <narimiran@disroot.org>
2020-06-25 13:03:12 +0200

    update test command for 'norm' package

--- testament/important_packages.nim
+++ testament/important_packages.nim
@@ -63,7 +63,7 @@ pkg "nimsl", true
 pkg "nimsvg"
 # pkg "nimterop", true
 # pkg "nimx", true, "nim c --threads:on test/main.nim"
-pkg "norm", true, "nim c -r tests/tsqlite.nim"
+pkg "norm", true, "nim c -r tests/tsqliterows.nim"
 pkg "npeg"
 pkg "ormin", true, "nim c -o:orminn ormin.nim"
 pkg "parsetoml"
