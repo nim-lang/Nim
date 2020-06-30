97c5e7e1abbb012743c0f77a52952625631962d9
Ray Imber <ray@crankuptheamps.com>
2020-06-29 19:43:55 -0700

    Removed debug statements and increased timeouts by 1 order of magnitude to account for slow Azure VMs

--- tests/async/t14820.nim
+++ tests/async/t14820.nim
@@ -13,15 +13,13 @@ import asyncdispatch, times
 
 var done = false
 proc somethingAsync() {.async.} =
-  yield sleepAsync 500
+  yield sleepAsync 5000
   echo "async done"
   done = true
   
 asyncCheck somethingAsync()
 var count = 0
-let s0 = now()
 while not done:
   count += 1
-  drain 100
+  drain 1000
   echo "iteration: ", count 
-  echo "ms: ", (now() - s0).inMilliseconds
