a5355934eb03e149c4d2504c431ec18e88ab9934
Juan Carlos <JuanCarlospaco@gmail.com>
2020-07-04 09:13:15 -0300

    https://github.com/nim-lang/Nim/pull/14866#discussion_r449766534

--- lib/pure/os.nim
+++ lib/pure/os.nim
@@ -1686,19 +1686,19 @@ proc setFilePermissions*(filename: string, permissions: set[FilePermission]) {.
       var res2 = setFileAttributesA(filename, res)
     if res2 == - 1'i32: raiseOSError(osLastError(), $(filename, permissions))
 
-func toFilePerm*(perm: Natural): set[FilePermission] {.since: (1, 3).} =
+func toFilePermissions*(perm: Natural): set[FilePermission] {.since: (1, 3).} =
   ## Convenience func to convert Unix like file permission to ``set[FilePermission]``.
   ##
   ## See also:
   ## * `getFilePermissions <#getFilePermissions,string>`_
   ## * `setFilePermissions <#setFilePermissions,string,set[FilePermission]>`_
   runnableExamples:
-    doAssert toFilePerm(0o700) == {fpUserExec, fpUserRead, fpUserWrite}
-    doAssert toFilePerm(0o070) == {fpGroupExec, fpGroupRead, fpGroupWrite}
-    doAssert toFilePerm(0o007) == {fpOthersExec, fpOthersRead, fpOthersWrite}
-    doAssert toFilePerm(0o644) == {fpUserWrite, fpUserRead, fpGroupRead, fpOthersRead}
-    doAssert toFilePerm(0o777) == {fpUserExec, fpUserWrite, fpUserRead, fpGroupExec, fpGroupWrite, fpGroupRead, fpOthersExec, fpOthersWrite, fpOthersRead}
-    doAssert toFilePerm(0o000) == {}
+    doAssert toFilePermissions(0o700) == {fpUserExec, fpUserRead, fpUserWrite}
+    doAssert toFilePermissions(0o070) == {fpGroupExec, fpGroupRead, fpGroupWrite}
+    doAssert toFilePermissions(0o007) == {fpOthersExec, fpOthersRead, fpOthersWrite}
+    doAssert toFilePermissions(0o644) == {fpUserWrite, fpUserRead, fpGroupRead, fpOthersRead}
+    doAssert toFilePermissions(0o777) == {fpUserExec, fpUserWrite, fpUserRead, fpGroupExec, fpGroupWrite, fpGroupRead, fpOthersExec, fpOthersWrite, fpOthersRead}
+    doAssert toFilePermissions(0o000) == {}
   var perm = uint perm
   for permBase in [fpOthersExec, fpGroupExec, fpUserExec]:
     if (perm and 1) != 0: result.incl permBase         # Exec
