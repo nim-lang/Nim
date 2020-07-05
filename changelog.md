83fe9d893c9c129dd95d53c72453c7c768c8ce59
Juan Carlos <JuanCarlospaco@gmail.com>
2020-07-05 07:04:32 -0300

    Fix logging tiny bug

--- changelog.md
+++ changelog.md
@@ -124,6 +124,10 @@
 - deprecations: `os.existsDir` => `dirExists`, `os.existsFile` => `fileExists`
 
 - Add `jsre` module, [Regular Expressions for the JavaScript target.](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions)
+- Changed `maxLines` to `Positive` on `logging.newRollingFileLogger`,
+  because negative values cause 1 new file per line of log, and make no sense whatsoever.
+- Changed `log` on `logging` to use proper log level on JavaScript target,
+  eg. `debug` uses `console.debug`, `info` uses `console.info`, `warn` uses `console.warn`, etc.
 
 
 ## Language changes
--- lib/pure/logging.nim
+++ lib/pure/logging.nim
@@ -364,7 +364,12 @@ method log*(logger: ConsoleLogger, level: Level, args: varargs[string, `$`]) =
     let ln = substituteLog(logger.fmtStr, level, args)
     when defined(js):
       let cln: cstring = ln
-      {.emit: "console.log(`cln`);".}
+      case level
+      of lvlDebug: {.emit: "console.debug(`cln`);".}
+      of lvlInfo:  {.emit: "console.info(`cln`);".}
+      of lvlWarn:  {.emit: "console.warn(`cln`);".}
+      of lvlError: {.emit: "console.error(`cln`);".}
+      else:        {.emit: "console.log(`cln`);".}
     else:
       try:
         var handle = stdout
@@ -503,12 +508,8 @@ when not defined(js):
 
   # ------
 
-  proc countLogLines(logger: RollingFileLogger): int =
-    result = 0
-    let fp = open(logger.baseName, fmRead)
-    for line in fp.lines():
-      result.inc()
-    fp.close()
+  template countLogLines(logger: RollingFileLogger): int =
+    readFile(logger.baseName).countLines()
 
   proc countFiles(filename: string): int =
     # Example: file.log.1
@@ -531,7 +532,7 @@ when not defined(js):
                             mode: FileMode = fmReadWrite,
                             levelThreshold = lvlAll,
                             fmtStr = defaultFmtStr,
-                            maxLines = 1000,
+                            maxLines: Positive = 1000,
                             bufSize: int = -1): RollingFileLogger =
     ## Creates a new `RollingFileLogger<#RollingFileLogger>`_.
     ##
