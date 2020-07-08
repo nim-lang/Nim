cb1ecbf95620bffd559610b70f99c6f16963ac5c
Araq <rumpf_a@web.de>
2020-07-08 10:51:26 +0200

    closes #14878

--- tests/lent/tlent_from_var.nim
+++ tests/lent/tlent_from_var.nim
@@ -1,5 +1,6 @@
 discard """
-  output: "x"
+  output: '''x
+[10, 11, 12, 13]'''
 """
 
 # bug #14805
@@ -12,3 +13,20 @@ proc bar(f: var Foo): lent string =
 
 var foo = Foo(a: "x")
 echo bar(foo)
+
+
+# bug #14878
+
+# proc byLentImpl[T](a: T): lent T = a
+proc byLentVar[T](a: var T): lent T =
+  result = a
+  # result = a.byLentImpl # this doesn't help
+
+var x = 3 # error: cannot take the address of an rvalue of type 'NI *'
+
+var xs = [10,11,12,13] # SIGSEGV
+
+let x2 = x.byLentVar
+
+let xs2 = xs.byLentVar
+echo xs2
