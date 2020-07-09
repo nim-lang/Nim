c38a459582fb4744ce4abcba2d278b48857c0a31
Andreas Rumpf <rumpf_a@web.de>
2020-07-09 17:24:23 +0200

    fixes #14925 (#14947)

--- compiler/ccgstmts.nim
+++ compiler/ccgstmts.nim
@@ -1218,6 +1218,10 @@ proc genTryGoto(p: BProc; t: PNode; d: var TLoc) =
   p.nestedTryStmts.add((fin, false, Natural lab))
 
   p.flags.incl nimErrorFlagAccessed
+
+  if not isEmptyType(t.typ) and d.k == locNone:
+    getTemp(p, t.typ, d)
+
   expr(p, t[0], d)
 
   if 1 < t.len and t[1].kind == nkExceptBranch:
