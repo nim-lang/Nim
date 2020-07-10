34d9255b9e8670181b1b7a19c0843e7dd850ddf3
Clyybber <darkmine956@gmail.com>
2020-07-10 16:54:42 +0200

    Add testcase for #4722

--- tests/template/template_issues.nim
+++ tests/template/template_issues.nim
@@ -8,6 +8,9 @@ hi
 Hello, World!
 (e: 42)
 hey
+foo
+foo
+foo
 '''
 """
 
@@ -245,3 +248,21 @@ proc foo(): auto =
     echo "hey"
 
 discard foo()
+
+
+# bug #4722
+type
+  IteratorF*[In] = iterator() : In {.closure.}
+
+template foof(In: untyped) : untyped = 
+  proc ggg*(arg: IteratorF[In]) =
+    for i in arg():
+      echo "foo"
+
+
+iterator hello() : int {.closure.} =
+  for i in 1 .. 3:
+    yield i
+    
+foof(int)
+ggg(hello)
