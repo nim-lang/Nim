c3e7f138bf124166bb0a90bdf341ac34562d1f4a
flywind <xzsflywind@gmail.com>
2020-07-11 23:40:37 +0800

    again

--- tests/errmsgs/t6608.nim
+++ tests/errmsgs/t6608.nim
@@ -1,5 +1,5 @@
 discard """
-  nimout: '''errmsgs/t6608.nim(12, 4) Error: type mismatch: got <>
+  nimout: '''t6608.nim(12, 4) Error: type mismatch: got <>
 but expected one of: 
 AcceptCB = proc (s: string){.closure.}'''
   line: 12
