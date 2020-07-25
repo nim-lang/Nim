aae998feff6d0234ee02311d59b299adfee0b01c
Andreas Rumpf <rumpf_a@web.de>
2020-07-25 22:29:45 +0200

    fixes #15038 [backport:1.2]

--- compiler/sempass2.nim
+++ compiler/sempass2.nim
@@ -840,6 +840,8 @@ proc track(tracked: PEffects, n: PNode) =
     useVar(tracked, n)
     if n.sym.typ != nil and tfHasAsgn in n.sym.typ.flags:
       tracked.owner.flags.incl sfInjectDestructors
+      # bug #15038: ensure consistency
+      if not hasDestructor(n.typ): n.typ = n.sym.typ
   of nkHiddenAddr, nkAddr:
     if n[0].kind == nkSym and isLocalVar(tracked, n[0].sym):
       useVarNoInitCheck(tracked, n[0], n[0].sym)
