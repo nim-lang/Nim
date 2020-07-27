df90a25590919f4efd2dc4d1414594cc4dbaedb0
Araq <rumpf_a@web.de>
2020-07-27 08:05:52 +0200

    compiler: minor code cleanups

--- compiler/isolation_check.nim
+++ compiler/isolation_check.nim
@@ -11,7 +11,7 @@
 ## https://github.com/nim-lang/RFCs/issues/244 for more details.
 
 import
-  ast, types, renderer, idents, intsets, options, msgs
+  ast, types, renderer, idents, intsets
 
 proc canAlias(arg, ret: PType; marker: var IntSet): bool
 
