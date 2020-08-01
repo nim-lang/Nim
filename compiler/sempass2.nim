0ddf7153b8242eaaf042a178deb47689a05d5e9c
Andreas Rumpf <rumpf_a@web.de>
2020-08-01 22:03:15 +0200

    fixes #15129 [backport:1.2] (#15144)
    (cherry picked from commit d6161e7e9ea1f40ad734c6e811e8169fe24b5e9f)

--- compiler/sempass2.nim
+++ compiler/sempass2.nim
@@ -785,7 +785,7 @@ proc track(tracked: PEffects, n: PNode) =
     if n.sym.typ != nil and tfHasAsgn in n.sym.typ.flags:
       tracked.owner.flags.incl sfInjectDestructors
       # bug #15038: ensure consistency
-      if not hasDestructor(n.typ): n.typ = n.sym.typ
+      if not hasDestructor(n.typ) and sameType(n.typ, n.sym.typ): n.typ = n.sym.typ
   of nkRaiseStmt:
     if n[0].kind != nkEmpty:
       n[0].info = n.info
