452242ba91de07c4c0d7d2f5405d7f5dca415333
flywind <xzsflywind@gmail.com>
2020-08-04 07:56:53 +0800

    tests

--- tests/async/t15148.nim
+++ tests/async/t15148.nim
@@ -1,11 +1,14 @@
-import asyncdispatch, asyncfile
+import asyncdispatch, asyncfile, os
 
+const filename = "t15148.txt"
 
 proc saveEmpty() {.async.} =
   let
     text = ""
-    file = open_async("t15148.txt", fmWrite)
+    file = open_async(filename, fmWrite)
   await file.write(text)
   file.close()
 
 waitFor saveEmpty()
+
+doAssert fileExists(filename)
