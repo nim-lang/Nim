05856bfcf6c3a467aabe2e54ee4cd75796623f5e
flywind <xzsflywind@gmail.com>
2020-08-03 08:04:26 +0800

    minor

--- tests/async/t15148.nim
+++ tests/async/t15148.nim
@@ -4,7 +4,7 @@ import asyncdispatch, asyncfile
 proc saveEmpty() {.async.} =
   let
     text = ""
-    file = open_async("test.txt", fmWrite)
+    file = open_async("t15148.txt", fmWrite)
   await file.write(text)
   file.close()
 
