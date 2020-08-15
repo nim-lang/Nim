4eac11664df7cb783222aaccb920de24870040c4
Juan Carlos <JuanCarlospaco@gmail.com>
2020-08-15 17:06:16 -0300

    ReSync with devel

--- testament/important_packages.nim
+++ testament/important_packages.nim
@@ -11,7 +11,7 @@ var packages2*: seq[tuple[name, cmd: string; hasDeps: bool; url: string, useHead
 # packages A-M
 # pkg1 "alea", true
 pkg1 "argparse"
-pkg1 "arraymancer", true, "nim c tests/tests_cpu.nim"
+#pkg1 "arraymancer", true, "nim c tests/tests_cpu.nim" # See https://github.com/nim-lang/Nim/pull/14862#issue-442239265
 #pkg1 "ast_pattern_matching", false, "nim c -r --oldgensym:on tests/test1.nim"
 pkg1 "awk", true
 pkg1 "bigints"
