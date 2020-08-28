d39b11fd5490df5dc26a473d44898e0cdd6a47ef
Andrey R (cooldome) <ariabushenko@gmail.com>
2020-08-28 14:40:22 +0100

    fix #15035

--- tests/arc/t14383.nim
+++ tests/arc/t14383.nim
@@ -3,6 +3,7 @@ discard """
   output: '''
 hello
 hello
+@[4, 3, 2, 1]
 '''
 """
 
@@ -11,3 +12,18 @@ import dmodule
 var val = parseMinValue()
 if val.kind == minDictionary:
   echo val
+
+
+#------------------------------------------------------------------------------
+# Issue #15035
+#------------------------------------------------------------------------------
+
+proc countRun(lst: sink openArray[int]): int =
+  discard
+
+proc timSort(lst: var openArray[int]) = 
+  let run = countRun(lst)
+
+var a = @[4, 3, 2, 1]
+timSort(a)
+echo a
