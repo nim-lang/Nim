6bac4768e1aceb208da0f976fe1cbe959b1c2030
bung87 <crc32@qq.com>
2020-08-29 00:33:31 +0800

    reformat

--- lib/impure/db_mysql.nim
+++ lib/impure/db_mysql.nim
@@ -122,20 +122,20 @@ proc dbQuote*(s: string): string =
   result.add "'"
   for c in items(s):
     # see https://cheatsheetseries.owasp.org/cheatsheets/SQL_Injection_Prevention_Cheat_Sheet.html#mysql-escaping
-    case c:
-      of '\0': result.add "\\0"
-      of '\b': result.add "\\b"
-      of '\t': result.add "\\t"
-      of '\l': result.add "\\n"
-      of '\r': result.add "\\r"
-      of '\x1a': result.add "\\Z"
-      of '"': result.add "\\\""
-      of '%': result.add "\\%"
-      of '\'': result.add "\\'"
-      of '\\': result.add "\\\\"
-      of '_': result.add "\\_"
-      of Letters+Digits: result.add c
-      else: result.add "\\" & c
+    case c
+    of '\0': result.add "\\0"
+    of '\b': result.add "\\b"
+    of '\t': result.add "\\t"
+    of '\l': result.add "\\n"
+    of '\r': result.add "\\r"
+    of '\x1a': result.add "\\Z"
+    of '"': result.add "\\\""
+    of '%': result.add "\\%"
+    of '\'': result.add "\\'"
+    of '\\': result.add "\\\\"
+    of '_': result.add "\\_"
+    of Letters+Digits: result.add c
+    else: result.add "\\" & c
   add(result, '\'')
 
 proc dbFormat(formatstr: SqlQuery, args: varargs[string]): string =
