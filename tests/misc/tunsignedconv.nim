ae688aa7f54d820ac7addcd080ff1c7bffbe9237
Andreas Rumpf <rumpf_a@web.de>
2020-08-31 14:46:23 +0200

    fixes #15210 [backport:1.2] (#15237)
    * fixes #15210 [backport:1.2]
    
    * use patched version of bigints library
    
    (cherry picked from commit ff13f8cc3c74d4048b6c2604fe4b8e5365f5c13f)

--- tests/misc/tunsignedconv.nim
+++ tests/misc/tunsignedconv.nim
@@ -75,3 +75,14 @@ let rangeVar = 0'u64 ..< limit
 doAssert repr(rangeVar) == """[a = 0,
 b = 0]
 """
+
+# bug #15210
+
+let a3 = not 0'u64
+var success = false
+try:
+  discard a3.int64
+except RangeDefect:
+  success = true
+
+doAssert success, "conversion should fail at runtime"
