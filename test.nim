a1fe3ed8d1c5c4ddcd9044c36c6d956c0fcf4d7d
flywind <xzsflywind@gmail.com>
2020-09-07 12:40:06 +0800

    tests

---
+++ test.nim
@@ -0,0 +1,7 @@
+import nativesockets
+
+
+echo "icmpv6: ", getProtoByName("ipv6-icmp")
+echo "icmp: ", getProtoByName("icmp")
+echo "ipv6: ", getProtoByName("ipv6")
+echo "ip: ", getProtoByName("ip")
