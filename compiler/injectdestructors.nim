86c2628fedfd0dd56bab53043abffa3b3613d6e0
Andrey R (cooldome) <ariabushenko@gmail.com>
2020-09-09 20:59:12 +0100

    fix #15286

--- compiler/injectdestructors.nim
+++ compiler/injectdestructors.nim
@@ -728,8 +728,11 @@ proc p(n: PNode; c: var Con; s: var Scope; mode: ProcessMode): PNode =
       for i in ord(n.kind in {nkObjConstr, nkClosure})..<n.len:
         if n[i].kind == nkExprColonExpr:
           result[i][1] = p(n[i][1], c, s, m)
+        elif n[i].kind == nkRange:
+          result[i][0] = p(n[i][0], c, s, m)
+          result[i][1] = p(n[i][1], c, s, m)
         else:
-          result[i] = p(n[i], c, s, m)
+          result[i] = p(n[i], c, s, m)          
       if mode == normal and isRefConstr:
         result = ensureDestruction(result, n, c, s)
     of nkCallKinds:
