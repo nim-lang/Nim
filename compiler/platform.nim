36a6faea7deea5658b02e847f087f666dcb7d9fc
Jaremy Creechley <creechley@gmail.com>
2020-09-11 00:26:16 -0600

    Adding test to catch issue #15302 -- lwip/freertos net library don't try to build / run on windows, it'll compile only but not run
    Fixing issue #15302 -- reworking test to compile on other platforms

--- compiler/platform.nim
+++ compiler/platform.nim
@@ -179,7 +179,7 @@ const
       props: {ospNeedsPIC, ospPosix}),
      (name: "FreeRTOS", parDir: "..", dllFrmt: "lib$1.so", altDirSep: "/",
       objExt: ".o", newLine: "\x0A", pathSep: ":", dirSep: "/",
-      scriptExt: ".sh", curDir: ".", exeExt: ".elf", extSep: ".",
+      scriptExt: ".sh", curDir: ".", exeExt: "", extSep: ".",
       props: {ospPosix}),
      (name: "Any", parDir: "..", dllFrmt: "lib$1.so", altDirSep: "/",
       objExt: ".o", newLine: "\x0A", pathSep: ":", dirSep: "/",
