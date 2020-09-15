3617c7de79d72ce602b5a5ec416a5cf287e475b9
James Osborn <osborn@alcf.anl.gov>
2020-09-15 00:02:46 -0500

    avoid megatest

---
+++ tests/overload/tmoretests.nim
@@ -0,0 +1,19 @@
+discard """
+  output: '''
+'''
+"""
+
+block:
+  type
+    TilesetCT[n: static int] = int
+    TilesetRT = int
+    Tileset = TilesetCT | TilesetRT
+
+  func prepareTileset(tileset: var Tileset) = discard
+
+  func prepareTileset(tileset: Tileset): Tileset =
+    result = tileset
+    result.prepareTileset
+
+  var parsedTileset: TilesetRT
+  prepareTileset(parsedTileset)
--- tests/overload/toverload_various.nim
+++ tests/overload/toverload_various.nim
@@ -465,18 +465,3 @@ block:
 
   #type
   #  f0 = proc(x:Foo)
-
-block:
-  type
-    TilesetCT[n: static int] = int
-    TilesetRT = int
-    Tileset = TilesetCT | TilesetRT
-
-  func prepareTileset(tileset: var Tileset) = discard
-
-  func prepareTileset(tileset: Tileset): Tileset =
-    result = tileset
-    result.prepareTileset
-
-  var parsedTileset: TilesetRT
-  prepareTileset(parsedTileset)
