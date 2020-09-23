9a98eb7736095f5eb711c3402f4fb4bc327bb0ce
Clyybber <darkmine956@gmail.com>
2020-09-23 20:23:17 +0200

    Protect static NimNode against stripping

--- compiler/vm.nim
+++ compiler/vm.nim
@@ -2220,7 +2220,7 @@ proc evalStaticStmt*(module: PSym; idgen: IdGenerator; g: ModuleGraph; e: PNode,
 proc setupCompileTimeVar*(module: PSym; idgen: IdGenerator; g: ModuleGraph; n: PNode) =
   discard evalConstExprAux(module, idgen, g, nil, n, emStaticStmt)
 
-proc prepareVMValue(arg: PNode): PNode =
+proc prepareVMValue(g: ModuleGraph, arg: PNode): PNode =
   ## strip nkExprColonExpr from tuple values recurively. That is how
   ## they are expected to be stored in the VM.
 
@@ -2228,21 +2228,28 @@ proc prepareVMValue(arg: PNode): PNode =
   if arg.kind in nkLiterals:
     return arg
 
+  if arg.kind == nkExprColonExpr and arg[0].typ != nil and
+     arg[0].typ.sym != nil and $arg[0].typ == "NimNode" and
+     arg[0].typ.owner == g.systemModule:
+    # Poor mans way of protecting static NimNodes
+    # XXX: Maybe we need a nkNimNode?
+    return arg
+
   result = copyNode(arg)
   if arg.kind == nkTupleConstr:
     for child in arg:
       if child.kind == nkExprColonExpr:
-        result.add prepareVMValue(child[1])
+        result.add prepareVMValue(g, child[1])
       else:
-        result.add prepareVMValue(child)
+        result.add prepareVMValue(g, child)
   else:
     for child in arg:
-      result.add prepareVMValue(child)
+      result.add prepareVMValue(g, child)
 
-proc setupMacroParam(x: PNode, typ: PType): TFullReg =
+proc setupMacroParam(g: ModuleGraph, x: PNode, typ: PType): TFullReg =
   case typ.kind
   of tyStatic:
-    putIntoReg(result, prepareVMValue(x))
+    putIntoReg(result, prepareVMValue(g, x))
   else:
     var n = x
     if n.kind in {nkHiddenSubConv, nkHiddenStdConv}: n = n[1]
@@ -2306,13 +2313,13 @@ proc evalMacroCall*(module: PSym; idgen: IdGenerator; g: ModuleGraph; templInstC
 
   # setup parameters:
   for i in 1..<sym.typ.len:
-    tos.slots[i] = setupMacroParam(n[i], sym.typ[i])
+    tos.slots[i] = setupMacroParam(g, n[i], sym.typ[i])
 
   let gp = sym.ast[genericParamsPos]
   for i in 0..<gp.len:
     let idx = sym.typ.len + i
     if idx < n.len:
-      tos.slots[idx] = setupMacroParam(n[idx], gp[i].sym.typ)
+      tos.slots[idx] = setupMacroParam(g, n[idx], gp[i].sym.typ)
     else:
       dec(g.config.evalMacroCounter)
       c.callsite = nil
