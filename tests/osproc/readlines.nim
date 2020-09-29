f50f3ee1dbeceabcf48b23d8c8f5047c731afca8
Charles Blake <charlechaud@gmail.com>
2020-09-29 10:57:08 -0400

    Add test for new osproc.readLines

---
+++ tests/osproc/readlines.nim
@@ -0,0 +1,19 @@
+discard """
+  output: '''Error: cannot open 'a.nim'
+Error: cannot open 'b.nim''''
+  targets: "c"
+"""
+
+import osproc
+
+const opts = {poUsePath, poDaemon, poStdErrToStdOut}
+
+var ps: seq[Process] # compile & run 2 progs in parallel
+for prog in ["a", "b"]:
+  ps.add startProcess("nim", "", ["r", prog], nil, opts)
+
+for p in ps:
+  let (lines, exCode) = p.readLines
+  if exCode != 0:
+    for line in lines: echo line
+  p.close
