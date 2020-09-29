2a9d766109801bf5a79dabec909328c47b7b399c
Charles Blake <charlechaud@gmail.com>
2020-09-29 13:38:29 -0400

    Shoulda picked a program with simpler syntax than ..lol

--- tests/osproc/treadlines.nim
+++ tests/osproc/treadlines.nim
@@ -10,7 +10,7 @@ import osproc
 var ps: seq[Process] # compile & run 2 progs in parallel
 for prog in ["a", "b"]:
   ps.add startProcess("nim", "",
-                      ["r", "--hint[Conf]=off", "hint[Processing]=off", prog],
+                      ["r", "--hint[Conf]=off", "--hint[Processing]=off", prog],
                       options = {poUsePath, poDaemon, poStdErrToStdOut})
 
 for p in ps:
