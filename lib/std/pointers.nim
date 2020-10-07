8ee5d184fd527d61b15c3ebd0ce5f107c2012047
Timothee Cour <timothee.cour2@gmail.com>
2020-10-07 11:46:15 -0700

    address comments

--- lib/std/pointers.nim
+++ lib/std/pointers.nim
@@ -2,8 +2,10 @@
 Convenience procs to deal with pointer-like variables.
 ]##
 
-proc toUncheckedArray*[T](a: ptr[T]): ptr UncheckedArray[T] {.inline.} =
-  ## calls `cast[ptr UncheckedArray[T]]` (less error prone).
+proc toUncheckedArray*[T](a: ptr T): ptr UncheckedArray[T] {.inline.} =
+  ## Shortcut for `cast[ptr UncheckedArray[T]](a)`, where T is inferred.
+  ## This allows array indexing operations on `a`.
+  ## This is unsafe as it returns `UncheckedArray`.
   runnableExamples:
     var a = @[10, 11, 12]
     let pa = a[1].addr.toUncheckedArray
