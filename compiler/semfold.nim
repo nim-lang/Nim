cf8890d38264eefe72df562ef91555e2bb9fee49
Araq <rumpf_a@web.de>
2020-10-09 14:56:03 +0200

    fixes #15532

--- compiler/semfold.nim
+++ compiler/semfold.nim
@@ -719,6 +719,8 @@ proc getConstExpr(m: PSym, n: PNode; g: ModuleGraph): PNode =
       result.typ = n.typ
   of nkBracketExpr: result = foldArrayAccess(m, n, g)
   of nkDotExpr: result = foldFieldAccess(m, n, g)
+  of nkCheckedFieldExpr:
+    result = foldFieldAccess(m, n[0], g)
   of nkStmtListExpr:
     var i = 0
     while i <= n.len - 2:
