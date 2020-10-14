47c5ca179ec59bd7410497137bd15236b90d9b95
Clyybber <darkmine956@gmail.com>
2020-10-14 23:54:39 +0200

    Assign hook name changed to `=copy`

--- compiler/sempass2.nim
+++ compiler/sempass2.nim
@@ -810,6 +810,7 @@ proc trackCall(tracked: PEffects; n: PNode) =
   if a.kind == nkSym and a.sym.name.s.len > 0 and a.sym.name.s[0] == '=' and
         tracked.owner.kind != skMacro:
     let opKind = find(AttachedOpToStr, a.sym.name.s.normalize)
+    if a.sym.name.s.normalize == "=": opKind = attachedAsgn
     if opKind != -1:
       # rebind type bounds operations after createTypeBoundOps call
       let t = n[1].typ.skipTypes({tyAlias, tyVar})
