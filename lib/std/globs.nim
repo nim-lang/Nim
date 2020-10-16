d72cb38861902345f16211058f955c5f59f21fd1
Timothee Cour <timothee.cour2@gmail.com>
2020-10-15 20:08:49 -0700

    works

--- lib/std/globs.nim
+++ lib/std/globs.nim
@@ -11,12 +11,13 @@ type
       ## absolute or relative path wrt globbed dir
     depth*: int
       ## depth wrt globbed dir
+    epilogue*: bool
 
-iterator glob*(dir: string, relative = false, checkDir = true, includeRoot = false,
+iterator glob*(dir: string, relative = false, checkDir = true, includeRoot = false, includeEpilogue = false,
     follow: proc(entry: PathEntry): bool = nil,
     sortCmp: proc (x, y: PathEntrySub): int = nil,
     topFirst = true):
-    PathEntry {.closure, tags: [ReadDirEffect].} =
+    PathEntry {.tags: [ReadDirEffect].} =
   ## Recursively walks `dir` which must exist when checkDir=true (else raises `OSError`).
   ## Paths in `result.path` are relative to `dir` unless `relative=false`,
   ## `result.depth >= 1` is the tree depth relative to the root `dir` (at depth 0).
@@ -32,69 +33,53 @@ iterator glob*(dir: string, relative = false, checkDir = true, includeRoot = fal
   note:
   * a yieldFilter, regex match etc isn't needed because caller can filter at
   call site, without loss of generality, unlike `follow`; this simplifies the API.
-  * a closure iterator is used since we need multiple yield statements and it simplifies code.
-    In practice optimized performance drops by less than 2%, likely 0 when filesystem is not "hot".
 
   Future work:
-  * need to document
-  * add a `sort` option, which can be implemented efficiently only here, not at call site.
   * provide a way to do error reporting, which is tricky because iteration cannot be resumed
   * `walkDirRec` can be implemented in terms of this to avoid duplication,
     modulo some refactoring.
   ]#
-  echo()
-  var stack = @[(0, ".")]
+  var entry = PathEntry(depth: 0, path: ".")
+  entry.kind = if symlinkExists(dir): pcLinkToDir else: pcDir
+  var stack: seq[PathEntry]
+
   var checkDir = checkDir
-  var entry: PathEntry
+  if dirExists(dir):
+    stack.add entry
+  elif checkDir:
+    raise newException(OSError, "invalid root dir: " & dir)
+
   var dirsLevel: seq[PathEntrySub]
-  if not dirExists(dir):
-    if checkDir:
-      raise newException(OSError, "invalid root dir: " & dir)
-    else:
-      return
-
-  if includeRoot:
-    if symlinkExists(dir):
-      entry.kind = pcLinkToDir
-    else:
-      entry.kind = pcDir
-    entry.path = if relative: "." else: dir
-    normalizePath(entry.path)
-    entry.depth = 0
-    yield entry
-
-  template processEntry(): untyped =
-    let rel = d / p
-    entry.depth = depth + 1
-    entry.kind = k
-    if relative: entry.path = rel
-    else: entry.path = dir / rel
-    normalizePath(entry.path) # pending https://github.com/timotheecour/Nim/issues/343
-    if k in {pcDir, pcLinkToDir}:
-      if follow == nil or follow(entry): stack.add (depth + 1, rel)
-    entry
-
   while stack.len > 0:
-    let (depth, d) = stack.pop()
-    # checkDir is still needed here in first iteration because things could
-    # fail for reasons other than `not dirExists`.
+    let current = stack.pop()
+    # let current = stack.popFront()
+    entry.epilogue = current.epilogue
+    entry.depth = current.depth
+    entry.kind = current.kind
+    entry.path = if relative: current.path else: dir / current.path
+    normalizePath(entry.path) # pending https://github.com/timotheecour/Nim/issues/343
 
-    if sortCmp != nil:
-      dirsLevel.setLen 0
-    # echo "...: ", d
-    for k, p in walkDir(dir / d, relative = true, checkDir = checkDir):
-      if sortCmp != nil:
-        dirsLevel.add PathEntrySub(kind: k, path: p)
-      else:
-        yield processEntry()
-    if sortCmp != nil:
-      sort(dirsLevel, sortCmp)
-      for ai in dirsLevel:
-        let k = ai.kind
-        let p = ai.path
-        yield processEntry()
+    if includeRoot or current.depth > 0:
+      yield entry
 
-    checkDir = false
-      # We only check top-level dir, otherwise if a subdir is invalid (eg. wrong
-      # permissions), it'll abort iteration and there would be no way to
-      # continue iteration.
+    if current.kind in {pcDir, pcLinkToDir} and not current.epilogue:
+      if follow == nil or follow(current):
+        if sortCmp != nil:
+          dirsLevel.setLen 0
+        if includeEpilogue:
+          stack.add PathEntry(depth: current.depth, path: current.path, kind: current.kind, epilogue: true)
+        # checkDir is still needed here in first iteration because things could
+        # fail for reasons other than `not dirExists`.
+        for k, p in walkDir(dir / current.path, relative = true, checkDir = checkDir):
+          if sortCmp != nil:
+            dirsLevel.add PathEntrySub(kind: k, path: p)
+          else:
+            stack.add PathEntry(depth: current.depth + 1, path: current.path / p, kind: k)
+        checkDir = false
+          # We only check top-level dir, otherwise if a subdir is invalid (eg. wrong
+          # permissions), it'll abort iteration and there would be no way to resume iteration.
+        if sortCmp != nil:
+          sort(dirsLevel, sortCmp)
+          for i in countdown(dirsLevel.len-1, 0):
+            let ai = dirsLevel[i]
+            stack.add PathEntry(depth: current.depth + 1, path: current.path / ai.path, kind: ai.kind)
