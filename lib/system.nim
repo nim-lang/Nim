f8890a0804b6d05a14c9d64d71e1cb27a36bc5ca
narimiran <narimiran@disroot.org>
2020-10-16 07:36:09 +0200

    change NimVersion to 1.5.1

--- lib/system.nim
+++ lib/system.nim
@@ -2102,11 +2102,11 @@ const
     ##   when (NimMajor, NimMinor, NimPatch) >= (1, 3, 1): discard
     # see also std/private/since
 
-  NimMinor* {.intdefine.}: int = 3
+  NimMinor* {.intdefine.}: int = 5
     ## is the minor number of Nim's version.
     ## Odd for devel, even for releases.
 
-  NimPatch* {.intdefine.}: int = 7
+  NimPatch* {.intdefine.}: int = 1
     ## is the patch number of Nim's version.
     ## Odd for devel, even for releases.
 
