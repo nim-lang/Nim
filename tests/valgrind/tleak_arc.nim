cc36a0d3922fbb4fbe7139b3ec57e56102143462
Clyybber <darkmine956@gmail.com>
2020-10-19 15:56:15 +0200

    Remove redundant disables

--- tests/valgrind/tleak_arc.nim
+++ tests/valgrind/tleak_arc.nim
@@ -3,11 +3,6 @@ valgrind: true
 cmd: "nim $target --gc:arc -d:useMalloc $options $file"
 exitcode: 1
 outputsub: "   definitely lost: 7 bytes in 2 blocks"
-disabled: "freebsd"
-disabled: "macosx"
-disabled: "openbsd"
-disabled: "windows"
-disabled: "32bit"
 """
 
 discard alloc(3)
