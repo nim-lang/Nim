e2ca1164a81fd65de1c9762c0fc8ace5ef796191
n5m <72841454+n5m@users.noreply.github.com>
2020-10-19 01:19:13 +0000

    remove non-malloc tests

--- tests/valgrind/tleak_arc_no_malloc.nim
+++
@@ -1,9 +0,0 @@
-discard """
-valgrind: true
-cmd: "nim $target --gc:arc $options $file"
-exitcode: 0
-"""
-
-# unlike tleak_arc_useMalloc.nim, tleak_arc_noUseMalloc.nim is powerless to
-# detect the memory leak because malloc is not used
-discard alloc(1)
