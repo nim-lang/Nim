527c532153646f7fc267c290211f279872d83236
Timothee Cour <timothee.cour2@gmail.com>
2020-10-23 12:35:52 +0200

    show declaration location also when type mismatch names clash

--- compiler/types.nim
+++ compiler/types.nim
@@ -130,13 +130,15 @@ proc addDeclaredLocMaybe*(result: var string, conf: ConfigRef; sym: PSym) =
   if optDeclaredLocs in conf.globalOptions and sym != nil:
     addDeclaredLoc(result, conf, sym)
 
+proc addDeclaredLoc(result: var string, conf: ConfigRef; typ: PType) =
+  let typ = typ.skipTypes(abstractInst - {tyRange})
+  result.add " [$1" % typ.kind.toHumanStr
+  if typ.sym != nil:
+    result.add " declared in " & toFileLineCol(conf, typ.sym.info)
+  result.add "]"
+
 proc addDeclaredLocMaybe*(result: var string, conf: ConfigRef; typ: PType) =
-  if optDeclaredLocs in conf.globalOptions:
-    let typ = typ.skipTypes(abstractInst - {tyRange})
-    result.add " [$1" % typ.kind.toHumanStr
-    if typ.sym != nil:
-      result.add " declared in " & toFileLineCol(conf, typ.sym.info)
-    result.add "]"
+  if optDeclaredLocs in conf.globalOptions: addDeclaredLoc(result, conf, typ)
 
 proc addTypeHeader*(result: var string, conf: ConfigRef; typ: PType; prefer: TPreferedDesc = preferMixed; getDeclarationPath = true) =
   result.add typeToString(typ, prefer)
@@ -1480,13 +1482,19 @@ proc skipHiddenSubConv*(n: PNode): PNode =
 
 proc typeMismatch*(conf: ConfigRef; info: TLineInfo, formal, actual: PType) =
   if formal.kind != tyError and actual.kind != tyError:
-    let named = typeToString(formal)
+    let actualStr = typeToString(actual)
+    let formalStr = typeToString(formal)
     let desc = typeToString(formal, preferDesc)
-    let x = if named == desc: named else: named & " = " & desc
-    var msg = "type mismatch: got <" & typeToString(actual) & ">"
-    msg.addDeclaredLocMaybe(conf, actual)
-    msg.add " but expected '" & x & "'"
-    msg.addDeclaredLocMaybe(conf, formal)
+    let x = if formalStr == desc: formalStr else: formalStr & " = " & desc
+    let verbose = actualStr == formalStr or optDeclaredLocs in conf.globalOptions
+    var msg = "type mismatch:"
+    if verbose: msg.add "\n"
+    msg.add  " got <$1>" % actualStr
+    if verbose:
+      msg.addDeclaredLoc(conf, actual)
+      msg.add "\n"
+    msg.add " but expected '$1'" % x
+    if verbose: msg.addDeclaredLoc(conf, formal)
 
     if formal.kind == tyProc and actual.kind == tyProc:
       case compatibleEffects(formal, actual)
