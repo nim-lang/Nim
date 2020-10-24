0c807f773ffec1cf2bbe20e6c94479cf183692d7
n5m <72841454+n5m@users.noreply.github.com>
2020-10-24 00:00:07 +0000

    highlight that a compilation failure is expected

--- testament/tests/shouldfail/treject.nim
+++ testament/tests/shouldfail/treject.nim
@@ -2,5 +2,6 @@ discard """
 action: "reject"
 """
 
-# test will fail because action="reject" in the config
+# Because we set action="reject", we expect this line to not compile. But the
+# line does compile, therefore the test fails.
 assert true
