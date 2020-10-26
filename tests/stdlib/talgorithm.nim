f39066286b8d643d6b543351d77fd7e2f6c6602b
shirleyquirk <31934565+shirleyquirk@users.noreply.github.com>
2020-10-26 15:06:27 +0000

    Create talgorithm.nim

---
+++ tests/stdlib/talgorithm.nim
@@ -0,0 +1,18 @@
+discard """
+  output:'''@[1, 2, 3]
+  '''
+"""
+#12928,10456
+import sequtils, strutils, algorithm, json
+
+proc test() = 
+  try: 
+    let info = parseJson("""
+    {"a": ["1", "2", "3"]}
+    """)
+    let prefixes = info["a"].getElems().mapIt(it.getStr()).sortedByIt(it).reversed()
+    echo prefixes
+  except:
+    discard
+  
+test()
