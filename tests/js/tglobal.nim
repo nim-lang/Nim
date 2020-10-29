9d3af21c28b781424151cba3f80ad0eed1bea970
flywind <xzsflywind@gmail.com>
2020-10-29 23:43:55 +0800

    add support for threadvar

--- tests/js/tglobal.nim
+++ tests/js/tglobal.nim
@@ -1,11 +1,22 @@
-proc getState*(): int =
-  var state0 {.global.}: int
-  inc state0
-  result = state0
+block global:
+  proc getState(): int =
+    var state0 {.global.}: int
+    inc state0
+    result = state0
 
-for i in 0 ..< 3:
-  doAssert getState() == i + 1
+  for i in 0 ..< 3:
+    doAssert getState() == i + 1
 
-for i in 0 ..< 3:
-  once:
-    doAssert i == 0
+  for i in 0 ..< 3:
+    once:
+      doAssert i == 0
+
+
+block:
+  proc getThreadState(): int =
+    var state0 {.threadvar.}: int
+    inc state0
+    result = state0
+
+  for i in 0 ..< 3:
+    doAssert getThreadState() == i + 1
