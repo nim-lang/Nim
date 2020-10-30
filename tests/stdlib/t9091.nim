7c0550b147239a2a11d4a7c3fdf24dc5ae717932
flywind <xzsflywind@gmail.com>
2020-10-30 16:07:44 +0800

    more tests

--- tests/stdlib/t9091.nim
+++ tests/stdlib/t9091.nim
@@ -1,19 +1,36 @@
 discard """
   targets:  "c"
-  nimout:   ""
+  output:   "test AObj"
   action:   "compile"
   exitcode: 0
   timeout:  60.0
 """
 import streams
 
-type Mine = ref object
-  a: int
+block:
+  type Mine = ref object
+    a: int
 
-proc write*(io: Stream, t: Mine) =
-  io.write("sure")
+  proc write(io: Stream, t: Mine) =
+    io.write("sure")
 
-let str = newStringStream()
-let mi = new Mine
+  let str = newStringStream()
+  let mi = new Mine
 
-str.write(mi)
+  str.write(mi)
+
+block:
+  type
+    AObj = object
+      x: int
+
+  proc foo(a: int): string = ""
+
+  proc test(args: varargs[string, foo]) =
+    echo "varargs"
+
+  proc test(a: AObj) =
+    echo "test AObj"
+
+  let x = AObj()
+  test(x)
