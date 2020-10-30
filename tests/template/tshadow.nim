7f743f30e2e7e167390fe3c1dccc5501c57bddac
flywind <xzsflywind@gmail.com>
2020-10-30 10:34:26 +0800

    add testcase for #7374

---
+++ tests/template/tshadow.nim
@@ -0,0 +1,24 @@
+block:
+  template init(initHook: proc(s: string)) =
+    proc dostuff =
+      var s = "fish"
+      initHook(s)
+    dostuff()
+
+  init do(s: string):
+    echo s
+
+import macros
+
+block:
+  macro init(initHook: proc(s: string)) =
+    result = newStmtList(
+      newProc(name = ident("dostuff"), body = newStmtList(
+        newVarStmt(ident("s"), newStrLitNode("fish")),
+        newCall(initHook, ident("s"))
+      )),
+      newCall("dostuff")
+    )
+
+  init proc(s: string) =
+    echo s
