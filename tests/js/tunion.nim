fdff1a0136761246de5e8adbe4ae107f642efe4c
flywind <xzsflywind@gmail.com>
2020-10-30 11:43:38 +0800

    more

--- tests/js/tunion.nim
+++ tests/js/tunion.nim
@@ -5,14 +5,3 @@ discard """
 type Foo {.union.} = object
   as_bytes: array[8, int8]
   data: int64
-
-
-var a = Foo(data: 12345)
-
-echo a.as_bytes
-echo a.data
-
-a.as_bytes[0] = 1
-
-echo a.data
-echo a.as_bytes
