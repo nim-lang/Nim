a3afa83804bf66773cbe9cd1f2f7cabc5517ca0c
flywind <xzsflywind@gmail.com>
2020-10-31 12:20:49 +0800

    minor

--- compiler/jsgen.nim
+++ compiler/jsgen.nim
@@ -1927,7 +1927,6 @@ proc genRepr(p: PProc, n: PNode, r: var TCompRes) =
     genReprAux(p, n, r, "reprJSONStringify")
   else:
     genReprAux(p, n, r, "reprAny", genTypeInfo(p, t))
-  echo r.kind
   r.kind = resExpr
 
 proc genOf(p: PProc, n: PNode, r: var TCompRes) =
