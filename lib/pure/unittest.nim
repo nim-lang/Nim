adde5ba6b06e12f74c7746f93883ad009cfb00b8
flywind <43030857+xflywind@users.noreply.github.com>
2020-11-05 21:04:24 +0800

    document #15618 (#15810)
    * document #15618
    
    * Update lib/pure/unittest.nim
    
    * Update lib/pure/unittest.nim
    
    Co-authored-by: Juan Carlos <juancarlospaco@gmail.com>
    
    * Update lib/pure/unittest.nim
    
    Co-authored-by: Andreas Rumpf <rumpf_a@web.de>
    Co-authored-by: Juan Carlos <juancarlospaco@gmail.com>

--- lib/pure/unittest.nim
+++ lib/pure/unittest.nim
@@ -92,6 +92,15 @@
 ##         discard v[4]
 ##
 ##     echo "suite teardown: run once after the tests"
+## 
+## Limitations/Bugs
+## ================
+## Since `check` will rewrite some programs for
+## supporting checkpoints (namely assigns expressions to variables).
+## Some type conversions are not supported.
+## For example `check 4.0 == 2 + 2` won't work. But `doAssert 4.0 == 2 + 2` works.
+## Make sure both sides of the operator (such as `==`, `>=` and so on) have the same type.
+##
 
 import std/private/since
 import std/exitprocs
