4003c6f7251baa40c5e5c99e252f395d12186d54
Timothee Cour <timothee.cour2@gmail.com>
2020-11-06 18:20:02 -0800

    fix test

--- tests/stdlib/tasserts.nim
+++ tests/stdlib/tasserts.nim
@@ -10,9 +10,9 @@ import std/strutils
 # line 10
 block: ## checks AST isn't transformed as it used to
   let a = 1
-  enforce a == 1
+  enforce a == 1, $a
   try:
-    enforce a > 1
+    enforce a > 1, $a
   except CatchableError as e:
-    assert e.msg.endsWith "tasserts.nim(15, 13) `a > 1` "
-  doAssertRaises(CatchableError): enforce a > 1
+    assert e.msg.endsWith "tasserts.nim(15, 13) `a > 1` 1"
+  doAssertRaises(CatchableError): enforce a > 1, $a
