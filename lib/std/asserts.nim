105c94cdc09be1af73d79c34bf41b0744f1c6f5f
Timothee Cour <timothee.cour2@gmail.com>
2020-11-06 17:01:18 -0800

    made `msg` required

--- lib/std/asserts.nim
+++ lib/std/asserts.nim
@@ -1,15 +1,13 @@
 import system/assertions
 import std/private/miscdollars
 
-template enforce*(cond: untyped, msg = "", typ: typedesc = CatchableError) =
+template enforce*(cond: untyped, msg: string, typ: typedesc = CatchableError) =
   ## similar to `doAssert` but defaults to raising `CatchableError` instead of
   ## `AssertionDefect`, and allows customizing the raised exception type.
   runnableExamples:
     let a = 1
-    enforce a == 1
     enforce a == 1, $(a,)
-    doAssertRaises(CatchableError): enforce a == 2
-    doAssertRaises(ValueError): enforce a == 2, typ = ValueError
+    doAssertRaises(CatchableError): enforce a == 2, $(a,)
     doAssertRaises(ValueError): enforce a == 2, $(a,), ValueError
 
   const
