b45a69bf81547b0c3bcfec4dbe33021632cd6f08
Juan Carlos <JuanCarlospaco@gmail.com>
2020-11-12 02:34:38 -0300

    allow to trash several files with the same name

--- lib/pure/os.nim
+++ lib/pure/os.nim
@@ -3303,7 +3303,7 @@ func isValidFilename*(filename: string, maxLen = 259.Positive): bool {.since: (1
 
 
 since (1, 5):
-  let osTrash*: string =
+  let osTrash* {.noWeirdTarget.}: string =
     when defined(linux) or defined(bsd):
       getEnv("XDG_DATA_HOME", getHomeDir()) / ".local/share/Trash"
     elif defined(osx):
@@ -3311,7 +3311,7 @@ since (1, 5):
     else:
       getTempDir() # Android has no Trash, some apps just use a temporary folder.
 
-  proc moveFileToTrash*(filename: string; trashPath = osTrash; postfixStart = 1.Positive): string =
+  proc moveFileToTrash*(filename: string; trashPath = osTrash; postfixStart = 1.Positive): string {.noWeirdTarget.} =
     ## Move file from `filename` to `trashPath`, `trashPath` defaults to `osTrash`.
     ##
     ## If a file with the same name already exists in the Trash folder,
@@ -3360,7 +3360,7 @@ since (1, 5):
       moveFile(expandFilename(filename), trashed)
     result = trashed
 
-  proc moveFileFromTrash*(filename: string; trashPath = osTrash) =
+  proc moveFileFromTrash*(filename: string; trashPath = osTrash) {.noWeirdTarget.} =
     ## Move file from `trashPath` to `filename`, `trashPath` defaults to `osTrash`.
     ##
     ## If a file with the same name already exists in the Trash folder,
