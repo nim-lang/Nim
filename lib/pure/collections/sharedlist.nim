3c511fa0b09b031afe2827afd05a1f2da910fd73
flywind <xzsflywind@gmail.com>
2020-11-13 09:36:55 +0800

    fix #15941

--- lib/pure/collections/sharedlist.nim
+++ lib/pure/collections/sharedlist.nim
@@ -65,11 +65,16 @@ iterator items*[A](x: var SharedList[A]): A =
 proc add*[A](x: var SharedList[A]; y: A) =
   withLock(x):
     var node: SharedListNode[A]
-    if x.tail == nil or x.tail.dataLen == ElemsPerNode:
+    if x.tail == nil:
       node = cast[type node](allocShared0(sizeof(node[])))
       node.next = x.tail
       x.tail = node
-      if x.head == nil: x.head = node
+      if x.head == nil:
+        x.head = node
+    elif x.tail.dataLen == ElemsPerNode:
+      node = cast[type node](allocShared0(sizeof(node[])))
+      x.tail.next = node
+      x.tail = node
     else:
       node = x.tail
     node.d[node.dataLen] = y
