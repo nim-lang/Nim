48828f5008f909495cc9b96ade6b3e9df7d1d9c1
Juan Carlos <JuanCarlospaco@gmail.com>
2020-11-12 22:50:56 -0300

    Improve a test

--- tests/stdlib/tstrmisc.nim
+++ tests/stdlib/tstrmisc.nim
@@ -10,12 +10,11 @@ func main() =
   doAssert parseFloatThousandSep("-1.0") == -1.0
   doAssert parseFloatThousandSep("1.000") == 1.0
   doAssert parseFloatThousandSep("-1.000") == -1.0
-  doAssertRaises(ValueError):
-    discard parseFloatThousandSep("1,0000.000")
-    discard parseFloatThousandSep("--")
-    discard parseFloatThousandSep("..")
-    discard parseFloatThousandSep("000,1.000,,,,,,,,,,,,,,000,,,,,0000")
-    discard parseFloatThousandSep("000.1,000..............000.....0000")
+  doAssertRaises(ValueError): discard parseFloatThousandSep("1,0000.000")
+  doAssertRaises(ValueError): discard parseFloatThousandSep("--")
+  doAssertRaises(ValueError): discard parseFloatThousandSep("..")
+  doAssertRaises(ValueError): discard parseFloatThousandSep("000,1.000,,,,,,,,,,,,,,000,,,,,0000")
+  doAssertRaises(ValueError): discard parseFloatThousandSep("000.1,000..............000.....0000")
 
 
 main()
