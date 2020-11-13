04ba7a12a3221e92768c831c9105d20d7f727411
bung87 <crc32@qq.com>
2020-11-13 16:19:01 +0800

    also affect to vm

--- tests/js/t8404.nim
+++ tests/js/t8404.nim
@@ -1,29 +1,32 @@
 discard """
   targets: "c cpp js"
 """
-block: # bug #8404
-  # can conv
-  template float2int(T) =
-    var a = -1.0
-    let b = T(a)
-    doAssert b < 0
-    let c = b + 1
-    doAssert c is T
-    doAssert c == 0
+template main() =
+  block: # bug #8404
+    # can conv
+    template float2int(T) =
+      var a = -1.0
+      let b = T(a)
+      doAssert b < 0
+      let c = b + 1
+      doAssert c is T
+      doAssert c == 0
 
-  float2int(int8)
-  float2int(int16)
-  float2int(int32)
-  float2int(int64)
+    float2int(int8)
+    float2int(int16)
+    float2int(int32)
+    float2int(int64)
 
-block:
-  # can handle middle conv
-  # `/` can trigger int to float
-  template float2int(T) =
-    let n = T(1 / 256)
-    doAssert n == 0
+  block:
+    # can handle middle conv
+    # `/` can trigger int to float
+    template float2int(T) =
+      let n = T(1 / 256)
+      doAssert n == 0
 
-  float2int(int8)
-  float2int(int16)
-  float2int(int32)
-  # float2int(int64)
+    float2int(int8)
+    float2int(int16)
+    float2int(int32)
+    # float2int(int64)
+static:
+  main()
