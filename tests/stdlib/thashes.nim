67adeaacec9fdbbf981fe61f1425e91d8e98abe3
Bung <crc32@qq.com>
2020-11-13 16:05:43 +0800

    Update thashes.nim
    no need mention PR #15915, fixed in https://github.com/nim-lang/Nim/pull/15937

--- tests/stdlib/thashes.nim
+++ tests/stdlib/thashes.nim
@@ -23,7 +23,7 @@ suite "hashes":
 
     test "VM and runtime should make the same hash value (hashWangYi1)":
       const wy123 = hashWangYi1(123)
-      doAssert wy123 != 0, "need result = in hashWangYi1 see PR 15915 "
+      check wy123 != 0
       check hashWangYi1(123) == wy123
 
     test "hashIdentity value incorrect at 456":
