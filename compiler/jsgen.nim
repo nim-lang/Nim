d14eab536085980a5b421afae9cd941ed9a8ec3e
flywind <xzsflywind@gmail.com>
2020-11-14 18:09:05 +0800

    minor

--- compiler/jsgen.nim
+++ compiler/jsgen.nim
@@ -1071,7 +1071,7 @@ proc genAsgnAux(p: PProc, x, y: PNode, noCopyNeeded: bool) =
     else:
       useMagic(p, "nimCopy")
       # supports proc getF(): var T
-      if x.len > 0 and x[0].kind in nkCallKinds:
+      if x.kind == nkHiddenDeref and x[0].kind in nkCallKinds:
           lineF(p, "nimCopy($1, $2, $3);$n", 
                 [a.res, b.res, genTypeInfo(p, y.typ)])
       else:
