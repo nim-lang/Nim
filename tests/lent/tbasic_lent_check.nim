e4bfa99ec2e2ca1d648a6a2230c48e79f133c2f1
Andrey R (cooldome) <ariabushenko@gmail.com>
2020-11-15 18:33:05 +0000

    more tests

--- tests/lent/tbasic_lent_check.nim
+++ tests/lent/tbasic_lent_check.nim
@@ -23,12 +23,18 @@ main()
 block:
   proc byLent[T](a: T): lent T = a
   let a = [11,12]
+  let b = @[21,23]
   doAssert byLent(a) == [11,12]
   doAssert byLent(a).unsafeAddr == a.unsafeAddr
+  doAssert byLent(b) == @[21,23]
+  doAssert byLent(b).unsafeAddr == b.unsafeAddr
 
   proc byLent2[T](a: openarray[T]): lent T = a[0]
   doAssert byLent2(a) == 11
   doAssert byLent2(a).unsafeAddr == a[0].unsafeAddr
+  doAssert byLent2(b) == 21
+  doAssert byLent2(b).unsafeAddr == b[0].unsafeAddr
+
 
   proc byLent3[T](a: varargs[T]): lent T = a[1]
   let 
