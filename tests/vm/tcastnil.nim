3581a15044dadd728c3d4bd827f3ac4af3e9ff8a
flywind <xzsflywind@gmail.com>
2020-11-17 12:14:22 +0800

    follow #16009 VM supports cast nil to ptr

---
+++ tests/vm/tcastnil.nim
@@ -0,0 +1,25 @@
+discard """
+  nimout: '''nil
+nil
+nil
+nil
+'''
+"""
+
+block:
+  static:
+    let a = cast[pointer](nil)
+    echo a.repr
+
+block:
+  static:
+    echo cast[ptr int](nil).repr
+
+block:
+  const str = cast[ptr int](nil)
+  static:
+    echo str.repr
+
+block:
+  static:
+    echo cast[ptr int](nil).repr
