b4c2aff0d926013b9080f4dc06625d6817fa6e0c
Juan Carlos <JuanCarlospaco@gmail.com>
2020-11-16 23:35:17 -0300

    use endOfFile

--- lib/pure/os.nim
+++ lib/pure/os.nim
@@ -3193,8 +3193,11 @@ proc sameFileContent*(path1, path2: string; checkSize = false; bufferSize = 8192
   ## if the files have different file sizes they can not have the same contents,
   ## `checkSize = true` may be faster, specially for very big files.
   ##
+  ## `bufferSize` must be a power of two.
+  ##
   ## See also:
   ## * `sameFile proc <#sameFile,string,string>`_
+  assert (bufferSize and (bufferSize - 1)) == 0
   var a, b: File
   var mustRead = true
   var bufA = alloc(bufferSize)
