8654d9ccb3205f06a241c5b12f5d67ccc68c8f29
flywind <xzsflywind@gmail.com>
2020-11-18 11:46:49 +0800

    tests

--- tests/ccgbugs/t15623.nim
+++ tests/ccgbugs/t15623.nim
@@ -1,3 +1,4 @@
+# bug #15623
 block:
   if false:
     echo cast[ptr int](nil)[]
@@ -6,3 +7,10 @@ block:
   if false:
     var x: ref int = nil
     echo cast[ptr int](x)[]
+
+block:
+  doAssert cast[int](cast[ptr int](nil)) == 0
+
+block:
+  var x: ref int = nil
+  doAssert cast[int](cast[ptr int](x)) == 0
--- tests/ccgbugs/t15623_2.nim
+++
@@ -1,7 +0,0 @@
-# bug #15623
-block:
-  doAssert cast[int](cast[ptr int](nil)) == 0
-
-block:
-  var x: ref int = nil
-  doAssert cast[int](cast[ptr int](x)) == 0
