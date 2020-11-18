828ad7463bcebb6092d96f5e50a88734bffef9a1
flywind <xzsflywind@gmail.com>
2020-11-18 11:48:20 +0800

    more

--- tests/misc/tcast.nim
+++ tests/misc/tcast.nim
@@ -29,3 +29,21 @@ reject: discard cast[enum](0)
 proc a = echo "hi"
 
 reject: discard cast[ptr](a)
+
+# bug #15623
+block:
+  if false:
+    echo cast[ptr int](nil)[]
+
+block:
+  if false:
+    var x: ref int = nil
+    echo cast[ptr int](x)[]
+
+block:
+  doAssert cast[int](cast[ptr int](nil)) == 0
+
+block:
+  var x: ref int = nil
+  doAssert cast[int](cast[ptr int](x)) == 0
+
