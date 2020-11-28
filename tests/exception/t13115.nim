1cceff66db9eb0bc9e775b4a2c9bbd4a89254ae4
Timothee Cour <timothee.cour2@gmail.com>
2020-11-27 20:26:06 -0800

    fix partially #13115 properly (works for c,js,cpp,vm; still fails for js on openbsd)

--- tests/exception/t13115.nim
+++ tests/exception/t13115.nim
@@ -1,12 +1,14 @@
 discard """
   exitcode: 1
-  targets: "c"
+  targets: "c js cpp"
   matrix: "-d:debug; -d:release"
   outputsub: ''' and works fine! [Exception]'''
 """
 
 # bug #13115
-# xxx bug: doesn't yet work for cpp
 
-var msg = "This char is `" & '\0' & "` and works fine!"
-raise newException(Exception, msg)
+template fn =
+  var msg = "This char is `" & '\0' & "` and works fine!"
+  raise newException(Exception, msg)
+# static: fn() # would also work
+fn()
