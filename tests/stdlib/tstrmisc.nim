fcc5c3594a197124fe7f402e17b9746b3cff5825
Juan Carlos <JuanCarlospaco@gmail.com>
2020-11-29 11:02:49 -0300

    We need doAssert that takes varargs for tests

--- tests/stdlib/tstrmisc.nim
+++ tests/stdlib/tstrmisc.nim
@@ -1,4 +1,4 @@
-import strmisc
+import strmisc, math
 
 
 func main() =
@@ -14,32 +14,16 @@ func main() =
   doAssert parseFloatThousandSep("6.283185307179586") == 6.283185307179586
   doAssert parseFloatThousandSep("2.718281828459045") == 2.718281828459045
 
-  doAssertRaises(ValueError): discard parseFloatThousandSep("1,11")
-  doAssertRaises(ValueError): discard parseFloatThousandSep("1,1")
-  doAssertRaises(ValueError): discard parseFloatThousandSep("1,0000.000")
-  doAssertRaises(ValueError): discard parseFloatThousandSep("--")
-  doAssertRaises(ValueError): discard parseFloatThousandSep("..")
-  doAssertRaises(ValueError): discard parseFloatThousandSep("1,,000")
-  doAssertRaises(ValueError): discard parseFloatThousandSep("1..000")
-  doAssertRaises(ValueError): discard parseFloatThousandSep("1,000000")
-  doAssertRaises(ValueError): discard parseFloatThousandSep(",1")
-  doAssertRaises(ValueError): discard parseFloatThousandSep("1,")
-  doAssertRaises(ValueError): discard parseFloatThousandSep("1.")
-  doAssertRaises(ValueError): discard parseFloatThousandSep(".1")
   doAssertRaises(ValueError): discard parseFloatThousandSep(" ", {pfDotOptional})
   doAssertRaises(ValueError): discard parseFloatThousandSep(".1.", {pfLeadingDot,pfTrailingDot})
-  doAssertRaises(ValueError): discard parseFloatThousandSep("10,00.0")
-  doAssertRaises(ValueError): discard parseFloatThousandSep("1,000.000ee9")
   doAssertRaises(ValueError): discard parseFloatThousandSep("1ee9", {pfDotOptional})
-  doAssertRaises(ValueError): discard parseFloatThousandSep("1e02.2")
-  doAssertRaises(ValueError): discard parseFloatThousandSep("1.0e--9")
-  doAssertRaises(ValueError): discard parseFloatThousandSep("Inf")
-  doAssertRaises(ValueError): discard parseFloatThousandSep("-Inf")
-  doAssertRaises(ValueError): discard parseFloatThousandSep("+Inf")
-  doAssertRaises(ValueError): discard parseFloatThousandSep("NaN")
   doAssertRaises(ValueError): discard parseFloatThousandSep("aNa", {pfNanInf})
   doAssertRaises(ValueError): discard parseFloatThousandSep("fnI", {pfNanInf})
   doAssertRaises(ValueError): discard parseFloatThousandSep("1,000.000,000,E,+,9,0", {pfSepAnywhere})
+  for s in ["1,11", "1,1", "1,0000.000", "--", "..", "1,,000", "1..000",
+    "1,000000", ",1", "1,", "1.", ".1", "10,00.0", "1,000.000ee9", "1e02.2",
+    "1.0e--9", "Inf", "-Inf", "+Inf", "NaN"]:
+    doAssertRaises(ValueError): discard parseFloatThousandSep(s)
 
   doAssert parseFloatThousandSep("10,00.0", {pfSepAnywhere}) == 1000.0
   doAssert parseFloatThousandSep("0", {pfDotOptional}) == 0.0
@@ -68,7 +52,7 @@ func main() =
   doAssert parseFloatThousandSep("1000.000000E+90") == 1e93
   doAssert parseFloatThousandSep("-10 000 000 000.0001", sep=' ') == -10000000000.0001
   doAssert parseFloatThousandSep("-10 000 000 000,0001", sep=' ', decimalDot = ',') == -10000000000.0001
-  discard parseFloatThousandSep("NaN", {pfNanInf})
+  doAssert classify(parseFloatThousandSep("NaN", {pfNanInf})) == fcNan
 
 
 main()
