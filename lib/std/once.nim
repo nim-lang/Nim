96b385efc50c0ab85e9be08474872e1c257e9e64
flywind <xzsflywind@gmail.com>
2020-11-30 17:55:08 +0800

    add std/once

---
+++ lib/std/once.nim
@@ -0,0 +1,31 @@
+import atomics
+import locks
+
+
+type
+  Once = object
+    finished: Atomic[bool]
+    lock: Lock
+
+proc initOnce*(once: var Once) =
+  once.finished.store(false)
+  initLock(once.lock)
+
+template once*(alreadyExecuted: Once, body: untyped) =
+  runnableExamples:
+    var block1: Once
+    var count = 0
+    initOnce(block1)
+
+
+    for i in 1 .. 10:
+      onlyOnce(block1):
+        inc count
+
+    doAssert count == 1
+  if not alreadyExecuted.finished.load:
+    withLock alreadyExecuted.lock:
+      try:
+        body
+      finally:
+        alreadyExecuted.finished.store(true)
