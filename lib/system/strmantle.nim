af21575cacfe47434bfd21b78f8ab6f9a1712d8d
flywind <xzsflywind@gmail.com>
2020-12-01 16:54:59 +0800

    comments

--- lib/system/strmantle.nim
+++ lib/system/strmantle.nim
@@ -14,6 +14,17 @@ const digitsTable = "0001020304050607080910111213141516171819" &
     "4041424344454647484950515253545556575859" &
     "6061626364656667686970717273747576777879" &
     "8081828384858687888990919293949596979899"
+  # Inspired by https://engineering.fb.com/2013/03/15/developer-tools/three-optimization-tips-for-c
+  # Generates:
+  # .. code-block:: nim
+  #   var res = ""
+  #   for i in 0 .. 99:
+  #     if i < 10:
+  #       res.add "0" & $i
+  #     else:
+  #       res.add $i
+  #   doAssert res == digitsTable
+  
 
 func digits10(num: uint64): int {.noinline.} =
   if num < 10:
