3ab4d88506144ce0a8fa351ce6f104fa238442d4
deech <aditya.siram@gmail.com>
2020-12-02 15:11:15 -0600

    Moved test into a block and removed some boilerplate.

--- tests/stdlib/tmacros.nim
+++ tests/stdlib/tmacros.nim
@@ -1,13 +1,9 @@
 import macros
 
-macro m(u:untyped):untyped =
-  doAssert hasArgOfName(params u,"s")
-  doAssert hasArgOfName(params u,"i")
-  doAssert hasArgOfName(params u,"j")
-  doAssert hasArgOfName(params u,"k")
-  doAssert hasArgOfName(params u,"b")
-  doAssert hasArgOfName(params u,"xs")
-  doAssert hasArgOfName(params u,"ys")
-  doAssert not hasArgOfName(params u,"nonexistent")
+block: # hasArgOfName
+  macro m(u:untyped): untyped =
+    for name in ["s","i","j","k","b","xs","ys"]:
+      doAssert hasArgOfName(params u,name)
+    doAssert not hasArgOfName(params u,"nonexistent")
 
-proc p(s:string; i,j,k:int; b:bool; xs,ys:seq[int] = @[]) {.m.} = discard
+  proc p(s:string; i,j,k:int; b:bool; xs,ys:seq[int] = @[]) {.m.} = discard
