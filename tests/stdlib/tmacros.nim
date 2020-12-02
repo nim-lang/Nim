dfddd6ab2f09d1d75c673a18afe5018c48ca9b29
deech <aditya.siram@gmail.com>
2020-12-02 16:00:02 -0600

    Fix some argument formatting.

--- tests/stdlib/tmacros.nim
+++ tests/stdlib/tmacros.nim
@@ -1,9 +1,9 @@
 import macros
 
 block: # hasArgOfName
-  macro m(u:untyped): untyped =
+  macro m(u: untyped): untyped =
     for name in ["s","i","j","k","b","xs","ys"]:
       doAssert hasArgOfName(params u,name)
     doAssert not hasArgOfName(params u,"nonexistent")
 
-  proc p(s:string; i,j,k:int; b:bool; xs,ys:seq[int] = @[]) {.m.} = discard
+  proc p(s: string; i,j,k: int; b: bool; xs,ys: seq[int] = @[]) {.m.} = discard
