649ee7b76e0f9e2745b56bb8558089af1a11e1fa
flywind <43030857+xflywind@users.noreply.github.com>
2020-12-03 09:40:27 +0800

    Update lib/system/setops.nim

--- lib/system/setops.nim
+++ lib/system/setops.nim
@@ -40,7 +40,7 @@ func card*[T](x: set[T]): int {.magic: "Card".} =
   ## Returns the cardinality of the set `x`, i.e. the number of elements
   ## in the set.
   runnableExamples:
-    var a = {1, 3, 5, 7}
+    var a = {1, 3, 5, 7, 5}
     assert card(a) == 4
 
 func len*[T](x: set[T]): int {.magic: "Card".}
