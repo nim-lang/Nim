abefb17eb26b8cd9360bdde63a8e22d5bcc64389
Jason <beefers331@gmail.com>
2020-12-10 14:55:35 -0700

    Nitpickyness cleaned

--- tests/stdlib/tsetutils.nim
+++ tests/stdlib/tsetutils.nim
@@ -4,8 +4,8 @@ discard """
 import std/setutils
 
 template main =
-  doAssert "abcbb".toSet == {'a','b','c'}
-  doAssert toSet([10u8,12,13]) == {10u8, 12, 13}
+  doAssert "abcbb".toSet == {'a', 'b', 'c'}
+  doAssert toSet([10u8, 12, 13]) == {10u8, 12, 13}
   doAssert toSet(0u16..30) == {0u16..30}
   type A = distinct char
   doAssert [A('x')].toSet == {A('x')}
