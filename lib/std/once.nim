5922d716f829b0a4fcb897bede951fb5b1f2bb5f
flywind <xzsflywind@gmail.com>
2020-12-10 21:20:04 +0800

    changes

--- lib/std/once.nim
+++ lib/std/once.nim
@@ -1,33 +1,39 @@
-import atomics
-import locks
+import std/[atomics, locks]
 
 
 type
-  Once* = object
+  Once* = object    ## A object that ensures a block of code is executed once.
     finished: Atomic[bool]
     lock: Lock
 
 proc initOnce*(once: var Once) =
+  ## Initializes a `Once` object.
   once.finished.store(false)
   initLock(once.lock)
 
-template once*(alreadyExecuted: Once, body: untyped) =
+template once*(cond: Once, body: untyped) =
+  ## Executes a block of code only once (the first time the block is reached).
+  ## It is thread-safe.
   runnableExamples:
     var block1: Once
     var count = 0
     initOnce(block1)
 
-
     for i in 1 .. 10:
       once(block1):
         inc count
 
+      # only the first `block1` is executed
+      once(block1):
+        count = 888
+
     doAssert count == 1
 
-  if not alreadyExecuted.finished.load:
-    withLock alreadyExecuted.lock:
-      if not alreadyExecuted.finished.load:
+  if not cond.finished.load(moAcquire):
+    withLock cond.lock:
+      # TODO load a value without atomic
+      if not cond.finished.load(moRelaxed):
         try:
           body
         finally:
-          alreadyExecuted.finished.store(true)
+          cond.finished.store(true, moRelease)
