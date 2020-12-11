67ace9fbe1005483610f1803d44788b8b33a87e9
Timothee Cour <timothee.cour2@gmail.com>
2020-12-10 18:49:48 -0800

    _

--- lib/std/threadutils.nim
+++ lib/std/threadutils.nim
@@ -1,9 +1,8 @@
 import std/locks
+import timn/dbgs
 
 var lock: Lock
-initLock(lock)
-
-# var lockSubs: seq[Lock]
+initLock(lock) # xxx instead, do this on 1st call to `onceGlobal`, using `atomic`.
 
 template onceGlobal*(body: untyped) =
   ## evaluate `body` once, even in presence of multiple threads.
@@ -16,20 +15,24 @@ template onceGlobal*(body: untyped) =
     for t in mitems(thr): t.createThread threadFunc
     joinThreads(thr)
 
-  var witness {.global.}: bool
-  # var lockIndex {.global.}: int
+  var witness {.global.}: int
+  var lock2 {.global.}: Lock
   # TODO is atomic needed?
-  if not witness:
-    # var lockSub {.global.}: Lock
-    # initLock(lock)
+  if witness < 2:
     withLock lock:
-      # lockSubs
-      if not witness:
-        witness = true
-        # this could take a while
-        # lockSubs
-        # lockIndex
-        body
+      if witness == 0:
+        # dbg "initLock", astToStr(lock2), astToStr(body)
+        initLock(lock2)
+        witness = 1
+    # we release the single `lock` to avoid blocking unrelated `onceGlobal` calls.
+    if witness == 1:
+      withLock lock2:
+        if witness == 1:
+          # TODO: add option `retryOnFailure`, see https://github.com/nim-lang/Nim/pull/16192/files#r540648534
+          # and make sure we don't nest 2 levels of try/finally (withLock uses 1 already)
+          body
+          witness = 2
+            # this must occur after `body` otherwise next callers will return immediately
 
 template onceThread*(body: untyped) =
   # var witness {.threadvar.}: bool
