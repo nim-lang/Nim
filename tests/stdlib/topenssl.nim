fbd92c4b95fef4cbaf32233054ba06197a31eaf5
lihaifeng <lihaifeng@wxm.com>
2020-12-13 09:25:38 +0800

    Add files via upload
    SSL PEM_read_RSAPUBKEY check

---
+++ tests/stdlib/topenssl.nim
@@ -0,0 +1,18 @@
+
+import unittest
+import openssl
+
+# bug 16308
+suite "SSL PEM_read_RSAPUBKEY check":
+  var rsa: PRSA
+  var file: File
+
+  if not file.open("topenssl_rsa_public_key.pem"):
+    raise newException(Exception, "Error opening file")
+
+  rsa = PEM_read_RSAPUBKEY(file, nil, nil, nil)
+  if rsa == nil:
+    raise newException(Exception, "Error reading public key")
+  RSA_free(rsa)
+  file.close()
+  echo "ok"
