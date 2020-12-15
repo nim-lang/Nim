c7d6ba97d2ed26749d26b53d242c686fb0e8d80e
Timothee Cour <timothee.cour2@gmail.com>
2020-12-15 03:53:50 -0800

    improve test

--- tests/stdlib/titerators.nim
+++ tests/stdlib/titerators.nim
@@ -2,12 +2,22 @@
 # rename as titerators_1?
 
 template main() =
-  block: # bug #16353
-    var c = 0
-    for i in high(int32) .. high(int32):
-      c.inc
-      doAssert c<=1
-    doAssert c == 1
+  template fn(T) = 
+    block: # bug #16353
+      var c = 0
+      for i in high(T) .. high(T):
+        c.inc
+        doAssert c<=1
+      doAssert c == 1
+
+  fn(int8)
+  fn(int16)
+  fn(int32)
+  fn(int64)
+  fn(uint8)
+  fn(uint16)
+  fn(uint32)
+  fn(uint64)
 
 static: main()
 main()
