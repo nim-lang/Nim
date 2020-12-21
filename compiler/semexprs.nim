79a16ad637a47cca75b59fa2afed13ddccf659ae
Clonkk <sidger@zoho.com>
2020-12-21 11:31:33 +0100

    added explicit deref as unsafe. Do not work for field access with dot syntax

--- compiler/semexprs.nim
+++ compiler/semexprs.nim
@@ -1448,7 +1448,12 @@ proc semDeref(c: PContext, n: PNode): PNode =
   result = n
   var t = skipTypes(n[0].typ, {tyGenericInst, tyVar, tyLent, tyAlias, tySink, tyOwned})
   case t.kind
-  of tyRef, tyPtr: n.typ = t.lastSon
+  of tyRef: n.typ = t.lastSon
+  of tyPtr:
+    n.typ = t.lastSon
+    # Is it possible to create a ptr T without cast / addr /unsafeaddr / importc ?
+    # In theory, the danger of derefencing should be covered by pointer creation
+    incl(a.sym.flags, sfMemUnsafe)
   else: result = nil
   #GlobalError(n[0].info, errCircumNeedsPointer)
 
