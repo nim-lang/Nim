305b17e0e6f308d19fc9bab036034fa9f791cbe0
shadowninja55 <49539636+shadowninja55@users.noreply.github.com>
2020-12-21 19:24:04 -0500

    updated doc comment to add sequtils

--- lib/prelude.nim
+++ lib/prelude.nim
@@ -16,7 +16,7 @@
 ## Same as:
 ##
 ## .. code-block:: nim
-##   import os, strutils, times, parseutils, hashes, tables, sets
+##   import os, strutils, times, parseutils, hashes, tables, sets, sequtils
 ##   when not defined(js): import parseopt
 
 import os, strutils, times, parseutils, hashes, tables, sets, sequtils
