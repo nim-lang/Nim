8a8ccce6fe942aaffa4e2c74ab9076ecf55e5261
shadowninja55 <shadowninja1050@gmail.com>
2020-12-22 09:23:49 -0500

    added test

--- tests/stdlib/tmacros.nim
+++ tests/stdlib/tmacros.nim
@@ -7,3 +7,10 @@ block: # hasArgOfName
     doAssert not hasArgOfName(params u,"nonexistent")
 
   proc p(s: string; i,j,k: int; b: bool; xs,ys: seq[int] = @[]) {.m.} = discard
+
+block: # newVarStmt
+  macro define(name: string, value: untyped): untyped =
+    newVarStmt(name, value)
+  
+  define("x", 5 + 5)
+  doAssert x == 10
