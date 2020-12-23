214b9d7438e4c2318e987919c0faa329f10cd4be
flywind <xzsflywind@gmail.com>
2020-12-23 12:08:42 +0800

    one more tests

--- tests/stdlib/tmath.nim
+++ tests/stdlib/tmath.nim
@@ -286,6 +286,18 @@ block:
       doAssert euclMod(8.0, -3.0) ==~ 2.0
       doAssert euclMod(-8.5, 3.0) ==~ 0.5
 
+      doAssert euclDiv(9, 3) == 3
+      doAssert euclMod(9, 3) == 0
+
+      doAssert euclDiv(9, -3) == -3
+      doAssert euclMod(9, -3) == 0
+
+      doAssert euclDiv(-9, 3) == -3
+      doAssert euclMod(-9, 3) == 0
+
+      doAssert euclDiv(-9, -3) == 3
+      doAssert euclMod(-9, -3) == 0
+
     block: # log
       doAssert log(4.0, 3.0) ==~ ln(4.0) / ln(3.0)
       doAssert log2(8.0'f64) == 3.0'f64
