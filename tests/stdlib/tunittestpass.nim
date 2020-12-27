e76f216f2cb43e59295ca82b1a40c4d440681685
flywind <xzsflywind@gmail.com>
2020-12-27 09:17:54 +0800

    more tests

--- tests/stdlib/tunittestpass.nim
+++ tests/stdlib/tunittestpass.nim
@@ -11,6 +11,9 @@ block:
   check (typeof(1)) isnot float
   check typeof(1) isnot float
 
+  check 1.0 is float
+  check 1 isnot float
+
   type T = type(0.1)
   check T is float
   check T isnot int
