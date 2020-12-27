d365299734df56a33f426013dbb76a394079c110
flywind <xzsflywind@gmail.com>
2020-12-27 08:30:41 +0800

    fix #16474

---
+++ tests/stdlib/tunittestpass.nim
@@ -0,0 +1,16 @@
+discard """
+  targets: "c js"
+"""
+
+
+import unittest
+
+block:
+  check (type(1.0)) is float
+  check type(1.0) is float
+  check (typeof(1)) isnot float
+  check typeof(1) isnot float
+
+  type T = type(0.1)
+  check T is float
+  check T isnot int
