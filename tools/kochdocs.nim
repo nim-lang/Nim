e450d19b49cef65659fdafadbd9d4c9cc9973871
Juan Carlos <JuanCarlospaco@gmail.com>
2020-12-28 12:33:18 -0300

    is dangerous

--- tools/kochdocs.nim
+++ tools/kochdocs.nim
@@ -14,9 +14,10 @@ const
   webUploadOutput = "web/upload"
 
 var nimExe*: string
+const allowList = ["jsbigints.nim"] # Nim for JavaScript target outside of lib/js/
 
 template isJsOnly(file: string): bool =
-  file.isRelativeTo("lib/js") or file.extractFilename.startsWith"js"
+  file.isRelativeTo("lib/js") or file.extractFilename in allowList
 
 proc exe*(f: string): string =
   result = addFileExt(f, ExeExt)
