485022a1567f4bd288de27260bb0c7452a52167b
planetis-m <b3lievem@yandex.com>
2020-12-29 13:52:13 +0200

    fix test

--- tests/stdlib/tmemory.nim
+++ tests/stdlib/tmemory.nim
@@ -10,5 +10,6 @@ block: # cmpMem
   a[^1] = byte(1)
   let c = a
 
-  doAssert cmpMem(a.addr, b.addr, sizeof(SomeHash)) == 1
+  doAssert cmpMem(a.addr, b.addr, sizeof(SomeHash)) > 0
+  doAssert cmpMem(b.addr, a.addr, sizeof(SomeHash)) < 0
   doAssert cmpMem(a.addr, c.unsafeAddr, sizeof(SomeHash)) == 0
