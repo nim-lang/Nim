aba6cec94cb9c869fac4ccdcd136dabbb96e1018
Timothee Cour <timothee.cour2@gmail.com>
2020-12-30 17:25:46 -0800

    fixup

--- tests/misc/tstandalone.nim
+++ tests/misc/tstandalone.nim
@@ -4,12 +4,12 @@ discard """
   exitcode: 1
   output: '''
 hi 4778
-panic: exception: AssertionDefect, message: tstandalone.nim(36, 10) `false` foo, arg: 
+panic: exception: AssertionDefect, message: tstandalone.nim(37, 10) `false` foo, arg: 
 '''
   matrix: "--os:standalone --gc:none"
 """
 
-# issue  #2041: Macros need to be available for os:standalone!
+# bug  #2041: Macros need to be available for os:standalone!
 import macros
 
 from std/private/fatal import setPanicCallback
@@ -27,10 +27,11 @@ setPanicCallback(panicCallback)
 
 var x = 0
 inc x
-printf("hi %ld\n", x+4777)
+printf("hi %ld\n", x + 4777)
 
-proc substr(a: string): string = a[0 .. 3] # This should compile. See #9762
+proc substr(a: string): string = a[0 .. 3] # This should compile. See: bug #9762
 const a = substr("foobar")
+doAssert a == "foob"
 
-## line 35
+## line 36
 doAssert false, "foo"
