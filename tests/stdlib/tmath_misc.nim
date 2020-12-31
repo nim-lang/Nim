a7c22a7e5a29798fe13b8517f51d960230be2b29
flywind <43030857+xflywind@users.noreply.github.com>
2020-12-30 21:45:48 -0600

    Update tests/stdlib/tmath_misc.nim
    Co-authored-by: Timothee Cour <timothee.cour2@gmail.com>

--- tests/stdlib/tmath_misc.nim
+++ tests/stdlib/tmath_misc.nim
@@ -12,8 +12,9 @@ proc main() =
     doAssert 1.0 / abs(0.0) == Inf
     doAssert -1.0 / abs(-0.0) == -Inf
     doAssert -1.0 / abs(0.0) == -Inf
-doAssert abs(0.0) == 0.0
-doAssert abs(0.0'f32) == 0.0'f32
+    doAssert abs(0.0) == 0.0
+    doAssert abs(0.0'f32) == 0.0'f32
+
     doAssert abs(Inf) == Inf
     doAssert abs(-Inf) == Inf
     doAssert abs(NaN).isNaN
