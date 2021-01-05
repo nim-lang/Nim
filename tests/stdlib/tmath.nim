674a7719f0303f9e372364015f88530f134dd4fd
Timothee Cour <timothee.cour2@gmail.com>
2021-01-05 00:00:32 -0800

    merge tmath_misc.nim into tmath.nim

--- tests/stdlib/tmath.nim
+++ tests/stdlib/tmath.nim
@@ -357,5 +357,18 @@ template main =
         doAssert copySign(10.0, -NaN) == 10.0
         doAssert copySign(1.0, copySign(NaN, -1.0)) == -1.0 # fails in VM
 
+  block:
+    doAssert 1.0 / abs(-0.0) == Inf
+    doAssert 1.0 / abs(0.0) == Inf
+    doAssert -1.0 / abs(-0.0) == -Inf
+    doAssert -1.0 / abs(0.0) == -Inf
+    doAssert abs(0.0) == 0.0
+    doAssert abs(0.0'f32) == 0.0'f32
+
+    doAssert abs(Inf) == Inf
+    doAssert abs(-Inf) == Inf
+    doAssert abs(NaN).isNaN
+    doAssert abs(-NaN).isNaN
+
 static: main()
 main()
