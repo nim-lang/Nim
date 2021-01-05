df9e74b510a84b0050ffe022e77d42c6f949c2bc
flywind <43030857+xflywind@users.noreply.github.com>
2021-01-05 10:47:10 -0600

    fix #9125 (#16582)
    * fix #9125
    
    * Update tests/stdlib/tmath.nim
    
    Co-authored-by: Timothee Cour <timothee.cour2@gmail.com>
    
    * back
    
    Co-authored-by: Andreas Rumpf <rumpf_a@web.de>
    Co-authored-by: Timothee Cour <timothee.cour2@gmail.com>

--- tests/stdlib/tmath.nim
+++ tests/stdlib/tmath.nim
@@ -10,6 +10,10 @@ import std/[math, random, os]
 import std/[unittest]
 import std/[sets, tables]
 
+
+# Function for approximate comparison of floats
+proc `==~`(x, y: float): bool = (abs(x-y) < 1e-9)
+
 block: # random int
   block: # there might be some randomness
     var set = initHashSet[int](128)
@@ -157,10 +161,6 @@ block:
     doAssert(lgamma(1.0) == 0.0) # ln(1.0) == 0.0
     doAssert(erf(6.0) > erf(5.0))
     doAssert(erfc(6.0) < erfc(5.0))
-
-    proc `==~`(x, y: float): bool = (abs(x-y) < 1e-9)
-      # Function for approximate comparison of floats
-      # xxx use `almostEqual`
 
     block: # prod
       doAssert prod([1, 2, 3, 4]) == 24
@@ -348,6 +348,28 @@ template main =
     doAssert copySign(NaN, -0.0).isNaN
     doAssert copySign(-NaN, 0.0).isNaN
     doAssert copySign(-NaN, -0.0).isNaN
+
+    block: # round() tests
+      # Round to 0 decimal places
+      doAssert round(54.652) == 55.0
+      doAssert round(54.352) == 54.0
+      doAssert round(-54.652) == -55.0
+      doAssert round(-54.352) == -54.0
+      doAssert round(0.0) == 0.0
+      doAssert 1 / round(0.0) == Inf
+      doAssert 1 / round(-0.0) == -Inf
+      doAssert round(Inf) == Inf
+      doAssert round(-Inf) == -Inf
+      doAssert round(NaN).isNaN
+      doAssert round(-NaN).isNaN
+      doAssert round(-0.5) == -1.0
+      doAssert round(0.5) == 1.0
+      doAssert round(-1.5) == -2.0
+      doAssert round(1.5) == 2.0
+      doAssert round(-2.5) == -3.0
+      doAssert round(2.5) == 3.0
+      doAssert round(2.5'f32) == 3.0'f32
+      doAssert round(2.5'f64) == 3.0'f64
 
     when nimvm:
       discard
