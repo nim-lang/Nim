d2d3d377201a968bacb3fda95f7e72ce5c0600d8
Timothee Cour <timothee.cour2@gmail.com>
2021-01-05 20:32:34 -0800

    add -d:nimHasJsBigIntBackend

--- tests/js/tbigint_backend.nim
+++ tests/js/tbigint_backend.nim
@@ -16,21 +16,23 @@ func toString*(this: JsBigInt): cstring {.importjs: "#.toString()".}
 func `$`*(this: JsBigInt): string =
   $toString(this)
 
-let z1=big"10"
-let z2=big"15"
-doAssert z1 == big"10"
-doAssert z1 == z1
-doAssert z1 != z2
-var s: seq[cstring]
-for i in z1 .. z2:
-  s.add $i
-doAssert s == @["10".cstring, "11", "12", "13", "14", "15"]
 block:
-  var a=big"3"
-  a.inc
-  doAssert a == big"4"
-var z: JsBigInt
-doAssert $z == "0"
-doAssert z.jsTypeOf == "bigint" # would fail without codegen change
-doAssert z != big(1)
-doAssert z == big"0" # ditto
+  doAssert defined(nimHasJsBigIntBackend)
+  let z1=big"10"
+  let z2=big"15"
+  doAssert z1 == big"10"
+  doAssert z1 == z1
+  doAssert z1 != z2
+  var s: seq[cstring]
+  for i in z1 .. z2:
+    s.add $i
+  doAssert s == @["10".cstring, "11", "12", "13", "14", "15"]
+  block:
+    var a=big"3"
+    a.inc
+    doAssert a == big"4"
+  var z: JsBigInt
+  doAssert $z == "0"
+  doAssert z.jsTypeOf == "bigint" # would fail without codegen change
+  doAssert z != big(1)
+  doAssert z == big"0" # ditto
