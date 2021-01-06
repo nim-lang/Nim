add46c39e4c9b4db97c6112a2d80d2b6ec43f99d
Timothee Cour <timothee.cour2@gmail.com>
2021-01-05 20:51:27 -0800

    more tests

--- tests/js/tbigint_backend.nim
+++ tests/js/tbigint_backend.nim
@@ -31,8 +31,28 @@ block:
     var a=big"3"
     a.inc
     doAssert a == big"4"
-  var z: JsBigInt
-  doAssert $z == "0"
-  doAssert z.jsTypeOf == "bigint" # would fail without codegen change
-  doAssert z != big(1)
-  doAssert z == big"0" # ditto
+  block:
+    var z: JsBigInt
+    doAssert $z == "0"
+    doAssert z.jsTypeOf == "bigint" # would fail without codegen change
+    doAssert z != big(1)
+    doAssert z == big"0" # ditto
+
+  # ditto below
+  block:
+    let z: JsBigInt = big"1"
+    doAssert $z == "1"
+    doAssert z.jsTypeOf == "bigint"
+    doAssert z == big"1"
+
+  block:
+    let z = JsBigInt.default
+    doAssert $z == "0"
+    doAssert z.jsTypeOf == "bigint"
+    doAssert z == big"0"
+
+  block:
+    var a: seq[JsBigInt]
+    a.setLen 3
+    doAssert a[^1].jsTypeOf == "bigint"
+    doAssert a[^1] == big"0"
