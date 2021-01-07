05f3fcb3050ed5cc906da906e491b2de95703eb3
Juan Carlos <JuanCarlospaco@gmail.com>
2021-01-07 11:33:26 -0300

    Use BiggestInt

--- lib/std/jsbigints.nim
+++ lib/std/jsbigints.nim
@@ -44,7 +44,7 @@ func wrapToUint*(this: JsBigInt; bits: Natural): JsBigInt {.importjs:
   runnableExamples:
     doAssert newJsBigInt("2147483647").wrapToUint(32) == newJsBigInt"2147483647"
 
-func unsafeToNumber*(this: JsBigInt): int {.importjs: "Number(#)".} =
+func unsafeToNumber*(this: JsBigInt): BiggestInt {.importjs: "Number(#)".} =
   ## Does not do any bounds check and may or may not return an inexact representation.
   runnableExamples:
     doAssert unsafeToNumber(newJsBigInt"2147483647") == 2147483647
