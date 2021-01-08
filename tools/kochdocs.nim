a7a9dcffd0c3300ef66291e69aec8b131aa3ca7f
Juan Carlos <JuanCarlospaco@gmail.com>
2021-01-08 17:36:50 -0300

    Fix CI

--- tools/kochdocs.nim
+++ tools/kochdocs.nim
@@ -14,10 +14,12 @@ const
   webUploadOutput = "web/upload"
 
 var nimExe*: string
+const allowList = ["jsbigints.nim"]
 
 template isJsOnly(file: string): bool =
   file.isRelativeTo("lib/js") or
-  file.isRelativeTo("lib/fusion/js")
+  file.isRelativeTo("lib/fusion/js") or
+  file.extractFilename in allowList
 
 proc exe*(f: string): string =
   result = addFileExt(f, ExeExt)
