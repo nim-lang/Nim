ffb130b59c3f3d03e694b76af37563b070e28af0
Timothee Cour <timothee.cour2@gmail.com>
2021-01-08 14:09:58 -0800

    skip docs for `lib/fusion` (docs already run in fusion repo) (#16645)
    * run CI docs on koch.nim changes to avoid future regressions
    
    * kochdocs: skip lib/fusion

--- tools/kochdocs.nim
+++ tools/kochdocs.nim
@@ -15,9 +15,7 @@ const
 
 var nimExe*: string
 
-template isJsOnly(file: string): bool =
-  file.isRelativeTo("lib/js") or
-  file.isRelativeTo("lib/fusion/js")
+template isJsOnly(file: string): bool = file.isRelativeTo("lib/js")
 
 proc exe*(f: string): string =
   result = addFileExt(f, ExeExt)
@@ -187,7 +185,8 @@ lib/system/widestrs.nim
 """.splitWhitespace()
 
   proc follow(a: PathEntry): bool =
-    a.path.lastPathPart notin ["nimcache", "htmldocs", "includes", "deprecated", "genode"]
+    result = a.path.lastPathPart notin ["nimcache", "htmldocs", "includes", "deprecated", "genode"] and
+      not a.path.isRelativeTo("lib/fusion")
   for entry in walkDirRecFilter("lib", follow = follow):
     let a = entry.path
     if entry.kind != pcFile or a.splitFile.ext != ".nim" or
