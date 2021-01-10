e0e5fc6b017603519376fdccb9b4f6c56c095334
flywind <xzsflywind@gmail.com>
2021-01-10 15:43:52 +0800

    better error messages

--- compiler/semtypes.nim
+++ compiler/semtypes.nim
@@ -265,9 +265,11 @@ proc semRange(c: PContext, n: PNode, prev: PType): PType =
   result = nil
   if n.len == 2:
     if isRange(n[1]):
-      if n[1].kind == nkInfix and considerQuotedIdent(c, n[1][0]).s == "..<" and 
-                    n[1][2].kind in {nkCharLit..nkUInt64Lit} and n[1][2].intVal != low(BiggestInt):
+      if n[1].kind == nkInfix and considerQuotedIdent(c, n[1][0]).s == "..<":
+        if n[1][2].kind in {nkCharLit..nkUInt64Lit} and n[1][2].intVal != low(BiggestInt):
           dec n[1][2].intVal
+        else:
+          localError(c.config, n.info, "ordinal type expected")
       result = semRangeAux(c, n[1], prev)
       let n = result.n
       if n[0].kind in {nkCharLit..nkUInt64Lit} and n[0].intVal > 0:
@@ -1721,6 +1723,8 @@ proc semTypeNode(c: PContext, n: PNode, prev: PType): PType =
     elif ident != nil and ident.id == ord(wDotDotLt):
       if n.kind == nkInfix and n[2].kind in {nkCharLit..nkUInt64Lit} and n[2].intVal != low(BiggestInt):
         dec n[2].intVal
+      else:
+        localError(c.config, n.info, "ordinal type expected")
       result = semRangeAux(c, n, prev)
     elif n[0].kind == nkNilLit and n.len == 2:
       result = semTypeNode(c, n[1], prev)
