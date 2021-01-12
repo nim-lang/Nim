8d5c707688ffb7235ce070a953012351c0104338
Timothee Cour <timothee.cour2@gmail.com>
2021-01-12 01:55:49 -0800

    improve test

--- tests/errmsgs/tnim_echo_quitting.nim
+++ tests/errmsgs/tnim_echo_quitting.nim
@@ -1,8 +1,12 @@
 discard """
   cmd: "nim check -d:nimEchoQuitting $file"
   action: "reject"
-  nimout: "nim quitting"
+  nimout: '''
+tnim_echo_quitting.nim(12, 1) Error: undeclared identifier: 'nonexistant'
+nim quitting
+'''
 """
+
 static: echo "ok1"
 static: echo "ok2"
 nonexistant
