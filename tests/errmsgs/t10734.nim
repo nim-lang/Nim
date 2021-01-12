3d9df6de1cf9ca3ac0c7dc229bbf5ac3bd09454d
flywind <xzsflywind@gmail.com>
2021-01-12 15:24:38 +0800

    fix

--- tests/errmsgs/t10734.nim
+++ tests/errmsgs/t10734.nim
@@ -1,13 +1,13 @@
 discard """
   cmd: "nim check $file"
   errormsg: ""
-  nimout: '''t10734.nim(16, 1) Error: invalid indentation
-t10734.nim(16, 6) Error: invalid indentation
-t10734.nim(17, 7) Error: expression expected, but found '[EOF]'
-t10734.nim(15, 5) Error: 'proc' is not a concrete type; for a callback without parameters use 'proc()'
-t10734.nim(16, 6) Error: undeclared identifier: 'p'
-t10734.nim(16, 6) Error: 'p' cannot be assigned to
-t10734.nim(14, 3) Hint: 'T' is declared but not used [XDeclaredButNotUsed]
+  nimout: '''t10734.nim(17, 1) Error: invalid indentation
+t10734.nim(17, 6) Error: invalid indentation
+t10734.nim(18, 7) Error: expression expected, but found '[EOF]'
+t10734.nim(16, 5) Error: 'proc' is not a concrete type; for a callback without parameters use 'proc()'
+t10734.nim(17, 6) Error: undeclared identifier: 'p'
+t10734.nim(17, 6) Error: 'p' cannot be assigned to
+t10734.nim(15, 3) Hint: 'T' is declared but not used [XDeclaredButNotUsed]
 '''
 """
 
