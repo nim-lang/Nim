2a5cc45daac54fa65e0e93d135b8595080ad3f09
Juan Carlos <juancarlospaco@gmail.com>
2021-01-15 19:59:47 -0300

    Update compiler/commands.nim
    Co-authored-by: Timothee Cour <timothee.cour2@gmail.com>

--- compiler/commands.nim
+++ compiler/commands.nim
@@ -670,6 +670,7 @@ proc processSwitch*(switch, arg: string, pass: TCmdLinePass, info: TLineInfo;
     processOnOffSwitchG(conf, {optThreads}, arg, pass, info)
     #if optThreads in conf.globalOptions: conf.setNote(warnGcUnsafe)
   of "tlsemulation": processOnOffSwitchG(conf, {optTlsEmulation}, arg, pass, info)
+  of "taintmode": discard
   of "taintmode": discard  # TODO: https://github.com/nim-lang/Nim/pull/15423/files/bc158f0b9a940044dfbf6701d50c0c3dbed2774e#r558482102
   of "implicitstatic":
     processOnOffSwitch(conf, {optImplicitStatic}, arg, pass, info)
