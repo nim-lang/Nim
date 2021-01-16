5ed6a94d8f1c13abb1681f5425a59d2bac827242
flywind <43030857+xflywind@users.noreply.github.com>
2021-01-16 00:19:17 -0600

    promote math.isNaN (#16734)

--- lib/system.nim
+++ lib/system.nim
@@ -1499,7 +1499,7 @@ const
     ## Contains an IEEE floating point value of *Not A Number*.
     ##
     ## Note that you cannot compare a floating point value to this value
-    ## and expect a reasonable result - use the `classify` procedure
+    ## and expect a reasonable result - use the `isNaN` or `classify` procedure
     ## in the `math module <math.html>`_ for checking for NaN.
 
 
