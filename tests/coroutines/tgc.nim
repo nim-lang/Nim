90a7917ffda717bc50c482e88fbc642bb6e0db26
Timothee Cour <timothee.cour2@gmail.com>
2021-01-21 02:15:16 -0800

    fixup

--- tests/coroutines/tgc.nim
+++ tests/coroutines/tgc.nim
@@ -1,21 +1,22 @@
 discard """
   matrix: "--gc:refc; --gc:arc; --gc:orc"
   target: "c"
-  disabled: "openbsd"
 """
-# openbsd gave: stdlib_coro.nim.c:406:22: error: array type 'jmp_buf' (aka 'long [11]') is not assignable (*dest).execContext = src.execContext;
-import coro
 
-var maxOccupiedMemory = 0
+when compileOption("gc", "refc") or not defined(openbsd):
+  # xxx openbsd gave: stdlib_coro.nim.c:406:22: error: array type 'jmp_buf' (aka 'long [11]') is not assignable (*dest).execContext = src.execContext;
+  import coro
 
-proc testGC() =
-  var numbers = newSeq[int](100)
-  maxOccupiedMemory = max(maxOccupiedMemory, getOccupiedMem())
-  suspend(0)
+  var maxOccupiedMemory = 0
 
-start(testGC)
-start(testGC)
-run()
+  proc testGC() =
+    var numbers = newSeq[int](100)
+    maxOccupiedMemory = max(maxOccupiedMemory, getOccupiedMem())
+    suspend(0)
 
-GC_fullCollect()
-doAssert(getOccupiedMem() < maxOccupiedMemory, "GC did not free any memory allocated in coroutines")
+  start(testGC)
+  start(testGC)
+  run()
+
+  GC_fullCollect()
+  doAssert(getOccupiedMem() < maxOccupiedMemory, "GC did not free any memory allocated in coroutines")
