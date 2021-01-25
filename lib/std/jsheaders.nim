8be32d9fa23365863fd0856c75feba59af6d9ebb
Juan Carlos <JuanCarlospaco@gmail.com>
2021-01-25 10:11:31 -0300

    Improvements and fixes

--- lib/std/jsheaders.nim
+++ lib/std/jsheaders.nim
@@ -8,9 +8,11 @@ func newHeaders*(): Headers {.importjs: "new Headers()".}
   ## https://developer.mozilla.org/en-US/docs/Web/API/Headers
 
 func add*(this: Headers; key: cstring; value: cstring) {.importjs: "#.append(#, #)".}
+  ## Allows duplicated keys.
   ## https://developer.mozilla.org/en-US/docs/Web/API/Headers/append
 
 func delete*(this: Headers; key: cstring) {.importjs: "#.$1(#)".}
+  ## Delete *all* items with `key` from the headers, including duplicated keys.
   ## https://developer.mozilla.org/en-US/docs/Web/API/Headers/delete
 
 func hasKey*(this: Headers; key: cstring): bool {.importjs: "#.has(#)".}
@@ -22,30 +24,54 @@ func keys*(this: Headers): seq[cstring] {.importjs: "Array.from(#.$1())".}
 func values*(this: Headers): seq[cstring] {.importjs: "Array.from(#.$1())".}
   ## https://developer.mozilla.org/en-US/docs/Web/API/Headers/values
 
-func entries*(this: Headers): seq[array[2, cstring]] {.importjs: "Array.from(#.$1())".}
+func entries*(this: Headers): seq[tuple[key, value: cstring]] {.importjs: "Array.from(#.$1())".}
   ## https://developer.mozilla.org/en-US/docs/Web/API/Headers/entries
 
 func `[]`*(this: Headers; key: cstring): cstring {.importjs: "#.get(#)".}
+  ## Get *all* items with `key` from the headers, including duplicated values.
   ## https://developer.mozilla.org/en-US/docs/Web/API/Headers/get
 
 func `[]=`*(this: Headers; key: cstring; value: cstring) {.importjs: "#.set(#, #)".}
+  ## Do *not* allow duplicated keys, overwrites duplicated keys.
   ## https://developer.mozilla.org/en-US/docs/Web/API/Headers/set
 
 func clear*(this: Headers) {.importjs:
   "(() => { const header = #; Array.from(header.keys()).forEach((key) => header.delete(key)) })()".}
   ## Convenience func to delete all items from `Headers`.
 
+func toCstring*(this: Headers): cstring {.importjs: "JSON.stringify(Array.from(#.entries()))".}
+  ## Returns a `cstring` representation of `Headers`.
+
+func `$`*(this: Headers): string = $this.toCstring
+
 
 runnableExamples:
   if defined(nimJsHeadersTests):
-    let header = newHeaders()
-    header.add("key", "value")
-    doAssert header.hasKey("key")
-    doAssert header.keys() == @["key".cstring]
-    doAssert header.values() == @["value".cstring]
-    doAssert header["key"] == "value".cstring
-    header["other"] = "another".cstring
-    doAssert header["other"] == "another".cstring
-    doAssert header.entries() == @[["key".cstring, "value"], ["other".cstring, "another"]]
-    header.delete("other")
-    doAssert header.entries() == @[]
+    block:
+      let header = newHeaders()
+      header.add("key", "value")
+      doAssert header.hasKey("key")
+      doAssert header.keys() == @["key".cstring]
+      doAssert header.values() == @["value".cstring]
+      doAssert header["key"] == "value".cstring
+      header["other"] = "another".cstring
+      doAssert header["other"] == "another".cstring
+      doAssert header.entries() == @[["key".cstring, "value"], ["other".cstring, "another"]]
+      doAssert header.toCstring() == """[["key","value"],["other","another"]]""".cstring
+      header.delete("other")
+      header.clear()
+      doAssert header.entries() == @[]
+    block:
+      let header = newHeaders()
+      header.add("key", "a")
+      header.add("key", "b")  ## Duplicated.
+      header.add("key", "c")  ## Duplicated.
+      doAssert header["key"] == "a, b, c".cstring
+      header["key"] = "value".cstring
+      doAssert header["key"] == "value".cstring
+    block:
+      let header = newHeaders()
+      header["key"] = "a"
+      header["key"] = "b"  ## Duplicated.
+      header["key"] = "c"  ## Duplicated.
+      doAssert header["key"] == "c".cstring
