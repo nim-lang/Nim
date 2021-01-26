9972b31ea3f049342aff33ce156937b5f1c99c8f
flywind <xzsflywind@gmail.com>
2021-01-26 15:33:54 +0800

    restore

--- lib/std/sysrand.nim
+++ lib/std/sysrand.nim
@@ -177,29 +177,24 @@ elif defined(openbsd):
       result = getentropy(addr p[base], cint(left))
 
 elif defined(freebsd):
-  let freebsdVersion {.importc: "__FreeBSD_version", header: "<sys/param.h>".}: int
-
   type cssize_t {.importc: "ssize_t", header: "<sys/types.h>".} = int
 
   proc getrandom(p: pointer, size: csize_t, flags: cuint): cssize_t {.importc: "getrandom", header: "<sys/random.h>".}
 
   proc urandom*(p: var openArray[byte]): int =
-    if freebsdVersion >= 1200000:
-      let size = p.len
-      if size > 0:
-        let
-          chunks = (size - 1) div batchSize
-          left = size - chunks * batchSize
-        var base = 0
-        for i in 0 ..< chunks:
-          let readBytes = getrandom(addr p[base], csize_t(batchSize), 0)
-          if readBytes < 0:
-            return readBytes
-          inc(base, batchSize)
+    let size = p.len
+    if size > 0:
+      let
+        chunks = (size - 1) div batchSize
+        left = size - chunks * batchSize
+      var base = 0
+      for i in 0 ..< chunks:
+        let readBytes = getrandom(addr p[base], csize_t(batchSize), 0)
+        if readBytes < 0:
+          return readBytes
+        inc(base, batchSize)
 
-        result = getrandom(addr p[base], csize_t(left), 0)
-    else:
-      result = getDevUrandom(p)
+      result = getrandom(addr p[base], csize_t(left), 0)
 
 elif defined(ios):
   {.passL: "-framework Security".}
