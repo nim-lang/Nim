74e9a3e82e0a9c8dc15c8fc74687f5f14f9f71d9
flywind <xzsflywind@gmail.com>
2021-01-31 18:24:39 +0800

    fix #16884

---
+++ tests/js/t16884.nim
@@ -0,0 +1,13 @@
+# bug #16884
+var scores: seq[(set[char], int)] = @{{'/'} : 10}
+
+var x: set[char]
+for item in items(scores):
+  x = item[0]
+
+doAssert x == {'/'}
+
+for (chars, value) in items(scores):
+  x = chars
+
+doAssert x == {'/'}
