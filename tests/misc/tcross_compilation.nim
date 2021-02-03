26ca9efe1a39b985ef901c2a1c485077b1f85756
Timothee Cour <timothee.cour2@gmail.com>
2021-02-02 17:20:58 -0800

    add test

---
+++ tests/misc/tcross_compilation.nim
@@ -0,0 +1,28 @@
+discard """
+  cmd: "nim $target --os:windows --compileonly $file"
+  action: compile
+  disabled: windows # so that test runs on posix host but windows target
+"""
+
+# xxx add a way to test this at RT to make sure windows semantics are used.
+
+import os, strutils
+
+proc main() =
+  doAssert not defined(posix)
+  doAssert defined(windows)
+  doAssert "foo" / "bar" == "foo/bar"
+  const s = currentSourcePath
+  doAssert '\\' notin s
+  doAssert '/' in s
+  doAssert DirSep == '/'
+  let s2 = currentSourcePath
+  doAssert s2 == s
+  let s3 = s2.parentDir / "baz"
+  doAssert s3.endsWith "tests/misc/baz"
+  doAssert s3.isAbsolute
+
+static: main()
+main()
+  # the doAsserts inside here would need to be adjusted, this is just used
+  # to make sure it compiles without `static`
