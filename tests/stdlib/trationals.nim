c3b228232c94dd9f52851e7be21d960d644d57d6
flywind <xzsflywind@gmail.com>
2021-02-06 22:07:35 +0800

    close #16921

--- tests/stdlib/trationals.nim
+++ tests/stdlib/trationals.nim
@@ -96,5 +96,7 @@ template main() =
   doAssert floorMod(3 // 10, 1 // 1) == 3 // 10
   doAssert floorMod(-3 // 10, 1 // 1) == 7 // 10
 
+  doAssert almostEqual(PI.toRational.toFloat, PI)
+
 static: main()
 main()
