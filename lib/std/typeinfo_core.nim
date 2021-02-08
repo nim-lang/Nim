55fe9fcfacb63efe39eb99eda8f21aec4f10ed1b
Timothee Cour <timothee.cour2@gmail.com>
2021-02-07 19:33:24 -0800

    fixup

--- lib/std/typeinfo_core.nim
+++ lib/std/typeinfo_core.nim
@@ -1,53 +1,51 @@
-when not defined nimExperimentalTypeInfoCore:
-  static: doAssert false, "Experimental API, subject to change"
+when defined nimExperimentalTypeInfoCore:
+  import std/private/hti
 
-import std/private/hti
+  export PNimType
 
-export PNimType
-
-when defined(nimV2):
-  type
-    TNimTypeV2 {.compilerproc.} = object
-      destructor*: pointer
-      size*: int
-      align*: int
-      name*: cstring
-      traceImpl*: pointer
-      disposeImpl*: pointer
-      typeInfoV1*: pointer # for backwards compat, usually nil (is really a `PNimType`)
-    PNimTypeV2* = ptr TNimTypeV2
-    PNimTypeAlt* = PNimTypeV2
-else:
-  type
-    PNimTypeAlt* = PNimType
-
-proc getDynamicTypeInfoImpl[T](x: T): PNimTypeAlt {.magic: "GetDynamicTypeInfo", noSideEffect, locks: 0.}
-
-proc getDynamicTypeInfo*[T: object | ref object](x: T): PNimTypeAlt =
-  ## Returns the dynamic type of `x`, which is an implementation defined runtime
-  ## representation of the type of `x`.
-  runnableExamples("-d:nimExperimentalTypeInfoCore"):
+  when defined(nimV2):
     type
-      Base = ref object of RootObj
-      Sub = ref object of Base
-        x: array[100, byte]
-    var a: Base = Sub()
-    assert a.getDynamicTypeInfo == Sub().getDynamicTypeInfo
-    assert a[].getDynamicTypeInfo == Sub().getDynamicTypeInfo
-    assert a.getDynamicTypeInfo.size >= 100 # exact size implementation defined
-    assert Base(nil).getDynamicTypeInfo == nil
-  runnableExamples("-d:nimTypeNames -d:nimExperimentalTypeInfoCore"):
-    type
-      Base = ref object of RootObj
-      Sub = object of Base
-      PSub = ref Sub
-    var a: Base = PSub()
-    # `name` is only available for `-d:nimTypeNames` for `gc:refc`
-    # `name` is implementation defined for `gc:arc|orc`.
-    assert a.getDynamicTypeInfo.name == "Sub"
-    assert Base().getDynamicTypeInfo.name == "Base:ObjectType" # implementation defined
-  when T is ref:
-    if x != nil: 
-      result = getDynamicTypeInfoImpl(x[])
+      TNimTypeV2 {.compilerproc.} = object
+        destructor*: pointer
+        size*: int
+        align*: int
+        name*: cstring
+        traceImpl*: pointer
+        disposeImpl*: pointer
+        typeInfoV1*: pointer # for backwards compat, usually nil (is really a `PNimType`)
+      PNimTypeV2* = ptr TNimTypeV2
+      PNimTypeAlt* = PNimTypeV2
   else:
-    result = getDynamicTypeInfoImpl(x)
+    type
+      PNimTypeAlt* = PNimType
+
+  proc getDynamicTypeInfoImpl[T](x: T): PNimTypeAlt {.magic: "GetDynamicTypeInfo", noSideEffect, locks: 0.}
+
+  proc getDynamicTypeInfo*[T](x: T): PNimTypeAlt =
+    ## Returns the dynamic type of `x`, which is an implementation defined runtime
+    ## representation of the type of `x`. If `T` is `ref|ptr`, x will first be dereferenced
+    ## unless x == nil.
+    runnableExamples("-d:nimExperimentalTypeInfoCore"):
+      type
+        Base = ref object of RootObj
+        Sub = ref object of Base
+      var a: Base = Sub()
+      assert a.getDynamicTypeInfo == Sub().getDynamicTypeInfo
+      assert a[].getDynamicTypeInfo == Sub().getDynamicTypeInfo
+      assert 0.getDynamicTypeInfo.size == int.sizeof
+      assert Base(nil).getDynamicTypeInfo == nil
+    runnableExamples("-d:nimTypeNames -d:nimExperimentalTypeInfoCore"):
+      type
+        Base = ref object of RootObj
+        Sub = object of Base
+        PSub = ref Sub
+      var a: Base = PSub()
+      # `name` is only available for `-d:nimTypeNames` for `gc:refc`
+      # `name` is implementation defined for `gc:arc|orc`.
+      assert a.getDynamicTypeInfo.name == "Sub"
+      assert Base().getDynamicTypeInfo.name == "Base:ObjectType" # implementation defined
+    when T is ref|ptr:
+      if x != nil: 
+        result = getDynamicTypeInfoImpl(x[])
+    else:
+      result = getDynamicTypeInfoImpl(x)
