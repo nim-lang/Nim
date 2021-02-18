2018d0e11470f454d05a58b34fd63a1911d2b454
Timothee Cour <timothee.cour2@gmail.com>
2021-02-17 23:51:25 -0800

    fixup

--- tests/stdlib/tenumutils.nim
+++ tests/stdlib/tenumutils.nim
@@ -9,8 +9,8 @@ template main =
   block: # items
     type A = enum a0 = 2, a1 = 4, a2
     type B[T] = enum b0 = 2, b1 = 4
-    assert A.toSeq == [a0, a1, a2]
-    assert B[float].toSeq == [B[float].b0, B[float].b1]
+    doAssert A.toSeq == [a0, a1, a2]
+    doAssert B[float].toSeq == [B[float].b0, B[float].b1]
 
 static: main()
 main()
