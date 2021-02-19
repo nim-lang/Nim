e12d5eb0089902fe569112d0c51cdb2310631c8f
Timothee Cour <timothee.cour2@gmail.com>
2021-02-19 12:55:58 -0800

    fix for windows

--- lib/pure/prelude.nim
+++ lib/pure/prelude.nim
@@ -13,14 +13,16 @@ when isMainModule and compileSettings.querySetting(compileSettings.SingleValueSe
   ## This is an include file that simply imports common modules for your convenience.
   runnableExamples:
     include std/prelude
-    # same as:
-    # import std/[os, strutils, times, parseutils, hashes, tables, sets, sequtils, parseopt]
-    when not defined js:
-      let t = now()
-      assert getCurrentDir().len > 0
-      assert now() > t
+      # same as:
+      # import std/[os, strutils, times, parseutils, hashes, tables, sets, sequtils, parseopt]
     let x = 1
     assert "foo $# $#" % [$x, "bar"] == "foo 1 bar"
     assert toSeq(1..3) == @[1, 2, 3]
+    when not defined(js) or defined(nodejs):
+      assert getCurrentDir().len > 0
+      assert ($now()).startsWith "20"
+
+# xxx `nim doc -b:js -d:nodejs --doccmd:-d:nodejs lib/pure/prelude.nim` fails for some reason
+# specific to `nim doc`, but the code otherwise works with nodejs.
 
 import std/[os, strutils, times, parseutils, hashes, tables, sets, sequtils, parseopt]
