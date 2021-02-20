37b4a067f14935976d59f787ee8f8d6bf93481b0
konsumlamm <konsumlamm@gmail.com>
2021-02-20 17:24:14 +0100

    Add example for pattern matching on options

--- lib/pure/options.nim
+++ lib/pure/options.nim
@@ -39,6 +39,27 @@ runnableExamples:
 ## inherits from `system.Defect` and should therefore never be caught.
 ## Instead, rely on checking if the option contains a value with the
 ## `isSome <#isSome,Option[T]>`_ and `isNone <#isNone,Option[T]>`_ procs.
+##
+##
+## Pattern matching
+## ================
+##
+## [fusion/matching](https://nim-lang.github.io/fusion/src/fusion/matching.html)
+## supports pattern matching on `Option`s, with the `Some(<pattern>)` and
+## `None()` patterns.
+
+runnableExamples:
+  {.experimental: "caseStmtMacros".}
+
+  import fusion/matching
+
+  case some(42)
+  of Some(@a):
+    assert a == 42
+  of None():
+    assert false
+
+  Some(Some(None())) := some(some(none(int)))
 
 
 import std/typetraits
