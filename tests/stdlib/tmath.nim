2ff517462bf8609b30e6134c96658aa7912b628a
narimiran <narimiran@disroot.org>
2021-02-22 13:51:56 +0100

    remove tests for stuff not available in 1.4

--- tests/stdlib/tmath.nim
+++ tests/stdlib/tmath.nim
@@ -222,15 +222,9 @@ block:
 
         # check gamma function
         doAssert gamma(5.0) == 24.0 # 4!
-        doAssert almostEqual(gamma(0.5), sqrt(PI))
-        doAssert almostEqual(gamma(-0.5), -2 * sqrt(PI))
         doAssert lgamma(1.0) == 0.0 # ln(1.0) == 0.0
-        doAssert almostEqual(lgamma(0.5), 0.5 * ln(PI))
         doAssert erf(6.0) > erf(5.0)
         doAssert erfc(6.0) < erfc(5.0)
-
-    when not defined(js) and not defined(windows): # xxx pending bug #17017
-      doAssert gamma(-1.0).isNaN
 
     block: # sgn() tests
       assert sgn(1'i8) == 1
