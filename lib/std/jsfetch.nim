7e8f773244952ee9de49f891bc57e463d2582046
Juan Carlos <JuanCarlospaco@gmail.com>
2021-02-27 20:23:22 -0300

    Otherwise doc is empty

--- lib/std/jsfetch.nim
+++ lib/std/jsfetch.nim
@@ -3,7 +3,7 @@
 when not defined(js):
   {.fatal: "Module jsfetch is designed to be used with the JavaScript backend.".}
 
-when defined(nimExperimentalJsfetch):
+when defined(nimExperimentalJsfetch) or defined(nimdoc):
   import std/[asyncjs, jsheaders, jsformdata]
   from std/httpcore import HttpMethod
   from std/jsffi import JsObject
