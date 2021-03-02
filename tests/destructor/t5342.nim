85c00ba5181d02995e66de3ee39127083c6b823b
flywind <xzsflywind@gmail.com>
2021-03-02 15:41:23 +0800

    update the example

--- tests/destructor/t5342.nim
+++ tests/destructor/t5342.nim
@@ -1,22 +1,23 @@
 discard """
   matrix: "--gc:refc; --gc:arc"
   output: '''
-~A
-~A
-~A
-~A
+1
+2
+here
+2
+1
 '''
 """
 
 
 type
   A = object
+    id: int
   B = object
     a: A
-
-proc `=destroy`(a: var A) = echo "~A"
-
-var x = A()
-var y = B()
+proc `=destroy`(a: var A) = echo a.id
+var x = A(id: 1)
+var y = B(a: A(id: 2))
 `=destroy`(x)
 `=destroy`(y)
+echo "here"
