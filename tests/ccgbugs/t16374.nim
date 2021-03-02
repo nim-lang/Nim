22c7155dae51b06e697c72b6640ae315e2b4a8e7
flywind <xzsflywind@gmail.com>
2021-03-02 16:30:26 +0800

    attempt to fix #16374

---
+++ tests/ccgbugs/t16374.nim
@@ -0,0 +1,23 @@
+discard """
+  matrix: "--gc:refc; --gc:orc"
+"""
+
+iterator mvalues(t: var seq[seq[int]]): var seq[int] =
+  yield t[0]
+
+var t: seq[seq[int]]
+
+while false:
+  for v in t.mvalues:
+    discard
+
+proc ok =
+  while false:
+    for v in t.mvalues:
+      discard
+
+ok()
+
+while false:
+  for v in t.mvalues:
+    discard
