c11610d53076dc53b0edb0add70605a371fc0f9e
Juan Carlos <juancarlospaco@gmail.com>
2021-03-04 05:07:02 -0300

    Update lib/std/jsbigints.nim
    Co-authored-by: flywind <xzsflywind@gmail.com>

--- lib/std/jsbigints.nim
+++ lib/std/jsbigints.nim
@@ -8,7 +8,7 @@ type JsBigInt* = distinct JsBigIntImpl        ## Arbitrary precision integer for
 
 func big*(integer: SomeInteger): JsBigInt {.importjs: "BigInt(#)".} =
   ## Constructor for `JsBigInt`.
-  when nimvm: doAssert off, "JsBigInt can not be used at compile-time nor static context" else: discard
+  when nimvm: doAssert false, "JsBigInt can not be used at compile-time nor static context" else: discard
   runnableExamples:
     doAssert big(1234567890) == big"1234567890"
     doAssert 0b1111100111.big == 0o1747.big and 0o1747.big == 999.big
