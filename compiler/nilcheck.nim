d134876e650f9cd18c9917fcd8bad845e1db8edb
Andreas Rumpf <rumpf_a@web.de>
2021-03-09 20:19:24 +0100

    fixes #11225; generic sandwich problems; [backport:1.2] (#17255)
    * fixes #11225; generic sandwich problems; [backport:1.2]
    * progress
    * delegating these symbols must be done via 'bind'

--- compiler/nilcheck.nim
+++ compiler/nilcheck.nim
@@ -1260,7 +1260,8 @@ proc check(n: PNode, ctx: NilCheckerContext, map: NilMap): Check =
   of nkNone..pred(nkSym), succ(nkSym)..nkNilLit, nkTypeSection, nkProcDef, nkConverterDef,
       nkMethodDef, nkIteratorDef, nkMacroDef, nkTemplateDef, nkLambda, nkDo,
       nkFuncDef, nkConstSection, nkConstDef, nkIncludeStmt, nkImportStmt,
-      nkExportStmt, nkPragma, nkCommentStmt, nkBreakState, nkTypeOfExpr:
+      nkExportStmt, nkPragma, nkCommentStmt, nkBreakState,
+      nkTypeOfExpr, nkMixinStmt, nkBindStmt:
 
     discard "don't follow this : same as varpartitions"
     result = Check(nilability: Nil, map: map)
