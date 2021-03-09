70fe3f2b548745048885da5c794b9c1b59bd440b
flywind <xzsflywind@gmail.com>
2021-03-09 19:36:49 +0800

    add comments

--- lib/std/channels.nim
+++ lib/std/channels.nim
@@ -9,7 +9,13 @@
 
 
 # Based on https://github.com/mratsim/weave/blob/5696d94e6358711e840f8c0b7c684fcc5cbd4472/unused/channels/channels_legacy.nim
-
+# Those are translations of @aprell (Andreas Prell) original channels from C to Nim
+# (https://github.com/aprell/tasking-2.0/blob/master/src/channel_shm/channel.c)
+# And in turn they are an implementation of Michael & Scott lock-based queues
+# (note the paper has 2 channels: lock-free and lock-based) with additional caching:
+# Simple, Fast, and Practical Non-Blocking and Blocking Concurrent Queue Algorithms
+# Maged M. Michael, Michael L. Scott, 1996
+# https://www.cs.rochester.edu/~scott/papers/1996_PODC_queues.pdf
 
 ## This module only works with `--gc:arc` or `--gc:orc`.
 ##
