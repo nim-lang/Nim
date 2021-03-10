b42cfc0892175d111981e30d3374a3426e5647b0
flywind <xzsflywind@gmail.com>
2021-03-10 12:05:53 +0800

    close #16607 add testcase

---
+++ tests/destructor/t16607.nim
@@ -0,0 +1,22 @@
+discard """
+  matrix: "--gc:refc; --gc:arc"
+"""
+
+type
+  O {.requiresInit.} = object
+    initialized: bool
+
+proc `=destroy`(o: var O) =
+  doAssert o.initialized, "O was destroyed before initialization!"
+
+proc initO(): O =
+  O(initialized: true)
+
+proc pair(): tuple[a, b: O] =
+  result.a = initO()
+  result.b = initO()
+
+proc main() =
+  discard pair()
+
+main()
