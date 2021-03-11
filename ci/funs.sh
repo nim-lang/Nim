2823e37fc1ea773143212674bf337b365591d486
Timothee Cour <timothee.cour2@gmail.com>
2021-03-10 18:27:33 -0800

    fixup

--- ci/funs.sh
+++ ci/funs.sh
@@ -1,5 +1,7 @@
-# used for CI code
+# utilities used in CI pipelines to avoid duplication.
+
 echo_run () {
+  # echo's a command before running it, which helps understanding logs
   echo ""
   echo "$@"
   "$@"
