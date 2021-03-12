a7570ff0c2fa2539b8732808d06967e9d17d1284
xioren <40043405+xioren@users.noreply.github.com>
2021-03-11 23:09:39 -0800

    Relocate 4xx/5xx exception in downloadFile (#17332) [backport:1.2]
    Move 4xx/5xx exception to before disk i/o. As it stands an empty file is created on http error 4xx/5xx.
    
    (cherry picked from commit 3d198fdcc237aa3927ad5c3b934688de24499b03)

--- lib/pure/httpclient.nim
+++ lib/pure/httpclient.nim
@@ -1140,14 +1140,14 @@ proc downloadFile*(client: HttpClient, url: string, filename: string) =
     client.getBody = true
   let resp = client.get(url)
 
+  if resp.code.is4xx or resp.code.is5xx:
+    raise newException(HttpRequestError, resp.status)
+
   client.bodyStream = newFileStream(filename, fmWrite)
   if client.bodyStream.isNil:
     fileError("Unable to open file")
   parseBody(client, resp.headers, resp.version)
   client.bodyStream.close()
-
-  if resp.code.is4xx or resp.code.is5xx:
-    raise newException(HttpRequestError, resp.status)
 
 proc downloadFile*(client: AsyncHttpClient, url: string,
                    filename: string): Future[void] =
