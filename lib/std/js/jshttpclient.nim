3e67862da57e38dc20464309abf7ac010f9b386e
Juan Carlos <JuanCarlospaco@gmail.com>
2021-03-14 11:49:03 -0300

    How is that path

--- lib/std/js/jshttpclient.nim
+++ lib/std/js/jshttpclient.nim
@@ -1,5 +1,5 @@
 ## HttpClient for JavaScript targets.
 ## * Asyncronous `HttpClient` implemented on top of `jsfetch`.
 ## * Syncronous `HttpClient` implemented on top of `jsxmlhttprequest`.
-import std/[jsasynchttpclient, jssynchttpclient]
+import std/js/[jsasynchttpclient, jssynchttpclient]
 export jsasynchttpclient, jssynchttpclient
