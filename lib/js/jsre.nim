5b82ca1ef2e969d4f1985c595855b8bb236a59e4
Juan Carlos <JuanCarlospaco@gmail.com>
2021-03-14 13:04:37 -0300

    Peer review feedbacks

--- lib/js/jsre.nim
+++ lib/js/jsre.nim
@@ -1,21 +1,9 @@
 ## Regular Expressions for the JavaScript target.
 ## * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
-
-runnableExamples:
-  let jsregex: RegExp = newRegExp(r"\s+", r"i")
-  jsregex.compile(r"\w+", r"i")
-  assert jsregex.test(r"nim javascript")
-  assert jsregex.exec(r"nim javascript") == @["nim".cstring]
-  assert jsregex.toString() == r"/\w+/i"
-  jsregex.compile(r"[0-9]", r"i")
-  assert jsregex.test(r"0123456789abcd")
-  assert $jsregex == "/[0-9]/i"
-  assert jsregex.len == 5
-
 when not defined(js):
   {.error: "This module only works on the JavaScript platform".}
 
-type RegExp* {.importjs.} = object    ## Regular Expressions for JavaScript target.
+type RegExp* {.importjs.} = ref object of JsRoot ## Regular Expressions for JavaScript target.
   flags* {.importjs.}: cstring        ## cstring that contains the flags of the RegExp object.
   dotAll* {.importjs.}: bool          ## Whether `.` matches newlines or not.
   global* {.importjs.}: bool          ## Whether to test against all possible matches in a string, or only against the first.
@@ -40,12 +28,27 @@ func compile*(self: RegExp; pattern: cstring; flags: cstring) {.importjs: "#.com
 func exec*(self: RegExp; pattern: cstring): seq[cstring] {.importjs: "#.exec(#)".}
   ## Executes a search for a match in its string parameter.
 
-func test*(self: RegExp; pattern: cstring): bool {.importjs: "#.test(#)".}
+func match*(self: RegExp; pattern: cstring): bool {.importjs: "#.test(#)".}
   ## Tests for a match in its string parameter.
 
-func toString*(self: RegExp): cstring {.importjs: "#.toString()".}
+func toCstring*(self: RegExp): cstring {.importjs: "#.toString()".}
   ## Returns a string representing the RegExp object.
 
-func `$`*(self: RegExp): string = $toString(self)
+func `$`*(self: RegExp): string = $toCstring(self)
 
-func len*(self: RegExp): int = self.source.len
+func test*(self: RegExp; pattern: cstring): bool {.importjs: "#.test(#)", deprecated: "Use match instead".}
+  ## .. warning:: Deprecated, do not use.
+
+func toString*(self: RegExp): cstring {.importjs: "#.toString()", deprecated: "Use toCstring instead".}
+  ## .. warning:: Deprecated, do not use.
+
+
+runnableExamples:
+  let jsregex: RegExp = newRegExp(r"\s+", r"i")
+  jsregex.compile(r"\w+", r"i")
+  assert jsregex.match(r"nim javascript")
+  assert jsregex.exec(r"nim javascript") == @["nim".cstring]
+  assert jsregex.toCstring() == r"/\w+/i"
+  jsregex.compile(r"[0-9]", r"i")
+  assert jsregex.match(r"0123456789abcd")
+  assert $jsregex == "/[0-9]/i"
