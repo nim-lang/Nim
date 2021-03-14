45483850cb7e9da0a3659d82c827a90824bd8211
John Dupuy <jdupuy98@gmail.com>
2021-03-14 12:25:05 -0500

    Improved lexer AST test macro.

--- tests/lexer/mlexerutils.nim
+++ tests/lexer/mlexerutils.nim
@@ -6,9 +6,4 @@ macro assertAST*(expected: string, struct: untyped): untyped =
   var ast = newLit(struct.treeRepr)
   result = quote do:
     if `ast` != `expected`:
-      echo "Got:"
-      echo `ast`.indent(2)
-      echo "Expected:"
-      echo `expected`.indent(2)
-      raise newException(ValueError, "Failed to lex properly")
-
+      doAssert false, "\nGot:\n" & `ast`.indent(2) & "\nExpected:\n" & `expected`.indent(2)
