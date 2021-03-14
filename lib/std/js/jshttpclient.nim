776d73c8a212a7abb4bc1194b1a3641be1f0314a
Juan Carlos <JuanCarlospaco@gmail.com>
2021-03-14 11:19:10 -0300

    Add jshttpclient

--- lib/std/js/jshttpclient.nim
+++ lib/std/js/jshttpclient.nim
@@ -1,5 +1,5 @@
 ## HttpClient for JavaScript targets.
 ## * Asyncronous `HttpClient` implemented on top of `jsfetch`.
 ## * Syncronous `HttpClient` implemented on top of `jsxmlhttprequest`.
-import jsasynchttpclient, jssynchttpclient
+import std/[jsasynchttpclient, jssynchttpclient]
 export jsasynchttpclient, jssynchttpclient
