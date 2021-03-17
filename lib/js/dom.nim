5dea264eb4e779e2f03ac78c0a45ccd78652c037
Thiago Navarro <74574275+thisago@users.noreply.github.com>
2021-03-17 20:08:43 -0300

    Possibility to use async procs as parameter of dom.js bindings
    Affected functions:
    - addEventListener
    - removeEventListener
    - setTimeout
    - setInterval
    
    These functions now can receive async procs as parameters

--- lib/js/dom.nim
+++ lib/js/dom.nim
@@ -1471,16 +1471,16 @@ else:
   proc createTextNode*(d: Document, identifier: cstring): Node {.importcpp.}
   proc createComment*(d: Document, data: cstring): Node {.importcpp.}
 
-proc setTimeout*(action: proc(); ms: int): TimeOut {.importc, nodecl.}
+proc setTimeout*[T](action: proc (): T; ms: int): TimeOut {.importc, nodecl.}
 proc clearTimeout*(t: TimeOut) {.importc, nodecl.}
 
 {.push importcpp.}
 
 # EventTarget "methods"
-proc addEventListener*(et: EventTarget, ev: cstring, cb: proc(ev: Event), useCapture: bool = false)
-proc addEventListener*(et: EventTarget, ev: cstring, cb: proc(ev: Event), options: AddEventListenerOptions)
+proc addEventListener*[T](et: EventTarget, ev: cstring, cb: proc (ev: Event): T, useCapture: bool = false)
+proc addEventListener*[T](et: EventTarget, ev: cstring, cb: proc (ev: Event): T, options: AddEventListenerOptions)
 proc dispatchEvent*(et: EventTarget, ev: Event)
-proc removeEventListener*(et: EventTarget; ev: cstring; cb: proc(ev: Event))
+proc removeEventListener*[T](et: EventTarget; ev: cstring; cb: proc (ev: Event): T)
 
 # Window "methods"
 proc alert*(w: Window, msg: cstring)
@@ -1511,11 +1511,11 @@ proc routeEvent*(w: Window, event: Event)
 proc scrollBy*(w: Window, x, y: int)
 proc scrollTo*(w: Window, x, y: int)
 proc setInterval*(w: Window, code: cstring, pause: int): ref Interval
-proc setInterval*(w: Window, function: proc (), pause: int): ref Interval
+proc setInterval*[T](w: Window, function: proc (): T, pause: int): ref Interval
 proc setTimeout*(w: Window, code: cstring, pause: int): ref TimeOut
-proc setTimeout*(w: Window, function: proc (), pause: int): ref Interval
+proc setTimeout*[T](w: Window, function: proc (): T, pause: int): ref Interval
 proc stop*(w: Window)
-proc requestAnimationFrame*(w: Window, function: proc (time: float)): int
+proc requestAnimationFrame*[T](w: Window, function: proc (time: float): T): int
 proc cancelAnimationFrame*(w: Window, id: int)
 
 # Node "methods"
