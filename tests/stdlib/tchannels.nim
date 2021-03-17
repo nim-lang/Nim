a84b5506b678e89ee7535f66bb7b612b67f2681a
flywind <xzsflywind@gmail.com>
2021-03-17 23:12:56 +0800

    follow up #17391

---
+++ tests/stdlib/tchannels.nim
@@ -0,0 +1,26 @@
+discard """
+  targets: "--gc:orc --threads:on -d:danger"
+"""
+
+import std/[channels, isolation]
+
+var
+  sender: array[10, Thread[void]]
+  receiver: array[5, Thread[void]] 
+
+var chan = newChannel[seq[string]](40)
+proc sendHandler() =
+  chan.send(isolate(@["Hello, Nim"]))
+proc recvHandler() =
+  var x = chan.recv()
+  discard x
+
+template benchmark() =
+  for t in mitems(sender):
+    t.createThread(sendHandler)
+  joinThreads(sender)
+  for i in 0 .. receiver.high:
+    createThread(receiver[i], recvHandler)
+  joinThreads(receiver)
+
+benchmark()
