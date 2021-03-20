7ec8368f371efe25b2bbe280a6e22d1f91a793c7
Leorize <leorize+oss@disroot.org>
2021-03-19 23:22:25 -0500

    t13115: disable for linux i386
    Node.js no longer officially support this platform.

--- tests/exception/t13115.nim
+++ tests/exception/t13115.nim
@@ -13,10 +13,10 @@ else:
     const nim = getCurrentCompilerExe()
     const file = currentSourcePath
     for b in "c js cpp".split:
-      when defined(openbsd):
+      when defined(openbsd) or (defined(linux) and defined(i386)):
         if b == "js":
           # xxx bug: pending #13115
-          # remove special case once nodejs updated >= 12.16.2
+          # remove special case for openbsd once nodejs updated >= 12.16.2
           # refs https://github.com/nim-lang/Nim/pull/16167#issuecomment-738270751
           continue
 
