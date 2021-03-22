05039e604ee3d329929a3a942c0582eeb69f70b2
Timothee Cour <timothee.cour2@gmail.com>
2021-03-22 12:53:29 -0700

    fix #17458

--- tests/stdlib/tnetconnect.nim
+++ tests/stdlib/tnetconnect.nim
@@ -10,21 +10,17 @@ proc test() =
   let ctx = newContext()
 
   proc fn(url: string) =
-    echo (url,)
     let socket = newSocket()
     defer: close(socket)
-    if url == "www.nim-lang.org":
-      connect(socket, url, Port(443), 4) # typically 20 could be enough
-    else:
-      connect(socket, url, Port(443), 5000) # typically 20 could be enough
+    connect(socket, url, Port(443), 5000) # typically 20 could be enough
     send(socket, "GET / HTTP/1.0\nHost: $#\nConnection: close\n\n" % [url])
     wrapSocket(ctx, socket)
 
+  # trying 2 sites makes it more resilent: refs #17458 this could give:
+  # Error: unhandled exception: Call to 'connect' timed out. [TimeoutError]
   try:
     fn("www.nim-lang.org")
   except TimeoutError:
-    # refs #17458 this can give:
-    # Error: unhandled exception: Call to 'connect' timed out. [TimeoutError]
     fn("www.google.com")
 
 test()
