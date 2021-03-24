aa802f8cd9af59e5e33dea19b713a64e92d0a596
flywind <xzsflywind@gmail.com>
2021-03-24 11:15:54 +0800

    better

--- tests/stdlib/tstrbasics.nim
+++ tests/stdlib/tstrbasics.nim
@@ -126,29 +126,30 @@ proc main() =
 static: main()
 main()
 
-# when not defined(js):
-#   block: # setLen
-#     var a = "abc"
-#     a.setLen 0
-#     a.setLen 3, isInit = false
-#     doAssert a[1] == 'b'
-#     a.setLen 0
-#     a.setLen 3, isInit = true
-#     doAssert a[1] == '\0'
+when false:
+  when not defined(js):
+    block: # setLen
+      var a = "abc"
+      a.setLen 0
+      a.setLen 3, isInit = false
+      doAssert a[1] == 'b'
+      a.setLen 0
+      a.setLen 3, isInit = true
+      doAssert a[1] == '\0'
 
-#   block: # forceCopy
-#     var a: string
-#     a = "foo"
-#     shallow(a)
-#     var b: string
-#     b = a
-#     doAssert b[0].addr == a[0].addr
-#     var c: string
-#     c.forceCopy a
-#     doAssert c == a
-#     doAssert c[0].addr != a[0].addr
+    block: # forceCopy
+      var a: string
+      a = "foo"
+      shallow(a)
+      var b: string
+      b = a
+      doAssert b[0].addr == a[0].addr
+      var c: string
+      c.forceCopy a
+      doAssert c == a
+      doAssert c[0].addr != a[0].addr
 
-#   block: # dataPointer
-#     var a: string
-#     discard a.dataPointer
-#     # doAssert a.dataPointer == nil # not guaranteed
+    block: # dataPointer
+      var a: string
+      discard a.dataPointer
+      # doAssert a.dataPointer == nil # not guaranteed
