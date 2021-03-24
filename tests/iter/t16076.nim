15e00a15351b9e93423b08631475aced4988a051
flywind <xzsflywind@gmail.com>
2021-03-24 15:05:25 +0800

    test more branches

--- tests/iter/t16076.nim
+++ tests/iter/t16076.nim
@@ -1,41 +1,46 @@
 discard """
   targets: "c js"
+  nimout: '''
+0
+10
+'''
   output: '''
 0
 10
 '''
 """
 
-block: # bug #17485
-  type
-    O = ref object
-      i: int
+proc main() =
+  block: # bug #17485
+    type
+      O = ref object
+        i: int
 
-  iterator t(o: O): int =
-    if o != nil:
-      yield o.i
-    yield 0
+    iterator t(o: O): int =
+      if o != nil:
+        yield o.i
+      yield 0
 
-  proc m =
-    for i in t(nil):
-      echo i
+    proc m =
+      for i in t(nil):
+        echo i
 
-  m()
+    m()
 
 
-block: # bug #16076
-  type
-    R = ref object
-      z: int
+  block: # bug #16076
+    type
+      R = ref object
+        z: int
 
-  iterator foo(x: int; y: R = nil): int {.inline.} =
-    if y == nil:
-      yield x
-    else:
-      yield y.z
+    iterator foo(x: int; y: R = nil): int {.inline.} =
+      if y == nil:
+        yield x
+      else:
+        yield y.z
 
-  proc main() =
     for b in foo(10):
       echo b
 
-  main()
+static: main()
+main()
