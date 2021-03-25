fe0fd360631bdaba73192398e383e5a9ffb3cec5
flywind <xzsflywind@gmail.com>
2021-03-25 16:48:16 +0800

    relieve

--- lib/pure/collections/tables.nim
+++ lib/pure/collections/tables.nim
@@ -650,7 +650,7 @@ template withValue*[A, B](t: var Table[A, B], key: A,
     body2
 
 
-iterator pairs*[A, B](t: Table[A, B]): lent (A, B) =
+iterator pairs*[A, B](t: Table[A, B]): (A, B) =
   ## Iterates over any `(key, value)` pair in the table `t`.
   ##
   ## See also:
@@ -1094,7 +1094,7 @@ proc `==`*[A, B](s, t: TableRef[A, B]): bool =
 
 
 
-iterator pairs*[A, B](t: TableRef[A, B]): lent (A, B) =
+iterator pairs*[A, B](t: TableRef[A, B]): (A, B) =
   ## Iterates over any `(key, value)` pair in the table `t`.
   ##
   ## See also:
@@ -1669,7 +1669,7 @@ proc `==`*[A, B](s, t: OrderedTable[A, B]): bool =
 
 
 
-iterator pairs*[A, B](t: OrderedTable[A, B]): lent (A, B) =
+iterator pairs*[A, B](t: OrderedTable[A, B]): (A, B) =
   ## Iterates over any `(key, value)` pair in the table `t` in insertion
   ## order.
   ##
@@ -2077,7 +2077,7 @@ proc `==`*[A, B](s, t: OrderedTableRef[A, B]): bool =
 
 
 
-iterator pairs*[A, B](t: OrderedTableRef[A, B]): lent (A, B) =
+iterator pairs*[A, B](t: OrderedTableRef[A, B]): (A, B) =
   ## Iterates over any `(key, value)` pair in the table `t` in insertion
   ## order.
   ##
@@ -2491,7 +2491,7 @@ proc `==`*[A](s, t: CountTable[A]): bool =
   equalsImpl(s, t)
 
 
-iterator pairs*[A](t: CountTable[A]): lent (A, int) =
+iterator pairs*[A](t: CountTable[A]): (A, int) =
   ## Iterates over any `(key, value)` pair in the table `t`.
   ##
   ## See also:
@@ -2767,7 +2767,7 @@ proc `==`*[A](s, t: CountTableRef[A]): bool =
   else: result = s[] == t[]
 
 
-iterator pairs*[A](t: CountTableRef[A]): lent (A, int) =
+iterator pairs*[A](t: CountTableRef[A]): (A, int) =
   ## Iterates over any `(key, value)` pair in the table `t`.
   ##
   ## See also:
