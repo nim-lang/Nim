b799940f36cdfe0b0a1479d2d30168650f61ecbe
flywind <xzsflywind@gmail.com>
2021-03-27 22:23:04 +0800

    add testcase for #9466

---
+++ tests/notnil/tnotnil5.nim
@@ -0,0 +1,29 @@
+discard """
+  matrix: "--threads:on"
+"""
+
+{.experimental: "parallel".}
+{.experimental: "notnil".}
+import threadpool
+
+type
+  AO = object
+    x: int
+
+  A = ref AO not nil
+
+proc process(a: A): A =
+  return A(x: a.x+1)
+
+proc processMany(ayys: openArray[A]): seq[A] =
+  var newAs: seq[FlowVar[A]]
+
+  parallel:
+    for a in ayys:
+      newAs.add(spawn process(a))
+  for newAflow in newAs:
+    let newA = ^newAflow
+    if isNil(newA):
+      return @[]
+    # Error: cannot prove 'newA' is not nil
+    result.add(newA)
