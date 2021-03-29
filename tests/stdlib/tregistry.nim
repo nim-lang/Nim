b164243490e4b35244b5f101bc7829e55274fa65
flywind <xzsflywind@gmail.com>
2021-03-29 11:54:10 +0800

    Update tregistry.nim

--- tests/stdlib/tregistry.nim
+++ tests/stdlib/tregistry.nim
@@ -3,12 +3,13 @@ discard """
   matrix: "--gc:refc; --gc:arc"
 """
 
-import std/registry
+when defined(windows):
+  import std/registry
 
-block: # bug #14010
-  let path = "Environment"
-  let key = "D20210328T202842_key"
-  let val = "D20210328T202842_val"
-  let handle = HKEY_CURRENT_USER
-  setUnicodeValue("Environment", key, val, handle)
-  doAssert getUnicodeValue(path, key, handle) == val
+  block: # bug #14010
+    let path = "Environment"
+    let key = "D20210328T202842_key"
+    let val = "D20210328T202842_val"
+    let handle = HKEY_CURRENT_USER
+    setUnicodeValue("Environment", key, val, handle)
+    doAssert getUnicodeValue(path, key, handle) == val
