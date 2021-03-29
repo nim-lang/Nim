47f873aacf0f6accc11f4048ef9265c939782085
Araq <rumpf_a@web.de>
2021-03-29 12:58:57 +0200

    added nkError to the AST

--- lib/core/macros.nim
+++ lib/core/macros.nim
@@ -85,7 +85,8 @@ type
     nnkState,
     nnkBreakState,
     nnkFuncDef,
-    nnkTupleConstr
+    nnkTupleConstr,
+    nnkError  ## errornous AST node
 
   NimNodeKinds* = set[NimNodeKind]
   NimTypeKind* = enum  # some types are no longer used, see ast.nim
