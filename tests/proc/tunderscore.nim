c90644ef84a044ee7cca57da521e4a13cb69e8ba
flywind <xzsflywind@gmail.com>
2021-03-30 14:14:04 +0800

    fix #13443

---
+++ tests/proc/tunderscore.nim
@@ -0,0 +1,23 @@
+discard """
+  targets: "c cpp js"
+"""
+
+var x = 0
+
+block:
+  proc foo(_, _: int) = x += 5
+
+  foo(1, 2)
+  doAssert x == 5
+
+block:
+  proc foo(_: int, _: float) = x += 5
+
+  foo(1, 2)
+  doAssert x == 10
+
+block:
+  proc foo(_: int, _: float, _: string) = x += 5
+
+  foo(1, 2, "5")
+  doAssert x == 15
