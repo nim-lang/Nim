12a376a6aa5cf0d6607af913c413220d2eeb20eb
flywind <xzsflywind@gmail.com>
2021-03-30 13:06:03 +0800

    Apply suggestions from code review

--- tests/tools/tnimscriptwithnimext.nim
+++ tests/tools/tnimscriptwithnimext.nim
@@ -1,22 +1,7 @@
 discard """
 cmd: "nim e $file"
-output: '''
-foobar
-nothing
-hallo
-'''
 """
 
 # nimscript files can any extension
-import macros
-
-macro foobar(): void =
-  result = newCall(bindSym"echo", newLit("nothing"))
-
-echo "foobar"
-
-let x = 123
-
-foobar()
-
-exec "echo hallo"
+let a = 11
+doAssert a == 11
