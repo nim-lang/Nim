f0e9d3658fdd7e188d8efbd4ffba2e8c1279aa04
flywind <xzsflywind@gmail.com>
2021-03-30 16:50:59 +0800

    fix tests

--- tests/proc/tunderscore.nim
+++ tests/proc/tunderscore.nim
@@ -2,7 +2,26 @@ discard """
   targets: "c cpp js"
 """
 
+block:
+  proc ok(_, _: int) = discard
+  foo(4, 2)
+
+block:
+  proc ok(_, _: int) = discard
+  foo(4, 2)
+
+block:
+  proc ok(_: int, _: float) = discard
+  foo(1, 2.0)
+
+block:
+  proc ok(_: int, _: float, _: string) = discard
+  foo(1, 2.6, "5")
+
 proc main() =
+  when defined(js):
+    when nimvm:
+      return
   var x = 0
 
   block:
