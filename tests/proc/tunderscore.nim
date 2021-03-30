75e7fc594e829bf15702e90adea7b6d172db983d
flywind <xzsflywind@gmail.com>
2021-03-30 14:22:00 +0800

    better

--- tests/proc/tunderscore.nim
+++ tests/proc/tunderscore.nim
@@ -2,22 +2,27 @@ discard """
   targets: "c cpp js"
 """
 
-var x = 0
+proc main() =
+  var x = 0
 
-block:
-  proc foo(_, _: int) = x += 5
+  block:
+    proc foo(_, _: int) = x += 5
 
-  foo(1, 2)
-  doAssert x == 5
+    foo(1, 2)
+    doAssert x == 5
 
-block:
-  proc foo(_: int, _: float) = x += 5
+  block:
+    proc foo(_: int, _: float) = x += 5
 
-  foo(1, 2)
-  doAssert x == 10
+    foo(1, 2)
+    doAssert x == 10
 
-block:
-  proc foo(_: int, _: float, _: string) = x += 5
+  block:
+    proc foo(_: int, _: float, _: string) = x += 5
 
-  foo(1, 2, "5")
-  doAssert x == 15
+    foo(1, 2, "5")
+    doAssert x == 15
+
+
+static: main()
+main()
