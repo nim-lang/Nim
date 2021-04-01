e852f6b44f03fd44dbcd0e5617b3a296f5676d09
flywind <xzsflywind@gmail.com>
2021-04-01 12:17:35 +0800

    b

--- tests/system/tstatic.nim
+++ tests/system/tstatic.nim
@@ -4,42 +4,47 @@ discard """
 
 import std/strutils
 
-
-block: # bug #6133
+# bug #6133
+template main() =
   block:
-    proc foo(q: string, a: int): int =
-      result = q.len
+    block:
+      proc foo(q: string, a: int): int =
+        result = q.len
 
-    proc foo(q: static[string]): int =
-      result = foo(q, 5)
+      proc foo(q: static[string]): int =
+        result = foo(q, 5)
 
-    doAssert foo("123") == 3
+      doAssert foo("123") == 3
 
-  block:
-    type E = enum A
+    block:
+      type E = enum A
 
-    if false:
-      var e = A
-      discard $e
+      if false:
+        var e = A
+        discard $e
 
-    proc foo(a: string): int =
-      len(a) # 16640
+      proc foo(a: string): int =
+        len(a) # 16640
 
-    proc foo(a: static[bool]): int {.used.} =
-      discard
+      proc foo(a: static[bool]): int {.used.} =
+        discard
 
-    doAssert foo("") == 0
+      doAssert foo("") == 0
 
-  block:
-    proc foo(a: string): int =
-      len(a)
+    block:
+      proc foo(a: string): int =
+        len(a)
 
-    proc foo(a: static[bool]): int {.used.} =
-      discard
+      proc foo(a: static[bool]): int {.used.} =
+        discard
 
-    doAssert foo("abc") == 3
+      doAssert foo("abc") == 3
 
-  block:
-    proc parseInt(f: static[bool]): int {.used.} = discard
+    block:
+      proc parseInt(f: static[bool]): int {.used.} = discard
 
-    doAssert "123".parseInt == 123
+      doAssert "123".parseInt == 123
+
+
+static: main()
+main()
