3155d9b5ae2dab86859e6a1deafb66f53bf79634
flywind <xzsflywind@gmail.com>
2021-04-04 10:22:37 +0800

    Update twrongcolon.nim

--- tests/errmsgs/twrongcolon.nim
+++ tests/errmsgs/twrongcolon.nim
@@ -1,10 +1,11 @@
 discard """
 errormsg: "in expression ' do:"
 nimout: '''
+Error: in expression ' do:
   890': identifier expected, but found ''
 '''
 
-line: 10
+line: 11
 """
 
 var n: int : 890
