39257aa26cf5d8f985e9db71511676928ef718c5
Timothee Cour <timothee.cour2@gmail.com>
2021-04-05 09:26:16 -0700

    fixup

--- tests/stdlib/treflection.nim
+++ tests/stdlib/treflection.nim
@@ -5,26 +5,27 @@ discard """
 import std/reflection
 from std/strutils import contains
 
-
 block: # getOwnerName, getBackendProcName
   proc fn1 =
-    doAssert getOwnerName() == "fn1"
-    static: doAssert getOwnerName() == "fn1"
-    doAssert "fn1" in getBackendProcName()
+    doAssert getOwnerName == "fn1"
+    static: doAssert getOwnerName == "fn1"
+    doAssert "fn1" in getBackendProcName
   fn1()
 
   proc fn3(a: int, b = 3.4, c: static string = "def") =
-    let s1 = getOwnerName()
-    let s2 = getOwnerName() # check we can call this twice
+    let s1 = getOwnerName
+    let s2 = getOwnerName # check we can call this twice
+    let s2b = getOwnerName() # check we can call this with `()`
     doAssert s1 == s2
+    doAssert s1 == s2b
     doAssert s1 == "fn3"
     doAssert getOwnerTypeString == """proc (a: int; b: float64; c: "def")"""
-    const x = getOwnerName()
+    const x = getOwnerName
     doAssert x == s1
-    let s3 = getBackendProcName()
+    let s3 = getBackendProcName
     doAssert "fn3" in s3
     doAssert s3 != "fn3"
-    let s4 = getBackendProcName() # check we can call this twice
+    let s4 = getBackendProcName # check we can call this twice
     doAssert s4 == s3
   fn3(3)
 
@@ -37,21 +38,21 @@ block: # getOwnerName, getBackendProcName
   doAssert fn3b() == 3
 
   proc `fn1b aux`(): (string, string) =
-    doAssert "fn1b" in getBackendProcName()
-    doAssert "aux" in getBackendProcName()
-    (getOwnerName(), getOwnerTypeString)
+    doAssert "fn1b" in getBackendProcName
+    doAssert "aux" in getBackendProcName
+    (getOwnerName, getOwnerTypeString)
 
   doAssert `fn1b aux`() == ("fn1baux", "proc (): (string, string)")
 
   proc `fn1c+aux`(): (string, string) =
-    doAssert "fn1c" in getBackendProcName()
-    doAssert "aux" in getBackendProcName()
-    (getOwnerName(), getOwnerTypeString)
+    doAssert "fn1c" in getBackendProcName
+    doAssert "aux" in getBackendProcName
+    (getOwnerName, getOwnerTypeString)
   doAssert `fn1c+aux`() == ("fn1c+aux", "proc (): (string, string)")
 
   var witness = 0
   iterator fn4(): int =
-    doAssert getOwnerName() == "fn4"
+    doAssert getOwnerName == "fn4"
     doAssert getOwnerTypeString == "proc (): int"
     witness.inc
   for a in fn4(): discard
@@ -60,26 +61,26 @@ block: # getOwnerName, getBackendProcName
   when not defined(js):
     witness = 0
     iterator fn5(): int {.closure.} =
-      doAssert getOwnerName() == "fn5"
+      doAssert getOwnerName == "fn5"
       doAssert getOwnerTypeString == "proc (): int"
-      doAssert "fn5" in getBackendProcName()
+      doAssert "fn5" in getBackendProcName
       witness.inc
     for a in fn5(): discard
     doAssert witness == 1
 
   func fn6 =
-    doAssert getOwnerName() == "fn6"
+    doAssert getOwnerName == "fn6"
     doAssert getOwnerTypeString == "proc ()"
-    doAssert "fn6" in getBackendProcName()
+    doAssert "fn6" in getBackendProcName
   fn6()
 
 
 type Foo = ref object of RootObj
 
 method fn7(self: Foo): int {.base.} =
-  doAssert getOwnerName() == "fn7"
+  doAssert getOwnerName == "fn7"
   doAssert getOwnerTypeString == "proc (self: Foo): int"
-  doAssert "fn7" in getBackendProcName()
+  doAssert "fn7" in getBackendProcName
   3
 
 var a = Foo()
@@ -87,10 +88,13 @@ doAssert fn7(a) == 3
 
 const moduleName = "treflection"
 
-doAssert getOwnerName() == moduleName
+doAssert getOwnerName == moduleName
+doAssert getOwnerTypeString == ""
 block:
-  doAssert getOwnerName() == moduleName
+  doAssert getOwnerName == moduleName
+  doAssert getOwnerTypeString == ""
   template fn8(): string =
+    doAssert getOwnerTypeString() == "" # must be called with `()` inside a template
     let x = getOwnerName()
     x
   doAssert fn8() == moduleName
@@ -98,7 +102,7 @@ block:
 import std/macros
 
 macro fn8(a: int, b: static int = 0): (string, string) =
-  let a1 = getOwnerName()
+  let a1 = getOwnerName
   let a2 = getOwnerTypeString
   result = quote: (`a1`, `a2`)
 
