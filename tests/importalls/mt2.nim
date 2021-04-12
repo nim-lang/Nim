13b3f0bbf0534f6d6a886fe59b68c6564dc83c9d
Timothee Cour <timothee.cour2@gmail.com>
2021-04-12 14:03:58 -0700

    _

--- tests/importalls/mt2.nim
+++ tests/importalls/mt2.nim
@@ -1,39 +1,43 @@
-from ./m1 {.all.} as m import foo1, Foo5 {.fields.}
-from ./m1 {.all.} as m2 import foo7
+from ./m1 {.all.} as r1 import foo1, Foo5 {.fields.}
+from ./m1 {.all.} as r2 import foo7
 
 doAssert foo1 == 2
-doAssert m.foo1 == 2
-doAssert m.foo2 == 2
+doAssert r1.foo1 == 2
+doAssert r1.foo2 == 2
 doAssert compiles(foo1)
-doAssert compiles(m.foo2)
+doAssert compiles(r1.foo2)
 doAssert not compiles(foo2)
 doAssert not compiles(m3h2)
-doAssert m.foo3 == 2
-doAssert m.foo6() == 2
-doAssert m.foo6b() == 2
+doAssert r1.foo3 == 2
+doAssert r1.foo6() == 2
+doAssert r1.foo6b() == 2
 doAssert foo7() == 2
-doAssert m2.foo6b() == 2
+doAssert r2.foo6b() == 2
 doAssert not compiles(foo10())
-doAssert compiles(m.Foo5)
-doAssert not compiles(m.Foo11)
-doAssert not compiles(m.kg1b)
-doAssert not compiles(m.foo12())
+doAssert compiles(r1.Foo5)
+doAssert not compiles(r1.Foo11)
+doAssert not compiles(r1.kg1b)
+doAssert not compiles(r1.foo12())
 
 ## field access
-var x = m.Foo5(z1: "foo", z2: m.kg1)
+var x = r1.Foo5(z1: "foo", z2: r1.kg1)
 doAssert x.z1 == "foo"
 
-var f0: m.Foo5
+var f0: r1.Foo5
 f0.z3 = 3
 doAssert f0.z3 == 3
-var f = m.initFoo5(z3=3)
+var f = r1.initFoo5(z3=3)
 doAssert f.z3 == 3
-doAssert m.z3(f) == 30
+doAssert r1.z3(f) == 30
 
-import ./m1
+import ./m1 as r3
 doAssert not declared(foo2)
-doAssert not declared(m1.foo2)
+doAssert not declared(r3.foo2)
+
+from ./m1 {.all.} as r4 import nil
+doAssert not declared(foo2)
+doAssert declared(r4.foo2)
+
 from ./m1 {.all.} import nil
 doAssert not declared(foo2)
 doAssert declared(m1.foo2)
-import ./m1 {.all.}
