9b3fd345983edab93c9f28d213842c3e0ed19241
Timothee Cour <timothee.cour2@gmail.com>
2021-04-12 19:37:56 -0700

    fix tests

--- tests/importalls/m4.nim
+++ tests/importalls/m4.nim
@@ -1,6 +1,10 @@
 {.warning[UnusedImport]: off.} # xxx bug: this shouldn't be needed since we have `export m3`
 import ./m3 {.all.}
-export m3
-# export m3 {.all.} # xxx this could be supported in future
+import ./m3 as m3b
+export m3b
 export m3h3
 export m3.m3h4
+
+import ./m2 {.all.} as m2b
+export m2b except bar3
+
--- tests/importalls/mt9.nim
+++ tests/importalls/mt9.nim
@@ -6,3 +6,7 @@ doAssert m3p1 == 2
 doAssert not declared(m3h2)
 doAssert m3h3 == 3
 doAssert m3h4 == 4
+
+doAssert bar1 == 2
+doAssert bar2 == 2
+doAssert not declared(bar3)
