e604f410ee665f00fe5ea6b696053e179a4773ef
Timothee Cour <timothee.cour2@gmail.com>
2021-04-16 00:15:45 -0700

    cleanup

--- lib/stdx/lang_prefixes.nim
+++ lib/stdx/lang_prefixes.nim
@@ -1,102 +1,70 @@
 ##[
-This module defines templates that can help tooling (e.g. syntax highlighters or linters)
-for interpreting string literals as being in a specified language.
+This module allows annotating string literals with a language prefix which can be
+exploited by tooling (e.g. for language specific syntax highlighting or linting).
 ]##
 
-when false:
- runnableExamples:
-  when defined(c):
-    {.emit: cLang"""
-    #include <stdio.h>
-    void fn(){} /* comment */
-    """.}
+runnableExamples:
+  # syntax highlighters can recognize `highlight"""js` and highlight the string as js.
+  let a = highlight"""js
+var a = 3n;
+var b = 4n;"""
+  doAssert a == "var a = 3n;\nvar b = 4n;" # js prefix is removed.
 
-  when defined(cpp):
-    {.emit: cppLang"""
+  # works with emit sections
+  {.emit: highlight"""c
+/*INCLUDESECTION*/
+#include <stdio.h>""".}
+
+  # works with emit strings containing backticks.
+  proc fn(n: int): int =
+    {.emit: highlight"""c
+`result` = 3 * `n`; /* comment */
+""".}
+  doAssert fn(2) == 3 * 2
+
+  # works with strutils.dedent
+  from std/strutils import dedent
+  let a2 = highlight"""cpp
     #include <vector>
     typedef std::vector<int> VInt; // some comment
-    """.}
+    """.dedent
+  assert a2 == "#include <vector>\ntypedef std::vector<int> VInt; // some comment\n"
 
-  when false:
-    # `asm """ ... """` would work too but the syntax highlighter can't
-    # tell whether `asm` should highlight as assembly or js code, which can
-    # cause catastrophic results, e.g. with """ var a = 1; """, which
-    # messes up syntax highlight for the remainder of the file due to `;`
-    # being a comment in asm.
-    {.emit: asmLang"""
+  # one liner example
+  let a3 = highlight"""js console.log(typeof(12n) == "bigint");"""
+  assert a3 == """console.log(typeof(12n) == "bigint");"""
+
+  # `highlight` avoids inherient ambiguities for syntax highlighters;
+  # `asm """ ... """` could mean either assembly or js depending on backend so
+  # cannot be highlighted correctly by itself.
+  const a4 = highlight"""asm
     mov eax, ecx ; comment
     mov ecx, edx
     xor edx, edx
     call `raiseOverflow` ; backtick syntax
-    ret """.}
+    ret """
+  when false:
+    {.emit: a4.} # example showing defered use of a literal in an emit statement.
 
-  when defined(js):
-    {.emit: jsLang"""console.log(typeof(12n) == "bigint");""".}
-  
-  # this can be used outside of `emit`, e.g.:
-  discard cLang"""
-    #include <stdio.h>
-    void fn(){}
-    """
-
-  # another example using defered emit:
-  const jsMathTrunc = jsLang"""
-if (!Math.trunc) {
-  Math.trunc = function(v) {
-    v = +v;
-    if (!isFinite(v)) return v;
-    return (v - v % 1) || (v < 0 ? -0 : v === 0 ? v : 0);
-  };
-}
-"""
-  when defined(js): {.emit: jsMathTrunc.}
-
-  const nimModule = nimLang"""
+  const nimModule = highlight"""nim
 import std/os
 echo @[1, 2]
 """
   # use `nimModule`, e.g. with `macros.parseStmt` or `writeFile`.
 
-
 #[
 xxx support in compiler asm with a non-string-literal, e.g.:
-  asm sLang"""console.log(typeof(12n) == "bigint");"""
-  asm asmLang"""mov eax, ecx"""
-  asm cppLang"""#include <stdio.h>"""
-]#
-
-#[
-# template nimLang*(a: string{lit}): string = a
-# template cLang*(a: string{lit}): string = a
-# template cppLang*(a: string{lit}): string = a
-# template jsLang*(a: string{lit}): string = a
-# template asmLang*(a: string{lit}): string = a
-# template pyLang*(a: string{lit}): string = a
-# # add more as needed
-
+  asm highlight"""js console.log(typeof(12n) == "bigint");"""
+  asm highlight"""asm mov eax, ecx"""
+  asm highlight"""cpp #include <stdio.h>"""
 ]#
 
 proc highlightImpl(a: string): string =
   for i in 0..<a.len:
-    if a[i] == '\n':
+    if a[i] == '\n' or a[i] == ' ':
       return a[i+1..a.len-1]
 
 template highlight*(a: string{lit}): string =
+  ## returns `a` stripped from its language prefix, see examples.
   const b = highlightImpl(a)
   b
-
-runnableExamples:
-  let a = highlight"""js
-var a = 3n;
-var b = 4n;"""
-  doAssert a == "var a = 3n;\nvar b = 4n;"
-
-  {.emit: highlight"""c
-/*INCLUDESECTION*/
-#include <stdio.h>""".}
-
-  proc fn(n: int): int =
-    {.emit: highlight"""c
-`result` = 3 * `n`; // some comment
-""".}
-  doAssert fn(2) == 3 * 2
