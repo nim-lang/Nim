0fc815ee8fe3785bd2b67638cd7d049119f7d41c
Benjamin Shirley-Quirk <shirleyquirk@gmail.com>
2021-04-18 01:34:33 +0200

    cheers timothee

--- tests/stdlib/tnetpsk.nim
+++ tests/stdlib/tnetpsk.nim
@@ -1,45 +1,35 @@
 discard """
   joinable:false
   batchable:false
-  action: "run"
   matrix: "--threads:on -d:ssl"
   targets: "c cpp"
   timeout:5
-  output:'''
-accepted connection
-connected with aethelfridda
-goodbye from bartholomew
-hello from aethelfridda
-identity hint "bartholomew"
-'''
-  sortoutput:true
 """
 import net
 from openssl import SSL_CTX_ctrl
 #using channels_builtin
-var serverChannel:Channel[bool]
+var serverChannel:Channel[Port]
 
 proc clientFunc(identityHint: string): tuple[identity: string, psk: string] =
-  echo "identity hint \"", identityHint,"\""
+  doAssert identityHint == "bartholomew"
   return ("aethelfridda","aethelfridda-loves-"&identityHint)
 
-proc client(){.thread.}=
+proc client(p:Port){.thread.}=
   let context = newContext(cipherList="PSK-AES256-CBC-SHA")
   defer: context.destroyContext()
 
   #turn off tls1_3 to force connection over psk
-  assert context.context.SSL_CTX_ctrl(124,0x0303,nil) > 0#SSL_CTX_set_max_proto_version(TLS1_2)
-
+  doAssert context.context.SSL_CTX_ctrl(124,0x0303,nil) > 0#SSL_CTX_set_max_proto_version(TLS1_2)
   context.clientGetPskFunc = clientFunc
 
   let sock = newSocket()
   defer: sock.close()
 
-  sock.connect("localhost", Port(8800))
+  sock.connect("localhost", p)
   context.wrapConnectedSocket(sock, handshakeAsClient)
-  #send some data
+
   sock.send("hello from aethelfridda\r\l")
-  echo sock.recvLine()
+  doAssert sock.recvLine() == "goodbye from bartholomew"
 
 proc server(){.thread.}=
   let context = newContext(cipherList="PSK-AES256-CBC-SHA")
@@ -51,37 +41,33 @@ proc server(){.thread.}=
   defer:
     sock.close()
     context.destroyContext()
-  var boundAddr = true
-  try:
-    sock.bindAddr(Port(8800))
-  except ValueError,OsError:
-    boundAddr = false
-
-  serverChannel.send(boundAddr)
-  if not boundAddr:
-    return
-
+  sock.bindAddr(Port(0))
+  let (_,port) = sock.getLocalAddr()
+  serverChannel.send(port)
   sock.listen()
   var client = new(Socket)
   sock.accept(client)
   sock.setSockOpt(OptReuseAddr, true)
-  echo "accepted connection"
   context.wrapConnectedSocket(client, handshakeAsServer)
-  echo "connected with ", client.getPskIdentity()
-  echo recvLine(client)
+  doAssert client.getPskIdentity() == "aethelfridda"
+  doAssert recvLine(client) == "hello from aethelfridda"
   client.send("goodbye from bartholomew\r\l")
-proc main()=
 
+proc main()=
   var
-    srv,cli:Thread[void]
+    srv:Thread[void]
+    cli:Thread[Port]
   serverChannel.open()
   defer: serverChannel.close()
 
   createThread(srv,server)
-  #wait till we are signalled that server has bound the port
-  if not serverChannel.recv():
-    quit "failed to bind port"
-  createThread(cli,client)
-  joinThreads(srv,cli)
+
+  #wait for server to bind a port
+  let port = serverChannel.recv()
+
+  createThread(cli,client,port)
+
+  joinThread(srv)
+  joinThread(cli)
 
 main()
