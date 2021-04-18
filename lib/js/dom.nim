783e619ef35c5109a7ddb6172e85f6e2aad9e9f7
Juan Carlos <juancarlospaco@gmail.com>
2021-04-18 20:25:18 -0300

    https://github.com/timotheecour/Nim/issues/674

--- lib/js/dom.nim
+++ lib/js/dom.nim
@@ -1497,6 +1497,8 @@ proc find*(w: Window, text: cstring, caseSensitive = false,
 proc focus*(w: Window)
 proc forward*(w: Window)
 proc getComputedStyle*(w: Window, e: Node, pe:Node = nil): Style
+  ## .. warning:: The returned Style may or may not be read-only at run-time in the browser. getComputedStyle is performance costly.
+
 proc handleEvent*(w: Window, e: Event)
 proc home*(w: Window)
 proc moveBy*(w: Window, x, y: int)
