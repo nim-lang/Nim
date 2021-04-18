df0afab172d0d026ef75e72064e6fdddce579c20
flywind <xzsflywind@gmail.com>
2021-04-18 19:32:37 +0800

    close

--- tests/stdlib/ttempfiles.nim
+++ tests/stdlib/ttempfiles.nim
@@ -7,5 +7,7 @@ block:
   let t1 = createTempFile("nim", ".tmp")
   let t2 = createTempFile("nim", ".tmp")
   doAssert t1.path != t2.path
+  close(t1.fd)
+  close(t2.fd)
   removeFile(t1.path)
   removeFile(t2.path)
