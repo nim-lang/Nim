e3019dc76e8086589681922aef7b13e89913b211
flywind <xzsflywind@gmail.com>
2021-04-18 21:08:13 +0800

    Update once.nim

--- lib/std/once.nim
+++ lib/std/once.nim
@@ -1,23 +1,32 @@
-import std/[atomics, locks]
+when compileOption("threads"):
+  import std/[atomics, locks]
 
 
 type
   Once* = object    ## A object that ensures a block of code is executed once.
-    finished: Atomic[bool]
-    lock: Lock
+    when compileOption("threads"):
+      finished: Atomic[bool]
+      lock: Lock
+    else:
+      finished: bool
 
-proc initOnce*(once: var Once) =
+proc `=destroy`*(once: var Once) {.inline.} =
+  when compileOption("threads"):
+    deinitLock(once.lock)
+
+proc init*(once: var Once) =
   ## Initializes a `Once` object.
-  once.finished.store(false)
-  initLock(once.lock)
+  when compileOption("threads"):
+    once.finished.store(false, moRelaxed)
+    initLock(once.lock)
 
-template once*(cond: Once, body: untyped) =
+template once*(cond: var Once, body: untyped) =
   ## Executes a block of code only once (the first time the block is reached).
   ## It is thread-safe.
-  runnableExamples:
+  runnableExamples("--gc:orc --threads:on"):
     var block1: Once
     var count = 0
-    initOnce(block1)
+    init(block1)
 
     for i in 1 .. 10:
       once(block1):
@@ -27,13 +36,52 @@ template once*(cond: Once, body: untyped) =
       once(block1):
         count = 888
 
-    doAssert count == 1
+    assert count == 1
 
-  if not cond.finished.load(moAcquire):
-    withLock cond.lock:
-      # TODO load a value without atomic
-      if not cond.finished.load(moRelaxed):
-        try:
-          body
-        finally:
-          cond.finished.store(true, moRelease)
+  when compileOption("threads"):
+    if not cond.finished.load(moAcquire):
+      withLock cond.lock:
+        if not cond.finished.load(moRelaxed):
+          try:
+            body
+          finally:
+            cond.finished.store(true, moRelease)
+  else:
+    if not cond.finished:
+      try:
+        body
+      finally:
+        cond.finished = true
+
+
+## The code block is executed only once among threads.
+runnableExamples("--gc:orc --threads:on"):
+  block:
+    var thr: array[0..4, Thread[void]]
+    var block1: Once
+    var count = 0
+    init(block1)
+    proc threadFunc() {.thread.} =
+      for i in 1 .. 10:
+        once(block1):
+          inc count
+    for i in 0..high(thr):
+      createThread(thr[i], threadFunc)
+    joinThreads(thr)
+    assert count == 1
+
+## The code blocks is executed per thread.
+runnableExamples("--gc:orc --threads:on"):
+  block:
+    var thr: array[0..4, Thread[void]]
+    var count = 0
+    proc threadFunc() {.thread.} =
+      var block1: Once
+      init(block1)
+      for i in 1 .. 10:
+        once(block1):
+          inc count
+    for i in 0..high(thr):
+      createThread(thr[i], threadFunc)
+    joinThreads(thr)
+    assert count == thr.len
