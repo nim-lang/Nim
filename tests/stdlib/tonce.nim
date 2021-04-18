b648d031d3e0d801ac74c3c63485d598e5872e2b
flywind <xzsflywind@gmail.com>
2021-04-18 21:09:08 +0800

    Update tonce.nim

--- tests/stdlib/tonce.nim
+++ tests/stdlib/tonce.nim
@@ -1,34 +1,34 @@
 discard """
-  cmd: "nim c -r --threads:on $options $file"
-  matrix: "-d:caseA; -d:caseB"
+  matrix: "--gc:orc --threads:on"
 """
 import std/once
 
-when defined(caseA):
-  block:
-    var thr: array[0..4, Thread[void]]
+
+block:
+  var thr: array[0..4, Thread[void]]
+  var block1: Once
+  var count = 0
+  initOnce(block1)
+  proc threadFunc() {.thread.} =
+    for i in 1 .. 10:
+      once(block1):
+        inc count
+  for i in 0..high(thr):
+    createThread(thr[i], threadFunc)
+  joinThreads(thr)
+  doAssert count == 1
+
+
+block:
+  var thr: array[0..4, Thread[void]]
+  var count = 0
+  proc threadFunc() {.thread.} =
     var block1: Once
-    var count = 0
     initOnce(block1)
-    proc threadFunc() {.thread.} =
-      for i in 1 .. 10:
-        once(block1):
-          inc count
-    for i in 0..high(thr):
-      createThread(thr[i], threadFunc)
-    joinThreads(thr)
-    doAssert count == 1
-elif defined(caseB):
-  block:
-    var thr: array[0..4, Thread[void]]
-    var count = 0
-    proc threadFunc() {.thread.} =
-      var block1: Once
-      initOnce(block1)
-      for i in 1 .. 10:
-        once(block1):
-          inc count
-    for i in 0..high(thr):
-      createThread(thr[i], threadFunc)
-    joinThreads(thr)
-    doAssert count == 5
+    for i in 1 .. 10:
+      once(block1):
+        inc count
+  for i in 0..high(thr):
+    createThread(thr[i], threadFunc)
+  joinThreads(thr)
+  doAssert count == 5
