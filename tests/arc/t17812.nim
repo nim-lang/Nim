4f1aaab7cf694177bb74bfb5bcabfb328b139581
flywind <xzsflywind@gmail.com>
2021-04-22 11:11:31 +0800

    fix #17812

---
+++ tests/arc/t17812.nim
@@ -0,0 +1,28 @@
+discard """
+  matrix: "--gc:refc; --gc:arc"
+"""
+
+import std/times
+
+block: # bug #17812
+  block:
+    type
+      Task = object
+        cb: proc ()
+
+    proc hello() = discard
+
+
+    let t = Task(cb: hello)
+
+    doAssert t.repr.len > 0
+
+
+  block:
+    type MyObj = object
+      field: DateTime
+
+
+    proc `$`(o: MyObj): string = o.repr
+
+    doAssert ($MyObj()).len > 0
