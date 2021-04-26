ad3191fdb76b8389d3aec74e0044d8a7bc8875c8
Timothee Cour <timothee.cour2@gmail.com>
2021-04-26 13:13:55 -0700

    continue

--- tests/errmsgs/tundeclared_routine.nim
+++ tests/errmsgs/tundeclared_routine.nim
@@ -6,10 +6,10 @@ tundeclared_routine.nim(24, 17) Error: attempting to call routine: 'myiter'
   found tundeclared_routine.myiter(a: string) [iterator declared in tundeclared_routine.nim(22, 12)]
   found tundeclared_routine.myiter() [iterator declared in tundeclared_routine.nim(23, 12)]
 tundeclared_routine.nim(29, 28) Error: invalid pragma: myPragma
+tundeclared_routine.nim(36, 13) Error: undeclared field: 'bar' for type tundeclared_routine.Foo [type declared in tundeclared_routine.nim(33, 8)]
+  found tundeclared_routine.bar() [iterator declared in tundeclared_routine.nim(35, 12)]
 '''
 """
-
-
 
 
 
@@ -28,3 +28,9 @@ block:
   iterator myPragma():int=discard
   proc myfun(a:int): int {.myPragma.} = 1
   let a = myfun(1)
+
+block:
+  type Foo = object
+  var a = Foo()
+  iterator bar():int=discard
+  let a2 = a.bar
