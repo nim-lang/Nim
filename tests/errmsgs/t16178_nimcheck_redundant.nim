52228e7f03e59f9b6f2ee60319289d01a5751448
Timothee Cour <timothee.cour2@gmail.com>
2021-04-27 15:46:19 -0700

    fix 1 test

--- tests/errmsgs/t16178_nimcheck_redundant.nim
+++ tests/errmsgs/t16178_nimcheck_redundant.nim
@@ -1,7 +1,7 @@
 discard """
 cmd: '''nim check --hints:off $file'''
 action: reject
-# nimoutFull: true # pending https://github.com/nim-lang/Nim/pull/17865
+nimoutFull: true
 nimout: '''
 t16178_nimcheck_redundant.nim(22, 11) Error: undeclared identifier: 'bad5'
 t16178_nimcheck_redundant.nim(22, 15) Error: expression '' has no type (or is ambiguous)
@@ -10,13 +10,13 @@ t16178_nimcheck_redundant.nim(26, 11) Error: expression '' has no type (or is am
 t16178_nimcheck_redundant.nim(26, 7) Error: 'let' symbol requires an initialization
 t16178_nimcheck_redundant.nim(30, 11) Error: expression '' has no type (or is ambiguous)
 t16178_nimcheck_redundant.nim(34, 15) Error: expression '' has no type (or is ambiguous)
-
 '''
 """
 #[
 xxx the line `Error: 'let' symbol requires an initialization` is redundant and should not
 be reported; likewise with `t16178_nimcheck_redundant.nim(22, 15) Error: expression '' has no type (or is ambiguous)`
 ]#
+
 # line 20
 block:
   let a = bad5(1)
