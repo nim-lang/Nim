f5b9b3ac55057c6226b1c58b75cc1a9450939263
flywind <xzsflywind@gmail.com>
2021-04-30 15:38:06 +0800

    Update lib/std/winapis.nim

--- lib/std/winapis.nim
+++ lib/std/winapis.nim
@@ -1,4 +1,4 @@
-## An inplacement for `std/winlean` with presice and stable types definitions.
+## An replacement for `std/winlean` with precise and stable types definitions.
 
 type
   DWORD* = culong  ## https://docs.microsoft.com/en-us/windows/win32/winprog/windows-data-types#ulong
@@ -11,4 +11,3 @@ proc getTempPath*(
   ): DWORD {.stdcall, dynlib: "kernel32.dll", importc: "GetTempPathW".} =
   ## Retrieves the path of the directory designated for temporary files.
   ## See https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-gettemppathw
-
