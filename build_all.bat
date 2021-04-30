72ed2ad0a82422b389432f9c0d385de5feaef6e1
Timothee Cour <timothee.cour2@gmail.com>
2021-04-29 20:09:26 -0700

    reuse config/build_config.txt for all bootstrap scripts (posix + windows + ci)

--- build_all.bat
+++ build_all.bat
@@ -1,20 +1,29 @@
 @echo off
-rem build development version of the compiler; can be rerun safely
-rem TODO: call nimBuildCsourcesIfNeeded or auto-generate this file (from a nim script)
-rem to avoid duplication.
-if not exist csources_v1 (
-  git clone --depth 1 https://github.com/nim-lang/csources_v1.git
+rem Build development version of the compiler; can be rerun safely
+rem bare bones version of ci/funs.sh adapted for windows.
+
+rem Read in some common shared variables (shared with other tools),
+rem see https://stackoverflow.com/questions/3068929/how-to-read-file-contents-into-a-variable-in-a-batch-file
+for /f "delims== tokens=1,2" %%G in (config/build_config.txt) do set %%G=%%H
+SET nim_csources=bin\nim_csources_%nim_csourcesHash%.exe
+echo "building from csources" 
+echo %nim_csources%
+
+if not exist %nim_csourcesDir% (
+  git clone -q --depth 1 %nim_csourcesUrl% %nim_csourcesDir%
 )
-if not exist bin\nim.exe (
-  cd csources_v1
-  git checkout a8a5241f9475099c823cfe1a5e0ca4022ac201ff
+
+if not exist %nim_csources% (
+  cd %nim_csourcesDir%
+  git checkout %nim_csourcesHash%
+  git heckout $nim_csourcesHash
   if PROCESSOR_ARCHITECTURE == AMD64 (
     SET ARCH=64
   )
   CALL build.bat
   cd ..
 )
-bin\nim.exe c --skipUserCfg --skipParentCfg koch
-koch.exe boot -d:release --skipUserCfg --skipParentCfg
-koch.exe tools --skipUserCfg --skipParentCfg
 
+bin\nim.exe c --skipUserCfg --skipParentCfg --hints:off koch
+koch.exe boot -d:release --skipUserCfg --skipParentCfg --hints:off
+koch.exe tools --skipUserCfg --skipParentCfg --hints:off
