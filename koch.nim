d0485e326ab56af367246227dedc088839678b6b
Timothee Cour <timothee.cour2@gmail.com>
2021-04-30 13:27:53 -0700

    bring back std/ prefix within compiler and ensure it works in bootstrap + bsd (#17902)
    * [WIP] bring back std/ prefix within compiler and ensure it works in bootstrap + bsd
    
    * refs https://github.com/nim-lang/Nim/pull/16282#discussion_r616846863
    * sounds very similar to https://github.com/nim-lang/Nim/pull/14291
    * more: vmops
    * update tools/ci_generate.nim
    * auto-generate freebsd.yml as well, to avoid duplication with openbsd.yml
    * cleanup
    * undo temporary CI removal

--- koch.nim
+++ koch.nim
@@ -534,7 +534,9 @@ proc runCI(cmd: string) =
   echo "runCI: ", cmd
   echo hostInfo()
   # boot without -d:nimHasLibFFI to make sure this still works
-  kochExecFold("Boot in release mode", "boot -d:release -d:nimStrictMode")
+  # `--lib:lib` is needed for bootstrap on openbsd, for reasons described in
+  # https://github.com/nim-lang/Nim/pull/14291 (`getAppFilename` bugsfor older nim on openbsd).
+  kochExecFold("Boot in release mode", "boot -d:release -d:nimStrictMode --lib:lib")
 
   when false: # debugging: when you need to run only 1 test in CI, use something like this:
     execFold("debugging test", "nim r tests/stdlib/tosproc.nim")
