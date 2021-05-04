79dc691719693e47477be0d2a75d26887d82dd7f
Timothee Cour <timothee.cour2@gmail.com>
2021-05-04 11:41:09 -0700

    --import works without giving spurious unused warnings

--- tests/pragmas/tused2.nim
+++ tests/pragmas/tused2.nim
@@ -7,15 +7,10 @@ mused2a.nim(20, 7) Hint: 'fn7' is declared but not used [XDeclaredButNotUsed]
 mused2a.nim(23, 6) Hint: 'T1' is declared but not used [XDeclaredButNotUsed]
 mused2a.nim(12, 6) Hint: 'fn1' is declared but not used [XDeclaredButNotUsed]
 mused2a.nim(16, 5) Hint: 'fn4' is declared but not used [XDeclaredButNotUsed]
-mused2a.nim(1, 2) Warning: imported and not used: 'mused2e' [UnusedImport]
 mused2a.nim(2, 11) Warning: imported and not used: 'sugar' [UnusedImport]
 mused2a.nim(3, 9) Warning: imported and not used: 'os' [UnusedImport]
 mused2a.nim(5, 23) Warning: imported and not used: 'typetraits2' [UnusedImport]
 mused2a.nim(6, 9) Warning: imported and not used: 'setutils' [UnusedImport]
-mused2c.nim(1, 2) Warning: imported and not used: 'mused2e' [UnusedImport]
-mused2b.nim(1, 2) Warning: imported and not used: 'mused2e' [UnusedImport]
-mused2d.nim(1, 2) Warning: imported and not used: 'mused2e' [UnusedImport]
-tused2.nim(1, 2) Warning: imported and not used: 'mused2e' [UnusedImport]
 tused2.nim(42, 8) Warning: imported and not used: 'mused2a' [UnusedImport]
 tused2.nim(43, 8) Warning: imported and not used: 'mused2b' [UnusedImport]
 tused2.nim(45, 11) Warning: imported and not used: 'strutils' [UnusedImport]
@@ -31,6 +26,11 @@ we should improve this.
 xxx tused2.nim(32, 8) Warning: imported and not used: 'mused2b' [UnusedImport]
 should not be generated (refs bug #17510)
 ]#
+
+
+
+
+
 
 
 
