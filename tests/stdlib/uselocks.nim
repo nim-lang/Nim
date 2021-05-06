dfc0fad3fdda4362a79a80e81aea7afe34cc9b19
Timothee Cour <timothee.cour2@gmail.com>
2021-05-05 20:05:14 -0700

    add test

--- tests/stdlib/uselocks.nim
+++ tests/stdlib/uselocks.nim
@@ -14,3 +14,16 @@ block:
   # bug #14873
   var l: Lock
   doAssert $l == "()"
+
+when true: # intentional
+  # bug https://github.com/nim-lang/Nim/issues/14873#issuecomment-784241605
+  type
+    Test = object
+      path: string # Removing this makes both cases work.
+      lock: Lock
+  # A: This is not fine.
+  var a = Test()
+  proc main(): void =
+    # B: This is fine.
+    var b = Test()
+  main()
