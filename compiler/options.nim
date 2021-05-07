51f3ef6cb8ac9fbbc9533c1de2af219dc4ecabe7
Andreas Rumpf <rumpf_a@web.de>
2021-05-07 16:50:11 +0200

    fixes #15848 [backport:1.2] (#17959)

--- compiler/options.nim
+++ compiler/options.nim
@@ -60,6 +60,7 @@ type                          # please make sure we have under 32 options
     optUseNimcache,           # save artifacts (including binary) in $nimcache
     optStyleHint,             # check that the names adhere to NEP-1
     optStyleError,            # enforce that the names adhere to NEP-1
+    optStyleUsages,           # only enforce consistent **usages** of the symbol
     optSkipSystemConfigFile,  # skip the system's cfg/nims config file
     optSkipProjConfigFile,    # skip the project's cfg/nims config file
     optSkipUserConfigFile,    # skip the users's cfg/nims config file
