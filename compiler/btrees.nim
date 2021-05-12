22623607784f7a86de3c62cb6bf3a8affc441c58
Araq <rumpf_a@web.de>
2021-05-12 20:58:17 +0200

    minor style changes

--- compiler/btrees.nim
+++ compiler/btrees.nim
@@ -16,7 +16,7 @@ const
   Mhalf = M div 2
 
 type
-  Node {.acyclic.} [Key, Val] = ref object
+  Node[Key, Val] {.acyclic.} = ref object
     entries: int
     keys: array[M, Key]
     case isInternal: bool
