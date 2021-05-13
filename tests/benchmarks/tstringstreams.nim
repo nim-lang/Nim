4af9324c53e94f9cab05eb03bf2abcd69527ba54
Jason <beefers331@gmail.com>
2021-05-13 17:37:06 -0600

    Added more reallistic  benchmarks i believe [skip ci]

--- tests/benchmarks/tstringstreams.nim
+++ tests/benchmarks/tstringstreams.nim
@@ -9,70 +9,63 @@ discard """
 import pkg/benchy
 import std/streams
 
+type
+  PacketID = enum
+    jump, shoot, crouch, hit
+  Entity = object
+    id: int
 
+proc writePacket(str: StringStream, kind: PacketID, ent: Entity, target: Entity = Entity(id: -1)) =
+  str.write kind
+  str.write ent.id
+  case kind:
+  of shoot:
+    str.write 10
+    str.write target.id
+  of hit:
+    str.write 3
+  else: discard
 
-timeIt "Tightly Sized uint32", 1000:
-  var s = streams.newStringStream()
-  for i in 0 .. 100000:
-    s.write(i.uint32)
-  s.setPosition(0)
-  while not s.atEnd:
-    keep s.readUint32()
+# Benchmark using string streams as a network buffer shows ~`0.005ms` improvement in this small example
+timeIt "Normal growth rate Network", 1000:
+  var s = newStringStream()
+  for i in 0..1000:
+    let event = i.mod(PacketID.high.ord).PacketID
+    if event == shoot:
+      s.writePacket(event, Entity(id: i), Entity(id: i + 1))
+    else:
+      s.writePacket(event, Entity(id: i))
   keep s.data
 
-timeIt "Two Times Growth uint32", 1000:
-  var s = streams.newStringStream(growthRate = 2)
-  for i in 0 .. 100000:
-    s.write(i.uint32)
-  s.setPosition(0)
-  while not s.atEnd:
-    keep s.readUint32()
+timeIt "Twice growth rate Network", 1000:
+  var s = newStringStream(growthRate = 2)
+  for i in 0..1000:
+    let event = i.mod(PacketID.high.ord).PacketID
+    if event == shoot:
+      s.writePacket(event, Entity(id: i), Entity(id: i + 1))
+    else:
+      s.writePacket(event, Entity(id: i))
   keep s.data
 
-timeIt "Tightly Sized uint8", 1000:
-  var s = streams.newStringStream()
-  for i in 0 .. 100000:
-    s.write(i.uint8)
-  s.setPosition(0)
-  while not s.atEnd:
-    keep s.readUint8()
-  keep s.data
 
-timeIt "Two Times Growth uint8", 1000:
-  var s = streams.newStringStream(growthRate = 2)
-  for i in 0 .. 100000:
-    s.write(i.uint8)
-  s.setPosition(0)
-  while not s.atEnd:
-    keep s.readUint8()
-  keep s.data
+let data = newSeq[(byte, byte, byte)](300 * 500)
 
-timeIt "Tightly Sized uint16", 1000:
-  var s = streams.newStringStream()
-  for i in 0 .. 100000:
-    s.write(i.uint16)
-  s.setPosition(0)
-  while not s.atEnd:
-    keep s.readUint16()
-  keep s.data
+# Basic pseudo image example using a header and data
+# Results on my machine shows constant 2 times growth is roughly ~`.5ms` faster
+timeIt "Normal growth rate image saving", 1000:
+  var s = newStringStream()
+  s.write("Width:300, Height:500") # Simulated header format
+  for x in data:
+    s.write(x)
 
-timeIt "Two Times Growth uint16", 1000:
-  var s = streams.newStringStream(growthRate = 2)
-  for i in 0 .. 100000:
-    s.write(i.uint16)
-  s.setPosition(0)
-  while not s.atEnd:
-    keep s.readUint16()
-  keep s.data
+timeIt "Twice growth rate image saving", 1000:
+  var s = newStringStream(growthRate = 2)
+  s.write("Width:300, Height:500") # Simulated header format
+  for x in data:
+    s.write(x)
 
-timeIt "Tightly Sized String", 1000:
-  var s = streams.newStringStream()
-  for i in 0 .. 100000:
-    s.write("Hello World")
-  keep s.data
-
-timeIt "Two Times Growth String", 1000:
-  var s = streams.newStringStream(growthRate = 2)
-  for i in 0 .. 100000:
-    s.write("Hello World")
-  keep s.data
+timeIt "Four times growth rate image saving", 1000:
+  var s = newStringStream(growthRate = 3)
+  s.write("Width:300, Height:500") # Simulated header format
+  for x in data:
+    s.write(x)
