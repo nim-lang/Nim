4857c462d5a9dc68519ab3c6996523bdcccb883f
Andreas Rumpf <rumpf_a@web.de>
2021-05-15 06:50:39 +0200

    Revert "fix #14873 properly by skipping `abi` field in importc type (#17944)" (#17992)
    This reverts commit 98c29c01eb91a0c6ce7da09380a272eebe6bca6f.

--- tests/stdlib/uselocks.nim
+++ tests/stdlib/uselocks.nim
@@ -11,21 +11,5 @@ proc use* (m: var MyType): int =
     result = 3
 
 block:
-  # bug #14873
   var l: Lock
-  doAssert ($l).len > 0
-    # on posix, "()", on windows, something else, but that shouldn't be part of the spec
-    # what matters is that `$` doesn't cause the codegen bug mentioned
-
-when true: # intentional
-  # bug https://github.com/nim-lang/Nim/issues/14873#issuecomment-784241605
-  type
-    Test = object
-      path: string # Removing this makes both cases work.
-      lock: Lock
-  # A: This is not fine.
-  var a = Test()
-  proc main(): void =
-    # B: This is fine.
-    var b = Test()
-  main()
+  doAssert $l == "()"
