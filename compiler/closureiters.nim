0f1feb22c223c8713d1f48e19d791a8490f578bb
Yuriy Glukhov <yglukhov@users.noreply.github.com>
2021-05-25 11:16:13 +0300

    Update compiler/closureiters.nim
    Co-authored-by: Andreas Rumpf <rumpf_a@web.de>

--- compiler/closureiters.nim
+++ compiler/closureiters.nim
@@ -606,12 +606,11 @@ proc lowerStmtListExprs(ctx: var Ctx, n: PNode, needsSplit: var bool): PNode =
             internalError(ctx.g.config, "lowerStmtListExpr(nkCaseStmt): " & $branch.kind)
         result.add(n)
         result.add(ctx.newEnvVarAccess(tmp))
-      else:
+      elif n[0].kind == nkStmtListExpr:
         result = newNodeI(nkStmtList, n.info)
-        if n[0].kind == nkStmtListExpr:
-          let (st, ex) = exprToStmtList(n[0])
-          result.add(st)
-          n[0] = ex
+        let (st, ex) = exprToStmtList(n[0])
+        result.add(st)
+        n[0] = ex
         result.add(n)
 
   of nkCallKinds, nkChckRange, nkChckRangeF, nkChckRange64:
