b422cb938d8bb1dbd9b6ae3c5e7c95e2870d35d3
Timothee Cour <timothee.cour2@gmail.com>
2021-06-01 21:58:29 -0700

    refs #17898

--- tests/stdlib/tmonotimes.nim
+++ tests/stdlib/tmonotimes.nim
@@ -4,17 +4,29 @@ discard """
 
 import std/[monotimes, times]
 
-let d = initDuration(nanoseconds = 10)
-let t1 = getMonoTime()
-let t2 = t1 + d
+template main =
+  block:
+    let d = initDuration(nanoseconds = 10)
+    let t1 = getMonoTime()
+    let t2 = t1 + d
 
-doAssert t2 - t1 == d
-doAssert t1 == t1
-doAssert t1 != t2
-doAssert t2 - d == t1
-doAssert t1 < t2
-doAssert t1 <= t2
-doAssert t1 <= t1
-doAssert not(t2 < t1)
-doAssert t1 < high(MonoTime)
-doAssert low(MonoTime) < t1
+    doAssert t2 - t1 == d
+    doAssert t1 == t1
+    doAssert t1 != t2
+    doAssert t2 - d == t1
+    doAssert t1 < t2
+    doAssert t1 <= t2
+    doAssert t1 <= t1
+    doAssert not(t2 < t1)
+    doAssert t1 < high(MonoTime)
+    doAssert low(MonoTime) < t1
+
+  block:
+    for i in 0..<1000000: # test should take ~ 1sec
+      # this would fail with getTime instead of getMonoTime, as expected
+      let a = getMonoTime()
+      let b = getMonoTime()
+      doAssert b > a
+
+main()
+# static: main() # xxx support
