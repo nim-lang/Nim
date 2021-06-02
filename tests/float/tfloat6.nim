a19572e26524cda39909dcdfc3792de077f98c24
Araq <rumpf_a@web.de>
2021-06-02 20:37:01 +0200

    fixes #18418

--- tests/float/tfloat6.nim
+++ tests/float/tfloat6.nim
@@ -21,3 +21,8 @@ echo "0.00000_1".parseFloat(), " : ", 1E-6
 
 echo "1_0.00_0001".parseFloat(), " : ", 10.000001
 echo "1__00.00_0001".parseFloat(), " : ", 1_00.000001
+
+# bug #18148
+
+var a = 1.1'f32
+doAssert $a == "1.1", $a # fails
