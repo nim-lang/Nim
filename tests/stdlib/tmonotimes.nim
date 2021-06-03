ee236ed3203c18ab09ee3daa9ad50ef83589780d
Timothee Cour <timothee.cour2@gmail.com>
2021-06-02 18:43:02 -0700

    multi impl

--- tests/stdlib/tmonotimes.nim
+++ tests/stdlib/tmonotimes.nim
@@ -3,43 +3,85 @@ discard """
   matrix: "; -d:danger" # for stricter tests
 """
 
-import std/[monotimes, times]
+# import std/[monotimes, times]
 
-# template main =
+{.emit:"""
+#include <stdint.h>
+
+//  Windows
+#ifdef _WIN32
+
+#include <intrin.h>
+inline uint64_t rdtsc(){
+    return __rdtsc();
+}
+
+//  Linux/GCC
+#else
+
+inline uint64_t rdtsc(){
+    unsigned int lo,hi;
+    __asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
+    return ((uint64_t)hi << 32) | lo;
+}
+
+#endif
+""".}
+
+proc rdtsc(): uint64 {.importc.}
+
+import posix
+var CLOCK_MONOTONIC* {.importc, header: "<time.h>".}: cint
+var CLOCK_MONOTONIC_RAW* {.importc, header: "<time.h>".}: cint
+var CLOCK_REALTIME* {.importc, header: "<time.h>".}: cint
+var CLOCK_MONOTONIC_RAW_APPROX* {.importc, header: "<time.h>".}: cint
+var CLOCK_UPTIME_RAW* {.importc, header: "<time.h>".}: cint
+var CLOCK_UPTIME_RAW_APPROX* {.importc, header: "<time.h>".}: cint
+var CLOCK_PROCESS_CPUTIME_ID* {.importc, header: "<time.h>".}: cint
+
+template algoClock(clock): untyped = 
+  var ts: Timespec
+  discard clock_gettime(clock, ts)
+  ts.tv_nsec.int64
+
+template algo1(dummy: int): untyped = algoClock(CLOCK_MONOTONIC_RAW) # works?
+template algo2(dummy: int): untyped =  algoClock(CLOCK_MONOTONIC)
+template algo3(dummy: int): untyped =  algoClock(CLOCK_REALTIME)
+template algo4(dummy: int): untyped =  algoClock(CLOCK_MONOTONIC_RAW_APPROX)
+template algo5(dummy: int): untyped =  algoClock(CLOCK_UPTIME_RAW) # works?
+template algo6(dummy: int): untyped =  algoClock(CLOCK_UPTIME_RAW_APPROX)
+template algo7(dummy: int): untyped =  algoClock(CLOCK_PROCESS_CPUTIME_ID)
+template algo8(dummy: int): untyped =  rdtsc() # works
+
+var msg = ""
+template mainImpl(algo) = 
+  var c2 = 0
+  let n = 100
+  for i in 0..<n:
+    # this could fail with getTime instead of getMonoTime, as expected
+    let a = algo(0)
+    let b = algo(0)
+    echo (b - a, a, b)
+    if b <= a: c2.inc
+    # when defined(windows) and not defined(js):
+    #   # bug #18158
+    #   doAssert b >= a
+    # else:
+    #   doAssert b > a
+    doAssert b >= a
+  let msgi = astToStr(algo) & ": " & $c2
+  echo msgi
+  msg.add msgi & "\n"
+
 proc main =
-  block:
-    let d = initDuration(nanoseconds = 10)
-    let t1 = getMonoTime()
-    let t2 = t1 + d
-
-    doAssert t2 - t1 == d
-    doAssert t1 == t1
-    doAssert t1 != t2
-    doAssert t2 - d == t1
-    doAssert t1 < t2
-    doAssert t1 <= t2
-    doAssert t1 <= t1
-    doAssert not(t2 < t1)
-    doAssert t1 < high(MonoTime)
-    doAssert low(MonoTime) < t1
-
-  block:
-    const n = when defined(js): 20000 else: 1000000 # keep test under ~ 1sec
-    var c1 = 0
-    var c2 = 0
-    for i in 0..<n:
-      # this could fail with getTime instead of getMonoTime, as expected
-      let a = getMonoTime()
-      let b = getMonoTime()
-      echo (b - a, a, b)
-      if b < a: c1.inc
-      if b <= a: c2.inc
-      # when defined(windows) and not defined(js):
-      #   # bug #18158
-      #   doAssert b >= a
-      # else:
-      #   doAssert b > a
-    echo (c1, c2, n)
+  mainImpl(algo1)
+  mainImpl(algo2)
+  mainImpl(algo3)
+  mainImpl(algo4)
+  mainImpl(algo5)
+  mainImpl(algo6)
+  mainImpl(algo7)
+  mainImpl(algo8)
+  echo msg
 
 main()
-# static: main() # xxx support
