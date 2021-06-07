429b1286325e7b6cf86ed4e29cea3e11a8c0e2df
Bung <crc32@qq.com>
2021-06-07 19:57:42 +0800

    change mimedb stroe stringtable to orderedtable (#18065)
    * change mimedb stroe stringtable to orderedtable
    
    * Update lib/pure/mimetypes.nim
    
    Co-authored-by: Andreas Rumpf <rumpf_a@web.de>

--- tests/stdlib/tmimetypes.nim
+++ tests/stdlib/tmimetypes.nim
@@ -6,6 +6,7 @@ import std/mimetypes
 template main() =
   var m = newMimetypes()
   doAssert m.getMimetype("mp4") == "video/mp4"
+  doAssert m.getExt("application/json") == "json"
   # see also `runnableExamples`.
   # xxx we should have a way to avoid duplicating code between runnableExamples and tests
 
