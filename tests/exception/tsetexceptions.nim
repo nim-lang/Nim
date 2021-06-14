33ac8de2733deede6be18e437d791d6db214edeb
Timothee Cour <timothee.cour2@gmail.com>
2021-06-14 10:56:48 -0700

    tests/exception/tsetexceptions.nim not joinable

--- tests/exception/tsetexceptions.nim
+++ tests/exception/tsetexceptions.nim
@@ -1,6 +1,9 @@
 discard """
   targets: "c cpp js"
+  joinable: false
 """
+
+# refs https://github.com/nim-lang/Nim/pull/18247#issuecomment-860877161
 
 let ex = newException(CatchableError, "test")
 setCurrentException(ex)
