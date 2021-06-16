c801284cd1459191db5113f85757e5fb70bfb998
flywind <xzsflywind@gmail.com>
2021-06-16 16:16:54 +0800

    cvlose #17696

--- tests/stdlib/tsharedlist.nim
+++ tests/stdlib/tsharedlist.nim
@@ -1,17 +1,44 @@
-import sharedlist
+import std/sharedlist
 
-var
-  list: SharedList[int]
-  count: int
+block:
+  var
+    list: SharedList[int]
+    count: int
 
-init(list)
+  init(list)
 
-for i in 1 .. 250:
-  list.add i
+  for i in 1 .. 250:
+    list.add i
 
-for i in list:
-  inc count
+  for i in list:
+    inc count
 
-doAssert count == 250
+  doAssert count == 250
 
-deinitSharedList(list)
+  deinitSharedList(list)
+
+
+block: # bug #17696
+  var keysList = SharedList[string]()
+  init(keysList)
+
+  keysList.add("a")
+  keysList.add("b")
+  keysList.add("c")
+  keysList.add("d")
+  keysList.add("e")
+  keysList.add("f")
+
+
+  # Remove element "b" and "d" from the list. 
+  keysList.iterAndMutate(proc (key: string): bool =
+    if key == "b" or key == "d": # remove only "b" and "d"
+      return true
+    return false
+  )
+
+  var results: seq[string]
+  for key in keysList.items:
+    results.add key
+
+  doAssert results == @["a", "f", "c", "e"]
