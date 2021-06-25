0046a95f6cda4326fb17d94e11afe520e28f31a9
Araq <rumpf_a@web.de>
2021-06-25 11:29:18 +0200

    ensure 'koch boot --gc:orc' stays green

--- koch.nim
+++ koch.nim
@@ -582,6 +582,10 @@ proc runCI(cmd: string) =
     when defined(posix):
       execFold("Run nimsuggest tests", "nim r nimsuggest/tester")
 
+  when not defined(bsd):
+    # the BSDs are overwhelmed already, so only run this test on the other machines:
+    kochExecFold("Boot Nim ORC", "boot -d:release --gc:orc --lib:lib")
+
 proc testUnixInstall(cmdLineRest: string) =
   csource("-d:danger" & cmdLineRest)
   xz(false, cmdLineRest)
