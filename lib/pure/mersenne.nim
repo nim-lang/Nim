b7f164965aa203e043754e35e5336207649dcc51
pyautogui <rami-rosy@outlook.com>
2021-06-29 17:53:13 -0700

    Add functions to the mersenne module. Add tests for them. I am very inexperienced.

--- lib/pure/mersenne.nim
+++ lib/pure/mersenne.nim
@@ -19,7 +19,6 @@ runnableExamples:
 ## See also
 ## ========
 ## * `random module<random.html>`_ for Nim's standard random number generator
-
 type
   MersenneTwister* = object
     ## The Mersenne Twister.
@@ -53,3 +52,99 @@ proc getNum*(m: var MersenneTwister): uint32 =
   result = result xor ((result shl 7'u32) and 0x9d2c5680'u32)
   result = result xor ((result shl 15'u32) and 0xefc60000'u32)
   result = result xor (result shr 18'u32)
+
+proc getSeq*(m: var MersenneTwister, len: int): seq[int] =
+  ## returns seq of pseudorandom ints len long.
+  for i in 1..len:
+    result.add(int(getNum(m)))
+  ## Might be useful at some point to someone.
+
+
+proc sample*(q: var MersenneTwister, arr: seq[int]): int =
+  ## take random sample of an seq[int].
+  var correspondingvalues: seq[uint32]
+  var maxVal: uint32 = uint32(4294967295)
+  let maxValdivlenarr: uint32 = uint32(float32(maxVal) / float32(len(arr)))
+  for i in arr:
+    correspondingvalues.add(maxVal)
+    maxVal = maxVal - maxValdivlenarr
+  let num = uint32(getNum(q))
+  var largenumrindex: uint32 = 0
+  var smallernumindex: uint32 = 1
+  while largenumrindex < uint32(len(correspondingvalues))  and smallernumindex < uint32(len(correspondingvalues)):
+   if num < correspondingvalues[largenumrindex] and num > correspondingvalues[smallernumindex]:
+    result = arr[largenumrindex]
+    break
+   elif num < correspondingvalues[correspondingvalues.high]:
+     result = arr[correspondingvalues.high]
+     break
+   largenumrindex = largenumrindex + 1
+   smallernumindex = smallernumindex + 1
+
+
+proc sample*(q: var MersenneTwister, arr: seq[char]): char =
+  ## take random sample of an seq[char].
+  var correspondingvalues: seq[uint32]
+  var maxVal: uint32 = uint32(4294967295)
+  let maxValdivlenarr: uint32 = uint32(float32(maxVal) / float32(len(arr)))
+  for i in arr:
+    correspondingvalues.add(maxVal)
+    maxVal = maxVal - maxValdivlenarr
+  let num = uint32(getNum(q))
+  var largenumrindex: uint32 = 0
+  var smallernumindex: uint32 = 1
+  while largenumrindex < uint32(len(correspondingvalues))  and smallernumindex < uint32(len(correspondingvalues)):
+   if num < correspondingvalues[largenumrindex] and num > correspondingvalues[smallernumindex]:
+    result = arr[largenumrindex]
+    break
+   elif num < correspondingvalues[correspondingvalues.high]:
+     result = arr[correspondingvalues.high]
+     break
+   largenumrindex = largenumrindex + 1
+   smallernumindex = smallernumindex + 1
+
+proc sample*(q: var MersenneTwister, arr: seq[string]): string =
+  ## take random sample of an seq[string].
+  var correspondingvalues: seq[uint32]
+  var maxVal: uint32 = uint32(4294967295)
+  let maxValdivlenarr: uint32 = uint32(float32(maxVal) / float32(len(arr)))
+  for i in arr:
+    correspondingvalues.add(maxVal)
+    maxVal = maxVal - maxValdivlenarr
+  let num = uint32(getNum(q))
+  var largenumrindex: uint32 = 0
+  var smallernumindex: uint32 = 1
+  while largenumrindex < uint32(len(correspondingvalues))  and smallernumindex < uint32(len(correspondingvalues)):
+   if num < correspondingvalues[largenumrindex] and num > correspondingvalues[smallernumindex]:
+    result = arr[largenumrindex]
+    break
+   elif num < correspondingvalues[correspondingvalues.high]:
+     result = arr[correspondingvalues.high]
+     break
+   largenumrindex = largenumrindex + 1
+   smallernumindex = smallernumindex + 1
+
+
+
+
+proc sample*(q: var MersenneTwister, arr: seq[float]): float =
+  ## take random sample of an seq[float].
+
+  var correspondingvalues: seq[uint32]
+  var maxVal: uint32 = uint32(4294967295)
+  let maxValdivlenarr: uint32 = uint32(float32(maxVal) / float32(len(arr)))
+  for i in arr:
+    correspondingvalues.add(maxVal)
+    maxVal = maxVal - maxValdivlenarr
+  let num = uint32(getNum(q))
+  var largenumrindex: uint32 = 0
+  var smallernumindex: uint32 = 1
+  while largenumrindex < uint32(len(correspondingvalues))  and smallernumindex < uint32(len(correspondingvalues)):
+   if num < correspondingvalues[largenumrindex] and num > correspondingvalues[smallernumindex]:
+    result = arr[largenumrindex]
+    break
+   elif num < correspondingvalues[correspondingvalues.high]:
+     result = arr[correspondingvalues.high]
+     break
+   largenumrindex = largenumrindex + 1
+   smallernumindex = smallernumindex + 1
