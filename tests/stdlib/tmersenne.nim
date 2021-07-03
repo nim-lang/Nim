1ca1688b91a05ca8e05e4959064f110f35c2cbbc
pyautogui <rami-rosy@outlook.com>
2021-07-02 22:53:51 -0700

    Remove useless procs, deprecate whole module.

--- tests/stdlib/tmersenne.nim
+++ tests/stdlib/tmersenne.nim
@@ -6,12 +6,6 @@ template main() =
   doAssert mt.getNum == 407788156'u32
   doAssert mt.getNum == 1071751096'u32
   doAssert mt.getNum == 3805347140'u32
-  doAssert sample(mt, @[30, 20, 10]) == 20
-  doAssert sample(mt, @[30.4, 20.54, 10.5]) == 10.5
-  doAssert sample(mt, @['a', 'b', 'c']) == 'b'
-  doAssert sample(mt, @["abc", "def", "ghi"]) == "def"
-  
-
 
 
 static: main()
