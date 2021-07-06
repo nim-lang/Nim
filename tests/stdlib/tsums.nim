9b956f39de1a0cb274eabbf64da5d268f1f5dd3e
Antonis Geralis <43617260+planetis-m@users.noreply.github.com>
2021-07-06 13:39:11 +0300

    add tests

--- tests/stdlib/tsums.nim
+++ tests/stdlib/tsums.nim
@@ -5,18 +5,24 @@ var epsilon = 1.0
 while 1.0 + epsilon != 1.0:
   epsilon /= 2.0
 let data = @[1.0, epsilon, -epsilon]
-doAssert sumKbn(data) == 1.0
+assert sumKbn(data) == 1.0
+assert sumKbk(data) == 1.0
 # doAssert sumPairs(data) != 1.0 # known to fail in 64 bits
-doAssert (1.0 + epsilon) - epsilon != 1.0
+assert sumShewchuck(data) == 1.0
+assert (1.0 + epsilon) - epsilon != 1.0
 
 var tc1: seq[float]
 for n in 1 .. 1000:
   tc1.add 1.0 / n.float
-doAssert sumKbn(tc1) == 7.485470860550345
-doAssert sumPairs(tc1) == 7.485470860550345
+assert sumKbn(tc1) == 7.485470860550345
+assert sumKbk(tc1) == 7.485470860550345
+assert sumPairs(tc1) == 7.485470860550345
+assert sumShewchuck(tc1) == 7.485470860550345
 
 var tc2: seq[float]
 for n in 1 .. 1000:
   tc2.add pow(-1.0, n.float) / n.float
-doAssert sumKbn(tc2) == -0.6926474305598203
-doAssert sumPairs(tc2) == -0.6926474305598204
+assert sumKbn(tc2) == -0.6926474305598203
+assert sumKbk(tc2) == -0.6926474305598203
+assert sumPairs(tc2) == -0.6926474305598204
+assert sumShewchuck(tc2) == -0.6926474305598203
