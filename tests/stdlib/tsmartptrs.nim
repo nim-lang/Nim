b17c2bad80164648aed43bc648af56f2fe1c9c39
Antonis Geralis <43617260+planetis-m@users.noreply.github.com>
2021-07-07 23:27:58 +0300

    Create tsmartptrs.nim

---
+++ tests/stdlib/tsmartptrs.nim
@@ -0,0 +1,49 @@
+import std/smartptrs
+
+block:
+  var a1: UniquePtr[float]
+  var a2 = newUniquePtr(0)
+
+  assert $a1 == "nil"
+  assert a1.isNil
+  assert $a2 == "UniquePtr(0)"
+  assert not a2.isNil
+  assert a2[] == 0
+
+  # UniquePtr can't be copied but can be moved
+  let a3 = move a2
+
+  assert $a2 == "nil"
+  assert a2.isNil
+
+  assert $a3 == "UniquePtr(0)"
+  assert not a3.isNil
+  assert a3[] == 0
+
+block:
+  var a1: SharedPtr[float]
+  let a2 = newSharedPtr(0)
+  let a3 = a2
+
+  assert $a1 == "nil"
+  assert a1.isNil
+  assert $a2 == "SharedPtr(0)"
+  assert not a2.isNil
+  assert a2[] == 0
+  assert $a3 == "SharedPtr(0)"
+  assert not a3.isNil
+  assert a3[] == 0
+
+block:
+  var a1: ConstPtr[float]
+  let a2 = newConstPtr(0)
+  let a3 = a2
+
+  assert $a1 == "nil"
+  assert a1.isNil
+  assert $a2 == "ConstPtr(0)"
+  assert not a2.isNil
+  assert a2[] == 0
+  assert $a3 == "ConstPtr(0)"
+  assert not a3.isNil
+  assert a3[] == 0
