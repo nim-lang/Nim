19c2c83e39883e507bfbe7c2df3ce0cec0f82c37
Caden Haustein <code@brightlysalty.33mail.com>
2021-07-19 22:16:00 +0000

    Fix style in tests/stdlib/tos.nim
    Co-authored-by: Timothee Cour <timothee.cour2@gmail.com>

--- tests/stdlib/tos.nim
+++ tests/stdlib/tos.nim
@@ -614,7 +614,7 @@ block osenv:
     doAssert existsEnv(dummyEnvVar) == false
     delEnv(dummyEnvVar)         # deleting an already deleted env var
     doAssert existsEnv(dummyEnvVar) == false
-  block putEnv:
+  block: # putEnv
     # raises OSError on invalid input
     doAssertRaises(OSError, putEnv("DUMMY_ENV_VAR_PUT=DUMMY_VALUE", "NEW_DUMMY_VALUE"))
     doAssertRaises(OSError, putEnv("", "NEW_DUMMY_VALUE"))
