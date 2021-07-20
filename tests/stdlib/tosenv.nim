2f2e833e3bffb1a8c8b021a67d8f6eba2311e859
Caden Haustein <code@brightlysalty.33mail.com>
2021-07-20 16:35:21 -0500

    Rework osenv tests

--- tests/stdlib/tosenv.nim
+++ tests/stdlib/tosenv.nim
@@ -1,22 +1,41 @@
 discard """
   matrix: "--threads"
   joinable: false
-  target: "c cpp"
+  targets: "c cpp"
 """
+import os
 
-import os, system
+block delEnv:
+    const dummyEnvVar = "DUMMY_ENV_VAR" # This env var wouldn't be likely to exist to begin with
+    doAssert existsEnv(dummyEnvVar) == false
+    putEnv(dummyEnvVar, "1")
+    doAssert existsEnv(dummyEnvVar) == true
+    delEnv(dummyEnvVar)
+    doAssert existsEnv(dummyEnvVar) == false
+    delEnv(dummyEnvVar) # deleting an already deleted env var
+    doAssert existsEnv(dummyEnvVar) == false
 
-proc c_getenv(env: cstring): cstring {.importc: "getenv", header: "<stdlib.h>".}
-var thr: Thread[void]
-proc threadFunc {.thread.} =
-  putEnv("foo", "fooVal2")
+block putEnv:
+    # raises OSError on invalid input
+    doAssertRaises(OSError, putEnv("DUMMY_ENV_VAR_PUT=DUMMY_VALUE",
+            "NEW_DUMMY_VALUE"))
+    doAssertRaises(OSError, putEnv("", "NEW_DUMMY_VALUE"))
 
 block:
-  putEnv("foo", "fooVal1")
-  echo getEnv("foo")
-  createThread(thr, threadFunc)
-  joinThreads(thr)
-  doAssert getEnv("foo") == $c_getenv("foo")
+    doAssert getEnv("DUMMY_ENV_VAR_NONEXISTENT", "") == ""
+    doAssert getEnv("DUMMY_ENV_VAR_NONEXISTENT", " ") == " "
+    doAssert getEnv("DUMMY_ENV_VAR_NONEXISTENT", "Arrakis") == "Arrakis"
 
-block:
-  doAssertRaises(OSError): delEnv("foo=bar")
+block: # bug 18533
+    proc c_getenv(env: cstring): cstring {.importc: "getenv",
+            header: "<stdlib.h>".}
+  var thr: Thread[void]
+    proc threadFunc {.thread.} = putEnv("foo", "fooVal2")
+
+    putEnv("foo", "fooVal1")
+    echo getEnv("foo")
+    createThread(thr, threadFunc)
+    joinThreads(thr)
+    doAssert getEnv("foo") == $c_getenv("foo")
+
+    doAssertRaises(OSError): delEnv("foo=bar")
