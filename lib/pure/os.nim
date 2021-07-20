53a904dc47e47e6c638e90627e05000bb75c0a6a
flywind <xzsflywind@gmail.com>
2021-07-20 09:21:06 +0800

    eliminate blank lines

--- lib/pure/os.nim
+++ lib/pure/os.nim
@@ -13,13 +13,9 @@
 
 runnableExamples("-r:off"):
   let myFile = "/path/to/my/file.nim"
-
   assert splitPath(myFile) == (head: "/path/to/my", tail: "file.nim")
-
   assert parentDir(myFile) == "/path/to/my"
-
   assert splitFile(myFile) == (dir: "/path/to/my", name: "file", ext: ".nim")
-
   assert myFile.changeFileExt("c") == "/path/to/my/file.c"
 
 ## **See also:**
