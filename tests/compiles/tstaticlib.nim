f8da41f5227cfc875bf823a3af236288b5c4890f
flywind <xzsflywind@gmail.com>
2021-07-25 23:06:31 +0800

    tiny

--- tests/compiles/tstaticlib.nim
+++ tests/compiles/tstaticlib.nim
@@ -2,11 +2,11 @@ import std/[os, osproc, strformat]
 
 
 const dir = "tests/compiles"
-const file = dir  / "staticlibtest.nim"
+const fileName = dir / "staticlibtest.nim"
 
 block: # bug #18578
   const libName = dir / "tstaticlib1.a"
-  let (_, status) = execCmdEx(fmt"nim c -o:{libName} --app:staticlib {file}")
+  let (_, status) = execCmdEx(fmt"nim c -o:{libName} --app:staticlib {fileName}")
   doAssert status == 0
   doAssert fileExists(libName)
   removeFile(libName)
@@ -14,6 +14,6 @@ block: # bug #18578
 block: # bug #16947
   const libName = dir / "tstaticlib2.a"
   doAssert fileExists(libName)
-  let (_, status) = execCmdEx(fmt"nim c -o:{libName} --app:staticlib {file}")
+  let (_, status) = execCmdEx(fmt"nim c -o:{libName} --app:staticlib {fileName}")
   doAssert status == 0
   doAssert fileExists(libName)
