d4eea9b2cb2cff20362b408ca82b67e509734f00
flywind <xzsflywind@gmail.com>
2021-07-26 21:41:59 +0800

    apply changes

--- tests/compiles/tstaticlib.nim
+++ tests/compiles/tstaticlib.nim
@@ -2,18 +2,22 @@ import std/[os, osproc, strformat]
 
 
 const dir = "tests/compiles"
-const fileName = dir / "staticlibtest.nim"
+const fileName = dir / "mstaticlib.nim"
+# const nim = getCurrentCompilerExe()
+const nim = "bin/nim_temp"
 
 block: # bug #18578
   const libName = dir / "tstaticlib1.a"
-  let (_, status) = execCmdEx(fmt"nim c -o:{libName} --app:staticlib {fileName}")
+  let (_, status) = execCmdEx(fmt"{nim} c -o:{libName} --app:staticlib {fileName}")
   doAssert status == 0
   doAssert fileExists(libName)
   removeFile(libName)
 
 block: # bug #16947
   const libName = dir / "tstaticlib2.a"
+  writeFile(libName, "echo 124")
   doAssert fileExists(libName)
-  let (_, status) = execCmdEx(fmt"nim c -o:{libName} --app:staticlib {fileName}")
+  let (_, status) = execCmdEx(fmt"{nim} c -o:{libName} --app:staticlib {fileName}")
   doAssert status == 0
   doAssert fileExists(libName)
+  removeFile(libName)
