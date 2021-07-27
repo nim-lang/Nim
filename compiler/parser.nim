1e586d166d07ac6a836c0e719e584321e94fa424
Timothee Cour <timothee.cour2@gmail.com>
2021-07-26 19:41:02 -0700

    fix tdumpastgen.nim

--- compiler/parser.nim
+++ compiler/parser.nim
@@ -1795,13 +1795,14 @@ proc parseRoutine(p: var Parser, kind: TNodeKind): PNode =
   else:
     result.add(p.emptyNode)
   indAndComment(p, result, maybeMissEquals)
-  if result[^1].kind == nkStmtList and result[^1].len > 0 and result.sons[^1][0].comment.len > 0:
+  let body = result[^1]
+  if body.kind == nkStmtList and body.len > 0 and body[0].comment.len > 0 and body[0].kind != nkCommentStmt:
     assert result.comment.len == 0
     #[
     proc fn*(a: int): int = a ## foo
     => moves comment `foo` to `fn`
     ]#
-    swap(result.comment, result.sons[^1][0].comment)
+    swap(result.comment, body[0].comment)
 
 proc newCommentStmt(p: var Parser): PNode =
   #| commentStmt = COMMENT
