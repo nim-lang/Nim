b001582891fac1762a76543db6a868e3addce310
Timothee Cour <timothee.cour2@gmail.com>
2021-08-12 13:23:28 -0700

    simplify reprDiscriminant

--- lib/std/private/repr_impl.nim
+++ lib/std/private/repr_impl.nim
@@ -8,5 +8,5 @@ other APIs common to system/repr and system/reprjs could be refactored here, eg:
 proc reprDiscriminant*(e: int, typ: PNimType): string {.compilerRtl.} =
   case typ.kind
   of tyEnum: reprEnum(e, typ)
-  of tyBool: (if e == 0: "false" else: "true")
+  of tyBool: $(e != 0)
   else: $e
