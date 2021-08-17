a4a167c476280e34239c80575740a00e0d01e363
flywind <xzsflywind@gmail.com>
2021-08-17 11:38:31 +0800

    both

--- compiler/msgs.nim
+++ compiler/msgs.nim
@@ -670,6 +670,7 @@ proc genSuccessX*(conf: ConfigRef) =
   let loc = $conf.linesCompiled
   var build = ""
   var flags = ""
+  const debugModeHints = "none (DEBUG BUILD, `-d:release` generates faster code)"
   if conf.cmd in cmdBackends:
     if conf.backend != backendJs:
       build.add "gc: $#; " % $conf.selectedGC
@@ -677,7 +678,7 @@ proc genSuccessX*(conf: ConfigRef) =
       build.add "opt: "
       if optOptimizeSpeed in conf.options: build.add "speed"
       elif optOptimizeSize in conf.options: build.add "size"
-      else: build.add "none (DEBUG BUILD, `-d:release` generates faster code)"
+      else: build.add debugModeHints
         # pending https://github.com/timotheecour/Nim/issues/752, point to optimization.html
       if isDefined(conf, "danger"): flags.add " -d:danger"
       elif isDefined(conf, "release"): flags.add " -d:release"
@@ -689,7 +690,7 @@ proc genSuccessX*(conf: ConfigRef) =
       elif isDefined(conf, "release"):
         build.add "speed"
         flags.add " -d:release"
-      else: build.add "none (DEBUG BUILD, `-d:release` generates faster code)"
+      else: build.add debugModeHints
     if flags.len > 0: build.add "; options:" & flags
   let sec = formatFloat(epochTime() - conf.lastCmdTime, ffDecimal, 3)
   let project = if conf.filenameOption == foAbs: $conf.projectFull else: $conf.projectName
