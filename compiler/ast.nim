5f4053b9bf14821e0e79a073667c336ba4fcb54f
flywind <xzsflywind@gmail.com>
2021-08-19 20:29:37 +0800

    remove tyVarargs

--- compiler/ast.nim
+++ compiler/ast.nim
@@ -1591,7 +1591,7 @@ proc propagateToOwner*(owner, elem: PType; propagateHasAsgn = true) =
   if mask != {} and propagateHasAsgn:
     let o2 = owner.skipTypes({tyGenericInst, tyAlias, tySink})
     if o2.kind in {tyTuple, tyObject, tyArray,
-                   tySequence, tySet, tyDistinct, tyVarargs}:
+                   tySequence, tySet, tyDistinct}:
       o2.flags.incl mask
       owner.flags.incl mask
 
