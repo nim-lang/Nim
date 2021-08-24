9b47614ba15f5446587627f50136739c46a7f1d1
narimiran <narimiran@disroot.org>
2021-08-24 11:32:28 +0200

    we can't use `collect` like that in 1.2

--- tests/stdlib/tparseutils.nim
+++ tests/stdlib/tparseutils.nim
@@ -1,4 +1,4 @@
-import parseutils, sequtils, sugar
+import parseutils, sequtils
 
 
 let input = "$test{}  $this is ${an{  example}}  "
@@ -41,12 +41,3 @@ doAssert value == 1_000_000
 var i64Value: int64
 discard parseBiggestInt("9223372036854775807", i64Value)
 doAssert i64Value == 9223372036854775807
-
-block:
-  var f: float
-  let res = collect:
-    for x in ["9.123456789012345+","11.123456789012345+","9.123456789012345-","8.123456789012345+","9.12345678901234-","9.123456789012345"]:
-      (parseFloat(x, f, 0), $f)
-  doAssert res == @[(17, "9.123456789012344"), (18, "11.123456789012344"),
-                    (17, "9.123456789012344"), (17, "8.123456789012344"),
-                    (16, "9.12345678901234"), (17, "9.123456789012344")]
