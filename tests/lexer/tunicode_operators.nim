d84ce44a1529eecaa14c9821c40221b841ecc818
Araq <rumpf_a@web.de>
2021-09-03 12:09:16 +0200

    better test

--- tests/lexer/tunicode_operators.nim
+++ tests/lexer/tunicode_operators.nim
@@ -3,12 +3,12 @@
 proc `⊙`(x, y: int): int = x * y
 proc `⊙=`(x: var int, y: int) = x *= y
 
-proc `⊞`(x, y: int): int = x + y
+proc `⊞++`(x, y: int): int = x + y
 
 var x = 45
-x ⊙= 9 ⊙ 4 ⊞ 3
+x ⊙= 9 ⊞++ 4 ⊙ 3
 
 var y = 45
-y *= 9 * 4 + 3
+y *= 9 + 4 * 3
 
 assert x == y
