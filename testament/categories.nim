3d3384ad39dcea0d5bf7352b74ced183a8774b12
flywind <xzsflywind@gmail.com>
2021-09-05 07:56:25 +0800

    typo

--- testament/categories.nim
+++ testament/categories.nim
@@ -459,11 +459,11 @@ proc testNimblePackages(r: var TResults; cat: Category; packageFilter: string) =
       r.addResult(packageFileTest, targetC, "", "", reBuildFailed)
 
   except JsonParsingError:
-    error = 1
+    errors = 1
     r.addResult(packageFileTest, targetC, "", "Invalid package file", reBuildFailed)
     raise
   except ValueError:
-    error = 1
+    errors = 1
     r.addResult(packageFileTest, targetC, "", "Unknown package", reBuildFailed)
     raise
   finally:
