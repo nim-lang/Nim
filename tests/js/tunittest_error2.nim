65554df75ef6e254369561c83e6063d77e7def0e
flywind <xzsflywind@gmail.com>
2021-09-08 09:42:21 +0800

    fix upstream changes

--- tests/js/tunittest_error2.nim
+++ tests/js/tunittest_error2.nim
@@ -1,7 +1,7 @@
 discard """
   exitcode: 1
   outputsub: '''
-Unhandled exception: Cannot read property 'charCodeAt' of null [<foreign exception>]
+Unhandled exception: Cannot read properties of null (reading 'charCodeAt') [<foreign exception>]
 [FAILED] Bad test
   '''
   matrix: "-d:nodejs"
