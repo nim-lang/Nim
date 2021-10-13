727c6378d2464090564dbcd9bc8b9ac648467e38
narimiran <narimiran@disroot.org>
2021-10-13 20:32:33 +0200

    bump NimVersion to 1.6.0

--- lib/system.nim
+++ lib/system.nim
@@ -2121,11 +2121,11 @@ const
     ##   when (NimMajor, NimMinor, NimPatch) >= (1, 3, 1): discard
     # see also std/private/since
 
-  NimMinor* {.intdefine.}: int = 5
+  NimMinor* {.intdefine.}: int = 6
     ## is the minor number of Nim's version.
     ## Odd for devel, even for releases.
 
-  NimPatch* {.intdefine.}: int = 1
+  NimPatch* {.intdefine.}: int = 0
     ## is the patch number of Nim's version.
     ## Odd for devel, even for releases.
 
