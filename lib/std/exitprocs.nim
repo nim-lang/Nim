582468da1cc266d0dd922ec5c620713bbc45d02a
Tail Wag Games <carterza@gmail.com>
2021-10-25 03:13:30 -0500

    deinitializing locks at program exit (#19043)
    * deinitializing locks at program exit
    
    * deinitLock shouldn't be called for js backend I guess...
    
    * I suppose this is the best way to detect the
    ewruntime option
    
    * I guess I need these guards here too...
    
    * fixing merge conflict

--- lib/std/exitprocs.nim
+++ lib/std/exitprocs.nim
@@ -44,6 +44,9 @@ proc callClosures() {.noconv.} =
       of kClosure: fun.fun1()
       of kNoconv: fun.fun2()
 
+  when not defined(js) and not defined(nimOwnedEnabled):
+    deinitLock(gFunsLock)
+
 template fun() =
   if gFuns.len == 0:
     addAtExit(callClosures)
