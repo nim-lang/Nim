287abfc4e6fd01ac8e28ac04efc2bff1d4ccb28b
Clay Sweetser <Varriount@users.noreply.github.com>
2021-11-13 23:21:03 -0500

    Update duration_lit.nim

--- lib/pure/includes/duration_lit.nim
+++ lib/pure/includes/duration_lit.nim
@@ -1,46 +1,54 @@
-proc `'ns`*(num: string): Duration {.inline.} =
+proc `'ns`*(num: string): TimeInterval {.inline.} =
+  ## Custom 
   runnableExamples:
-    assert 19'ns == initDuration(nanoseconds = 19)
+    assert 19'ns == initTimeInterval(nanoseconds = 19)
     assert 1000_000_000'ns == 1's
-  initDuration(nanoseconds = parseInt(num))
+  parseInt(num).nanoseconds
 
-proc `'us`*(num: string): Duration {.inline.} =
+proc `'us`*(num: string): TimeInterval {.inline.} =
+  ## Custom 
   runnableExamples:
-    assert 3'us == initDuration(microseconds = 3)
+    assert 3'us == initTimeInterval(microseconds = 3)
     assert 1000_000'us == 1's
-  initDuration(microseconds = parseInt(num))
+  parseInt(num).microseconds
 
-proc `'ms`*(num: string): Duration {.inline.} =
+proc `'ms`*(num: string): TimeInterval {.inline.} =
+  ## Custom 
   runnableExamples:
-    assert 11'ms == initDuration(milliseconds = 11)
+    assert 11'ms == initTimeInterval(milliseconds = 11)
     assert 1000'ms == 1's
-  initDuration(milliseconds = parseInt(num))
+  parseInt(num).milliseconds
 
-proc `'s`*(num: string): Duration {.inline.} =
+proc `'s`*(num: string): TimeInterval {.inline.} =
+  ## Custom 
   runnableExamples:
-    assert 2's == initDuration(seconds = 2)
+    assert 2's == initTimeInterval(seconds = 2)
     assert 60's == 1'min
-  initDuration(seconds = parseInt(num))
+  parseInt(num).seconds
 
-proc `'min`*(num: string): Duration {.inline.} =
+proc `'minutes`*(num: string): TimeInterval {.inline.} =
+  ## Custom 
   runnableExamples:
-    assert 13'min == initDuration(minutes = 13)
+    assert 13'min == initTimeInterval(minutes = 13)
     assert 60'min == 1'hour
-  initDuration(minutes = parseInt(num))
+  parseInt(num).minutes
 
-proc `'hour`*(num: string): Duration {.inline.} =
+proc `'hours`*(num: string): TimeInterval {.inline.} =
+  ## Custom 
   runnableExamples:
-    assert 17'hour == initDuration(hours = 17)
+    assert 17'hour == initTimeInterval(hours = 17)
     assert 24'hour == 1'day
-  initDuration(hours = parseInt(num))
+  parseInt(num).hours
 
-proc `'day`*(num: string): Duration {.inline.} =
+proc `'days`*(num: string): TimeInterval {.inline.} =
+  ## Custom 
   runnableExamples:
-    assert 5'day == initDuration(days = 5)
+    assert 5'day == initTimeInterval(days = 5)
     assert 7'day == 1'week
-  initDuration(days = parseInt(num))
+  parseInt(num).days
 
-proc `'week`*(num: string): Duration {.inline.} =
+proc `'weeks`*(num: string): TimeInterval {.inline.} =
+  ## Custom 
   runnableExamples:
-    assert 19'week == initDuration(weeks = 19)
-  initDuration(weeks = parseInt(num))
+    assert 19'week == initTimeInterval(weeks = 19)
+  parseInt(num).weeks
