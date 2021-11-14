fd47f63338aa64107a0a8b63d603d55705aba588
Clay Sweetser <Varriount@users.noreply.github.com>
2021-11-13 23:34:55 -0500

    Update duration_lit.nim

--- lib/pure/includes/duration_lit.nim
+++ lib/pure/includes/duration_lit.nim
@@ -3,52 +3,52 @@ proc `'ns`*(num: string): TimeInterval {.inline.} =
   runnableExamples:
     assert 19'ns == initTimeInterval(nanoseconds = 19)
     assert 1000_000_000'ns == 1's
-  parseInt(num).nanoseconds
+  nanoseconds(parseInt(num))
 
 proc `'us`*(num: string): TimeInterval {.inline.} =
   ## Custom numeric literal for a TimeInterval of `num` microseconds (10^−6 seconds).
   runnableExamples:
     assert 3'us == initTimeInterval(microseconds = 3)
     assert 1000_000'us == 1's
-  parseInt(num).microseconds
+  microseconds(parseInt(num))
 
 proc `'ms`*(num: string): TimeInterval {.inline.} =
   ## Custom numeric literal for a TimeInterval of `num` milliseconds (10^−3 seconds).
   runnableExamples:
     assert 11'ms == initTimeInterval(milliseconds = 11)
     assert 1000'ms == 1's
-  parseInt(num).milliseconds
+  milliseconds(parseInt(num))
 
 proc `'seconds`*(num: string): TimeInterval {.inline.} =
   ## Custom numeric literal for a TimeInterval of `num` seconds.
   runnableExamples:
     assert 2's == initTimeInterval(seconds = 2)
     assert 60's == 1'min
-  parseInt(num).seconds
+  seconds(parseInt(num))
 
 proc `'minutes`*(num: string): TimeInterval {.inline.} =
   ## Custom numeric literal for a TimeInterval of `num` minutes.
   runnableExamples:
     assert 13'min == initTimeInterval(minutes = 13)
     assert 60'min == 1'hour
-  parseInt(num).minutes
+  minutes(parseInt(num))
 
 proc `'hours`*(num: string): TimeInterval {.inline.} =
   ## Custom numeric literal for a TimeInterval of `num` hours.
   runnableExamples:
     assert 17'hour == initTimeInterval(hours = 17)
     assert 24'hour == 1'day
-  parseInt(num).hours
+  hours(parseInt(num))
 
 proc `'days`*(num: string): TimeInterval {.inline.} =
   ## Custom numeric literal for a TimeInterval of `num` days.
   runnableExamples:
     assert 5'day == initTimeInterval(days = 5)
     assert 7'day == 1'week
-  parseInt(num).days
+  days(parseInt(num))
 
 proc `'weeks`*(num: string): TimeInterval {.inline.} =
   ## Custom numeric literal for a TimeInterval of `num` weeks.
   runnableExamples:
     assert 19'week == initTimeInterval(weeks = 19)
-  parseInt(num).weeks
+  weeks(parseInt(num))
