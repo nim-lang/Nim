6910d45622f52f3399bba1c57ac73a1a3edd3af8
treeform <starplant@gmail.com>
2021-11-19 09:51:08 -0800

    Fix .=

--- tests/specialops/tdotopserrors3.nim
+++ tests/specialops/tdotopserrors3.nim
@@ -2,12 +2,7 @@ discard """
   cmd: "nim check --hints:off $options $file"
   action: "reject"
   nimout: '''
-tdotopserrors3.nim(20, 5) Error: expression 'x=' cannot be called
-tdotopserrors3.nim(20, 2) Error: undeclared field: 'x' for type tdotopserrors3.Bar [type declared in tdotopserrors3.nim(17, 6)]
-tdotopserrors3.nim(20, 2) Error: undeclared field: '.' for type tdotopserrors3.Bar [type declared in tdotopserrors3.nim(17, 6)]
-tdotopserrors3.nim(20, 2) Error: expression '.' cannot be called
-tdotopserrors3.nim(20, 2) Error: expression '' has no type (or is ambiguous)
-tdotopserrors3.nim(20, 2) Error: '' cannot be assigned to
+tdotopserrors3.nim(15, 2) Error: undeclared field: 'x' for type tdotopserrors3.Bar [type declared in tdotopserrors3.nim(17, 6)]
 '''
 """
 
