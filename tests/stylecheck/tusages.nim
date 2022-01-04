95e78148e42475e92e3392b222bd07798284b9db
flywind <xzsflywind@gmail.com>
2022-01-04 15:12:50 +0800

    check usages

--- tests/stylecheck/tusages.nim
+++ tests/stylecheck/tusages.nim
@@ -1,6 +1,7 @@
 discard """
   action: reject
   nimout: '''tusages.nim(22, 5) Error: 'BAD_STYLE' should be: 'BADSTYLE' [proc declared in tusages.nim(11, 6)]'''
+  matrix: "--styleCheck:error --styleCheck:usages"
 """
 
 
