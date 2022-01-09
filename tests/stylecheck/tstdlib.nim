nim-lang/Nim

commit ff5ef95414e48714efd0ce2b3b694de1017032dc

Fixes #12832 (#12842) [backport]
* Fix #12832
* nimVm -> nimvm in json.nim
* Use suggestSym() instead of markUsed(); Also use styleCheckUse() for finding style violations

compiler/semtempl.nim
@@ -261,6 +261,11 @@ proc semTemplSymbol(c: PContext, n: PNode, s: PSym; isField: bool): PNode =
   else:
     if isField and sfGenSym in s.flags: result = n
     else: result = newSymNode(s, n.info)
+    # Issue #12832
+    when defined(nimsuggest):
+      suggestSym(c.config, n.info, s, c.graph.usageSym, false)
+    if {optStyleHint, optStyleError} * c.config.globalOptions != {}:
+      styleCheckUse(c.config, n.info, s)
 
 proc semRoutineInTemplName(c: var TemplCtx, n: PNode): PNode =
   result = n

lib/pure/json.nim
@@ -1135,7 +1135,7 @@ when defined(nimFixedForwardGeneric):
         when defined js:
           `tmpSym`.`kindSym` = kindTmp
         else:
-          when nimVm:
+          when nimvm:
             `tmpSym`.`kindSym` = kindTmp
           else:
             # fuck it, assign kind field anyway

commit 76983481a432f794c6c7c983961601c7e6bc4e53

fix stylecheck error with asyncdispatch
it is a partial regression since #12842

compiler/semtempl.nim
@@ -270,7 +270,8 @@ proc semTemplSymbol(c: PContext, n: PNode, s: PSym; isField: bool): PNode =
     # Issue #12832
     when defined(nimsuggest):
       suggestSym(c.graph, n.info, s, c.graph.usageSym, false)
-    if {optStyleHint, optStyleError} * c.config.globalOptions != {}:
+    # field access (dot expr) will be handled by builtinFieldAccess
+    if not isField and {optStyleHint, optStyleError} * c.config.globalOptions != {}:
       styleCheckUse(c.config, n.info, s)
 
 proc semRoutineInTemplName(c: var TemplCtx, n: PNode): PNode =

tests/stylecheck/tstdlib.nim
@@ -1,0 +1,5 @@
+discard """
+  matrix: "--styleCheck:error --styleCheck:usages"
+"""
+
+import asyncdispatch