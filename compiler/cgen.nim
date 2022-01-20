52d2ff601b1720ac4c33f611e7da237619487206
flywind <xzsflywind@gmail.com>
2022-01-20 20:59:36 +0800

    enable weave (#19363) [backport:1.6]
    * enable weave
    * workaround CI
    
    (cherry picked from commit 927fa890ec91cf13d7e3aa87267b994c2812bdc8)

--- compiler/cgen.nim
+++ compiler/cgen.nim
@@ -48,8 +48,13 @@ proc addForwardedProc(m: BModule, prc: PSym) =
   m.g.forwardedProcs.add(prc)
 
 proc findPendingModule(m: BModule, s: PSym): BModule =
-  let ms = s.itemId.module  #getModule(s)
-  result = m.g.modules[ms]
+  # TODO fixme
+  if m.config.symbolFiles == v2Sf:
+    let ms = s.itemId.module  #getModule(s)
+    result = m.g.modules[ms]
+  else:
+    var ms = getModule(s)
+    result = m.g.modules[ms.position]
 
 proc initLoc(result: var TLoc, k: TLocKind, lode: PNode, s: TStorageLoc) =
   result.k = k
