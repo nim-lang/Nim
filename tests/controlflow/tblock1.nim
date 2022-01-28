a291ff3f64f1b6f8c4739237fe526240918548fc
flywind <xzsflywind@gmail.com>
2022-01-28 22:10:33 +0800

    fix test

--- tests/controlflow/tblock1.nim
+++ tests/controlflow/tblock1.nim
@@ -6,9 +6,6 @@ discard """
 # check for forward label and
 # for failure when label is not declared
 
-when defined(nimSlimSystem):
-  import std/syncio
-
 proc main =
   block endLess:
     write(stdout, "Muaahh!\N")
