690e58663b505e2c71f88cafea19e0904f357087
xflywind <xzsflywind@gmail.com>
2022-02-25 10:19:30 +0800

    split tests

--- tests/exception/texcas.nim
+++ tests/exception/texcas.nim
@@ -33,11 +33,3 @@ proc testTryAsExpr(i: int) =
 test[Exception]()
 test2()
 testTryAsExpr(5)
-
-# see bug #7115
-doAssert(not compiles(
-  try: 
-    echo 1
-  except KeyError as ex1, ValueError as ex2:
-    echo 2
-))
