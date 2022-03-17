23b53edf0f449b169e6533f677a18726d6e2c569
xflywind <xzsflywind@gmail.com>
2022-03-17 18:10:12 +0800

    fix nimsuggest

--- nimsuggest/tests/ttemplate_highlight.nim
+++ nimsuggest/tests/ttemplate_highlight.nim
@@ -4,6 +4,7 @@ doAssert true#[!]#
 discard """
 $nimsuggest --tester $1
 >highlight $1
+Module;;1;;10;;0
 highlight;;skTemplate;;2;;0;;8
 highlight;;skTemplate;;2;;0;;8
 highlight;;skEnumField;;2;;9;;4
