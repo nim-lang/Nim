55907a8bf885692e7c9899f38ce204349cd1e2dc
narimiran <narimiran@disroot.org>
2022-03-31 17:49:04 +0200

    Various std net improvements (#19132)
    * Variant of  that works with raw IpAddresses.
    
    - Add doc tests for new net proc's.
    - Aadd recvFrom impl
    - Add recvFrom impl -- tweak handling data var
    
    - Update lib/pure/net.nim
    	Co-authored-by: Dominik Picheta <dominikpicheta@googlemail.com>
    
    - cleaning up sendTo args
    - remove extra connect test
    - cleaning up sendTo args
    - fix inet_ntop test
    - fix test failing - byte len
    
    * fix test failing - byte len
    
    * debugging odd windows build failure
    
    * debugging odd windows build failure
    
    * more experiments to figure out the windows failure
    
    * try manual assigment on InAddr
    
    Co-authored-by: Jaremy Creechley <jaremy.creechley@panthalassa.com>
    (cherry picked from commit 4b5cecd902cc4126ff9d6cda9edb78a13a421239)

--- lib/posix/posix.nim
+++ lib/posix/posix.nim
@@ -155,9 +155,9 @@ when not defined(zephyr):
   proc inet_addr*(a1: cstring): InAddrT {.importc, header: "<arpa/inet.h>".}
   proc inet_ntoa*(a1: InAddr): cstring {.importc, header: "<arpa/inet.h>".}
 
-proc inet_ntop*(a1: cint, a2: pointer, a3: cstring, a4: int32): cstring {.
+proc inet_ntop*(a1: cint, a2: pointer | ptr InAddr | ptr In6Addr, a3: cstring, a4: int32): cstring {.
   importc:"(char *)$1", header: "<arpa/inet.h>".}
-proc inet_pton*(a1: cint, a2: cstring, a3: pointer): cint {.
+proc inet_pton*(a1: cint, a2: cstring, a3: pointer | ptr InAddr | ptr In6Addr): cint {.
   importc, header: "<arpa/inet.h>".}
 
 var
