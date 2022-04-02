e70d5e839213c71b2c8a6767095ebdf6c852061c
huantian <davidtianli@gmail.com>
2022-04-01 23:12:54 -0700

    temp: disable windows noasync test

--- tests/async/tasync_noasync.nim
+++ tests/async/tasync_noasync.nim
@@ -4,36 +4,37 @@ discard """
   nimout: '''
 stack trace: (most recent call last)
 asyncmacro.nim(136, 18)  nonAsyncProc
-tasync_noasync.nim(46, 9) template/generic instantiation of `await` from here
+tasync_noasync.nim(47, 9) template/generic instantiation of `await` from here
 ../../lib/pure/asyncmacro.nim(136, 18) Error: Can only 'await' inside a proc marked as 'async'. Use 'waitFor' when calling an 'async' proc in a non-async scope instead
 stack trace: (most recent call last)
 asyncmacro.nim(136, 18)  nested
-tasync_noasync.nim(48, 27) template/generic instantiation of `async` from here
-tasync_noasync.nim(50, 11) template/generic instantiation of `await` from here
+tasync_noasync.nim(49, 27) template/generic instantiation of `async` from here
+tasync_noasync.nim(51, 11) template/generic instantiation of `await` from here
 ../../lib/pure/asyncmacro.nim(136, 18) Error: Can only 'await' inside a proc marked as 'async'. Use 'waitFor' when calling an 'async' proc in a non-async scope instead
 stack trace: (most recent call last)
 asyncmacro.nim(136, 18)  customIterator
-tasync_noasync.nim(53, 9) template/generic instantiation of `await` from here
+tasync_noasync.nim(54, 9) template/generic instantiation of `await` from here
 ../../lib/pure/asyncmacro.nim(136, 18) Error: Can only 'await' inside a proc marked as 'async'. Use 'waitFor' when calling an 'async' proc in a non-async scope instead
 stack trace: (most recent call last)
 asyncmacro.nim(136, 18)  awaitInMacro
-tasync_noasync.nim(56, 9) template/generic instantiation of `await` from here
+tasync_noasync.nim(57, 9) template/generic instantiation of `await` from here
 ../../lib/pure/asyncmacro.nim(136, 18) Error: Can only 'await' inside a proc marked as 'async'. Use 'waitFor' when calling an 'async' proc in a non-async scope instead
 stack trace: (most recent call last)
 asyncmacro.nim(136, 18)  awaitInMethod
-tasync_noasync.nim(60, 9) template/generic instantiation of `await` from here
+tasync_noasync.nim(61, 9) template/generic instantiation of `await` from here
 ../../lib/pure/asyncmacro.nim(136, 18) Error: Can only 'await' inside a proc marked as 'async'. Use 'waitFor' when calling an 'async' proc in a non-async scope instead
 stack trace: (most recent call last)
 asyncmacro.nim(136, 18)  improperMultisync
-tasync_noasync.nim(62, 26) template/generic instantiation of `multisync` from here
-tasync_noasync.nim(63, 9) template/generic instantiation of `await` from here
+tasync_noasync.nim(63, 26) template/generic instantiation of `multisync` from here
+tasync_noasync.nim(64, 9) template/generic instantiation of `await` from here
 ../../lib/pure/asyncmacro.nim(136, 18) Error: Can only 'await' inside a proc marked as 'async'. Use 'waitFor' when calling an 'async' proc in a non-async scope instead
 stack trace: (most recent call last)
 asyncmacro.nim(136, 18)  tasync_noasync
-tasync_noasync.nim(65, 7) template/generic instantiation of `await` from here
+tasync_noasync.nim(66, 7) template/generic instantiation of `await` from here
 ../../lib/pure/asyncmacro.nim(136, 18) Error: Can only 'await' inside a proc marked as 'async'. Use 'waitFor' when calling an 'async' proc in a non-async scope instead
 '''
   nimoutFull: true
+  disabled: "win"
   file: "asyncmacro.nim"
 """
 import async
