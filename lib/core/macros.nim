43573338dbd7f20de09294a4e138afa34e949719
Juan Carlos <juancarlospaco@gmail.com>
2022-05-06 23:36:33 -0300

    .

--- lib/core/macros.nim
+++ lib/core/macros.nim
@@ -145,10 +145,6 @@ const
 
 {.push warnings: off.}
 
-proc toNimIdent*(s: string): NimIdent {.magic: "StrToIdent", noSideEffect, deprecated:
-  "Deprecated since version 0.18.0: Use 'ident' or 'newIdentNode' instead.".}
-  ## Constructs an identifier from the string `s`.
-
 proc `==`*(a, b: NimIdent): bool {.magic: "EqIdent", noSideEffect, deprecated:
   "Deprecated since version 0.18.1; Use '==' on 'NimNode' instead.".}
   ## Compares two Nim identifiers.
