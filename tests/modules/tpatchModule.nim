5b7baf91081368df729953ea068b4467f03f7693
quantimnot <quantimnot@users.noreply.github.com>
2022-07-10 17:28:20 -0400

    Change method of testing msgs in test

--- tests/modules/tpatchModule.nim
+++ tests/modules/tpatchModule.nim
@@ -1,6 +1,7 @@
 discard """
-  nimoutFull: true
+  nimoutFull: false
   joinable: false
+  varSub: true
   nimout: '''
 tpatchModule.nims(24, 12) Warning: cannot open: missingTarget_uasdygf8a7fg8uq23vfquoevfqo8ef [CannotOpen]
 tpatchModule.nims(27, 12) Warning: cannot open: missingPatch_uasdygf8a7fg8uq23vfquoevfqo8ef [CannotOpen]
@@ -8,10 +9,7 @@ tpatchModule.nims(30, 12) Warning: cannot open: missingTarget_uasdygf8a7fg8uq23v
 tpatchModule.nims(33, 12) Warning: cannot open:  [CannotOpen]
 tpatchModule.nims(36, 12) Warning: cannot open:  [CannotOpen]
 tpatchModule.nims(39, 12) Warning: cannot open:  [CannotOpen]
-tpatchModule.nim(25, 11) Hint: std/httpclient patched with tests/modules/mpatchModule [Patch]
-a/module_name_clashes.nim(3, 12) Hint: tests/modules/b/module_name_clashes patched with tests/modules/mpatchModule [Patch]
-tpatchModule.nim(37, 11) Hint: std/db_postgres patched with tests/modules/mpatchModule [Patch]
-tpatchModule.nim(42, 8) Hint: std/oids patched with tests/modules/mpatchModule [Patch]
+a${/}module_name_clashes.nim(3, 12) Hint: tests/modules/b/module_name_clashes patched with tests/modules/mpatchModule [Patch]
 '''
 """
 
@@ -22,7 +20,8 @@ tpatchModule.nim(42, 8) Hint: std/oids patched with tests/modules/mpatchModule [
 # * `mpatchModule.nim` is the module that patches the target modules.
 
 # Test patching foreign and `stdlib` modules:
-import std/httpclient
+import std/httpclient #[tt.Hint
+          ^ std/httpclient patched with tests/modules/mpatchModule [Patch] ]#
 var client = newHttpClient()
 doAssert client.getContent("https://example.com") == "patched!"
 
@@ -34,12 +33,14 @@ doAssert typeof(A.b) is int
 # Test patching absolute paths and patch paths that are relative to the configuring
 # script given in `tpatchModule.nims`. This also tests that an alias symbol is created
 # for the patching module (the `db_postgres.open` call would use the real module otherwise):
-import std/db_postgres
+import std/db_postgres #[tt.Hint
+          ^ std/db_postgres patched with tests/modules/mpatchModule [Patch] ]#
 let db = db_postgres.open("/run/postgresql", "user", "password", "database")
 doAssert db.getAllRows(sql"SELECT version();")[0][0] == "patched!"
 
 # Test patching an absolute import:
-import "$lib/pure/oids"
+import "$lib/pure/oids" #[tt.Hint
+       ^ std/oids patched with tests/modules/mpatchModule [Patch] ]#
 doAssert genOid() == genOid() # `genOid` is patched to always return the same value
 
 # # Test how `link` pragma directives are handled:
