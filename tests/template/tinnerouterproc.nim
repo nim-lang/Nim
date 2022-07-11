5b486924c518e6422dd7c354c8aacafa3ee40b38
hlaaftana <10591326+metagn@users.noreply.github.com>
2022-07-11 14:13:30 +0300

    add issue comment to test [skip ci]

--- tests/template/tinnerouterproc.nim
+++ tests/template/tinnerouterproc.nim
@@ -1,7 +1,8 @@
-proc bar(x: int): int = 10
-template foo =
-  proc bar(x: int): int {.inject.} = x + 2
-  doAssert bar(3) == 5
-  doAssert 3.bar == 5
-block:
-  foo()
+block: # #20000
+  proc bar(x: int): int = 10
+  template foo =
+    proc bar(x: int): int {.inject.} = x + 2
+    doAssert bar(3) == 5
+    doAssert 3.bar == 5
+  block:
+    foo()
