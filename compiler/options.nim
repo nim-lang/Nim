3fd11d7e96aaa654795a4e14eb6becd22f5294f1
flywind <43030857+xflywind@users.noreply.github.com>
2022-07-13 01:35:08 +0800

    fix #18735; genDepend broken for duplicate module names in separate folders (#19988)
    (cherry picked from commit 0180c6179aa0ff2dacaa6c063c0fa24cf459a84d)

--- compiler/options.nim
+++ compiler/options.nim
@@ -850,7 +850,7 @@ when (NimMajor, NimMinor) < (1, 1) or not declared(isRelativeTo):
     let ret = relativePath(path, base)
     result = path.len > 0 and not ret.startsWith ".."
 
-const stdlibDirs = [
+const stdlibDirs* = [
   "pure", "core", "arch",
   "pure/collections",
   "pure/concurrency",
