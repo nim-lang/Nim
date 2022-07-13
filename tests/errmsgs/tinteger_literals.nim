08f3fb44e9f2f3c2e0ec01416b8ef957c35e236f
quantimnot <quantimnot@users.noreply.github.com>
2022-07-13 10:54:13 -0400

    Fixed more broken tests

--- tests/errmsgs/tinteger_literals.nim
+++ tests/errmsgs/tinteger_literals.nim
@@ -1,15 +1,14 @@
 discard """
   cmd: "nim check $file"
 """
-# unhandled exception: 
 # high(uint64) + 1
 discard 18446744073709551616'u64 #[tt.Error
-        ^ unhandled exception: number out of range: 18446744073709551616 [OverflowDefect] ]#
+        ^ number out of range: '18446744073709551616'u64' ]#
 # high(int64) + 1
 discard 9223372036854775808'i64 #[tt.Error
-        ^ unhandled exception: number out of range: 9223372036854775808 [OverflowDefect] ]#
+        ^ number out of range: '9223372036854775808'i64' ]#
 # high(int64) + 1
 discard 9223372036854775808 #[tt.Error
-        ^ unhandled exception: number out of range: 9223372036854775808 [OverflowDefect] ]#
+        ^ number out of range: '9223372036854775808' ]#
 discard 300'u8 #[tt.Error
-        ^ unhandled exception: number out of range: 300 [OverflowDefect] ]#
+        ^ number out of range: '300'u8' ]#
