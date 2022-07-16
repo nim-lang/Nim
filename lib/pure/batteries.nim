6c7d999774dea21af63cdd1b45d14bb833ed0088
Angel Ezquerra <angel_ezquerra@keysight.com>
2022-07-17 01:48:04 +0200

    [batteries] add tests for the imported modules

--- lib/pure/batteries.nim
+++ lib/pure/batteries.nim
@@ -34,13 +34,58 @@ when defined(nimdoc) and isMainModule:
     ## don't use any if the modules it imports.
     runnableExamples:
       import std/batteries
-        # same as:
-        # import std/[os, strutils, times, parseutils, hashes, tables, sets,
-        #   sequtils, parseopt, strformat, sugar, options, strscans, algorithm,
-        #   math]
+      # same as:
+      # import std/[os, strutils, times, parseutils, hashes, tables, sets,
+      #   sequtils, parseopt, strformat, sugar, options, strscans, algorithm,
+      #   math]
+
+      # Test the different imports, starting with os
+      assert addFileExt("foo", "baz") == "foo.baz"
+
+      # Test strutils
       let x = 1
       assert "foo $# $#" % [$x, "bar"] == "foo 1 bar"
+
+      # Test times
+      let t = now()
+
+      # Test parseutils
+      assert parseInt("1") == 1
+
+      # Test hashes
+      let hs = hash("hash me!")
+
+      # Test tables
+      let tbl = {1: "one", 2: "two"}.toTable
+      assert tbl[2] == "two"
+
+      # Test sets
+      let st = toHashSet([3, 5, 7])
+
+      # Test sequtils
       assert toSeq(1..3) == @[1, 2, 3]
+
+      # Test strformat
+      assert &"{x}" == "1"
+
+      # Test options
+      let a = some(1)
+
+      # Test sugar
+      dump(x + 1)
+
+      # Test strscans
+      var y: int
+      assert scanf("(1)", "($i)", y)
+
+      # Test algorithm
+      var z = @[3, 2, 1]
+      z.sort() 
+      assert z == @[1, 2, 3]
+
+      # Test math
+      assert sqrt(4.0) == 2.0
+
       when not defined(js) or defined(nodejs):
         assert getCurrentDir().len > 0
         assert ($now()).startsWith "20"
