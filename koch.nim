edc3e47703fd91597e91297a0d0d938a9e11ecf7
xflywind <43030857+ringabout@users.noreply.github.com>
2022-07-28 18:36:37 +0800

    [wip] bump nimble hash

--- koch.nim
+++ koch.nim
@@ -10,7 +10,7 @@
 #
 
 const
-  NimbleStableCommit = "d13f3b8ce288b4dc8c34c219a4e050aaeaf43fc9" # master
+  NimbleStableCommit = "aef6f4fbc9ce810140db7061d8d524ba2e37ded9" # master
   # examples of possible values: #head, #ea82b54, 1.2.3
   FusionStableHash = "#372ee4313827ef9f2ea388840f7d6b46c2b1b014"
   HeadHash = "#head"
