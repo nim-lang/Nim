b43e89ebfbb9f9f7f4f43a69d86c5eaf287cf46a
ringabout <43030857+ringabout@users.noreply.github.com>
2022-08-08 18:57:04 +0800

    Update tests/vm/triangle_array.nim

--- tests/vm/triangle_array.nim
+++ tests/vm/triangle_array.nim
@@ -10,4 +10,4 @@ proc initCombinations: array[11, array[11, int]] =
   result[6][6 .. 10] =             [52,53,54,55,56]
 
 const combinations = initCombinations()
-doAssert $combinations[6][10] == "56"
+doAssert combinations[6][10] == 56
