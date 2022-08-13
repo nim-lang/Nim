d3d3b194afbb0d2c332bb58eab9f532145afe12c
hlaaftana <10591326+metagn@users.noreply.github.com>
2022-08-13 07:18:20 +0300

    fix quote

--- compiler/semexprs.nim
+++ compiler/semexprs.nim
@@ -2023,6 +2023,8 @@ proc expectString(c: PContext, n: PNode): string =
 
 proc newAnonSym(c: PContext; kind: TSymKind, info: TLineInfo): PSym =
   result = newSym(kind, c.cache.idAnon, nextSymId c.idgen, getCurrOwner(c), info)
+  if kind == skTemplate:
+    result.flags.add(sfTemplateRedefinition)
 
 proc semExpandToAst(c: PContext, n: PNode): PNode =
   let macroCall = n[1]
