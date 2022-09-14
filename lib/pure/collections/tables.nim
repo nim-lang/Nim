e2f679b8d3d7d856ba252127bbf870c84bebd562
unknown <crc32@qq.com>
2022-09-14 23:51:51 +0800

    use default computation

--- lib/pure/collections/tables.nim
+++ lib/pure/collections/tables.nim
@@ -2893,15 +2893,15 @@ iterator mvalues*[A](t: CountTableRef[A]): var int =
 
 proc hash*[K,V](s: Table[K,V]): Hash =
   for p in pairs(s):
-    result = result xor hash(p)
+    result = result !& hash(p)
   result = !$result
 
 proc hash*[K,V](s: OrderedTable[K,V]): Hash =
   for p in pairs(s):
-    result = result xor hash(p)
+    result = result !& hash(p)
   result = !$result
 
 proc hash*[V](s: CountTable[V]): Hash =
   for p in pairs(s):
-    result = result xor hash(p)
+    result = result !& hash(p)
   result = !$result
