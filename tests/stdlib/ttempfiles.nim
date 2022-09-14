76d3c5a84f50b63c8980cf1477fc197c63e22eff
xflywind <43030857+ringabout@users.noreply.github.com>
2022-09-14 13:45:07 +0800

    fixes three more tests

--- tests/stdlib/ttempfiles.nim
+++ tests/stdlib/ttempfiles.nim
@@ -4,7 +4,7 @@ discard """
 
 import std/tempfiles
 import std/[os, nre]
-import std/assertions
+import std/[assertions, syncio]
 
 const
   prefix = "D20210502T100442" # safety precaution to only affect files/dirs with this prefix
