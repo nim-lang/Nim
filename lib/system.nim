a2705520fb8b2ccc4389ea506197fbdcafaa6a9d
hlaaftana <10591326+metagn@users.noreply.github.com>
2022-09-14 17:33:56 +0300

    move back nim version, fix windows echo

--- lib/system.nim
+++ lib/system.nim
@@ -1842,6 +1842,22 @@ template unlikely*(val: bool): bool =
     else:
       unlikelyProc(val)
 
+const
+  NimMajor* {.intdefine.}: int = 1
+    ## is the major number of Nim's version. Example:
+    ##   ```
+    ##   when (NimMajor, NimMinor, NimPatch) >= (1, 3, 1): discard
+    ##   ```
+    # see also std/private/since
+
+  NimMinor* {.intdefine.}: int = 7
+    ## is the minor number of Nim's version.
+    ## Odd for devel, even for releases.
+
+  NimPatch* {.intdefine.}: int = 1
+    ## is the patch number of Nim's version.
+    ## Odd for devel, even for releases.
+
 import system/dollars
 export dollars
 
@@ -2576,6 +2592,8 @@ export widestrs
 
 when declared(cstdout):
   when defined(windows) and compileOption("threads"):
+    when not declared(addSysExitProc):
+      proc addSysExitProc(quitProc: proc() {.noconv.}) {.importc: "atexit", header: "<stdlib.h>".}
     var echoLock: SysLock
     initSysLock echoLock
     addSysExitProc(proc() {.noconv.} = deinitSys(echoLock))
