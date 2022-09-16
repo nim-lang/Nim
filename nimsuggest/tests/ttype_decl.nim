5ee63d9059122eaca19bb28855b2119648192504
hlaaftana <10591326+metagn@users.noreply.github.com>
2022-09-16 08:10:10 +0300

    actually fix CI finally hopefully

--- nimsuggest/tests/ttype_decl.nim
+++ nimsuggest/tests/ttype_decl.nim
@@ -3,7 +3,7 @@ $nimsuggest --tester --maxresults:3 $file
 >sug $1
 sug;;skType;;ttype_decl.Other;;Other;;$file;;10;;2;;"";;100;;None
 sug;;skType;;system.int;;int;;*lib/system/basic_types.nim;;2;;2;;"";;100;;None
-sug;;skType;;system.string;;string;;*lib/system/basic_types.nim;;34;;2;;"";;100;;None
+sug;;skType;;system.string;;string;;*lib/system/basic_types.nim;;23;;2;;"";;100;;None
 """
 import strutils
 type
