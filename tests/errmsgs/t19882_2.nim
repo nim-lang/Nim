0afc263c1c7320978e6a7cab405f94f899e7c57c
Bung <crc32@qq.com>
2022-09-22 07:36:41 +0800

    Update tests/errmsgs/t19882_2.nim

--- tests/errmsgs/t19882_2.nim
+++ tests/errmsgs/t19882_2.nim
@@ -1,5 +1,5 @@
 discard """
-  errormsg: "cannot instantiate: 'A[T]'; Maybe generic arguments are missing?"
+  errormsg: "cannot instantiate: 'A[T]'; the object's generic parameters cannot be inferred and must be explicitly given"
 """
 type A[T] = object
 var a = A()
