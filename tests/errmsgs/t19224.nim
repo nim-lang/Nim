c214f52758de118ea59eb0dc3459dcb2575cae2e
Bung <crc32@qq.com>
2022-10-02 01:21:45 +0800

    Update t19224.nim

--- tests/errmsgs/t19224.nim
+++ tests/errmsgs/t19224.nim
@@ -2,8 +2,9 @@ discard """
 cmd: "nim check --hints:off $file"
 errormsg: ""
 nimout: '''
-t19224.nim(9, 10) Error: cannot infer element type of items([])
-t19224.nim(11, 10) Error: cannot infer element type of items(@[])
+t19224.nim(10, 10) Error: cannot infer element type of items([])
+t19224.nim(12, 10) Error: cannot infer element type of items(@[])
+'''
 """
 
 for _ in []: discard
