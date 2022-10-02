36601cb8f3bb7409d34bff07d6a13cd584f77a42
unknown <crc32@qq.com>
2022-10-02 10:55:22 +0800

    add more test case

--- tests/errmsgs/t19546.nim
+++ tests/errmsgs/t19546.nim
@@ -3,12 +3,13 @@ discard """
   cmd: "nim check --hints:off $file"
   errormsg: ""
   nimout: '''
-t19546.nim(16, 5) Error: array expects two type parameters
-t19546.nim(20, 5) Error: sequence expects one type parameter
-t19546.nim(24, 5) Error: set expects one type parameter
-t19546.nim(27, 3) Error: cannot use sink as a field type
-t19546.nim(28, 3) Error: openArray expects one type parameter
-t19546.nim(29, 3) Error: lent expects one type parameter
+t19546.nim(17, 5) Error: array expects two type parameters
+t19546.nim(21, 5) Error: sequence expects one type parameter
+t19546.nim(25, 5) Error: set expects one type parameter
+t19546.nim(28, 3) Error: cannot use sink as a field type
+t19546.nim(29, 3) Error: openArray expects one type parameter
+t19546.nim(30, 3) Error: lent expects one type parameter
+t19546.nim(47, 16) Error: sequence expects one type parameter
 '''
 """
 type
@@ -27,3 +28,20 @@ type A = object
   b: sink
   a: openarray
   c: lent
+
+type PropertyKind = enum
+  tInt,
+  tFloat,
+  tBool,
+  tString,
+  tArray
+
+type
+  Property = ref PropertyObj
+  PropertyObj = object
+    case kind: PropertyKind
+    of tInt: intValue: int
+    of tFloat: floatValue: float
+    of tBool: boolValue: bool
+    of tString: stringValue: string
+    of tArray: arrayValue: seq
