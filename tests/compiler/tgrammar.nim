30176a192e2f9ae033d71e8fcc7b1750bc062162
xflywind <43030857+ringabout@users.noreply.github.com>
2022-10-06 19:34:08 +0800

    add nimTestGrammar

--- tests/compiler/tgrammar.nim
+++ tests/compiler/tgrammar.nim
@@ -1,17 +1,7 @@
-import std/pegs
-from stdtest/specialpaths import buildDir, testsDir
-import std/os
+discard """
+  matrix: "-d:nimTestGrammar"
+"""
 
-const secondFile = buildDir / "grammar.txt"
+import compiler/parser
 
-if not dirExists(buildDir):
-  createDir(buildDir)
-
-var outp = open(secondFile, fmWrite)
-for line in lines("compiler/parser.nim"):
-  if line =~ peg" \s* '#| ' {.*}":
-    outp.write matches[0], "\L"
-outp.close
-
-doAssert sameFileContent(secondFile, "doc/grammar.txt"),
-        "execute 'nim r compiler.nim' to keep grammar.txt up-to-date"
+checkSameGrammar()
