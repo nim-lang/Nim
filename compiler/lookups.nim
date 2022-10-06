a1ba245e1d58a2289c62fb1dc88bc0f91379014c
unknown <crc32@qq.com>
2022-10-06 15:26:59 +0800

    follow suggestion

--- compiler/lookups.nim
+++ compiler/lookups.nim
@@ -488,11 +488,11 @@ proc errorUseQualifier*(c: PContext; info: TLineInfo; s: PSym) =
   var amb: bool
   discard errorUseQualifier(c, info, s, amb)
 
-proc errorUseQualifier(c: PContext; info: TLineInfo; candidates: seq[PSym]) =
+proc errorUseQualifier(c: PContext; info: TLineInfo; candidates: seq[PSym]; prefix = "use one of") =
   var err = "ambiguous identifier: '" & candidates[0].name.s & "'"
   var i = 0
   for candidate in candidates:
-    if i == 0: err.add " -- use one of the following:\n"
+    if i == 0: err.add " -- $1 the following:\n" % prefix
     else: err.add "\n"
     err.add "  " & candidate.owner.name.s & "." & candidate.name.s
     err.add ": " & typeToString(candidate.typ)
@@ -501,9 +501,10 @@ proc errorUseQualifier(c: PContext; info: TLineInfo; candidates: seq[PSym]) =
 
 proc errorUseQualifier*(c: PContext; info:TLineInfo; choices: PNode) =
   var candidates = newSeq[PSym](choices.len)
+  let prefix = if choices[0].typ.kind != tyProc: "use one of" else: "you need a helper proc to disambiguate"
   for i, n in choices:
     candidates[i] = n.sym
-  errorUseQualifier(c, info, candidates)
+  errorUseQualifier(c, info, candidates, prefix)
 
 proc errorUndeclaredIdentifier*(c: PContext; info: TLineInfo; name: string, extra = "") =
   var err = "undeclared identifier: '" & name & "'" & extra
--- tests/errmsgs/t18886.nim
+++ tests/errmsgs/t18886.nim
@@ -2,7 +2,7 @@ discard """
   cmd: "nim check --hints:off $file"
   errormsg: ""
   nimout: '''
-t18886.nim(18, 24) Error: ambiguous identifier: 'bar' -- use one of the following:
+t18886.nim(18, 24) Error: ambiguous identifier: 'bar' -- you need a helper proc to disambiguate the following:
   t18886.bar: proc (i: ptr int){.noSideEffect, gcsafe, locks: 0.}
   t18886.bar: proc (i: ptr char){.noSideEffect, gcsafe, locks: 0.}
 '''
