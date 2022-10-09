16b42a9cf89126be8cc97c9b0b313a6296de7db5
Andreas Rumpf <rumpf_a@web.de>
2022-10-09 20:09:35 +0200

    Update lib/pure/base64.nim

--- lib/pure/base64.nim
+++ lib/pure/base64.nim
@@ -161,8 +161,7 @@ proc encode*[T: byte|char](s: openArray[T], safe = false): string =
     assert encode([1'u8, 2, 3, 4, 5]) == "AQIDBAU="
   encodeImpl()
 
-proc encode*[T: SomeInteger and not byte](s: openArray[T], safe = false): string  =
-  {.warning: "Deprecated Element type of `s`. use `byte` or `char` instead".}
+proc encode*[T: SomeInteger and not byte](s: openArray[T], safe = false): string {.deprecated: "use `byte` or `char` instead".} =
   encodeImpl()
 
 proc encodeMime*(s: string, lineLen = 75.Positive, newLine = "\r\n"): string =
