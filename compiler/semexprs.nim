db3d2971cf048b92c6a8420153da62f7bb907b21
Bung <crc32@qq.com>
2022-10-10 17:09:54 +0800

    fix #18886 crash on ambiguous proc cast (#20472)
    * fix #18886 crash on ambiguous proc cast
    
    * follow suggestion

--- compiler/semexprs.nim
+++ compiler/semexprs.nim
@@ -371,6 +371,8 @@ proc semCast(c: PContext, n: PNode): PNode =
   checkSonsLen(n, 2, c.config)
   let targetType = semTypeNode(c, n[0], nil)
   let castedExpr = semExprWithType(c, n[1])
+  if castedExpr.kind == nkClosedSymChoice:
+    errorUseQualifier(c, n[1].info, castedExpr)
   if tfHasMeta in targetType.flags:
     localError(c.config, n[0].info, "cannot cast to a non concrete type: '$1'" % $targetType)
   if not isCastable(c, targetType, castedExpr.typ, n.info):
