abebafa24b336e8e2d2dd4a4bfcabaf69e35018b
xflywind <43030857+ringabout@users.noreply.github.com>
2022-10-12 17:02:00 +0800

    fixes tests

--- tests/errmsgs/tconcisetypemismatch.nim
+++ tests/errmsgs/tconcisetypemismatch.nim
@@ -1,14 +1,16 @@
 discard """
-  cmd: "nim c --hints:off $file"
-  errormsg: "type mismatch: got <int64, int literal(1000)>"
+  cmd: "nim c --hints:off --undef:nimLegacyTypeMismatch $file"
+  errormsg: "type mismatch"
   nimout: '''
-tconcisetypemismatch.nim(21, 43) Error: type mismatch: got <int64, int literal(1000)>
+tconcisetypemismatch.nim(23, 43) Error: type mismatch
+Expression: inNanoseconds(t2 - t1) / 1000
+  [1] inNanoseconds(t2 - t1): int64
+  [2] 1000: int literal(1000)
+
 Expected one of (first mismatch at position [#]):
 [1] proc `/`(x, y: float): float
 [1] proc `/`(x, y: float32): float32
 [1] proc `/`(x, y: int): float
-
-expression: inNanoseconds(t2 - t1) / 1000
 '''
 """
 
