0f677bd42d77a400232810f6d5caa9d9c79a019d
jason <beefers331@gmail.com>
2022-10-17 15:03:06 -0600

    Added string tests

--- tests/vm/topenarrays.nim
+++ tests/vm/topenarrays.nim
@@ -4,6 +4,11 @@ proc mutate(a: var openarray[int]) =
     x = i
     inc i
 
+proc mutate(a: var openarray[char]) =
+  var i = 1
+  for ch in a.mitems:
+    ch = 'a'
+
 static:
   var a = [10, 20, 30]
   assert a.toOpenArray(1, 2).len == 2
@@ -12,3 +17,11 @@ static:
   assert a.toOpenArray(0, 2) == [0, 1, 2]
   assert a.toOpenArray(0, 0) == [0]
   assert a.toOpenArray(1, 2) == [1, 2]
+  assert "Hello".toOpenArray(1, 4) == "ello"
+  var str = "Hello"
+  str.toOpenArray(2, 4).mutate()
+  assert str == "Heaaa"
+  assert str.toOpenArray(0, 4) == "Heaaa"
+  assert str.toOpenArray(0, 4).len == 5
+  assert str.toOpenArray(0, 0).len == 1
+  assert str.toOpenArray(0, 0).high == 0
