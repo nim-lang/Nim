0faae4d5e29ea4eb55f3b53b1f01b91637f2c6c7
Andreas Rumpf <rumpf_a@web.de>
2022-10-21 22:46:32 +0200

    fixes a strict case object problem that has been reported on the forum (#20614)

--- compiler/guards.nim
+++ compiler/guards.nim
@@ -68,9 +68,12 @@ proc isLetLocation(m: PNode, isApprox: bool): bool =
     case n.kind
     of nkDotExpr, nkCheckedFieldExpr, nkObjUpConv, nkObjDownConv:
       n = n[0]
-    of nkDerefExpr, nkHiddenDeref:
+    of nkDerefExpr:
       n = n[0]
       inc derefs
+    of nkHiddenDeref:
+      n = n[0]
+      if not isApprox: inc derefs
     of nkBracketExpr:
       if isConstExpr(n[1]) or isLet(n[1]) or isConstExpr(n[1].skipConv):
         n = n[0]
--- tests/effects/tstrict_caseobjects.nim
+++ tests/effects/tstrict_caseobjects.nim
@@ -1,9 +1,29 @@
 discard """
   errormsg: "field access outside of valid case branch: x.x"
-  line: 25
+  line: 45
 """
 
 {.experimental: "strictCaseObjects".}
+
+type
+  NodeKind = enum
+    nkParent,
+    nkChild
+  
+  Node {.acyclic.} = ref object
+    case kind: NodeKind
+    of nkParent:
+      children: seq[Node]
+    of nkChild:
+      name: string
+
+let list = @[Node(kind: nkParent, children: @[]), Node(kind: nkChild, name: "hello")]
+for node in list:
+  case node.kind
+  of nkChild: 
+    echo $node.name # here this time there is a warning
+  else: discard
+
 
 type
   Foo = object
