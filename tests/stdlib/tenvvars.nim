ea5263944807c7886919eb9ac7464495c477c498
xflywind <43030857+ringabout@users.noreply.github.com>
2022-10-31 11:52:04 +0800

    fixes tests

--- tests/stdlib/tenvvars.nim
+++ tests/stdlib/tenvvars.nim
@@ -7,7 +7,10 @@ discard """
 import std/envvars
 from std/sequtils import toSeq
 import stdtest/testutils
-import std/[assertions, threads]
+import std/[assertions]
+
+when not defined(js):
+  import std/threads
 
 # "LATIN CAPITAL LETTER AE" in UTF-8 (0xc386)
 const unicodeUtf8 = "\xc3\x86"
