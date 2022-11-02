fa5ffc520341df9c5e4c616e9239733dacab4733
ringabout <43030857+ringabout@users.noreply.github.com>
2022-11-02 19:41:54 +0800

    fixes tests

--- compiler/semtypes.nim
+++ compiler/semtypes.nim
@@ -1313,7 +1313,7 @@ proc semProcTypeNode(c: PContext, n, genericParams: PNode,
             def.typ = makeTypeFromExpr(c, def.copyTree)
             break determineType
 
-        def = semExprWithType(c, def, {efDetermineType})
+        def = semExprWithType(c, def, {efDetermineType}, expectedType = typ)
         if def.referencesAnotherParam(getCurrOwner(c)):
           def.flags.incl nfDefaultRefsParam
 
