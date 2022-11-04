677509158e0a969752a0a4a24865efc9374d852e
n5m <72841454+n5m@users.noreply.github.com>
2022-11-04 00:00:01 +0000

    place test case in own block

--- tests/stdlib/tposix.nim
+++ tests/stdlib/tposix.nim
@@ -20,43 +20,44 @@ when not defined(windows):
   writeLine(stdout, u.machine)
 
   when not (defined(nintendoswitch) or defined(macos) or defined(macosx)):
-    type Message = object
-      value: int
-
-    const MQ_PATH: cstring = "/top_level_file"
-    const MQ_PRIORITY: cuint = 170
-    const MQ_MESSAGE_SIZE: csize_t = csize_t(sizeof(Message))
-
-    let mqd_a: posix.MqAttr = MqAttr(mq_maxmsg: 10, mq_msgsize: clong(MQ_MESSAGE_SIZE))
-    let writable: posix.Mqd = posix.mq_open(
-      MQ_PATH,
-      posix.O_CREAT or posix.O_WRONLY or posix.O_NONBLOCK,
-      posix.S_IRWXU,
-      addr(mqd_a)
-    )
-    let readable: posix.Mqd = posix.mq_open(
-      MQ_PATH,
-      posix.O_RDONLY or posix.O_NONBLOCK,
-      posix.S_IRWXU,
-      addr(mqd_a)
-    )
-
-    let sent: Message = Message(value: 88)
     block:
-      let success: int = writable.mq_send(
-        cast[cstring](sent.addr),
-        MQ_MESSAGE_SIZE,
-        MQ_PRIORITY
-      )
-      doAssert success == 0, $success
+      type Message = object
+        value: int
 
-    block:
-      var buffer: Message
-      var priority: cuint
-      let bytesRead: int = readable.mq_receive(
-        cast[cstring](buffer.addr),
-        MQ_MESSAGE_SIZE,
-        priority
+      const MQ_PATH: cstring = "/top_level_file"
+      const MQ_PRIORITY: cuint = 170
+      const MQ_MESSAGE_SIZE: csize_t = csize_t(sizeof(Message))
+
+      let mqd_a: posix.MqAttr = MqAttr(mq_maxmsg: 10, mq_msgsize: clong(MQ_MESSAGE_SIZE))
+      let writable: posix.Mqd = posix.mq_open(
+        MQ_PATH,
+        posix.O_CREAT or posix.O_WRONLY or posix.O_NONBLOCK,
+        posix.S_IRWXU,
+        addr(mqd_a)
       )
-      doAssert buffer == sent
-      doAssert bytesRead == int(MQ_MESSAGE_SIZE)
+      let readable: posix.Mqd = posix.mq_open(
+        MQ_PATH,
+        posix.O_RDONLY or posix.O_NONBLOCK,
+        posix.S_IRWXU,
+        addr(mqd_a)
+      )
+
+      let sent: Message = Message(value: 88)
+      block:
+        let success: int = writable.mq_send(
+          cast[cstring](sent.addr),
+          MQ_MESSAGE_SIZE,
+          MQ_PRIORITY
+        )
+        doAssert success == 0, $success
+
+      block:
+        var buffer: Message
+        var priority: cuint
+        let bytesRead: int = readable.mq_receive(
+          cast[cstring](buffer.addr),
+          MQ_MESSAGE_SIZE,
+          priority
+        )
+        doAssert buffer == sent
+        doAssert bytesRead == int(MQ_MESSAGE_SIZE)
