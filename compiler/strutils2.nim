8663cc200e2742c8557b71f2bf097ddc3df56b04
unknown <crc32@qq.com>
2022-11-04 09:58:27 +0800

    use std strutils toLowerAscii char

--- compiler/strutils2.nim
+++ compiler/strutils2.nim
@@ -8,6 +8,8 @@ Un-necessary allocations are avoided and appropriate algorithms are used at the
 expense of code clarity when justified.
 ]##
 
+from std/strutils import toLowerAscii
+
 proc dataPointer*[T](a: T): pointer =
   ## same as C++ `data` that works with std::string, std::vector etc.
   ## Note: safe to use when a.len == 0 but whether the result is nil or not
@@ -47,11 +49,5 @@ proc isUpperAscii(c: char): bool {.inline.} =
   c in {'A'..'Z'}
 
 proc toLowerAscii*(a: var string) =
-  ## optimized and inplace overload of strutils.toLowerAscii
-  # refs https://github.com/timotheecour/Nim/pull/54
-  # this is 10X faster than a naive implementation using a an optimization trick
-  # that can be adapted in similar contexts. Predictable writes avoid write
-  # hazards and lead to better machine code, compared to random writes arising
-  # from: `if c.isUpperAscii: c = ...`
   for c in mitems(a):
-    c = chr(c.ord + (if c.isUpperAscii: (ord('a') - ord('A')) else: 0))
+    c = toLowerAscii(c)
