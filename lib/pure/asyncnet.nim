b5ee8378f1098081fdcd1ede1f3c0f3d14c40f17
xflywind <43030857+xflywind@users.noreply.github.com>
2022-11-11 15:50:24 +0800

    another try another error

--- lib/pure/asyncnet.nim
+++ lib/pure/asyncnet.nim
@@ -247,7 +247,7 @@ when defineSsl:
       var data = await recv(socket.fd.AsyncFD, BufferSize, flags)
       let length = len(data)
       if length > 0:
-        let ret = bioWrite(socket.bioIn, addr data[0], length.cint)
+        let ret = bioWrite(socket.bioIn, cast[cstring](addr data[0]), length.cint)
         if ret < 0:
           raiseSSLError()
       elif length == 0:
