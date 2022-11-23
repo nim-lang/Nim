b79976bad8c901870275d605749523aa1b35ba57
Juan_Carlos <juancarlospaco@gmail.com>
2022-11-23 12:47:10 -0300

    Borrow addInt, AddFloat for Path

--- lib/std/paths.nim
+++ lib/std/paths.nim
@@ -39,7 +39,7 @@ func add(x: var string, tail: string) =
 
 func add*(x: var Path, y: Path) {.borrow.}
 func addInt*(x: var Path, y: int) {.borrow.}          # Path("/folder").addInt 42
-func addFloat*(x: var Path, y: SomeFloat) {.borrow.}  # Path("/version").addFloat 2.0
+# func addFloat*(x: var Path, y: SomeFloat) {.borrow.}  # Path("/version").addFloat 2.0
 
 func `/`*(head, tail: Path): Path {.inline.} =
   ## Joins two directory names to one.
