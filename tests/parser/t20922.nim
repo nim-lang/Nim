c6bd0afa105c6a66d6e0c81b0657b3aa3261cdc3
Vindaar <basti90@gmail.com>
2022-11-29 00:00:34 +0100

    update test case to reflect new code

--- tests/parser/t20922.nim
+++ tests/parser/t20922.nim
@@ -2,20 +2,30 @@ discard """
   cmd: "nim check $options $file"
   action: "reject"
   nimout: '''
-t20922.nim(27, 5) Error: an 'of' branch must be succeeded by an expression, but got ':'
-t20922.nim(27, 3) Error: illformed AST:
-of :
-  '+':
-    incDataPtrByte
-t20922.nim(35, 7) Error: an 'of' branch must be succeeded by an expression, but got ':'
-t20922.nim(35, 5) Error: illformed AST:
-of :
-  x: int
-t20922.nim(23, 6) Hint: 'mapInstrToToken' is declared but not used [XDeclaredButNotUsed]
-t20922.nim(33, 3) Hint: 'Foo' is declared but not used [XDeclaredButNotUsed]
+t20922.nim(18, 5) Error: expression expected, but found ':'
+Error: in expression ' '+'': identifier expected, but found ''
+t20922.nim(18, 7) Error: attempting to call undeclared routine: '<Error>'
+Error: in expression ' '+'': identifier expected, but found ''
+t20922.nim(18, 7) Error: attempting to call undeclared routine: '<Error>'
+t20922.nim(18, 7) Error: expression '' cannot be called
+t20922.nim(18, 7) Error: expression '' has no type (or is ambiguous)
+t20922.nim(18, 7) Error: VM problem: dest register is not set
+t20922.nim(26, 7) Error: expression expected, but found ':'
+t20922.nim(27, 5) Error: ':' or '=' expected, but got 'keyword of'
+t20922.nim(26, 9) Error: undeclared identifier: 'x'
+t20922.nim(26, 9) Error: expression 'x' has no type (or is ambiguous)
+Error: in expression ' x': identifier expected, but found ''
+t20922.nim(26, 9) Error: attempting to call undeclared routine: '<Error>'
+Error: in expression ' x': identifier expected, but found ''
+t20922.nim(26, 9) Error: attempting to call undeclared routine: '<Error>'
+t20922.nim(26, 9) Error: expression '' cannot be called
+t20922.nim(26, 9) Error: expression '' has no type (or is ambiguous)
+t20922.nim(26, 9) Error: VM problem: dest register is not set
+t20922.nim(14, 6) Hint: 'mapInstrToToken' is declared but not used [XDeclaredButNotUsed]
+t20922.nim(24, 3) Hint: 'Foo' is declared but not used [XDeclaredButNotUsed]
 '''
 """
-# original test case issue #t20922
+# original test case issue #20922
 type Token = enum
   incDataPtr,
   incDataPtrByte
