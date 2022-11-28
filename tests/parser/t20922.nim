acce6a51d8ff089d51f1c9f83dea81789e8bf181
Vindaar <basti90@gmail.com>
2022-11-28 11:17:36 +0100

    fix line numbers in test case

--- tests/parser/t20922.nim
+++ tests/parser/t20922.nim
@@ -2,17 +2,17 @@ discard """
   cmd: "nim check $options $file"
   action: "reject"
   nimout: '''
-t20922.nim(18, 5) Error: an 'of' branch must be succeeded by an expression, but got ':'
-t20922.nim(18, 3) Error: illformed AST:
+t20922.nim(27, 5) Error: an 'of' branch must be succeeded by an expression, but got ':'
+t20922.nim(27, 3) Error: illformed AST:
 of :
   '+':
     incDataPtrByte
-t20922.nim(26, 7) Error: an 'of' branch must be succeeded by an expression, but got ':'
-t20922.nim(26, 5) Error: illformed AST:
+t20922.nim(35, 7) Error: an 'of' branch must be succeeded by an expression, but got ':'
+t20922.nim(35, 5) Error: illformed AST:
 of :
   x: int
-t20922.nim(14, 6) Hint: 'mapInstrToToken' is declared but not used [XDeclaredButNotUsed]
-t20922.nim(24, 3) Hint: 'Foo' is declared but not used [XDeclaredButNotUsed]
+t20922.nim(23, 6) Hint: 'mapInstrToToken' is declared but not used [XDeclaredButNotUsed]
+t20922.nim(33, 3) Hint: 'Foo' is declared but not used [XDeclaredButNotUsed]
 '''
 """
 # original test case issue #t20922
