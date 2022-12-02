79ec6d677cd7d63e3e8e90101c30b1dc1103dcc9
metagn <metagngn@gmail.com>
2022-12-02 11:19:26 +0300

    Add back error message to tprevent_forloopvar_mutations (#20992)

--- tests/varres/tprevent_forloopvar_mutations.nim
+++ tests/varres/tprevent_forloopvar_mutations.nim
@@ -1,5 +1,14 @@
 discard """
   errormsg: "type mismatch: got <int>"
+  nimout: '''tprevent_forloopvar_mutations.nim(16, 7) Error: type mismatch: got <int>
+but expected one of:
+proc inc[T, V: Ordinal](x: var T; y: V = 1)
+  first type mismatch at position: 1
+  required type for x: var T: Ordinal
+  but expression 'i' is immutable, not 'var'
+
+expression: inc i
+'''
 """
 
 for i in 0..10:
