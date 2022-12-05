149c0ad46f559e1922b20984454d094e79c0fcc4
Yuriy Glukhov <yglukhov@users.noreply.github.com>
2022-12-05 22:49:31 +0100

    Dont produce initing moves for sfNoInit variables in closure env (#21026)
    (cherry picked from commit c47d5196be8485532fc585a5d0fb219247e17b60)

--- compiler/injectdestructors.nim
+++ compiler/injectdestructors.nim
@@ -879,7 +879,10 @@ proc p(n: PNode; c: var Con; s: var Scope; mode: ProcessMode): PNode =
             if ri.kind != nkEmpty:
               result.add moveOrCopy(v, ri, c, s, isDecl = v.kind == nkSym)
             elif ri.kind == nkEmpty and c.inLoop > 0:
-              result.add moveOrCopy(v, genDefaultCall(v.typ, c, v.info), c, s, isDecl = v.kind == nkSym)
+              let skipInit = v.kind == nkDotExpr and # Closure var
+                             sfNoInit in v[1].sym.flags
+              if not skipInit:
+                result.add moveOrCopy(v, genDefaultCall(v.typ, c, v.info), c, s, isDecl = v.kind == nkSym)
         else: # keep the var but transform 'ri':
           var v = copyNode(n)
           var itCopy = copyNode(it)
