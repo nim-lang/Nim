d6cb271dc3250668ad61968519e9594eb75a5efc
Mutsuha Asada <momiimt@gmail.com>
2022-12-06 22:25:32 +0900

    add: testcase for multiline proc expr

---
+++ nimpretty/tests/multilines_proc_expr.nim
@@ -0,0 +1,17 @@
+import std/sequtils
+
+block:
+  block:
+    var _ = 0
+    proc f (): int {.used.} =
+                    return 0
+
+let _ = "foo,bar,baz"
+  .map(proc (c: char): char = c)
+  .map(
+    proc (c: char): char =
+          return c
+  )
+
+proc f: bool {.used.} =
+  return true
