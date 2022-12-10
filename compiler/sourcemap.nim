57d52ab1f2f1447d5e8be58523161e71b50081fc
Jake Leahy <jake@leahy.dev>
2022-12-10 12:37:21 +1100

    Properly handle columns

--- compiler/sourcemap.nim
+++ compiler/sourcemap.nim
@@ -1,18 +1,26 @@
-import os, strformat, strutils, tables, sets, ropes, json, strscans, std/options
+import std/[strformat, strutils, ropes, strscans, parseutils, assertions]
 
 type
-  SourceInfo = object
-    nodes: seq[Node]
-    names, files: seq[string]
+  Segment = object
+    ## Segment refers to a block of something in the JS output.
+    ## This could be a token or an entire line
+    original: int # Column in the Nim source
+    generated: int # Column in the generated JS
+    name: int # Index into names list (-1 for no name)
 
-  Node* = object
-    generated: int # Line in generated JS
-    case inSource: bool # Whether the line in generated has nim equivilant
+  Mapping = object
+    ## Mapping refers to a line in the JS output.
+    ## It is made up of segments which refer to the tokens in the line
+    case inSource: bool # Whether the line in JS has Nim equivilant
     of true:
-      original: int # Line in original, we don't have column info
       file: int # Index into files list
-      name: int # Index into names list (-1 for no name)
+      line: int # 0 indexed line of code in the Nim source
+      segments: seq[Segment]
     else: discard
+
+  SourceInfo = object
+    mappings: seq[Mapping]
+    names, files: seq[string]
 
   SourceMap* = object
     version*:   int
@@ -21,25 +29,35 @@ type
     mappings*:  string
     file*:      string
 
-func addNode(info: var SourceInfo, generated: int) =
-  ## Create node which doesn't appear in Nim code
-  info.nodes &= Node(generated: generated, inSource: false)
-
-func addNode*(info: var SourceInfo, generated, original: int, file: string, name = "") =
-  ## Create a node which does appear in Nim code
-  var node = Node(generated: generated, original: original, inSource: true)
-  # Set file to file position. Add in if needed
-  node.file = info.files.find(file)
-  if node.file == -1:
-    node.file = info.files.len
-    info.files &= file
-  # Do same for name if one is actually passed
+func addSegment(info: var SourceInfo, original, generated: int, name: string = "") {.raises: [].} =
+  ## Adds a new segment into the current line
+  assert info.mappings.len > 0, "No lines have been added yet"
+  var segment = Segment(original: original, generated: generated, name: -1)
   if name != "":
-    node.name = info.names.find(name)
-    if node.name == -1:
-      node.name = info.names.len
+    # Make name be index into names list
+    segment.name = info.names.find(name)
+    if segment.name == -1:
+      segment.name = info.names.len
       info.names &= name
-  info.nodes &= node
+
+  assert info.mappings[^1].inSource, "Current line isn't in Nim source"
+  info.mappings[^1].segments &= segment
+
+func newLine(info: var SourceInfo) {.raises: [].} =
+  ## Add new mapping which doesn't appear in the Nim source
+  info.mappings &= Mapping(inSource: false)
+
+func newLine(info: var SourceInfo, file: string, line: int) {.raises: [].} =
+  ## Starts a new line in the mappings. Call addSegment after this to add
+  ## segments into the line
+  var mapping = Mapping(inSource: true, line: line)
+  # Set file to file position. Add in if needed
+  mapping.file = info.files.find(file)
+  if mapping.file == -1:
+    mapping.file = info.files.len
+    info.files &= file
+  info.mappings &= mapping
+
 
 # base64_VLQ
 func encode*(values: seq[int]): string {.raises: [].} =
@@ -73,78 +91,76 @@ func encode*(values: seq[int]): string {.raises: [].} =
       if newVal == 0:
         break
 
-type TokenState = enum Normal, String, Ident, Mangled
+iterator tokenize*(line: string): (int, string) =
+  ## Goes through a line and splits it into Nim identifiers and
+  ## normal JS code. This allows us to map mangled names back to Nim names.
+  ## Yields (column, name). Doesn't yield anything but identifiers.
+  ## See mangleName in compiler/jsgen.nim for how name mangling is done
+  var
+    col = 0
+    token = ""
+  var times = 0
+  while col < line.len:
+    var
+      token: string
+      name: string
+    # First we find the next identifier
+    col += line.skipWhitespace(col)
+    col += line.skipUntil(IdentStartChars, col)
+    let identStart = col
+    col += line.parseIdent(token, col)
+    if times > 20:
+      echo line
+      quit 1
+    # Idents will either be originalName_randomInt or HEXhexCode_randomInt
+    if token.startsWith("HEX"):
+      var hex: int
+      # 3 = "HEX".len and we only want to parse the two integers after it
+      discard token[3 ..< 5].parseHex(hex)
+      name = $chr(hex)
+    elif not token.endsWith("_Idx"): # Ignore address indexes
+      # It might be in the form originalName_randomInt
+      let lastUnderscore = token.rfind('_')
+      if lastUnderscore != -1:
+        name = token[0..<lastUnderscore]
+    if name != "":
+      yield (identStart, name)
+    times += 1
 
-iterator tokenize*(line: string): (bool, string) =
-  # result = @[]
-  var state = Normal
-  var token = ""
-  var isMangled = false
-  for z, ch in line:
-    if ch.isAlphaAscii:
-      if state == Normal:
-        state = Ident
-        if token.len > 0:
-          yield (isMangled, token)
-        token = $ch
-        isMangled = false
-      else:
-        token.add(ch)
-    elif ch == '_':
-      if state == Ident:
-        state = Mangled
-        isMangled = true
-      token.add($ch)
-    elif ch != '"' and not ch.isAlphaNumeric:
-      if state in {Ident, Mangled}:
-        state = Normal
-        if token.len > 0:
-          yield (isMangled, token)
-        token = $ch
-        isMangled = false
-      else:
-        token.add($ch)
-    elif ch == '"':
-      if state != String:
-        state = String
-        if token.len > 0:
-          yield (isMangled, token)
-        token = $ch
-        isMangled = false
-      else:
-        state = Normal
-        token.add($ch)
-        if token.len > 0:
-          yield (isMangled, token)
-        isMangled = false
-        token = ""
-    else:
-      token.add($ch)
-  if token.len > 0:
-    yield (isMangled, token)
-
-proc parse*(source, path: string): SourceInfo =
-  let lines = source.splitLines()
-  # The JS file has header information that we can't map
-  var lastLocation: tuple[file: string, line: int] = ("", -1)
-  # TODO: Tokenise so we get
+proc parse*(source: string): SourceInfo =
+  ## Parses the JS output for embedded line info
+  ## So it can convert those into a series of mappings
+  var
+    skipFirstLine = true
+    currColumn = 0
+    currLine = 0
+    currFile = ""
   # Add each line as a node into the output
-  for i, originalLine in lines:
-    let line = originalLine.strip
+  for line in source.splitLines():
     var
       lineNumber: int
       linePath: string
-    if line.scanf("/* line $i \"$+\" */", lineNumber, linePath):
+      column: int
+    if line.strip().scanf("/* line $i:$i \"$+\" */", lineNumber, column, linePath):
+      # When we reach the first line mappinsegmentg then we can assume
+      # we can map the rest of the JS lines to Nim lines
+      currColumn = column # Column is already zero indexed
+      currLine = lineNumber - 1
+      currFile = linePath
       # Lines are zero indexed
-      lastLocation = (linePath, lineNumber - 1)
-      result.addNode(i)
-    elif lastLocation.line != -1:
-      # TODO: Tokenisation
-      result.addNode(i, lastLocation.line, lastLocation.file)
+      result.newLine(currFile, currLine)
+      # Skip whitespace to find the starting column
+      result.addSegment(currColumn, line.skipWhitespace())
+    elif currFile != "":
+      result.newLine(currFile, currLine)
+      # There mightn't be any tokens so add a starting segment
+      result.addSegment(currColumn, line.skipWhitespace())
+      for jsColumn, token in line.tokenize:
+        result.addSegment(currColumn, jsColumn, token)
     else:
-      result.addNode(i)
+      result.newLine()
 
-proc toSourceMap*(info: SourceInfo, file: string): SourceMap =
+func toSourceMap*(info: SourceInfo, file: string): SourceMap {.raises: [].} =
   ## Convert from high level SourceInfo into the required SourceMap object
   # Add basic info
   result.version = 3
@@ -160,24 +176,37 @@ proc toSourceMap*(info: SourceInfo, file: string): SourceMap =
     prevFile = 0
     prevLine = 0
     prevName = 0
-
-  for node in info.nodes:
-    # We know need to encode the node info into a segment with following fields
+    prevNimCol = 0
+  debugEcho info.mappings.len
+  for mapping in info.mappings:
+    # We know need to encode segments with the following fields
     # All these fields are relative to their previous values
     # - 0: Column in generated code
     # - 1: Index of Nim file in source list
     # - 2: Line in Nim source
     # - 3: Column in Nim source
     # - 4: Index in names list
-    if node.inSource:
-      result.mappings &= encode(@[0, node.file - prevFile, node.original - prevLine, 0]) & ";"
-      prevFile = node.file
-      prevLine = node.original
-    else:
-      result.mappings &= ";"
+    if mapping.inSource:
+      # JS Column is special in that it is reset after every line
+      var prevJSCol = 0
+      for segment in mapping.segments:
+        var values = @[segment.generated - prevJSCol, mapping.file - prevFile, mapping.line - prevLine, segment.original - prevNimCol]
+        # Add name field if needed
+        if segment.name != -1:
+          values &= segment.name - prevName
+          prevName = segment.name
+        prevJSCol = segment.generated
+        prevNimCol = segment.original
+        prevFile = mapping.file
+        prevLine = mapping.line
+        result.mappings &= encode(values) & ","
+      # Remove trailing ,
+      if mapping.segments.len > 0:
+        result.mappings.setLen(result.mappings.len - 1)
 
-proc genSourceMap*(source: string, outFile: string): (Rope, SourceMap) =
-  let node = parse(source, outFile)
-  let map = node.toSourceMap(outFile)
-  ((&"{source}\n//# sourceMappingURL={outFile}.map").rope, map)
+    result.mappings &= ";"
+
+proc genSourceMap*(source: string, outFile: string): SourceMap =
+  let node = parse(source)
+  result = node.toSourceMap(outFile)
 
