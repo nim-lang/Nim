3840474b20418fb9e89b9cff324d90f2f1314774
ringabout <43030857+ringabout@users.noreply.github.com>
2022-12-10 14:55:41 +0800

    ship a modern nimble with lock files support

--- koch.nim
+++ koch.nim
@@ -10,7 +10,7 @@
 #
 
 const
-  NimbleStableCommit = "d13f3b8ce288b4dc8c34c219a4e050aaeaf43fc9" # master
+  NimbleStableCommit = "85c9df5856c886d3b8b07f3c2d0005602a5e38ae" # master
   # examples of possible values: #head, #ea82b54, 1.2.3
   FusionStableHash = "#372ee4313827ef9f2ea388840f7d6b46c2b1b014"
   HeadHash = "#head"
