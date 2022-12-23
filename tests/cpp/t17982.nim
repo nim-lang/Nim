4e039b73e2d214c6f89d7d45d3728d927a8430a8
unknown <crc32@qq.com>
2022-12-23 23:01:53 +0800

    add test

---
+++ tests/cpp/t17982.nim
@@ -0,0 +1,20 @@
+discard """
+  targets: "cpp"
+  action: "compile"
+"""
+
+type
+  String* {.importcpp: "std::string", header: "string".} = object
+
+proc initString*(): String
+    {.importcpp: "std::string()", header: "string".}
+
+proc append*(this: var String, str: String): var String
+    # bug seems to trigger when `#`, `@`, or `$1` is used inside `importcpp`
+    {.importcpp: "#.append(@)", header: "string", discardable.} # <- changed from `importcpp: "append"`
+
+var
+  s1 = initString()
+  s2 = initString()
+
+s1.append s2
