95f355c0dd757218092f4f333821a08d93a7379e
unknown <crc32@qq.com>
2022-12-25 13:01:33 +0800

    fix #20883 Unspecified generic on default value segfaults the compiler

---
+++ tests/misc/t20883.nim
@@ -0,0 +1,11 @@
+discard """
+  action: reject
+  errormsg: "type mismatch: got <float64> but expected 'typeof(U(0.000001))'"
+  line: 7
+"""
+
+proc foo*[U](x: U = U(1e-6)) =
+  echo x
+
+foo[float]()
+foo()
