b6e92c031cea4dc2381c69f0bcdb8083e7c095eb
ringabout <43030857+ringabout@users.noreply.github.com>
2022-12-28 16:32:16 +0800

    fixes i386

--- tests/errmsgs/tconcisetypemismatch.nim
+++ tests/errmsgs/tconcisetypemismatch.nim
@@ -2,9 +2,9 @@ discard """
   cmd: "nim c --hints:off --skipParentCfg $file"
   errormsg: "type mismatch"
   nimout: '''
-tconcisetypemismatch.nim(23, 43) Error: type mismatch
-Expression: inNanoseconds(t2 - t1) / 100.5
-  [1] inNanoseconds(t2 - t1): int64
+tconcisetypemismatch.nim(23, 49) Error: type mismatch
+Expression: int64(inNanoseconds(t2 - t1)) / 100.5
+  [1] int64(inNanoseconds(t2 - t1)): int64
   [2] 100.5: float64
 
 Expected one of (first mismatch at position [#]):
@@ -20,4 +20,4 @@ from times import inNanoseconds
 let t1 = getMonotime()
 let result = 1 + 2
 let t2 = getMonotime()
-echo "Elapsed: ", (t2 - t1).inNanoseconds / 100.5
+echo "Elapsed: ", (t2 - t1).inNanoseconds.int64 / 100.5
