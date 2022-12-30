2863c7dfbf98014af399d4ac502b891a7fbc3b14
David Anes <david.anes@suse.com>
2022-12-30 09:37:26 +0100

    Bare excepts are deprecated, fix a warning in unittest

--- lib/pure/unittest.nim
+++ lib/pure/unittest.nim
@@ -550,7 +550,7 @@ template test*(name, body) {.dirty.} =
         defer: testTeardownIMPL()
       body
 
-    except:
+    except CatchableError:
       let e = getCurrentException()
       let eTypeDesc = "[" & exceptionTypeName(e) & "]"
       checkpoint("Unhandled exception: " & getCurrentExceptionMsg() & " " & eTypeDesc)
