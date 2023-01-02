bc33d5606b1e4b4950a324a1cabce1a825dd9a30
unknown <crc32@qq.com>
2023-01-02 15:11:08 +0800

    fix tests/parser/t20922.nim

--- tests/parser/t20922.nim
+++ tests/parser/t20922.nim
@@ -8,7 +8,7 @@ t20922.nim(37, 7) Error: attempting to call undeclared routine: '<Error>'
 Error: in expression ' '+'': identifier expected, but found ''
 t20922.nim(37, 7) Error: attempting to call undeclared routine: '<Error>'
 t20922.nim(37, 7) Error: expression '' cannot be called
-t20922.nim(37, 7) Error: expression '' has no type (or is ambiguous)
+t20922.nim(37, 7) Error: expression ' '+'' has no type (or is ambiguous)
 t20922.nim(37, 7) Error: VM problem: dest register is not set
 t20922.nim(45, 7) Error: expression expected, but found ':'
 t20922.nim(46, 5) Error: ':' or '=' expected, but got 'keyword of'
@@ -19,7 +19,7 @@ t20922.nim(45, 9) Error: attempting to call undeclared routine: '<Error>'
 Error: in expression ' x': identifier expected, but found ''
 t20922.nim(45, 9) Error: attempting to call undeclared routine: '<Error>'
 t20922.nim(45, 9) Error: expression '' cannot be called
-t20922.nim(45, 9) Error: expression '' has no type (or is ambiguous)
+t20922.nim(45, 9) Error: expression ' x' has no type (or is ambiguous)
 t20922.nim(45, 9) Error: VM problem: dest register is not set
 t20922.nim(33, 6) Hint: 'mapInstrToToken' is declared but not used [XDeclaredButNotUsed]
 t20922.nim(43, 3) Hint: 'Foo' is declared but not used [XDeclaredButNotUsed]
