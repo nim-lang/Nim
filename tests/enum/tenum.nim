31e1c32226e420240816697856a06695eca9f8a3
ringabout <43030857+ringabout@users.noreply.github.com>
2023-01-20 23:13:01 +0800

    Update tests/enum/tenum.nim

--- tests/enum/tenum.nim
+++ tests/enum/tenum.nim
@@ -176,13 +176,11 @@ block: # bug #12589
 
     doAssert $typ() == "wkbPoint25D"
 
-block: # bug #21280
-  type
-    Test = enum
-      B = 19
-      A = int64.high()
-
-  when not defined(i386):
+when not defined(i386):
+  block: # bug #21280
+    type
+      Test = enum
+        B = 19
+        A = int64.high()
+  
     doAssert ord(A) == int64.high()
-  else:
-    discard A
