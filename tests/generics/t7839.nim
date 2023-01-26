7494ea8fb3590ccd952489f810ca7235358472a2
ringabout <43030857+ringabout@users.noreply.github.com>
2023-01-26 09:39:02 +0800

    Apply suggestions from code review

--- tests/generics/t7839.nim
+++ tests/generics/t7839.nim
@@ -1,12 +1,9 @@
-discard """
-action: compile
-"""
 # the following code will break compilation without any error or hint
 # `exited with code=3221225725`
 type A[I: SomeOrdinal, E] = tuple # same for object
   length: int
  
-echo A.sizeof # works without the following proc
+doAssert A.sizeof == 8 # works without the following proc
  
 proc newA*[I: SomeOrdinal, E](): A[I, E] = # works without `SomeOrdinal`
   discard
