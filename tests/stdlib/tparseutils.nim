d943c7791113d1266ab0736c2ec9e8ace495d842
Charles Blake <charlechaud@gmail.com>
2023-02-11 07:36:36 -0500

    Lift 2-result matching into a `checkParseSize` template and format as a table of input & 2 expected outputs which seems nicer and to address https://github.com/nim-lang/Nim/pull/21349#pullrequestreview-1294407679

--- tests/stdlib/tparseutils.nim
+++ tests/stdlib/tparseutils.nim
@@ -61,36 +61,41 @@ static: test()
 
 block:  # With this included, static: test() crashes the compiler (from a
         # VM problem with parseSize calling parseFloat).
-  template ass(x) = doAssert(x) # Avoid hint[LineTooLong]
   var sz: int64
+  template checkParseSize(s, expectLen, expectVal) =
+    if (let got = parseSize(s, sz); got != expectLen):
+      raise newException(IOError, "got len " & $got & " != " & $expectLen)
+    if sz != expectVal:
+      raise newException(IOError, "got sz " & $sz & " != " & $expectVal)
+  #              STRING    LEN SZ
   # Good, complete parses
-  ass parseSize("1  b", sz)    == 4; ass sz == 1
-  ass parseSize("1  B", sz)    == 4; ass sz == 1
-  ass parseSize("1k", sz)      == 2; ass sz == 1000
-  ass parseSize("1 kib", sz)   == 5; ass sz == 1024
-  ass parseSize("1 ki", sz)    == 4; ass sz == 1024
-  ass parseSize("1mi", sz)     == 3; ass sz == 1048576
-  ass parseSize("1 mi", sz)    == 4; ass sz == 1048576
-  ass parseSize("1 mib", sz)   == 5; ass sz == 1048576
-  ass parseSize("1 Mib", sz)   == 5; ass sz == 1048576
-  ass parseSize("1 MiB", sz)   == 5; ass sz == 1048576
-  ass parseSize("1.23GiB", sz) == 7; ass sz == 1320702444 # 1320702443.52
-  ass parseSize("0.001k", sz)  == 6; ass sz == 1
-  ass parseSize("0.0004k", sz) == 7; ass sz == 0
-  ass parseSize("0.0006k", sz) == 7; ass sz == 1
+  checkParseSize "1  b"   , 4, 1
+  checkParseSize "1  B"   , 4, 1
+  checkParseSize "1k"     , 2, 1000
+  checkParseSize "1 kib"  , 5, 1024
+  checkParseSize "1 ki"   , 4, 1024
+  checkParseSize "1mi"    , 3, 1048576
+  checkParseSize "1 mi"   , 4, 1048576
+  checkParseSize "1 mib"  , 5, 1048576
+  checkParseSize "1 Mib"  , 5, 1048576
+  checkParseSize "1 MiB"  , 5, 1048576
+  checkParseSize "1.23GiB", 7, 1320702444 # 1320702443.52 rounded
+  checkParseSize "0.001k" , 6, 1
+  checkParseSize "0.0004k", 7, 0
+  checkParseSize "0.0006k", 7, 1
   # Incomplete parses
-  ass parseSize("1  ", sz)     == 1; ass sz == 1  # Trailing white IGNORED
-  ass parseSize("1  B ", sz)   == 4; ass sz == 1  # Trailing white IGNORED
-  ass parseSize("1  B/s", sz)  == 4; ass sz == 1  # Trailing junk IGNORED
-  ass parseSize("1 kX", sz)    == 3; ass sz == 1000
-  ass parseSize("1 kiX", sz)   == 4; ass sz == 1024
-  ass parseSize("1j", sz)      == 1; ass sz == 1  # Unknown prefix
-  ass parseSize("1 jib", sz)   == 2; ass sz == 1  # ..also IGNORED
-  ass parseSize("1  ji", sz)   == 3; ass sz == 1
-  # Bad parses; `sz` should stay last good|incomplete val
-  ass parseSize("-1b", sz)     == 0; ass sz == 1  # Negative numbers
-  ass parseSize("abc", sz)     == 0; ass sz == 1  # Non-numeric
-  ass parseSize(" 12", sz)     == 0; ass sz == 1  # Leading white
+  checkParseSize "1  "    , 1, 1          # Trailing white IGNORED
+  checkParseSize "1  B "  , 4, 1          # Trailing white IGNORED
+  checkParseSize "1  B/s" , 4, 1          # Trailing junk IGNORED
+  checkParseSize "1 kX"   , 3, 1000
+  checkParseSize "1 kiX"  , 4, 1024
+  checkParseSize "1j"     , 1, 1          # Unknown prefix IGNORED
+  checkParseSize "1 jib"  , 2, 1          # Unknown prefix post space
+  checkParseSize "1  ji"  , 3, 1
+  # Bad parses; `sz` should stay last good|incomplete value
+  checkParseSize "-1b"    , 0, 1          # Negative numbers
+  checkParseSize "abc"    , 0, 1          # Non-numeric
+  checkParseSize " 12"    , 0, 1          # Leading white
   # Value Edge cases
   when not defined(i386): # Transition/rounding value may need adjustment
-    ass parseSize("9223372036854775807", sz) == 19; ass sz == int64.high
+    checkParseSize "9223372036854775807", 19, int64.high
