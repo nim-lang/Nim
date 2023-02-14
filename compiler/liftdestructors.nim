2fac8dbbe540c544aaec2f503f8ab003395bc850
ringabout <43030857+ringabout@users.noreply.github.com>
2023-02-14 12:30:40 +0800

    fixes comments about type bound operations

--- compiler/liftdestructors.nim
+++ compiler/liftdestructors.nim
@@ -8,7 +8,7 @@
 #
 
 ## This module implements lifting for type-bound operations
-## (``=sink``, ``=``, ``=destroy``, ``=deepCopy``).
+## (``=sink``, ``=copy``, ``=destroy``, ``=deepCopy``).
 
 import modulegraphs, lineinfos, idents, ast, renderer, semdata,
   sighashes, lowerings, options, types, msgs, magicsys, tables, ccgutils
