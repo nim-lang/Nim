3f4d08c698d4be2f1cb39fd9ba7a29bed3f49af2
Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>
2023-02-17 17:19:36 +0900

    async: NuttX added supports getrlimit(RLIMIT_NOFILE), so remove NuttX-specific codes.
    Signed-off-by: Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>

--- lib/pure/asyncdispatch.nim
+++ lib/pure/asyncdispatch.nim
@@ -2033,9 +2033,6 @@ when defined(linux) or defined(windows) or defined(macosx) or defined(bsd) or
       result = 16_700_000
     elif defined(zephyr) or defined(freertos):
       result = FD_MAX
-    elif defined(nuttx):
-      # The maximum number of concurrently active UDP and TCP ports.
-      result = NACTIVESOCKETS
     else:
       var fdLim: RLimit
       if getrlimit(RLIMIT_NOFILE, fdLim) < 0:
