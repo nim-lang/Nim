19fc5393fc6fb380eb9f680369b81ae26c2544d0
Jake Leahy <jake@leahy.dev>
2023-02-20 09:31:13 +1100

    Clean up test program so it isn't using diff
    Also don't redirect the first nim compile to /dev/null so that we can check for any compilation errors
    
    I found the diff to be annoying to read (Seeing as the test script already performs diffing)

--- tests/untestable/gdb/gdb_pretty_printer_test_run.sh
+++ tests/untestable/gdb/gdb_pretty_printer_test_run.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
-# Exit if anything fails
 set -e
-#!/usr/bin/env bash
 # Compile the test project with fresh debug information.
-nim c --debugger:native --mm:orc --out:gdbNew gdb_pretty_printer_test_program.nim &> /dev/null
+nim c --debugger:native --mm:orc --out:gdbNew gdb_pretty_printer_test_program.nim
 # 2>&1 redirects stderr to stdout (all output in stdout)
 # <(...) is a bash feature that makes the output of a command into a
 # file handle.
-# diff compares the two files, the expected output, and the file
-# handle that is created by the execution of gdb.
-diff ./new_output.txt <(gdb -x gdb_pretty_printer_test.py --batch-silent --args gdbNew 2>&1)
-# The exit code of diff is forwarded as the exit code of this
-# script. So when the comparison fails, the exit code of this script
-# won't be 0. So this script should be embeddable in a test suite.
+gdb -x gdb_pretty_printer_test.py --batch-silent --args gdbNew 2>&1
+
 
 # Do it all again, but with old runtime
 nim c --debugger:native --mm:refc --out:gdbOld gdb_pretty_printer_test_program.nim &> /dev/null
-diff ./old_output.txt <(gdb -x gdb_pretty_printer_test.py --batch-silent --args gdbOld 2>&1)
+gdb -x gdb_pretty_printer_test.py --batch-silent --args gdbOld 2>&1
