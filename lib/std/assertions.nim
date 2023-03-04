c2d134c7ef1ad5ea268d4c60d31aa4a454fee14b
quantimnot <quantimnot@users.noreply.github.com>
2023-03-04 15:27:04 -0500

    refact: Remove asseertion effect hiding workaround
    There was a code comment to remove after bootstrapping with `nim >= 1.4.0`.

--- lib/std/assertions.nim
+++ lib/std/assertions.nim
@@ -38,12 +38,7 @@ proc raiseAssert*(msg: string) {.noinline, noreturn, nosinks.} =
 proc failedAssertImpl*(msg: string) {.raises: [], tags: [].} =
   ## Raises an `AssertionDefect` with `msg`, but this is hidden
   ## from the effect system. Called when an assertion failed.
-  # trick the compiler to not list `AssertionDefect` when called
-  # by `assert`.
-  # xxx simplify this pending bootstrap >= 1.4.0, after which cast not needed
-  # anymore since `Defect` can't be raised.
-  type Hide = proc (msg: string) {.noinline, raises: [], noSideEffect, tags: [].}
-  cast[Hide](raiseAssert)(msg)
+  raiseAssert(msg)
 
 template assertImpl(cond: bool, msg: string, expr: string, enabled: static[bool]) =
   when enabled:
