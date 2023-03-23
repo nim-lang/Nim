21a26548a49748776083ed0be4a7c971d61f1cca
ringabout <43030857+ringabout@users.noreply.github.com>
2023-03-23 23:10:14 +0800

    fixes #14255; Crash in compiler when using `system.any` by accident. (#21562)
    fixes #14255; Crash in compiler when using system.any by accident.
    
    (cherry picked from commit 55636a2913d0b0dec6b24568cb6baef43a9220c1)

--- compiler/semexprs.nim
+++ compiler/semexprs.nim
@@ -269,7 +269,7 @@ proc semConv(c: PContext, n: PNode): PNode =
   result = newNodeI(nkConv, n.info)
 
   var targetType = semTypeNode(c, n[0], nil)
-  case targetType.kind
+  case targetType.skipTypes({tyDistinct}).kind
   of tyTypeDesc:
     internalAssert c.config, targetType.len > 0
     if targetType.base.kind == tyNone:
