bde9abb2f729e880b2110f890196a48c4eab8259
ringabout <43030857+ringabout@users.noreply.github.com>
2023-03-25 09:45:01 +0800

    fixes tests

--- tests/constructors/t5965_1.nim
+++ tests/constructors/t5965_1.nim
@@ -1,3 +1,9 @@
+discard """
+  errormsg: "When mixing named fields and unnamed fields, every field needs to be initialized in order"
+  file: "t5965_1.nim"
+  line: 10
+"""
+
 type Foo = object
   a, b, c: int
 
