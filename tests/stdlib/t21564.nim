d0d6f06b2cfacc04b709f7e9b628a3efe979d3de
Lukas Niederbremer <lukas.niederbremer@gmail.com>
2023-04-01 23:53:32 +0200

    std/bitops: Add explicit type masking for the JS target
    Typecasts on the JavaScript backend do not function the same way as they
    do on C and C++ backends, so for bitwise operations we may need to mask them
    back down into their allowed range when they get shifted outside it.
    
    Since they do work as expected on the other backends, a default bitmask
    of all 1's is casted down into the target type as an easily optimizable
    "& 0xFF" operation for these backends.

---
+++ tests/stdlib/t21564.nim
@@ -0,0 +1,31 @@
+discard """
+nimout: "OK"
+output: '''
+OK
+'''
+"""
+
+import bitops
+import std/assertions
+
+proc main() =
+  # tesk bitops.bitsliced patch
+  doAssert(0x17.bitsliced(4..7) == 0x01)
+  doAssert(0x17.bitsliced(0..3) == 0x07)
+
+  # test in-place bitops.bitslice
+  var t = 0x12F4
+  t.bitslice(4..7)
+
+  doAssert(t == 0xF)
+
+  # test bitops.toMask patch via bitops.masked
+  doAssert(0x12FFFF34.masked(8..23) == 0x00FFFF00)
+
+
+  echo "OK"
+
+main()
+
+static:
+  main()
