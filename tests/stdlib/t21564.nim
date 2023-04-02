ff2cb0f88068414919f58f4bbabc08b7f8f2f715
Lukas Niederbremer <lukas.niederbremer@gmail.com>
2023-04-02 13:35:03 +0200

    Adapt testcase to contributor guide and best practices

--- tests/stdlib/t21564.nim
+++ tests/stdlib/t21564.nim
@@ -1,8 +1,4 @@
 discard """
-nimout: "OK"
-output: '''
-OK
-'''
 targets: "c js"
 """
 
@@ -10,21 +6,21 @@ import bitops
 import std/assertions
 
 proc main() =
-  # tesk bitops.bitsliced patch
-  doAssert(0x17.bitsliced(4..7) == 0x01)
-  doAssert(0x17.bitsliced(0..3) == 0x07)
+  block: # bug #21564
+    # tesk `bitops.bitsliced` patch
+    doAssert(0x17.bitsliced(4..7) == 0x01)
+    doAssert(0x17.bitsliced(0..3) == 0x07)
 
-  # test in-place bitops.bitslice
-  var t = 0x12F4
-  t.bitslice(4..7)
+  block:
+    # test in-place `bitops.bitslice`
+    var t = 0x12F4
+    t.bitslice(4..7)
 
-  doAssert(t == 0xF)
+    doAssert(t == 0xF)
 
-  # test bitops.toMask patch via bitops.masked
-  doAssert(0x12FFFF34.masked(8..23) == 0x00FFFF00)
-
-
-  echo "OK"
+  block:
+    # test `bitops.toMask` patch via bitops.masked
+    doAssert(0x12FFFF34.masked(8..23) == 0x00FFFF00)
 
 main()
 
