ebb931f9f472f3d9cf84c9013a01cab4a52cbe3f
ringabout <43030857+ringabout@users.noreply.github.com>
2023-04-03 14:39:17 +0800

    check refc first

--- compiler/semstmts.nim
+++ compiler/semstmts.nim
@@ -1857,7 +1857,7 @@ proc semOverride(c: PContext, s: PSym, n: PNode) =
     incl(s.flags, sfOverriden)
     if name == "=":
       message(c.config, n.info, warnDeprecated, "Overriding `=` hook is deprecated; Override `=copy` hook instead")
-    elif name == "=copy" and c.config.selectedGC == gcRefc:
+    elif c.config.selectedGC == gcRefc and name == "=copy":
       message(c.config, n.info, warnCopyHookForRefc)
     let t = s.typ
     if t.len == 3 and t[0] == nil and t[1].kind == tyVar:
