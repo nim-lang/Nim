a889ab3a33b0e4228b540d3f2d1e2c19d55a5f62
Lukas Niederbremer <lukas.niederbremer@gmail.com>
2023-04-05 21:40:36 +0200

    Move SHA-2/SHA-3 to lib/pure and simplify sha_utils

--- lib/pure/private/sha_utils.nim
+++ lib/pure/private/sha_utils.nim
@@ -4,7 +4,9 @@ when defined(js):
   type
     # JS target does not do well with 64 bits as integers are always represented
     # as doubles, so we only have 52 bits of perfect accuracy. That's why we represent
-    # a single uint64 as a combination of a high and low uint32.
+    # a single uint64 as a combination of a high and low uint32. BigInts are a possibility
+    # but they are annoying in the sense that they don't play well well in `const`s and
+    # the fact that array[JsBigInt; N] turns into Uint32Array, which doesn't work.
     CompatUint64* {.packed.} = tuple
       lo: uint32
       hi: uint32
@@ -42,9 +44,9 @@ when defined(js):
     char(x.lo and 0xFF)
 
 
-  func `xor`*(a, b: CompatUint64): CompatUint64 {.inline.} = (forceUnsigned a.lo xor forceUnsigned b.lo, forceUnsigned a.hi xor forceUnsigned b.hi)
-  func `and`*(a, b: CompatUint64): CompatUint64 {.inline.} = (forceUnsigned a.lo and forceUnsigned b.lo, forceUnsigned a.hi and forceUnsigned b.hi)
-  func `or`*(a, b: CompatUint64): CompatUint64 {.inline.} = (forceUnsigned a.lo or forceUnsigned b.lo, forceUnsigned a.hi or forceUnsigned b.hi)
+  func `xor`*(a, b: CompatUint64): CompatUint64 {.inline.} = (forceUnsigned (a.lo xor b.lo), forceUnsigned (a.hi xor b.hi))
+  func `or`*(a, b: CompatUint64): CompatUint64 {.inline.} = (forceUnsigned (a.lo or b.lo), forceUnsigned (a.hi or b.hi))
+  func `and`*(a, b: CompatUint64): CompatUint64 {.inline.} = (forceUnsigned (a.lo and b.lo), forceUnsigned (a.hi and b.hi))
   func `not`*(a: CompatUint64): CompatUint64 {.inline.} = (forceUnsigned(not a.lo), forceUnsigned(not a.hi))
 
   proc `+`*(a, b: CompatUint64): CompatUint64 {.inline.} =
@@ -57,7 +59,7 @@ when defined(js):
     #      operators, everything from the 32nd bit upwards is truncated. So this
     #      *must* be `> 0xffff_ffff` as the intuitive `and 0x1_0000_0000` will
     #      switch to fixed 32 bit bitwise operation and always yield `false`.
-    let carry = if (nlo > 0xffff_ffff'u64): 1'u else: 0'u
+    let carry = uint32(nlo > 0xffff_ffff'u64)
 
     (forceUnsigned nlo, forceUnsigned (nhi + carry))
 
@@ -84,29 +86,27 @@ when defined(js):
       return a
 
     if offset < 32:
-      let bitmask = forceUnsigned(uint32(a.hi shl (32 - offset)) and 0xFFFFFFFF'u32)
+      let bitmask = forceUnsigned(a.hi shl (32 - offset))
 
-      result = (
-        forceUnsigned((a.lo shr offset) or bitmask),
-        forceUnsigned(a.hi shr offset))
+      (forceUnsigned(a.lo shr offset or bitmask),
+       forceUnsigned(a.hi shr offset))
     else:
-      result = (forceUnsigned a.hi shr (offset - 32), 0)
+      (forceUnsigned a.hi shr (offset - 32), 0)
+
+  template rotate(opA, opB: untyped; a: CompatUint64; offset: int): untyped =
+    if offset < 32:
+      (forceUnsigned (opA(a.lo, offset) or opB(a.hi, 32 - offset)),
+       forceUnsigned (opA(a.hi, offset) or opB(a.lo, 32 - offset)))
+    else:
+      (forceUnsigned (opA(a.hi, offset - 32) or opB(a.lo, 64 - offset)),
+       forceUnsigned (opA(a.lo, offset - 32) or opB(a.hi, 64 - offset)))
 
   func rotateLeftBits*(a: CompatUint64; offset: int): CompatUint64 =
-    if offset < 32:
-      (forceUnsigned ((a.lo shl offset) or (a.hi shr (32 - offset))),
-       forceUnsigned ((a.hi shl offset) or (a.lo shr (32 - offset))))
-    else:
-      (forceUnsigned ((a.hi shl (offset - 32)) or (forceUnsigned (a.lo shr (64 - offset)))),
-       forceUnsigned ((a.lo shl (offset - 32)) or (forceUnsigned (a.hi shr (64 - offset)))))
+    rotate(`shl`, `shr`, a, offset)
 
   func rotateRightBits*(a: CompatUint64; offset: int): CompatUint64 =
-    if offset < 32:
-      (forceUnsigned ((a.lo shr offset) or (forceUnsigned (a.hi shl (32 - offset)))),
-       forceUnsigned ((a.hi shr offset) or (forceUnsigned (a.lo shl (32 - offset)))))
-    else:
-      (forceUnsigned ((a.hi shr (offset - 32)) or (forceUnsigned (a.lo shl (64 - offset)))),
-       forceUnsigned ((a.lo shr (offset - 32)) or (forceUnsigned (a.hi shl (64 - offset)))))
+    rotate(`shr`, `shl`, a, offset)
+
 else:
   type
     CompatUint64* = uint64
