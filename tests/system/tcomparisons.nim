683f787759d4fdad670e4c753891212630d416f3
metagn <10591326+metagn@users.noreply.github.com>
2023-04-22 17:15:49 +0300

    all targets

--- tests/system/tcomparisons.nim
+++ tests/system/tcomparisons.nim
@@ -1,3 +1,7 @@
+discard """
+  targets: "c cpp js"
+"""
+
 template main =
   block: # proc equality
     var prc: proc(): int {.closure.}
@@ -14,23 +18,24 @@ template main =
       result = 456
     doAssert prc != nil
     doAssert prc != prc2
-  when nimvm: discard # vm does not support closure iterators
-  else:
-    block: # iterator equality
-      var iter: iterator(): int {.closure.}
-      iter = nil
-      doAssert iter == nil
-      iter = iterator(): int =
-        yield 123
-      doAssert iter != nil
-      doAssert iter == iter
-      let iter2 = iter
-      doAssert iter == iter2
-      doAssert iter2 != nil
-      iter = iterator(): int =
-        yield 456
-      doAssert iter != nil
-      doAssert iter != iter2
+  block: # iterator equality
+    when nimvm: discard # vm does not support closure iterators
+    else:
+      when not defined(js): # js also does not support closure iterators
+        var iter: iterator(): int {.closure.}
+        iter = nil
+        doAssert iter == nil
+        iter = iterator(): int =
+          yield 123
+        doAssert iter != nil
+        doAssert iter == iter
+        let iter2 = iter
+        doAssert iter == iter2
+        doAssert iter2 != nil
+        iter = iterator(): int =
+          yield 456
+        doAssert iter != nil
+        doAssert iter != iter2
 
 static: main()
 main()
