2072eebcea833674d4a847c22b80fb0af8ed7c09
ringabout <43030857+ringabout@users.noreply.github.com>
2023-04-26 14:04:13 +0800

    fixes #21731; fixes #21537; disable `warnBareExcept` by default [backport] (#21728)
    * disable warnBareExcept for default
    
    * fixes a typo
    
    (cherry picked from commit 69c193e5e2dbdbadbf2d8767602b27451afd1e5e)

--- compiler/lineinfos.nim
+++ compiler/lineinfos.nim
@@ -227,7 +227,7 @@ type
   TNoteKinds* = set[TNoteKind]
 
 proc computeNotesVerbosity(): array[0..3, TNoteKinds] =
-  result[3] = {low(TNoteKind)..high(TNoteKind)} - {warnObservableStores, warnResultUsed, warnAnyEnumConv}
+  result[3] = {low(TNoteKind)..high(TNoteKind)} - {warnObservableStores, warnResultUsed, warnAnyEnumConv, warnBareExcept}
   result[2] = result[3] - {hintStackTrace, warnUninit, hintExtendedContext, hintDeclaredLoc, hintProcessingStmt}
   result[1] = result[2] - {warnProveField, warnProveIndex,
     warnGcUnsafe, hintPath, hintDependency, hintCodeBegin, hintCodeEnd,
