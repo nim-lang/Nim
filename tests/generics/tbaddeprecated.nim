3fdbf87855130b27141c092e079c8339c77cde1d
metagn <10591326+metagn@users.noreply.github.com>
2023-04-30 04:53:21 +0300

    ok final actual correct test

--- tests/generics/tbaddeprecated.nim
+++ tests/generics/tbaddeprecated.nim
@@ -1,6 +1,8 @@
 discard """
   output: '''
 not deprecated
+not deprecated
+not error
 not error
 '''
 """
@@ -19,10 +21,16 @@ block: # deprecated
       echo "not deprecated"
   gen(SomeObj(hey: true))
   doAssert not (compiles do:
+    proc hey(o: SomeObj) {.deprecated: "Shouldn't use this".} = echo "hey"
     proc gen2(o: auto) =
       if o.hey():
         echo "not deprecated"
     gen2(SomeObj(hey: true)))
+  proc hey(o: SomeObj) {.deprecated: "Shouldn't use this".} = echo "hey"
+  proc gen3(o: auto) =
+    if o.hey:
+      echo "not deprecated"
+  gen3(SomeObj(hey: true))
   {.pop.}
 block: # error
   type
@@ -35,7 +43,13 @@ block: # error
       echo "not error"
   gen(SomeObj(hey: true))
   doAssert not (compiles do:
+    proc hey(o: SomeObj) {.error: "Shouldn't use this".} = echo "hey"
     proc gen2(o: auto) =
       if o.hey():
-        echo "not deprecated"
+        echo "not error"
     gen2(SomeObj(hey: true)))
+  proc hey(o: SomeObj) {.error: "Shouldn't use this".} = echo "hey"
+  proc gen3(o: auto) =
+    if o.hey:
+      echo "not error"
+  gen3(SomeObj(hey: true))
