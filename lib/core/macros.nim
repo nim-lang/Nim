6fbff263e6163bbccc481810f626bc51f78c92be
metagn <metagngn@gmail.com>
2023-05-11 21:50:01 +0300

    just set CallNodes = nnkCallKinds, follows up #21829 (#21833)
    These sets are now equal
    
    (cherry picked from commit 9810b8cf7ff85a306699d8393ce516501d938e05)

--- lib/core/macros.nim
+++ lib/core/macros.nim
@@ -1176,8 +1176,7 @@ const
                    nnkIteratorDef, nnkTemplateDef, nnkConverterDef, nnkMacroDef}
   AtomicNodes* = {nnkNone..nnkNilLit}
   # see matching set nnkCallKinds above
-  CallNodes* = {nnkCall, nnkInfix, nnkPrefix, nnkPostfix, nnkCommand,
-    nnkCallStrLit, nnkHiddenCallConv}
+  CallNodes* = nnkCallKinds
 
 proc expectKind*(n: NimNode; k: set[NimNodeKind]) =
   ## Checks that `n` is of kind `k`. If this is not the case,
