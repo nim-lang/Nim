4f1386aee4141bbd3e905d3f71075aee345275db
Juan_Carlos <juancarlospaco@gmail.com>
2023-05-26 20:26:21 -0300

    Add __always_inline__ support

--- lib/nimbase.h
+++ lib/nimbase.h
@@ -88,26 +88,24 @@ __AVR__
 #  define __DECLSPEC_SUPPORTED 1
 #endif
 
-/* calling convention mess ----------------------------------------------- */
-#if defined(__GNUC__) || defined(__LCC__) || defined(__POCC__) \
-                      || defined(__TINYC__)
-  /* these should support C99's inline */
-  /* the test for __POCC__ has to come before the test for _MSC_VER,
-     because PellesC defines _MSC_VER too. This is brain-dead. */
-#  define N_INLINE(rettype, name) inline rettype name
-#elif defined(__BORLANDC__) || defined(_MSC_VER)
-/* Borland's compiler is really STRANGE here; note that the __fastcall
-   keyword cannot be before the return type, but __inline cannot be after
-   the return type, so we do not handle this mess in the code generator
-   but rather here. */
+
+/* Calling conventions and inline attributes for the supported C compilers */
+#if defined(__GNUC__) || defined(__clang__)  /* GCC and Clang */
+#  if __has_attribute(__always_inline__)
+#    define N_INLINE(rettype, name) __attribute__((__always_inline__)) rettype name
+#  else
+#    warning "C compiler do not support '__always_inline__', fallback to 'inline'."
+#    define N_INLINE(rettype, name) inline rettype name
+#  endif
+#elif defined(_MSC_VER) || defined(__TINYC__)  /* MSVC and TinyC */
 #  define N_INLINE(rettype, name) __inline rettype name
-#elif defined(__DMC__)
+#elif defined(__AVR__) || defined(__BORLANDC__)  /* Atmel RISC and BorlandC */
 #  define N_INLINE(rettype, name) inline rettype name
-#elif defined(__WATCOMC__)
-#  define N_INLINE(rettype, name) __inline rettype name
-#else /* others are less picky: */
-#  define N_INLINE(rettype, name) rettype __inline name
+#else  /* Unsupported C compilers */
+#  warning "C compiler do not support 'inline', fallback to non inline."
+#  define N_INLINE(rettype, name) rettype name
 #endif
+
 
 #define N_INLINE_PTR(rettype, name) rettype (*name)
 
