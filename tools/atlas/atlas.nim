e885ad717fff16dce876fe3cfca64d7707fc5574
Gruruya <gruruya.chi4c@slmails.com>
2023-05-27 10:59:40 -0400

    One last doc pass

--- tools/atlas/atlas.nim
+++ tools/atlas/atlas.nim
@@ -45,8 +45,8 @@ Command:
                         URL that matches `filter` if a filter is given
   tag [major|minor|patch]
                         add and push a new tag, input must be one of:
-                        'major'|'minor'|'patch' or a custom tag '1.0.3'
-                        or a letter from 'a' to 'z': a.b.c.d.e.f.g
+                        ['major'|'minor'|'patch'] or a SemVer tag like ['1.0.3']
+                        or a letter ['a'..'z']: a.b.c.d.e.f.g
   build|test|doc|tasks  currently delegates to `nimble build|test|doc`
   task <taskname>       currently delegates to `nimble <taskname>`
 
