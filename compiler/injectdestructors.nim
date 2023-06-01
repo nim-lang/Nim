ca5bee17e999367e3d70b64406bce0c830b7e135
ringabout <43030857+ringabout@users.noreply.github.com>
2023-06-01 20:47:22 +0800

    fixes #21974; fixes sameConstant fieldDefect

--- compiler/injectdestructors.nim
+++ compiler/injectdestructors.nim
@@ -1016,7 +1016,7 @@ proc p(n: PNode; c: var Con; s: var Scope; mode: ProcessMode; tmpFlags = {sfSing
 
 proc sameLocation*(a, b: PNode): bool =
   proc sameConstant(a, b: PNode): bool =
-    a.kind in nkLiterals and a.intVal == b.intVal
+    a.kind in nkLiterals and b.kind in nkLiterals and a.intVal == b.intVal
 
   const nkEndPoint = {nkSym, nkDotExpr, nkCheckedFieldExpr, nkBracketExpr}
   if a.kind in nkEndPoint and b.kind in nkEndPoint:
