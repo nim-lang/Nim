7f46843dbb6d612b5c4a3d2fdbf890b920fc7010
metagn <10591326+metagn@users.noreply.github.com>
2023-06-11 22:08:51 +0300

    fix renderer, add test for #7085

--- tests/template/mdotcall.nim
+++ tests/template/mdotcall.nim
@@ -1,3 +1,5 @@
+# issue #20073
+
 type Foo = object
 proc foo(f: Foo) = discard
 
@@ -9,3 +11,12 @@ template boom*() =
   var f: Foo
   f.foo() # Error: attempting to call undeclared routine: 'foo'
   f.foo # Error: undeclared field: 'foo' for type a.Foo
+
+# issue #7085
+
+proc bar(a: string): string =
+  return a & "bar"
+
+template baz*(a: string): string =
+  var b = a.bar()
+  b
--- tests/template/tdotcall.nim
+++ tests/template/tdotcall.nim
@@ -1,6 +1,10 @@
-# issue #20073
-
 import mdotcall
 
+# issue #20073
 works()
 boom()
+
+# issue #7085
+doAssert baz("hello") == "hellobar"
+doAssert baz"hello" == "hellobar"
+doAssert "hello".baz == "hellobar"
