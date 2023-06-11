d8d7ba2e5106cf7efe39bfad0b287686b5ea9634
metagn <10591326+metagn@users.noreply.github.com>
2023-06-11 18:57:41 +0300

    make old code work

--- compiler/semtempl.nim
+++ compiler/semtempl.nim
@@ -564,7 +564,9 @@ proc semTemplBody(c: var TemplCtx, n: PNode): PNode =
       if result[1].kind == nkSym and result[1].sym.kind in routineKinds:
         # prevent `dotTransformation` from rewriting this node to `nkIdent`
         # by making it a symchoice
-        var choice = newNodeIT(nkClosedSymChoice, n[1].info, newTypeS(tyNone, c.c))
+        # in generics this becomes `nkClosedSymChoice` but this breaks code
+        # as the old behavior here was that this became `nkIdent`
+        var choice = newNodeIT(nkOpenSymChoice, n[1].info, newTypeS(tyNone, c.c))
         choice.add result[1]
         result[1] = choice
     else:
