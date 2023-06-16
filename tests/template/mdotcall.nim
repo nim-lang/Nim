88388040db4b169d51345bcabedb8bb70729f094
metagn <metagngn@gmail.com>
2023-06-16 18:14:47 +0300

    add tests to close #7223, close #11733 (#22111)
    add test to close #7223, close #11733
    
    closes #7223, closes #11733, were fixed by #22076

--- tests/template/mdotcall.nim
+++ tests/template/mdotcall.nim
@@ -20,3 +20,31 @@ proc bar(a: string): string =
 template baz*(a: string): string =
   var b = a.bar()
   b
+
+# issue #7223
+
+import mdotcall2
+
+type
+  Bytes* = seq[byte]
+  
+  BytesRange* = object
+    bytes*: Bytes
+    ibegin*, iend*: int
+
+proc privateProc(r: BytesRange): int = r.ibegin
+
+template rangeBeginAddr*(r: BytesRange): pointer =
+  r.bytes.baseAddr.shift(r.privateProc)
+
+# issue #11733
+
+type ObjA* = object
+
+proc foo2(o: var ObjA) = discard
+proc bar2(o: var ObjA, arg: Natural) = discard
+
+template publicTemplateObjSyntax*(o: var ObjA, arg: Natural, doStuff: untyped) =
+  o.foo2()
+  doStuff
+  o.bar2(arg)
