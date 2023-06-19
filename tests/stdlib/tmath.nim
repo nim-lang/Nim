ef3a2a5c36de1bfc9644d2ca01e1f2c7cea25286
ryan <rammcconnell@gmail.com>
2023-06-19 11:19:21 -0400

    Add Overflow checks & test adjust

--- tests/stdlib/tmath.nim
+++ tests/stdlib/tmath.nim
@@ -190,10 +190,13 @@ template main() =
   block: # divmod
     doAssert divmod(int.high, 1) == (int.high, 0)
     doAssert divmod(-1073741823, 17) == (-63161283, -12)
-    when not defined(js):
-      doAssert divmod(int32.high, 1.int32) == (int32.high, 0.int32)
-      doAssert divmod(1073741823.int32, 5.int32) == (214748364.int32, 3.int32)
-      doAssert divmod(4611686018427387903.int64, 5.int64) == (922337203685477580.int64, 3.int64)
+    doAssert divmod(int32.high, 1.int32) == (int32.high, 0.int32)
+    doAssert divmod(1073741823.int32, 5.int32) == (214748364.int32, 3.int32)
+    doAssert divmod(4611686018427387903.int64, 5.int64) == (922337203685477580.int64, 3.int64)
+    when not defined(js) and not compileOption("panics"):
+      doAssertRaises(OverflowDefect, (discard divmod(cint.low, -1.cint)))
+      doAssertRaises(OverflowDefect, (discard divmod(clong.low, -1.clong)))
+      doAssertRaises(OverflowDefect, (discard divmod(clonglong.low, -1.clonglong)))
   
   block: # log
     doAssert log(4.0, 3.0) ==~ ln(4.0) / ln(3.0)
