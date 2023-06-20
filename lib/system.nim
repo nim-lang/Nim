daebe17063a8744a489caf1faadbc604f23139bc
Juan_Carlos <juancarlospaco@gmail.com>
2023-06-20 09:33:06 -0300

    Fix

--- lib/system.nim
+++ lib/system.nim
@@ -2811,5 +2811,5 @@ proc arrayWith*[T](y: T, size: static int): array[size, T] {.raises: [].} =
 
 
 when defined(nimV2):
-  from std/system/arc import isUniqueRef
+  from system/arc import isUniqueRef
   export isUniqueRef
