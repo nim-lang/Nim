86654db1a90702b231eca9ebe821d34e53152bf0
ringabout <43030857+ringabout@users.noreply.github.com>
2023-06-27 16:17:43 +0800

    fixes system

--- lib/system.nim
+++ lib/system.nim
@@ -365,7 +365,7 @@ proc arrGet[I: Ordinal;T](a: T; i: I): T {.
 proc arrPut[I: Ordinal;T,S](a: T; i: I;
   x: S) {.noSideEffect, magic: "ArrPut".}
 
-when defined(nimAllowNonVarDestructor):
+when defined(nimAllowNonVarDestructor) and defined(gcDestructors):
   proc `=destroy`*(x: string) {.inline, magic: "Destroy".} =
     discard
 
