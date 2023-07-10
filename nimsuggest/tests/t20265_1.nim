7f3ba8b314ff8cf20eb1e04a7b7d54644d932011
Jake Leahy <jake@leahy.dev>
2023-07-10 11:12:25 +1000

    Add tests
    Also test if exported all tuple fields works. This seems like a hacky solution so will try and dive further to find a better solution

---
+++ nimsuggest/tests/t20265_1.nim
@@ -0,0 +1,8 @@
+discard """
+$nimsuggest --tester $file
+>sug $1
+sug;;skField;;a;;int;;t20265_module.nim;;3;;2;;"";;100
+sug;;skField;;b;;int;;t20265_module.nim;;4;;2;;"";;100
+"""
+import module
+y.#[!]#
