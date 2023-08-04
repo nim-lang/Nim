008d446833e9040a041a058907e00d5dabdd4268
bung87 <crc32@qq.com>
2023-08-05 00:42:18 +0800

    clean test

--- tests/generics/t14193.nim
+++ tests/generics/t14193.nim
@@ -1,9 +1,5 @@
 type
   Task*[N: int] = object
-    ## Tasks are simple closures that are sent by shared-memory channels
-    ## In the future, if Nim built-in closures become threadsafe
-    ## they can be used directly instead.
-    fn*: proc(env: pointer) {.nimcall.}
     env*: array[N, byte]
 
 var task14193: Task[20]
