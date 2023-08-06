68df345e8de12d6e81cb0ac60ef3c90cd2ffc6dc
bung87 <crc32@qq.com>
2023-08-06 13:36:20 +0800

    fix test

--- tests/misc/t3907.nim
+++ tests/misc/t3907.nim
@@ -1,14 +1,10 @@
-discard """
-  disabled: "32bit"
-"""
-
 import std/assertions
 
-let a = 0'i64
+let a = 0
 let b = if false: -1 else: a
 doAssert b == 0
 
-let c: range[0'i64..high(int64)] = 0'i64
+let c: range[0..high(int)] = 0
 let d = if false: -1 else: c
 
 doAssert d == 0
