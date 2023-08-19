31e47951c39db19dcb9507eff3545d41dc898182
Ryan McConnell <rammcconnell@gmail.com>
2023-08-19 16:24:45 -0400

    [skip ci] Adding to test

--- tests/overload/tor_isnt_better.nim
+++ tests/overload/tor_isnt_better.nim
@@ -1,20 +1,19 @@
-# PR #22261
-#[
-  An amendment to this test has been made. Since D is a subset of D | E but
-  not the other way around `checkGeneric` should favor proc g(a: D) instead
-  of asserting ambiguity
-]#
-
-# bug #8568
-
 type
   D[T] = object
   E[T] = object
 
-proc g(a: D|E): string = "foo D|E"
-proc g(a: D): string = "foo D"
+block: # PR #22261
+  proc d(x: D):bool= false
+  proc d(x: int | D[SomeInteger]):bool= true
+  doAssert d(D[5]()) == false
 
-proc test() =
+
+block: # bug #8568
+#[
+  Since PR #22261 and amendment has been made. Since D is a subset of D | E but
+  not the other way around `checkGeneric` should favor proc g(a: D) instead
+  of asserting ambiguity
+]#
+  proc g(a: D|E): string = "foo D|E"
+  proc g(a: D): string = "foo D"
   doAssert g(D[int]()) == "foo D"
-
-test()
