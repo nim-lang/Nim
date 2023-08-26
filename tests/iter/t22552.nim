1c1b01c1ee5353cb7c9394b0d8a90cccf65bee53
ringabout <43030857+ringabout@users.noreply.github.com>
2023-08-26 12:06:22 +0800

    add a test case; thanks to @nitely

---
+++ tests/iter/t22552.nim
@@ -0,0 +1,25 @@
+#bug #22552
+
+type
+  Idx = object
+    i: int
+  Node = object
+    n: int
+    next: seq[Idx]
+  FooBar = object
+    s: seq[Node]
+
+proc `=copy`(dest: var Idx; source: Idx) {.error.}
+proc `=copy`(dest: var Node; source: Node) {.error.}
+proc `=copy`(dest: var FooBar; source: FooBar) {.error.}
+
+proc doSomething(ss: var seq[int], s: FooBar) =
+  for i in 0 .. s.s.len-1:
+    for elm in items s.s[i].next:
+      ss.add s.s[elm.i].n
+
+when isMainModule:
+  const foo = FooBar(s: @[Node(n: 1, next: @[Idx(i: 0)])])
+  var ss: seq[int]
+  doSomething(ss, foo)
+  doAssert ss == @[1]
