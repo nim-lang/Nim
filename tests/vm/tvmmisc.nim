2e4e2f8f5076b39e5976ba20f231f468b1b5052c
metagn <metagngn@gmail.com>
2023-08-30 08:23:14 +0300

    handle typedesc params in VM (#22581)
    * handle typedesc params in VM
    
    fixes #15760
    
    * add test
    
    * fix getType(typedesc) test

--- tests/vm/tvmmisc.nim
+++ tests/vm/tvmmisc.nim
@@ -1,10 +1,11 @@
-# bug #4462
 import macros
 import os
 
+# bug #4462
 block:
   proc foo(t: typedesc) {.compileTime.} =
-    assert sameType(getType(t), getType(int))
+    assert sameType(getType(t), getType(typedesc[int]))
+    assert sameType(getType(t), getType(type int))
 
   static:
     foo(int)
