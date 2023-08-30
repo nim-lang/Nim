e0aba67b445a095715bddba37a6498ea191cd497
Andrey Makarov <ph.makarov@gmail.com>
2023-08-29 21:20:01 -0600

    docgen: add Pandoc footnotes (fixes #21080)
    This implements Pandoc Markdown-style footnotes,
    that are compatible with Pandoc referencing syntax:
    
        Ref. [^ftn].
    
        [^ftn]: Block.
    
    See https://pandoc.org/MANUAL.html#footnotes for more examples.

--- tests/stdlib/trstgen.nim
+++ tests/stdlib/trstgen.nim
@@ -1128,7 +1128,7 @@ Test1
 
       Paragraph2 ref `internal anchor`_.
       """
-    let output9 = input9.toHtml
+    let output9 = input9.toHtml(preferRst)
     # _`internal anchor` got erased:
     check "href=\"#internal-anchor\"" notin output9
     check "href=\"#citation-another\"" in output9
@@ -1156,7 +1156,7 @@ Test1
     doAssert "<a href=\"#citation-third\">[Third]</a>" in output10
 
     let input11 = ".. [note]\n"  # should not crash
-    let output11 = input11.toHtml
+    let output11 = input11.toHtml(preferRst)
     doAssert "<a href=\"#citation-note\">[note]</a>" in output11
 
     # check that references to auto-numbered footnotes work
@@ -1443,7 +1443,7 @@ Test1
       Ref. target103_.
 
     """
-    let output2 = input2.toHtml
+    let output2 = input2.toHtml(preferRst)
     # "target101" should be erased and changed to "section-xyz":
     doAssert "href=\"#target300\"" notin output2
     doAssert "id=\"target300\""    notin output2
