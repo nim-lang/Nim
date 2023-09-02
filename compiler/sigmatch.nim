3f3fb1c0e8d6719636a4cab2698e177a7a1234f6
Ryan McConnell <rammcconnell@gmail.com>
2023-09-02 18:04:15 -0400

    rollback `tyPtr` and `tyRef`

--- compiler/sigmatch.nim
+++ compiler/sigmatch.nim
@@ -1401,20 +1401,18 @@ proc typeRel(c: var TCandidate, f, aOrig: PType,
             result = isNone
 
   of tyPtr, tyRef:
-    #skipOwned(a)
-    let effectiveArgType = getObjectTypeOrNil(a)
-    if effectiveArgType == nil: return isNone
-    if effectiveArgType.kind == f.kind:
+    skipOwned(a)
+    if a.kind == f.kind:
       # ptr[R, T] can be passed to ptr[T], but not the other way round:
-      if effectiveArgType.len < f.len: return isNone
+      if a.len < f.len: return isNone
       for i in 0..<f.len-1:
-        if typeRel(c, f[i], effectiveArgType[i], flags) == isNone: return isNone
-      result = typeRel(c, f.lastSon, effectiveArgType.lastSon, flags + {trNoCovariance})
+        if typeRel(c, f[i], a[i], flags) == isNone: return isNone
+      result = typeRel(c, f.lastSon, a.lastSon, flags + {trNoCovariance})
       subtypeCheck()
       if result <= isIntConv: result = isNone
-      elif tfNotNil in f.flags and tfNotNil notin effectiveArgType.flags:
+      elif tfNotNil in f.flags and tfNotNil notin a.flags:
         result = isNilConversion
-    elif effectiveArgType.kind == tyNil: result = f.allowsNil
+    elif a.kind == tyNil: result = f.allowsNil
     else: discard
   of tyProc:
     skipOwned(a)
