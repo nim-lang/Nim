6000cc8c0f5c71fd0495172b5680ab6b1945428c
metagn <metagngn@gmail.com>
2023-09-05 11:30:13 +0300

    fix sym of created generic instantiation type (#22642)
    fixes #22639
    
    A `tyGenericInst` has its last son as the instantiated body of the
    original generic type. However this type keeps its original `sym` field
    from the original generic types, which means the sym's type is
    uninstantiated. This causes problems in the implementation of `getType`,
    where it uses the `sym` fields of types to represent them in AST, the
    relevant example for the issue being
    [here](https://github.com/nim-lang/Nim/blob/d13aab50cf465a7f2edf9c37a4fa30e128892e72/compiler/vmdeps.nim#L191)
    called from
    [here](https://github.com/nim-lang/Nim/blob/d13aab50cf465a7f2edf9c37a4fa30e128892e72/compiler/vmdeps.nim#L143).
    
    To fix this, create a new symbol from the original symbol for the
    instantiated body during the creation of `tyGenericInst`s with the
    appropriate type. Normally `replaceTypeVarsS` would be used for this,
    but here it seems to cause some recursion issue (immediately gives an
    error like "cannot instantiate HSlice[T, U]"), so we directly set the
    symbol's type to the instantiated type.
    
    Avoiding recursion means we also cannot use `replaceTypeVarsN` for the
    symbol AST, and the symbol not having any AST crashes the implementation
    of `getType` again
    [here](https://github.com/nim-lang/Nim/blob/d13aab50cf465a7f2edf9c37a4fa30e128892e72/compiler/vmdeps.nim#L167),
    so the symbol AST is set to the original generic type AST for now which
    is what it was before anyway.
    
    Not sure about this because not sure why the recursion issue is
    happening, putting it at the end of the proc doesn't help either. Also
    not sure if the `cl.owner != nil and s.owner != cl.owner` condition from
    `replaceTypeVarsS` is relevant here. This might also break some code if
    it depended on the original generic type symbol being given.

--- compiler/sigmatch.nim
+++ compiler/sigmatch.nim
@@ -505,6 +505,13 @@ proc skipToObject(t: PType; skipped: var SkippedPtr): PType =
   if r.kind == tyObject and ptrs <= 1: result = r
   else: result = nil
 
+proc getObjectSym(t: PType): PSym =
+  if tfFromGeneric in t.flags and t.typeInst.kind == tyGenericInst:
+    var dummy: SkippedPtr
+    result = t.typeInst[0].skipToObject(dummy).sym
+  else:
+    result = t.sym
+
 proc isGenericSubtype(c: var TCandidate; a, f: PType, d: var int, fGenericOrigin: PType): bool =
   assert f.kind in {tyGenericInst, tyGenericInvocation, tyGenericBody}
   var askip = skippedNone
@@ -512,11 +519,12 @@ proc isGenericSubtype(c: var TCandidate; a, f: PType, d: var int, fGenericOrigin
   var t = a.skipToObject(askip)
   let r = f.skipToObject(fskip)
   if r == nil: return false
+  let rSym = getObjectSym(r)
   var depth = 0
   var last = a
   # XXX sameObjectType can return false here. Need to investigate
   # why that is but sameObjectType does way too much work here anyway.
-  while t != nil and r.sym != t.sym and askip == fskip:
+  while t != nil and rSym != getObjectSym(t) and askip == fskip:
     t = t[0]
     if t == nil: break
     last = t
@@ -1602,7 +1610,7 @@ proc typeRel(c: var TCandidate, f, aOrig: PType,
         # crossing path with metatypes/aliases, so we need to separate them
         # by checking sym.id
         let genericSubtype = isGenericSubtype(c, x, f, depth, f)
-        if not (genericSubtype and aobj.sym.id != fobj.sym.id) and aOrig.kind != tyGenericBody:
+        if not (genericSubtype and getObjectSym(aobj).id != getObjectSym(fobj).id) and aOrig.kind != tyGenericBody:
           depth = -1
 
       if depth >= 0:
