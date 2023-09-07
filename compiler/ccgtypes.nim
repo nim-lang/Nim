2d597aad5d9791e70531069835f70a6ccd25c2f8
jmgomez <info@jmgomez.me>
2023-09-07 19:21:51 +0100

    fixes  #22662 Procs with constructor pragma doesn't initialize object's fields

--- compiler/ccgtypes.nim
+++ compiler/ccgtypes.nim
@@ -649,6 +649,13 @@ proc mangleRecFieldName(m: BModule; field: PSym): Rope =
     result = rope(mangleField(m, field.name))
   if result == "": internalError(m.config, field.info, "mangleRecFieldName")
 
+proc hasCppCtor(m: BModule; typ: PType): bool = 
+  result = false
+  if m.compileToCpp and typ != nil and typ.itemId in m.g.graph.memberProcsPerType:
+    for prc in m.g.graph.memberProcsPerType[typ.itemId]:
+      if sfConstructor in prc.flags:
+        return true
+
 proc genRecordFieldsAux(m: BModule; n: PNode,
                         rectype: PType,
                         check: var IntSet; result: var Rope; unionPrefix = "") =
@@ -712,9 +719,9 @@ proc genRecordFieldsAux(m: BModule; n: PNode,
         result.addf("\t$1$4 $2:$3;$n", [getTypeDescAux(m, field.loc.t, check, dkField), sname, rope($field.bitsize), noAlias])
       else:
         # don't use fieldType here because we need the
-        # tyGenericInst for C++ template support
-        if fieldType.isOrHasImportedCppType():
-          result.addf("\t$1$3 $2{};$n", [getTypeDescAux(m, field.loc.t, check, dkField), sname, noAlias])
+        # tyGenericInst for C++ template support        
+        if fieldType.isOrHasImportedCppType() or hasCppCtor(m, field.owner.typ):
+          result.addf("\t$1$3 $2{};$n ", [getTypeDescAux(m, field.loc.t, check, dkField), sname, noAlias])
         else:
           result.addf("\t$1$3 $2;$n", [getTypeDescAux(m, field.loc.t, check, dkField), sname, noAlias])
   else: internalError(m.config, n.info, "genRecordFieldsAux()")
