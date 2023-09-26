6f54d59c5d17836fdbf388f2ecfa10d3d0bc3003
Juan_Carlos <juancarlospaco@gmail.com>
2023-09-26 16:15:58 -0300

    Fix #21407

---
+++ lib/std/jsviewport.nim
@@ -0,0 +1,12 @@
+## - Viewport API for the JavaScript target: https://developer.mozilla.org/en-US/docs/Web/API/VisualViewport
+when not defined(js):
+  {.fatal: "Module is designed to be used with the JavaScript backend.".}
+
+import std/dom
+
+type VisualViewport* {.importc.} = ref object of EventTarget
+  offsetLeft*, offsetTop*, pageLeft*, pageTop*, width*, height*, scale*: float
+  onResize*: proc (event: Event) {.closure.}
+  onScroll*: proc (event: Event) {.closure.}
+
+func visualViewport*(self: Window): VisualViewport {.importjs: "#.$1", nodecl.}
