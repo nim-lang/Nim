90fd49e1df2731d5790730a96d9fb44ba7dfd7b7
Juan Carlos <juancarlospaco@gmail.com>
2023-09-26 19:40:40 -0300

    Update lib/std/jsviewport.nim
    Co-authored-by: Amjad Ben Hedhili <amjadhedhili@outlook.com>

--- lib/std/jsviewport.nim
+++ lib/std/jsviewport.nim
@@ -6,7 +6,6 @@ import std/dom
 
 type VisualViewport* {.importc.} = ref object of EventTarget
   offsetLeft*, offsetTop*, pageLeft*, pageTop*, width*, height*, scale*: float
-  onResize*: proc (event: Event) {.closure.}
-  onScroll*: proc (event: Event) {.closure.}
+  onResize*, onScroll*: proc (event: Event) {.closure.}
 
 func visualViewport*(self: Window): VisualViewport {.importjs: "#.$1", nodecl.}
