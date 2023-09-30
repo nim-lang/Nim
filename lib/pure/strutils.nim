bb82d46ef18b36692cd2f383c9098415a4b496ba
ringabout <43030857+ringabout@users.noreply.github.com>
2023-09-30 12:27:27 +0800

    fixes #22696; func `strutils.join` for non-strings uses proc `$` which can have side effects  (#22770)
    fixes #22696
    partially revert https://github.com/nim-lang/Nim/pull/16281
    
    `join` calls `$` interally, which might introduce a sideeffect call.
    
    (cherry picked from commit 5eeafbf55029816e5022d9d6aa9ed3b0acf2d3ba)

--- lib/pure/strutils.nim
+++ lib/pure/strutils.nim
@@ -1852,7 +1852,7 @@ func join*(a: openArray[string], sep: string = ""): string {.rtl,
   else:
     result = ""
 
-func join*[T: not string](a: openArray[T], sep: string = ""): string =
+proc join*[T: not string](a: openArray[T], sep: string = ""): string =
   ## Converts all elements in the container `a` to strings using `$`,
   ## and concatenates them with `sep`.
   runnableExamples:
