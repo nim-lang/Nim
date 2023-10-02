29933a7594a679dcbc7db09500e20c48b91ef300
ringabout <43030857+ringabout@users.noreply.github.com>
2023-10-02 20:12:28 +0800

    typo

--- tests/generics/tuninstantiated_failure.nim
+++ tests/generics/tuninstantiated_failure.nim
@@ -13,4 +13,4 @@ func `[]`[T, K](x: var Test[T, K], idx: int): var Test[T, K] =
 var b: Something
 # Should give an error since Something isn't a valid Test
 b[0].name = "Test" #[tt.Error
- ^  expression '' has no type (or is ambiguous]#
+ ^  expression '' has no type (or is ambiguous)]#
