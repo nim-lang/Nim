71dde8f7cf6db7c86abbef26b9aca5d8d92cf8e7
Jacek Sieka <arnetheduck@gmail.com>
2023-11-06 07:57:29 +0100

    reserve `sysFatal` for `Defect` (#22158)
    Per manual, `panics:on` affects _only_ `Defect`:s - thus `sysFatal`
    should not redirect any other exceptions.
    
    Also, when `sysFatal` is used in `nimPanics` mode, it should use regular
    exception handling pipeline to ensure exception hooks are called
    consistently for all raised defects.
    
    (cherry picked from commit 58c44312afba755f73ba2ec7ab73daea7768e41d)

--- lib/system.nim
+++ lib/system.nim
@@ -2748,7 +2748,7 @@ when notJSnotNims:
                     hostOS != "any"
 
   proc raiseEIO(msg: string) {.noinline, noreturn.} =
-    sysFatal(IOError, msg)
+    raise newException(IOError, msg)
 
   proc echoBinSafe(args: openArray[string]) {.compilerproc.} =
     when defined(androidNDK):
