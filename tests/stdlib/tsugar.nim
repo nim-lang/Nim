cecaf9c56b1240a44a4de837e03694a0c55ec379
ringabout <43030857+ringabout@users.noreply.github.com>
2023-11-20 00:53:25 +0800

    fixes #22939; fixes #16890; push should but doesn't apply to importc … (#22944)
    …var/let symbols
    
    
    fixes #22939
    fixes #16890
    
    Besides, it was applied to let/const/var with pragmas, now it is
    universally applied.
    
    ```nim
    {.push exportc.}
    proc foo =
      let bar = 12
      echo bar
    {.pop.}
    ```
    
    For example, the `bar` variable will be affected by `exportc`.

--- tests/stdlib/tsugar.nim
+++ tests/stdlib/tsugar.nim
@@ -12,6 +12,23 @@ type # for capture test, ref #20679
   FooCapture = ref object
     x: int
 
+proc mainProc() =
+  block: # bug #16967
+    var s = newSeq[proc (): int](5)
+    {.push exportc.}
+    proc bar() =
+      for i in 0 ..< s.len:
+        let foo = i + 1
+        capture foo:
+          s[i] = proc(): int = foo
+    {.pop.}
+
+    bar()
+
+    for i, p in s.pairs:
+      let foo = i + 1
+      doAssert p() == foo
+
 template main() =
   block: # `=>`
     block:
@@ -85,22 +102,6 @@ template main() =
           capture i, j:
             closure2 = () => (i, j)
     doAssert closure2() == (5, 3)
-
-    block: # bug #16967
-      var s = newSeq[proc (): int](5)
-      {.push exportc.}
-      proc bar() =
-        for i in 0 ..< s.len:
-          let foo = i + 1
-          capture foo:
-            s[i] = proc(): int = foo
-      {.pop.}
-
-      bar()
-
-      for i, p in s.pairs:
-        let foo = i + 1
-        doAssert p() == foo
 
     block: # issue #20679
       # this should compile. Previously was broken as `var int` is an `nnkHiddenDeref`
@@ -298,6 +299,8 @@ template main() =
       doAssert ys == @[0, 1, 2, 3, 4, 5]
 
     test()
+
+  mainProc()
 
 when not defined(js): # TODO fixme JS VM
   static:
