81c051364426fdfa4310da7692d1d99600e51d8b
Jake Leahy <jake@leahy.dev>
2023-11-21 07:12:54 +1100

    Don't provide suggestions for enum fields (#22959)
    Currently the suggestions create a lot of noise when creating enum
    fields. I don't see any way of a macro creating fields (when called
    inside an enum) so it should be safe to not show suggestions
    
    ---------
    
    Co-authored-by: Andreas Rumpf <rumpf_a@web.de>

--- compiler/suggest.nim
+++ compiler/suggest.nim
@@ -732,6 +732,9 @@ proc suggestDecl*(c: PContext, n: PNode; s: PSym) =
   if attached: inc(c.inTypeContext)
   defer:
     if attached: dec(c.inTypeContext)
+  # If user is typing out an enum field, then don't provide suggestions
+  if s.kind == skEnumField and c.config.cmd == cmdIdeTools and exactEquals(c.config.m.trackPos, n.info):
+    suggestQuit()
   suggestExpr(c, n)
 
 proc suggestStmt*(c: PContext, n: PNode) =
