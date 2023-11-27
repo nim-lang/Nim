ebb251df714ff878a06797310142f005671b296f
Jake Leahy <jake@leahy.dev>
2023-11-27 19:21:58 +1100

    Fix misspelling in test

--- nimsuggest/tests/tuse_enum.nim
+++ nimsuggest/tests/tuse_enum.nim
@@ -1,8 +1,8 @@
 discard """
 $nimsuggest --tester $file
 >use $1
-def;;skEnum;;tuse_enum.Red;;Colour;;$file;;10;;4;;"";;100
-use;;skEnum;;tuse_enum.Red;;Colour;;$file;;14;;8;;"";;100
+def;;skEnumField;;tuse_enum.Colour.Red;;Colour;;$file;;10;;4;;"";;100
+use;;skEnumField;;tuse_enum.Colour.Red;;Colour;;$file;;14;;8;;"";;100
 """
 
 type
