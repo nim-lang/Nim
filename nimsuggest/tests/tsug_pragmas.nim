e3a1d83dea257ed8698835765fd3d21327123ed9
Jake Leahy <jake@leahy.dev>
2023-12-06 15:58:33 +1100

    Add test cases for most scenarios of pragmas

--- nimsuggest/tests/tsug_pragmas.nim
+++ nimsuggest/tests/tsug_pragmas.nim
@@ -1,18 +1,40 @@
 template fooBar1() {.pragma.}
 proc fooBar2() = discard
 macro fooBar3(x: untyped) = discard
+{.pragma: fooBar4 fooBar3.}
 
-# Procs shouldn't be suggested.
-# Not perfect, but cuts down on invalid suggestions
+# Check only macros/templates/pragmas are suggested
 proc test1() {.fooBar#[!]#.} = discard
 
-# We also want to suggest built in pragmas which
-# wouldn't normally come up in suggestions
-proc test2() {.stackTrac#[!]#.} = discard
+var test2 {.fooBar#[!]#.} = 9
+
+type
+  Person {.fooBar#[!]#.} = object
+    hello {.fooBar#[!]#.}: string
+  Callback = proc test() {.fooBar#[!]#.}
 
 discard """
 $nimsuggest --tester $file
 >sug $1
-sug;;skMacro;;fooBar1;;LogLevel;;$file;;1;;9;;"";;100;;Prefix
+sug;;skTemplate;;fooBar4;;;;$file;;4;;8;;"";;100;;Prefix
+sug;;skTemplate;;tsug_pragmas.fooBar1;;template ();;$file;;1;;9;;"";;100;;Prefix
+sug;;skMacro;;tsug_pragmas.fooBar3;;macro (x: untyped){.noSideEffect, gcsafe.};;$file;;3;;6;;"";;50;;Prefix
+>sug $2
+sug;;skTemplate;;fooBar4;;;;$file;;4;;8;;"";;100;;Prefix
+sug;;skTemplate;;tsug_pragmas.fooBar1;;template ();;$file;;1;;9;;"";;100;;Prefix
+sug;;skMacro;;tsug_pragmas.fooBar3;;macro (x: untyped){.noSideEffect, gcsafe.};;$file;;3;;6;;"";;50;;Prefix
+>sug $3
+sug;;skTemplate;;fooBar4;;;;$file;;4;;8;;"";;100;;Prefix
+sug;;skTemplate;;tsug_pragmas.fooBar1;;template ();;$file;;1;;9;;"";;100;;Prefix
+sug;;skMacro;;tsug_pragmas.fooBar3;;macro (x: untyped){.noSideEffect, gcsafe.};;$file;;3;;6;;"";;50;;Prefix
+>sug $4
+sug;;skTemplate;;fooBar4;;;;$file;;4;;8;;"";;100;;Prefix
+sug;;skTemplate;;tsug_pragmas.fooBar1;;template ();;$file;;1;;9;;"";;100;;Prefix
+sug;;skMacro;;tsug_pragmas.fooBar3;;macro (x: untyped){.noSideEffect, gcsafe.};;$file;;3;;6;;"";;50;;Prefix
+>sug $5
+sug;;skTemplate;;fooBar4;;;;$file;;4;;8;;"";;100;;Prefix
+sug;;skTemplate;;tsug_pragmas.fooBar1;;template ();;$file;;1;;9;;"";;100;;Prefix
+sug;;skMacro;;tsug_pragmas.fooBar3;;macro (x: untyped){.noSideEffect, gcsafe.};;$file;;3;;6;;"";;50;;Prefix
 """
+
 
