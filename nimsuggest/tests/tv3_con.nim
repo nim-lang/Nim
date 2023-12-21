c7b513ae0120f808c7a825bb4ea9475ed9216c2d
Juan M Gómez <info@jmgomez.me>
2023-12-21 20:35:46 +0000

    Update nimsuggest/tests/tv3_con.nim
    Co-authored-by: Jake Leahy <jake@leahy.dev>

--- nimsuggest/tests/tv3_con.nim
+++ nimsuggest/tests/tv3_con.nim
@@ -8,6 +8,6 @@ test(#[!]#
 discard """
 $nimsuggest --v3 --tester $file
 >con $1
-con     skProc  tv3_con.test    proc (a: string, b: string)     $file      3       5       ""      100
-con     skProc  tv3_con.test    proc (a: int)    $file    4       5       ""      100
+con;;skProc;;tv3_con.test;;proc (a: string, b: string);;$file;;3;;5;;"";;100
+con;;skProc;;tv3_con.test;;proc (a: int);;$file;;4;;5;;"";;100
 """
