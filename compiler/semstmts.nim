a997f24995abf9b3cc5396cf665ff75dbec7e234
ringabout <43030857+ringabout@users.noreply.github.com>
2024-01-08 11:34:52 +0000

    fixes #23180; prohibits invalid tuple unpacking code

--- compiler/semstmts.nim
+++ compiler/semstmts.nim
@@ -932,6 +932,8 @@ proc semForVars(c: PContext, n: PNode; flags: TExprFlags): PNode =
       if n[0].kind == nkVarTuple:
         if n[0].len-1 != iterAfterVarLent.len:
           return localErrorNode(c, n, n[0].info, errWrongNumberOfVariables)
+        elif n[1].typ.kind != tyTuple:
+          return localErrorNode(c, n, n[0].info, "Expect tuple type, but got: " & $n[1].typ)
 
         for i in 0..<n[0].len-1:
           var v = symForVar(c, n[0][i])
