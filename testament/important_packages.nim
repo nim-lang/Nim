a43efdb04082a083478a0028cfc6f2df3889a3ce
ringabout <43030857+ringabout@users.noreply.github.com>
2024-01-11 13:37:30 +0800

    Apply suggestions from code review

--- testament/important_packages.nim
+++ testament/important_packages.nim
@@ -46,7 +46,7 @@ pkg "BipBuffer"
 pkg "blscurve", allowFailure = true
 pkg "bncurve"
 pkg "brainfuck", "nim c -d:release -r tests/compile.nim"
-pkg "bump", "nimble --accept install https://github.com/disruptek/balls; nim c --mm:arc --path:. -r tests/tbump.nim", "https://github.com/disruptek/bump"
+pkg "bump", "nimble --accept install https://github.com/disruptek/balls; balls --path=.", "https://github.com/disruptek/bump"
 pkg "c2nim", "nim c testsuite/tester.nim"
 pkg "cascade"
 pkg "cello", url = "https://github.com/nim-lang/cello", useHead = true
