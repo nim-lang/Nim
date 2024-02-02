nim-lang/Nim

commit 866e1e62583e8ed1d8b36b5ff5690a32e1451b0f

* refactored the SuggestFileSymbolDatabase structure, so that it contains   caughtExceptions as a separate array. This will allow for the memory for   caughtExceptions not to be allocated, when the exception inlay hints are not   enabled (which is not done, yet). Many things are refactored to allow   converting SuggestFileSymbolDatabase from an array of structs, to a struct of   arrays. Extracting further fields as separate arrays will result in further   reduction of used memory, even when inlay hints for exceptions are turned off,   since the SymInfoPair structure wastes too much memory, due to alightment   padding.

nimsuggest/nimsuggest.nim
@@ -803,12 +803,39 @@ func deduplicateSymInfoPair[SymInfoPair](xs: seq[SymInfoPair]): seq[SymInfoPair]
       result.add(itm)
   result.reverse()
 
+func deduplicateSymInfoPair(xs: SuggestFileSymbolDatabase): SuggestFileSymbolDatabase =
+  # xs contains duplicate items and we want to filter them by range because the
+  # sym may not match. This can happen when xs contains the same definition but
+  # with different signature because suggestSym might be called multiple times
+  # for the same symbol (e. g. including/excluding the pragma)
+  result = SuggestFileSymbolDatabase(
+    items: newSeqOfCap[InternalSymInfoPair](xs.items.len),
+    caughtExceptions: newSeqOfCap[seq[PType]](xs.caughtExceptions.len),
+    isSorted: false
+  )
+  var i = xs.items.high
+  while i > 0:
+    let itm = xs.items[i]
+    var found = false
+    for res in result.items:
+      if res.info.exactEquals(itm.info):
+        found = true
+        break
+    if not found:
+      result.items.add(itm)
+      result.caughtExceptions.add(xs.caughtExceptions[i])
+    dec i
+  result.items.reverse()
+  result.caughtExceptions.reverse()
+
 proc findSymData(graph: ModuleGraph, trackPos: TLineInfo):
     ref SymInfoPair =
-  for s in graph.fileSymbols(trackPos.fileIndex).deduplicateSymInfoPair:
-    if isTracked(s.info, trackPos, s.sym.name.s.len):
+  let db = graph.fileSymbols(trackPos.fileIndex).deduplicateSymInfoPair
+  for i in db.items.low..db.items.high:
+    if isTracked(db.items[i].info, trackPos, db.items[i].sym.name.s.len):
+      var res = db.getSymInfoPair(i)
       new(result)
-      result[] = s
+      result[] = res
       break
 
 func isInRange*(current, startPos, endPos: TLineInfo, tokenLen: int): bool =
@@ -819,9 +846,10 @@ func isInRange*(current, startPos, endPos: TLineInfo, tokenLen: int): bool =
 proc findSymDataInRange(graph: ModuleGraph, startPos, endPos: TLineInfo):
     seq[SymInfoPair] =
   result = newSeq[SymInfoPair]()
-  for s in graph.fileSymbols(startPos.fileIndex).deduplicateSymInfoPair:
-    if isInRange(s.info, startPos, endPos, s.sym.name.s.len):
-      result.add(s)
+  let db = graph.fileSymbols(startPos.fileIndex).deduplicateSymInfoPair
+  for i in db.items.low..db.items.high:
+    if isInRange(db.items[i].info, startPos, endPos, db.items[i].sym.name.s.len):
+      result.add(db.getSymInfoPair(i))
 
 proc findSymData(graph: ModuleGraph, file: AbsoluteFile; line, col: int):
     ref SymInfoPair =
@@ -939,15 +967,16 @@ proc findDef(n: PNode, line: uint16, col: int16): PNode =
       let res = findDef(n[i], line, col)
       if res != nil: return res
 
-proc findByTLineInfo(trackPos: TLineInfo, infoPairs: seq[SymInfoPair]):
+proc findByTLineInfo(trackPos: TLineInfo, infoPairs: SuggestFileSymbolDatabase):
     ref SymInfoPair =
-  for s in infoPairs:
+  for i in infoPairs.items.low..infoPairs.items.high:
+    let s = infoPairs.getSymInfoPair(i)
     if s.info.exactEquals trackPos:
       new(result)
       result[] = s
       break
 
-proc outlineNode(graph: ModuleGraph, n: PNode, endInfo: TLineInfo, infoPairs: seq[SymInfoPair]): bool =
+proc outlineNode(graph: ModuleGraph, n: PNode, endInfo: TLineInfo, infoPairs: SuggestFileSymbolDatabase): bool =
   proc checkSymbol(sym: PSym, info: TLineInfo): bool =
     result = (sym.owner.kind in {skModule, skType} or sym.kind in {skProc, skMethod, skIterator, skTemplate, skType})
 
@@ -961,7 +990,7 @@ proc outlineNode(graph: ModuleGraph, n: PNode, endInfo: TLineInfo, infoPairs: se
        graph.suggestResult(sym, sym.info, ideOutline, endInfo.line, endInfo.col)
        return true
 
-proc handleIdentOrSym(graph: ModuleGraph, n: PNode, endInfo: TLineInfo, infoPairs: seq[SymInfoPair]): bool =
+proc handleIdentOrSym(graph: ModuleGraph, n: PNode, endInfo: TLineInfo, infoPairs: SuggestFileSymbolDatabase): bool =
   for child in n:
     if child.kind in {nkIdent, nkSym}:
       if graph.outlineNode(child, endInfo, infoPairs):
@@ -970,7 +999,7 @@ proc handleIdentOrSym(graph: ModuleGraph, n: PNode, endInfo: TLineInfo, infoPair
       if graph.handleIdentOrSym(child, endInfo, infoPairs):
         return true
 
-proc iterateOutlineNodes(graph: ModuleGraph, n: PNode, infoPairs: seq[SymInfoPair]) =
+proc iterateOutlineNodes(graph: ModuleGraph, n: PNode, infoPairs: SuggestFileSymbolDatabase) =
   var matched = true
   if n.kind == nkIdent:
     let symData = findByTLineInfo(n.info, infoPairs)
@@ -1075,7 +1104,11 @@ proc executeNoHooksV3(cmd: IdeCmd, file: AbsoluteFile, dirtyfile: AbsoluteFile, 
   of ideHighlight:
     let sym = graph.findSymData(file, line, col)
     if not sym.isNil:
-      let usages = graph.fileSymbols(fileIndex).filterIt(it.sym == sym.sym)
+      let fs = graph.fileSymbols(fileIndex)
+      var usages: seq[SymInfoPair] = @[]
+      for i in fs.items.low..fs.items.high:
+        if fs.items[i].sym == sym.sym:
+          usages.add(fs.getSymInfoPair(i))
       myLog fmt "Found {usages.len} usages in {file.string}"
       for s in usages:
         graph.suggestResult(s.sym, s.info)
@@ -1144,9 +1177,10 @@ proc executeNoHooksV3(cmd: IdeCmd, file: AbsoluteFile, dirtyfile: AbsoluteFile, 
       # find first mention of the symbol in the file containing the definition.
       # It is either the definition or the declaration.
       var first: SymInfoPair
-      for symbol in graph.fileSymbols(s.sym.info.fileIndex).deduplicateSymInfoPair:
-        if s.sym.symbolEqual(symbol.sym):
-          first = symbol
+      let db = graph.fileSymbols(s.sym.info.fileIndex).deduplicateSymInfoPair
+      for i in db.items.low..db.items.high:
+        if s.sym.symbolEqual(db.items[i].sym):
+          first = db.getSymInfoPair(i)
           break
 
       if s.info.exactEquals(first.info):

compiler/modulegraphs.nim
@@ -55,6 +55,12 @@ type
     concreteTypes*: seq[FullId]
     inst*: PInstantiation
 
+  InternalSymInfoPair* = object
+    sym*: PSym
+    info*: TLineInfo
+    isDecl*: bool
+    caughtExceptionsSet*: bool
+
   SymInfoPair* = object
     sym*: PSym
     info*: TLineInfo
@@ -77,7 +83,8 @@ type
     Docgen2Pass
 
   SuggestFileSymbolDatabase* = object
-    items*: seq[SymInfoPair]
+    items*: seq[InternalSymInfoPair]
+    caughtExceptions*: seq[seq[PType]]
     isSorted*: bool
 
   SuggestSymbolDatabase* = Table[FileIndex, SuggestFileSymbolDatabase]
@@ -160,6 +167,15 @@ type
                  process: TPassProcess,
                  close: TPassClose,
                  isFrontend: bool]
+
+proc getSymInfoPair*(s: SuggestFileSymbolDatabase; idx: int): SymInfoPair =
+  SymInfoPair(
+    sym: s.items[idx].sym,
+    info: s.items[idx].info,
+    caughtExceptions: s.caughtExceptions[idx],
+    caughtExceptionsSet: s.items[idx].caughtExceptionsSet,
+    isDecl: s.items[idx].isDecl
+  )
 
 proc resetForBackend*(g: ModuleGraph) =
   g.compilerprocs = initStrTable()
@@ -504,6 +520,7 @@ proc initOperators*(g: ModuleGraph): Operators =
 proc newSuggestFileSymbolDatabase*(): SuggestFileSymbolDatabase =
   SuggestFileSymbolDatabase(
     items: @[],
+    caughtExceptions: @[],
     isSorted: true
   )
 
@@ -726,7 +743,7 @@ func belongsToStdlib*(graph: ModuleGraph, sym: PSym): bool =
 proc `==`*(a, b: SymInfoPair): bool =
   result = a.sym == b.sym and a.info.exactEquals(b.info)
 
-func cmp*(a: SymInfoPair; b: SymInfoPair): int =
+func cmp*(a: InternalSymInfoPair; b: InternalSymInfoPair): int =
   result = cmp(int32(a.info.fileIndex), int32(b.info.fileIndex))
   if result == 0:
     result = cmp(a.info.line, b.info.line)
@@ -734,36 +751,91 @@ func cmp*(a: SymInfoPair; b: SymInfoPair): int =
     result = cmp(a.info.col, b.info.col)
   if result == 0:
     result = cmp(a.isDecl, b.isDecl)
+
+proc exchange(s: var SuggestFileSymbolDatabase; i, j: int) =
+  if i == j:
+    return
+  var tmp1 = s.items[i]
+  s.items[i] = s.items[j]
+  s.items[j] = tmp1
+  if s.caughtExceptions.len > 0:
+    var tmp2 = s.caughtExceptions[i]
+    s.caughtExceptions[i] = s.caughtExceptions[j]
+    s.caughtExceptions[j] = tmp2
+
+proc quickSort(s: var SuggestFileSymbolDatabase; ll, rr: int) =
+  var
+    i, j, pivotIdx: int
+    l = ll
+    r = rr
+  while true:
+    i = l
+    j = r
+    pivotIdx = l + ((r - l) shr 1)
+    while true:
+      while (i < pivotIdx) and (cmp(s.items[pivotIdx], s.items[i]) > 0):
+        inc i
+      while (j > pivotIdx) and (cmp(s.items[pivotIdx], s.items[j]) < 0):
+        dec j
+      if i < j:
+        s.exchange(i, j)
+        if pivotIdx == i:
+          pivotIdx = j
+          inc i
+        elif pivotIdx == j:
+          pivotIdx = i
+          dec j
+        else:
+          inc i
+          dec j
+      else:
+        break
+    if (pivotIdx - l) < (r - pivotIdx):
+      if (l + 1) < pivotIdx:
+        s.quickSort(l, pivotIdx - 1)
+      l = pivotIdx + 1
+    else:
+      if (pivotIdx + 1) < r:
+        s.quickSort(pivotIdx + 1, r)
+      if (l + 1) < pivotIdx:
+        r = pivotIdx - 1
+      else:
+        break
+    if l >= r:
+      break
+
 proc sort*(s: var SuggestFileSymbolDatabase) =
-  s.items.sort(cmp)
+  s.quickSort(s.items.low, s.items.high)
   s.isSorted = true
 
 proc add*(s: var SuggestFileSymbolDatabase; v: SymInfoPair) =
-  s.items.add(v)
+  s.items.add(InternalSymInfoPair(
+    sym: v.sym,
+    info: v.info,
+    isDecl: v.isDecl,
+    caughtExceptionsSet: v.caughtExceptionsSet
+  ))
+  s.caughtExceptions.add(v.caughtExceptions)
   s.isSorted = false
 
 proc add*(s: var SuggestSymbolDatabase; v: SymInfoPair) =
   s.mgetOrPut(v.info.fileIndex, newSuggestFileSymbolDatabase()).add(v)
 
-proc findSymInfo*(s: var SuggestFileSymbolDatabase; li: TLineInfo): ptr SymInfoPair =
+proc findSymInfoIndex*(s: var SuggestFileSymbolDatabase; li: TLineInfo): int =
   if not s.isSorted:
     s.sort()
-  var q = SymInfoPair(
+  var q = InternalSymInfoPair(
     info: li
   )
   var idx = binarySearch(s.items, q, cmp)
-  if idx != -1:
-    result = addr s.items[idx]
-  else:
-    result = nil
 
-proc fileSymbols*(graph: ModuleGraph, fileIdx: FileIndex): seq[SymInfoPair] =
-  result = graph.suggestSymbols.getOrDefault(fileIdx, newSuggestFileSymbolDatabase()).items
+proc fileSymbols*(graph: ModuleGraph, fileIdx: FileIndex): SuggestFileSymbolDatabase =
+  result = graph.suggestSymbols.getOrDefault(fileIdx, newSuggestFileSymbolDatabase())
 
 iterator suggestSymbolsIter*(g: ModuleGraph): SymInfoPair =
   for xs in g.suggestSymbols.values:
-    for x in xs.items:
-      yield x
+    for i in xs.items.low..xs.items.high:
+      yield xs.getSymInfoPair(i)
 
 iterator suggestErrorsIter*(g: ModuleGraph): Suggest =
   for xs in g.suggestErrors.values:

compiler/sempass2.nim
@@ -957,12 +957,12 @@ proc checkForSink(tracked: PEffects; n: PNode) =
 proc markCaughtExceptions(tracked: PEffects; g: ModuleGraph; info: TLineInfo; s: PSym; usageSym: var PSym) =
   when defined(nimsuggest):
     proc internalMarkCaughtExceptions(tracked: PEffects; q: var SuggestFileSymbolDatabase; info: TLineInfo) =
-      var si = q.findSymInfo(info)
-      if not isNil(si):
-        si.caughtExceptionsSet = true
+      var si = q.findSymInfoIndex(info)
+      if si != -1:
+        #q.items[si].caughtExceptionsSet = true
         for w1 in tracked.caughtExceptions.nodes:
           for w2 in w1:
-            si.caughtExceptions.add(w2)
+            q.caughtExceptions[si].add(w2)
 
     internalMarkCaughtExceptions(tracked, g.suggestSymbols.mgetOrPut(info.fileIndex, newSuggestFileSymbolDatabase()), info)
 

commit a365c1c7d3def034a4a8413e6f70bd8a856f84f0

* fixed off-by-one bug, accidentally introduced in commit 866e1e62583e8ed1d8b36b5ff5690a32e1451b0f

nimsuggest/nimsuggest.nim
@@ -827,7 +827,7 @@ func deduplicateSymInfoPair(xs: SuggestFileSymbolDatabase): SuggestFileSymbolDat
     isSorted: false
   )
   var i = xs.lineInfo.high
-  while i > 0:
+  while i >= 0:
     let itm = xs.lineInfo[i]
     var found = false
     for res in result.lineInfo: