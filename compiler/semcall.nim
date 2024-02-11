cd4a78c1f8ea1d0ed33dc11acf39732680c9c085
jmgomez <info@jmgomez.me>
2024-02-11 12:53:25 +0000

    revert test

--- compiler/semcall.nim
+++ compiler/semcall.nim
@@ -58,10 +58,8 @@ proc initCandidateSymbols(c: PContext, headSymbol: PNode,
       ]#
       for paramSym in searchInScopesAllCandidatesFilterBy(c, symx.name, {skConst}):
         let paramTyp = paramSym.typ
-        if paramTyp.sym == nil and paramTyp.n.kind == nkIntLit: continue 
         if paramTyp.n.sym.kind in filter:
           result.add((paramTyp.n.sym, o.lastOverloadScope))
-
 
     symx = nextOverloadIter(o, c, headSymbol)
   if result.len > 0:
