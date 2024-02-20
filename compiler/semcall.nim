7bf8cd3f8654c152b87052e28c515e525b43b38c
Juan M Gómez <info@jmgomez.me>
2024-02-20 06:36:50 +0000

    Fixes a nimsuggest crash when using chronos (#23293)
    The following would crash nimsuggest on init:
    ```nim
    import chronos
    type
      HistoryQuery = object
        start: int
        limit: int
    
      HistoryResult = object
        messages: string
    
    type HistoryQueryHandler* = proc(req: HistoryQuery): Future[HistoryResult] {.async, gcsafe.}
    ```

--- compiler/semcall.nim
+++ compiler/semcall.nim
@@ -58,9 +58,8 @@ proc initCandidateSymbols(c: PContext, headSymbol: PNode,
       ]#
       for paramSym in searchInScopesAllCandidatesFilterBy(c, symx.name, {skConst}):
         let paramTyp = paramSym.typ
-        if paramTyp.n.sym.kind in filter:
+        if paramTyp.n.kind == nkSym and paramTyp.n.sym.kind in filter:
           result.add((paramTyp.n.sym, o.lastOverloadScope))
-
 
     symx = nextOverloadIter(o, c, headSymbol)
   if result.len > 0:
