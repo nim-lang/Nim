2e188682895daf4c6368af00aeb235055ec7701c
ringabout <43030857+ringabout@users.noreply.github.com>
2024-02-28 14:51:13 +0000

    fixes `isLastRead`

--- compiler/injectdestructors.nim
+++ compiler/injectdestructors.nim
@@ -163,7 +163,7 @@ proc isLastReadImpl(n: PNode; c: var Con; scope: var Scope): bool =
     result = false
 
 proc isLastRead(n: PNode; c: var Con; s: var Scope): bool =
-  if not hasDestructor(c, n.typ): return true
+  if not hasDestructor(c, n.typ) and isTrival(getAttachedOp(c.graph, n.typ, attachedAsgn)): return true
 
   let m = skipConvDfa(n)
   result = (m.kind == nkSym and sfSingleUsedTemp in m.sym.flags) or
