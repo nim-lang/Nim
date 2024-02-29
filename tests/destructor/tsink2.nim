47fe80cf42c2233ea9c275ef6561a5c62c85b462
ringabout <43030857+ringabout@users.noreply.github.com>
2024-02-29 14:08:33 +0000

    adds a test case

---
+++ tests/destructor/tsink2.nim
@@ -0,0 +1,18 @@
+block: # bug #23354
+  type AnObject = object of RootObj
+    value: int
+
+  proc mutate(a: sink AnObject) =
+    a.value = 1
+
+  var obj = AnObject(value: 42)
+  mutate(obj)
+  doAssert obj.value == 42
+
+block: # bug #12340
+  func consume(x: sink seq[int]) =
+    x[0] += 5
+
+  let x = @[1, 2, 3, 4]
+  consume x
+  doAssert x == @[1, 2, 3, 4]
