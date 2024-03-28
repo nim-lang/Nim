5f8e08031dd411d0d20715c8d0ddc363eb80c27f
ringabout <43030857+ringabout@users.noreply.github.com>
2024-03-28 18:04:12 +0800

    fixes #23429; rework `--verbosity` with warnings/hints (#23441)
    fixes #23429
    
    (cherry picked from commit a24990bd8cca735fa1805279a7cd3f42e22240cd)

--- compiler/cmdlinehelper.nim
+++ compiler/cmdlinehelper.nim
@@ -55,6 +55,11 @@ proc loadConfigsAndProcessCmdLine*(self: NimProg, cache: IdentCache; conf: Confi
   if conf.cmd == cmdNimscript:
     incl(conf.globalOptions, optWasNimscript)
   loadConfigs(DefaultConfig, cache, conf, graph.idgen) # load all config files
+  # restores `conf.notes` after loading config files
+  # because it has overwrites the notes when compiling the system module which
+  # is a foreign module compared to the project
+  if conf.cmd in cmdBackends:
+    conf.notes = conf.mainPackageNotes
 
   if not self.suggestMode:
     let scriptFile = conf.projectFull.changeFileExt("nims")
