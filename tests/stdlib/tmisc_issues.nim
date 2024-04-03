55f1dd5b76a252fbbd720ddf0d4489cb8fc36a37
ringabout <43030857+ringabout@users.noreply.github.com>
2024-04-03 12:53:17 +0000

    fixes #16771; lower `swap` for JS backend

--- tests/stdlib/tmisc_issues.nim
+++ tests/stdlib/tmisc_issues.nim
@@ -18,3 +18,22 @@ type
 
 var x: Object = Object(data: Test(Data(id: 12)))
 doAssert Data(x.data).id == 12
+
+block: # bug #16771
+  type A = object
+    n: int
+
+  proc foo(a, b: var A) =
+    swap a, b
+
+  var a, b: A
+  a.n = 42
+  b.n = 1
+  doAssert a.n == 42
+  doAssert b.n == 1
+  a.swap b
+  doAssert a.n == 1
+  doAssert b.n == 42
+  a.foo b
+  doAssert a.n == 42
+  doAssert b.n == 1
