8ebb4dc30af3a5d3dd14bc651698d02b715befed
narimiran <narimiran@disroot.org>
2024-04-16 15:49:03 +0200

    bump NimVersion to 1.6.21

--- lib/compilation.nim
+++ lib/compilation.nim
@@ -10,6 +10,6 @@ const
     ## is the minor number of Nim's version.
     ## Odd for devel, even for releases.
 
-  NimPatch* {.intdefine.}: int = 20
+  NimPatch* {.intdefine.}: int = 21
     ## is the patch number of Nim's version.
     ## Odd for devel, even for releases.
