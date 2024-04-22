8c2222ce7094c70b26e5088f95c710c041bc3320
ringabout <43030857+ringabout@users.noreply.github.com>
2024-04-22 14:52:12 +0000

    `cursor` is already checked in `isAnalysableFieldAccess`

--- compiler/injectdestructors.nim
+++ compiler/injectdestructors.nim
@@ -1188,7 +1188,7 @@ proc moveOrCopy(dest, ri: PNode; c: var Con; s: var Scope, flags: set[MoveOrCopy
         result = newTree(nkStmtList, snk, c.genWasMoved(ri))
       elif ri.sym.kind != skParam and
           isAnalysableFieldAccess(ri, c.owner) and
-          isLastRead(ri, c, s) and canBeMoved(c, dest.typ) and not isCursor(ri):
+          isLastRead(ri, c, s) and canBeMoved(c, dest.typ):
         # Rule 3: `=sink`(x, z); wasMoved(z)
         let snk = c.genSink(s, dest, ri, flags)
         result = newTree(nkStmtList, snk, c.genWasMoved(ri))
