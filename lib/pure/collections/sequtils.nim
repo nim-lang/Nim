b844ab52d2d9f4f3ff8e3c4b04d28ed6569a33b6
Amjad Ben Hedhili <amjadhedhili@outlook.com>
2024-05-24 11:40:08 +0100

    Remove `since` annotations

--- lib/pure/collections/sequtils.nim
+++ lib/pure/collections/sequtils.nim
@@ -248,8 +248,7 @@ func minIndex*[T](s: openArray[T]): int {.since: (1, 1).} =
   for i in 1..high(s):
     if s[i] < s[result]: result = i
 
-func minIndex*[T](s: openArray[T], cmp: proc(a, b: T): bool {.closure.}): int {.
-  since: (1, 1), effectsOf: cmp.} =
+func minIndex*[T](s: openArray[T], cmp: proc(a, b: T): bool {.closure.}): int {.effectsOf: cmp.} =
   ## Returns the index of the minimum value of `s`.
   ## `cmp` should return true if `a` is *less* than `b`.
   runnableExamples:
@@ -281,8 +280,7 @@ func maxIndex*[T](s: openArray[T]): int {.since: (1, 1).} =
   for i in 1..high(s):
     if s[i] > s[result]: result = i
 
-func maxIndex*[T](s: openArray[T], cmp: proc(a, b: T): bool {.closure.}): int {.
-  since: (1, 1), effectsOf: cmp.} =
+func maxIndex*[T](s: openArray[T], cmp: proc(a, b: T): bool {.closure.}): int {.effectsOf: cmp.} =
   ## Returns the index of the maximum value of `s`.
   ## `cmp` should return true if `a` is *less* than `b`.
   runnableExamples:
