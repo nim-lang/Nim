85f3f184b163e3ef0d8dbbb7efc174bec9bc0f44
Juan M Gómez <info@jmgomez.me>
2024-05-25 15:30:21 +0100

    Update msgs.nim

--- compiler/msgs.nim
+++ compiler/msgs.nim
@@ -629,8 +629,7 @@ proc warningDeprecated*(conf: ConfigRef, info: TLineInfo = gCmdLineInfo, msg = "
   message(conf, info, warnDeprecated, msg)
 
 proc internalErrorImpl(conf: ConfigRef; info: TLineInfo, errMsg: string, info2: InstantiationInfo) =
-  if conf.cmd == cmdIdeTools and conf.structuredErrorHook.isNil or
-     conf.cmd == cmdCheck: return
+  if conf.cmd in { cmdIdeTools, cmdCheck } and conf.structuredErrorHook.isNil: return
   writeContext(conf, info)
   liMessage(conf, info, errInternal, errMsg, doAbort, info2)
 
