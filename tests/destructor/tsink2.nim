8b5d754caae500738eaca9b779dc3615e1cc1f1c
ringabout <43030857+ringabout@users.noreply.github.com>
2024-06-06 22:22:04 +0800

    fixes tests

--- tests/destructor/tsink2.nim
+++ tests/destructor/tsink2.nim
@@ -2,17 +2,6 @@ discard """
   matrix: "--mm:refc; --mm:arc"
 """
 
-block: # bug #23354
-  type AnObject = object of RootObj
-    value: int
-
-  proc mutate(a: sink AnObject) =
-    a.value = 1
-
-  var obj = AnObject(value: 42)
-  mutate(obj)
-  doAssert obj.value == 42
-
 block: # bug #12340
   func consume(x: sink seq[int]) =
     x[0] += 5
