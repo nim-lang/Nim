b3f0611067a2d1331f61cc700f9d3069890bcee7
Ryan McConnell <rammcconnell@gmail.com>
2024-06-28 20:15:49 -0400

    adding second test

--- tests/overload/t23755.nim
+++ tests/overload/t23755.nim
@@ -2,11 +2,25 @@ type
   BigInt[bits: static int] = object
     limbs: array[8, uint64]
 
-proc view[N](a: array[N, uint64]) =
-  discard
+block:
+  proc view[N](a: array[N, uint64]) =
+    discard
 
-proc view[N](a: var array[N, uint64]) =
-  discard
+  proc view[N](a: var array[N, uint64]) =
+    discard
 
-var r: BigInt[64]
-r.limbs.view()
+  var r: BigInt[64]
+  r.limbs.view()
+
+
+type Limbs[N: static int] = array[N, uint64]
+
+block:
+  proc view(a: Limbs) =
+    discard
+
+  proc view(a: var Limbs) =
+    discard
+
+  var r: BigInt[64]
+  r.limbs.view()
