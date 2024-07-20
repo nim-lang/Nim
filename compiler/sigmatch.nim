1920a3d8d10da1101ebebf779d2735182410090f
metagn <metagngn@gmail.com>
2024-07-19 21:42:00 -0600

    bypass constraints for tyFromExpr in generic bodies
    fixes #19819, fixes #23339

--- compiler/sigmatch.nim
+++ compiler/sigmatch.nim
@@ -1196,10 +1196,15 @@ proc typeRel(c: var TCandidate, f, aOrig: PType,
         return isGeneric
   of tyFromExpr:
     if c.c.inGenericContext > 0:
-      # generic type bodies can sometimes compile call expressions
-      # prevent expressions with unresolved types from
-      # being passed as parameters
-      return isNone
+      if not c.isNoCall:
+        # generic type bodies can sometimes compile call expressions
+        # prevent expressions with unresolved types from
+        # being passed as parameters
+        return isNone
+      else:
+        # Foo[templateCall(T)] shouldn't fail early if Foo has a constraint
+        # and we can't evaluate `templateCall(T)` yet
+        return isGeneric
   else: discard
 
   case f.kind
