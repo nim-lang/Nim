4a43ef78052d9d588f78cf3a9b704816265445bc
Buldram <buldram@proton.me>
2024-07-22 23:48:19 -0400

    Add test

--- tests/openarray/topenarray.nim
+++ tests/openarray/topenarray.nim
@@ -83,6 +83,19 @@ proc main =
       var d: array[1, int]
       foo task(d)[].toOpenArray(0, 0)
 
+proc runtimeOnly =
+  block: # issue 19171
+    let a = ['A']
+    proc mutB(x: var openArray[char]) =
+      x[0] = 'B'
+    mutB(toOpenArray(cast[ptr UncheckedArray[char]](addr a), 0, 0))
+    doAssert a[0] == 'B'
+    proc mutC(x: var openArray[char]; c: char) =
+      x[0] = c
+    let p = cast[ptr UncheckedArray[char]](addr a)
+    mutC(toOpenArray(p, 0, 0), 'C')
+    doAssert p[0] == 'C'
 
 main()
 static: main()
+runtimeOnly()
