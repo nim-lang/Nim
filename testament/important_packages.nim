fd98ddaa9ece193f03dedd81a94e0a3b37cf3087
ringabout <43030857+ringabout@users.noreply.github.com>
2024-08-14 22:10:48 +0800

    disable nimfp

--- testament/important_packages.nim
+++ testament/important_packages.nim
@@ -113,7 +113,7 @@ pkg "nigui", "nim c -o:niguii -r src/nigui.nim"
 pkg "nimcrypto", "nim r --path:. tests/testall.nim" # `--path:.` workaround needed, see D20210308T165435
 pkg "NimData", "nim c -o:nimdataa src/nimdata.nim"
 pkg "nimes", "nim c src/nimes.nim"
-pkg "nimfp", "nim c -o:nfp -r src/fp.nim"
+# pkg "nimfp", "nim c -o:nfp -r src/fp.nim"
 pkg "nimgame2", "nim c --mm:refc nimgame2/nimgame.nim"
 pkg "nimgen", "nim c -o:nimgenn -r src/nimgen/runcfg.nim"
 pkg "nimib"
