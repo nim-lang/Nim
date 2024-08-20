d38fd60db6d606104b8a8a87deff3af4e48e638b
ringabout <43030857+ringabout@users.noreply.github.com>
2024-08-20 17:53:16 +0800

    revert

--- testament/important_packages.nim
+++ testament/important_packages.nim
@@ -58,7 +58,7 @@ pkg "cligen", "nim c --path:. -r cligen.nim"
 pkg "combparser", "nimble test --mm:orc"
 pkg "compactdict"
 pkg "comprehension", "nimble test", "https://github.com/alehander92/comprehension"
-pkg "constantine", "nimble make_lib", url = "https://github.com/ringabout/constantine", useHead = true
+pkg "constantine", "nimble make_lib"
 pkg "cowstrings"
 pkg "criterion", allowFailure = true # needs testing binary
 pkg "datamancer"
