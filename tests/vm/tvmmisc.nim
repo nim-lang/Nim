6336d2681b16e089b8815fddd0e79abeda9ce85b
ringabout <43030857+ringabout@users.noreply.github.com>
2024-08-20 22:07:11 +0800

    adds a ubuntu 24.04 matrix with gcc 14 for tests (#23673)
    ref https://forum.nim-lang.org/t/11587

--- tests/vm/tvmmisc.nim
+++ tests/vm/tvmmisc.nim
@@ -355,7 +355,7 @@ block: # bug #8007
 block: # bug #14340
   block:
     proc opl3EnvelopeCalcSin0() = discard
-    type EnvelopeSinfunc = proc()
+    type EnvelopeSinfunc = proc() {.nimcall.} # todo: fixme 
     # const EnvelopeCalcSin0 = opl3EnvelopeCalcSin0 # ok
     const EnvelopeCalcSin0: EnvelopeSinfunc = opl3EnvelopeCalcSin0 # was bug
     const envelopeSin = [EnvelopeCalcSin0]
