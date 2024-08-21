2330c90337d8f35d8ef1769b73af6c3d63dd6090
metagn <metagngn@gmail.com>
2024-08-21 14:16:03 +0300

    remove t23898 merged into tambtype

--- tests/overload/m23898_2.nim
+++
@@ -1,4 +0,0 @@
-import ./m23898_1
-export m23898_1
-template K*(kind: static int): auto = typedesc[m23898_1.K]
-template B*(kind: static int): auto = typedesc[m23898_1.K]
--- tests/overload/t23898.nim
+++
@@ -1,8 +0,0 @@
-import ./m23898_2
-discard default(B(0))       # compiles
-discard default(m23898_2.B(0))     # compiles
-discard default(K(0))       # compiles
-template r() =
-  discard default(B(0))     # compiles
-  discard default(m23898_2.B(0))   # compiles
-  discard default(K(0))     # does not compile
