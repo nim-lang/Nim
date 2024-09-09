948d63f2643d1f2618c167568dbe825d976545f0
metagn <metagngn@gmail.com>
2024-09-09 13:15:06 +0300

    add tests for Dev and Nlink

--- tests/stdlib/twrongstattype.nim
+++ tests/stdlib/twrongstattype.nim
@@ -6,3 +6,9 @@ when defined(macosx) or defined(freebsd) or defined(openbsd) or defined(netbsd):
   var y: uint32
   let myUid = geteuid()
   discard myUid == uid(y)
+  proc dev(x: uint32): Dev = Dev(x)
+  let myDev = 1.Dev
+  discard myDev == dev(y)
+  proc nlink(x: uint32): Nlink = Nlink(x)
+  let myNlink = 1.Nlink
+  discard myNlink == nlink(y)
