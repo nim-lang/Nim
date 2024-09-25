7072f3965b788170a5e07a5c7b4f1afa56af6e51
metagn <metagngn@gmail.com>
2024-09-25 17:55:35 +0300

    add test for #19277

---
+++ tests/template/t19277.nim
@@ -0,0 +1,13 @@
+# issue #19277
+
+import m19277_1, m19277_2
+
+template injector(val: untyped): untyped =
+  template subtemplate: untyped = val
+  subtemplate()
+
+template methodCall(val: untyped): untyped = val
+
+{.push raises: [Defect].}
+
+foo(injector(0).methodCall())
