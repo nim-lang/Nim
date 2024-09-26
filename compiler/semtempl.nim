dd120988e73a4050d3113df82553cd4428baca5c
metagn <metagngn@gmail.com>
2024-09-26 23:28:59 +0300

    check for language pragmas instead

--- compiler/semtempl.nim
+++ compiler/semtempl.nim
@@ -541,10 +541,14 @@ proc semTemplBody(c: var TemplCtx, n: PNode): PNode =
   of nkPragma:
     for i in 0 ..< n.len:
       let x = n[i]
-      if x.kind == nkExprColonExpr:
-        x[1] = semTemplBody(c, x[1])
-      else:
+      let prag = whichPragma(x)
+      if prag == wInvalid:
+        # only sem if not a language-level pragma 
         result[i] = semTemplBody(c, x)
+      elif x.kind in nkPragmaCallKinds:
+        # is pragma, but value still needs to be checked
+        for j in 1 ..< x.len:
+          x[j] = semTemplBody(c, x[j])
   of nkBracketExpr:
     if n.typ == nil:
       # if a[b] is nested inside a typed expression, don't convert it
