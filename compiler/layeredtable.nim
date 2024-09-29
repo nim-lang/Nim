f9120a0cd95a5c5c9f3e2d955a09b55c32f3e1dd
metagn <metagngn@gmail.com>
2024-09-29 07:25:37 +0300

    CI passed, use object instead, premature optimize

--- compiler/layeredtable.nim
+++ compiler/layeredtable.nim
@@ -2,31 +2,44 @@ import std/tables
 import ast
 
 type
-  LayeredIdTable* {.acyclic.} = ref object
+  LayeredIdTable* {.acyclic.} = object
     topLayer*: TypeMapping
-    nextLayer*: LayeredIdTable
+    nextLayer*: ref LayeredIdTable
     previousLen*: int # used to track if bindings were added
 
 proc initLayeredTypeMap*(pt: sink TypeMapping = initTypeMapping()): LayeredIdTable =
-  result = LayeredIdTable()
-  result.topLayer = pt
+  result = LayeredIdTable(topLayer: pt, nextLayer: nil)
 
 proc currentLen*(pt: LayeredIdTable): int =
   pt.previousLen + pt.topLayer.len
 
 proc newTypeMapLayer*(pt: LayeredIdTable): LayeredIdTable =
-  result = LayeredIdTable(nextLayer: pt, topLayer: initTable[ItemId, PType](), previousLen: pt.currentLen)
+  result = LayeredIdTable(topLayer: initTable[ItemId, PType](), previousLen: pt.currentLen)
+  new(result.nextLayer)
+  result.nextLayer[] = pt
 
 proc setToPreviousLayer*(pt: var LayeredIdTable) =
-  pt = pt.nextLayer
+  # not splitting the expression breaks refc
+  let y = pt.nextLayer
+  pt = y[]
 
-proc lookup*(typeMap: LayeredIdTable, key: PType): PType =
-  result = nil
+proc lookup(typeMap: ref LayeredIdTable, key: ItemId): PType =
   var tm = typeMap
   while tm != nil:
-    result = getOrDefault(tm.topLayer, key.itemId)
+    result = getOrDefault(tm.topLayer, key)
     if result != nil: return
     tm = tm.nextLayer
 
-proc put*(typeMap: LayeredIdTable, key, value: PType) {.inline.} =
+proc lookup(typeMap: LayeredIdTable, key: ItemId): PType {.inline.} =
+  result = getOrDefault(typeMap.topLayer, key)
+  if result == nil and typeMap.nextLayer != nil:
+    result = lookup(typeMap.nextLayer, key)
+
+template lookup*(typeMap: ref LayeredIdTable, key: PType): PType =
+  lookup(typeMap, key.itemId)
+
+template lookup*(typeMap: LayeredIdTable, key: PType): PType =
+  lookup(typeMap, key.itemId)
+
+proc put*(typeMap: var LayeredIdTable, key, value: PType) {.inline.} =
   typeMap.topLayer[key.itemId] = value
