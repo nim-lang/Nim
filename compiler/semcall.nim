a04338f2b4362cd33364dbd1be2e4622e1c502cd
metagn <metagngn@gmail.com>
2024-10-15 19:08:01 +0300

    fix dotexpr

--- compiler/semcall.nim
+++ compiler/semcall.nim
@@ -64,14 +64,15 @@ proc initCandidateSymbols(c: PContext, headSymbol: PNode,
           result.add((paramTyp.n.sym, o.lastOverloadScope))
 
     symx = nextOverloadIter(o, c, headSymbol)
-  # add all type bound ops with this specific name
-  # may be slow to do this for every single call, but the nominal types
-  # imply a straightforward match
-  let name = considerQuotedIdent(c, headSymbol)
-  for boundOp in c.graph.typeBoundOps.getOrDefault(name, @[]):
-    if boundOp.id notin symMarker:
-      # no need to add to symMarker, type bound ops should be unique already
-      result.add((boundOp, -1))
+  if headSymbol.kind != nkDotExpr: # qualified, maybe whitelist ident/opensymchoice?
+    # add all type bound ops with this specific name
+    # may be slow to do this for every single call, but the nominal types
+    # imply a straightforward match
+    let name = considerQuotedIdent(c, headSymbol)
+    for boundOp in c.graph.typeBoundOps.getOrDefault(name, @[]):
+      if boundOp.id notin symMarker:
+        # no need to add to symMarker, type bound ops should be unique already
+        result.add((boundOp, -1))
   if result.len > 0:
     best = initCandidate(c, result[0].s, initialBinding,
                   result[0].scope, diagnostics)
