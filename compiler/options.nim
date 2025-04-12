ec1d68fc64e71ada8db2402e0aba55eaf2e7ab3e
Jake Leahy <jake@leahy.dev>
2025-04-12 16:40:25 +1000

    Allow specifiying path to use for stdin error messages (#24595)
    Implements #24569
    
    Adds `--stdinfile` flag for specifying the file to use in place of
    `stdinfile.nim` in error messages. Will enable easier integration of
    tooling with nim check
    
    (cherry picked from commit 0cba752c8a3387e46e650ecd9f62d846fc398129)

--- compiler/options.nim
+++ compiler/options.nim
@@ -404,6 +404,7 @@ type
     projectPath*: AbsoluteDir # holds a path like /home/alice/projects/nim/compiler/
     projectFull*: AbsoluteFile # projectPath/projectName
     projectIsStdin*: bool # whether we're compiling from stdin
+    stdinFile*: AbsoluteFile # Filename to use in messages for stdin
     lastMsgWasDot*: set[StdOrrKind] # the last compiler message was a single '.'
     projectMainIdx*: FileIndex # the canonical path id of the main module
     projectMainIdx2*: FileIndex # consider merging with projectMainIdx
@@ -580,6 +581,7 @@ proc newConfigRef*(): ConfigRef =
     projectPath: AbsoluteDir"", # holds a path like /home/alice/projects/nim/compiler/
     projectFull: AbsoluteFile"", # projectPath/projectName
     projectIsStdin: false, # whether we're compiling from stdin
+    stdinFile: AbsoluteFile"stdinfile",
     projectMainIdx: FileIndex(0'i32), # the canonical path id of the main module
     command: "", # the main command (e.g. cc, check, scan, etc)
     commandArgs: @[], # any arguments after the main command
