3e222de45ca0676ddc9e3c67f665cb7f4971a7f8
ringabout <43030857+ringabout@users.noreply.github.com>
2025-04-23 15:15:22 +0800

    adds a test case

---
+++ tests/generics/toptions.nim
@@ -0,0 +1,23 @@
+discard """
+  matrix: "--warningAsError:Deprecated"
+"""
+
+block: # bug #24905
+  proc y() {.deprecated.} = discard
+  proc v(_: int | int) =
+    {.push warning[Deprecated]: off.}
+    y()
+    {.pop.}
+
+  v(1)
+
+block: # bug #24903
+  block:
+    proc y() {.deprecated.} = discard
+    proc m(_: int | int) =
+      when false: y()
+
+  block:
+    proc y() {.error.} = discard
+    proc m(_: int | int) =
+      when false: y()
