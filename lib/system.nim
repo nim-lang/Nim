3fd9c986f613aaf193f932135667aaf492572266
ringabout <43030857+ringabout@users.noreply.github.com>
2025-05-20 03:40:35 +0800

    rework `nimOrcLeakDetector` (#24958)
    ref https://github.com/nim-lang/Nim/issues/22273#issuecomment-2888931920
    
    (cherry picked from commit c3f64fb12743dd02dd0541f2420e4aa386cd2144)

--- lib/system.nim
+++ lib/system.nim
@@ -1616,7 +1616,7 @@ when not defined(js) and defined(nimV2):
       align: int16
       depth: int16
       display: ptr UncheckedArray[uint32] # classToken
-      when defined(nimTypeNames) or defined(nimArcIds):
+      when defined(nimTypeNames) or defined(nimArcIds) or defined(nimOrcLeakDetector):
         name: cstring
       traceImpl: pointer
       typeInfoV1: pointer # for backwards compat, usually nil
