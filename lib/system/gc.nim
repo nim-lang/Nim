aba93615105b8243b95c534f2d7c39aa69ace997
Jacek Sieka <arnetheduck@gmail.com>
2025-06-18 14:38:01 +0200

    Ensure that gc interface remains non-raising (#25006)
    GC_fullCollect in particular has an annoying `Exception` effect

--- lib/system/gc.nim
+++ lib/system/gc.nim
@@ -62,6 +62,7 @@ comparisons).
 ]#
 
 {.push profiler:off.}
+{.push raises: [].}
 
 const
   CycleIncrease = 2 # is a multiplicative increase
@@ -914,4 +915,5 @@ when not defined(useNimRtl):
         result.add "[GC] stack bottom: " & gch.stack.bottom.repr
       result.add "[GC] max stack size: " & $gch.stat.maxStackSize & "\n"
 
+{.pop.} # raises: []
 {.pop.} # profiler: off, stackTrace: off
