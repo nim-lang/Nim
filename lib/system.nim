8ea9c6454cc9e8766c46965c982d0fd0191afe51
Jacek Sieka <arnetheduck@gmail.com>
2025-09-15 15:09:42 +0200

    remove alloc cruft (#25170)
    (cherry picked from commit 40fe59b6ef0025739998bc47ddde44d09601a861)

--- lib/system.nim
+++ lib/system.nim
@@ -2166,8 +2166,6 @@ when notJSnotNims and not gotoBasedExceptions:
     SafePoint = TSafePoint
 
 when not defined(js):
-  when declared(initAllocator):
-    initAllocator()
   when hasThreadSupport:
     when hostOS != "standalone":
       include system/threadimpl
