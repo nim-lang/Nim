52b3d64ac7195b4ef358043bd196f18f1fbe75b2
ringabout <43030857+ringabout@users.noreply.github.com>
2025-09-16 21:54:24 +0800

    fixes #25173; SinglyLinkedList.remove broken / AssertionDefect

--- lib/pure/collections/lists.nim
+++ lib/pure/collections/lists.nim
@@ -712,6 +712,8 @@ proc remove*[T](L: var SinglyLinkedList[T], n: SinglyLinkedNode[T]): bool {.disc
     L.head = n.next
     if L.tail.next == n:
       L.tail.next = L.head # restore cycle
+    if L.tail == n:
+      L.tail = nil # reset tail if we removed the last node
   else:
     var prev {.cursor.} = L.head
     while prev.next != n and prev.next != nil:
