7c0524af562e1228835556bcbbdeee8721dc779d
ringabout <43030857+ringabout@users.noreply.github.com>
2025-10-08 22:47:13 +0800

    fixes #25208; VM issue with globals and assignments

--- tests/vm/tvmmisc.nim
+++ tests/vm/tvmmisc.nim
@@ -794,3 +794,22 @@ block: # bug #23925
 static: # bug #21353
   var s: proc () = default(proc ())
   doAssert s == nil
+
+# bug #25208
+
+
+type Conf = object
+  val: int
+
+const defaultConf = Conf(val: 123)
+
+template foo2323(conf) =
+  assert conf.val == 123
+  var conf2 = conf
+  assert conf2.val == 123
+
+static:
+  var conf: Conf = defaultConf
+  conf = defaultConf  # removing this results in the expected output
+  conf.val = 2
+  foo2323(defaultConf)
