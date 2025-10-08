2136f349f40b499bc7157921cf44ce68d3d0abb9
narimiran <narimiran@disroot.org>
2025-10-08 07:03:51 +0200

    Revert "fixes #25205 #14873; resets `importc` obj with `nimZeroMem` in `specializeResetT` for `refc` (#25207)"
    This reverts commit 9628c7a4f820fec8a8cb901c6a527cd4a7012971.

--- tests/stdlib/tlocks.nim
+++ tests/stdlib/tlocks.nim
@@ -9,30 +9,3 @@ import std/assertions
 
 var m = createMyType[int]()
 doAssert m.use() == 3
-
-
-import std/locks
-
-type
-  S = object
-    r: proc()
-
-  B = object
-    d: Lock
-    w: S
-
-proc v(x: ptr B) {.exportc.} = reset(x[])
-
-type
-  Test = object
-    path: string # Removing this makes both cases work.
-    lock: Lock
-
-# A: This is not fine.
-var a = Test()
-
-proc main(): void =
-  # B: This is fine.
-  var b = Test()
-
-main()
