f7e6f4b5e5276b14a1b340b9e621ac3b9869cb89
demotomohiro <gpuppur@gmail.com>
2025-10-15 04:13:45 +0900

    fixes test code

--- tests/icnif/tencode.nim
+++ tests/icnif/tencode.nim
@@ -15,9 +15,13 @@ block:
   (sym :testmod2.0.tesfd9rxn1 +1 . . "/tmp/testnim/testmod2.nim"))
  (var
   (identdefs
-   (sym :x.0.tesvp2f3v +2 testmod.0.tesvp2f3v ueg +0) . +123))
+   (sym :x.0.tesvp2f3v +2 testmod.0.tesvp2f3v ueg +0)
+   (i -1
+    (tf c3)) +123))
  (var
   (identdefs
-   (sym :y.0.tesvp2f3v +3 testmod.0.tesvp2f3v g +0) . x.0.tesvp2f3v)))"""
+   (sym :y.0.tesvp2f3v +3 testmod.0.tesvp2f3v g +0)
+   (i -1
+    (tf c3)) x.0.tesvp2f3v)))"""
 
   assert loadNifFromBuffer(TestNif, AbsoluteFile"/tmp/testnim/testmod.nim", graph).saveNifToBuffer(graph.config) == TestNif
