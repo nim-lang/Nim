99a222d63d98aec420a813c1207400ba7eeca651
Yuriy Glukhov <yglukhov@users.noreply.github.com>
2025-10-31 16:59:24 +0100

    Respect noinit for generic types (#25250)

--- compiler/ccgtypes.nim
+++ compiler/ccgtypes.nim
@@ -247,6 +247,7 @@ proc isOrHasImportedCppType(typ: PType): bool =
   searchTypeFor(typ.skipTypes({tyRef}), isImportedCppType)
 
 proc hasNoInit(t: PType): bool =
+  let t = skipTypes(t, {tyGenericInst})
   result = t.sym != nil and sfNoInit in t.sym.flags
 
 proc getTypeDescAux(m: BModule; origTyp: PType, check: var IntSet; kind: TypeDescKind): Rope
