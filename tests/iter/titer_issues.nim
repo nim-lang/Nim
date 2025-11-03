4d3d9eccd48f0a22e0a13f45627cf170f5f5cb4a
ringabout <43030857+ringabout@users.noreply.github.com>
2025-11-03 21:32:42 +0800

    fixes #25251; SIGBUS with iterator over const Table lookup - premature temporary destruction

--- tests/iter/titer_issues.nim
+++ tests/iter/titer_issues.nim
@@ -432,3 +432,28 @@ block:
   let x = cast[typeof(aaa)](aaa)   # not even var
   for _ in x[]:
     discard
+
+import std/[tables, unicode, sequtils]
+
+const
+    myTable = {
+        "en": "abcdefghijklmnopqrstuvwxyz",
+    }.toTable
+
+proc buggyVersion(locale: string): seq[Rune] =
+    result = toSeq(runes(myTable[locale]))
+
+proc workingVersion(locale: string): seq[Rune] =
+    # string lifetime is extended
+    let str = myTable[locale]
+    result = toSeq(runes(str))
+
+# echo "Testing working version..."
+let runes2 = workingVersion("en")
+# echo "Got ", runes2.len, " runes"
+
+# echo "Testing buggy version..."
+let runes1 = buggyVersion("en")  # <-- CRASHES HERE
+
+doAssert runes1.len == runes2.len
+# echo "Got ", runes1.len, " runes"
