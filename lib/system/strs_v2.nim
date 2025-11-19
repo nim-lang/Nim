4d41a95fa9c5d9c414ad3cbfd0d1be06cdd77425
araq <araq4k@proton.me>
2025-11-19 20:35:44 +0100

    merge conflict problem

--- lib/system/strs_v2.nim
+++ lib/system/strs_v2.nim
@@ -11,8 +11,6 @@
 
 {.push overflowChecks: off, rangeChecks: off.}
 
-{.push overflowChecks: off, rangeChecks: off.}
-
 template isLiteral(s): bool = (s.p == nil) or (s.p.cap and strlitFlag) == strlitFlag
 
 template contentSize(cap): int = cap + 1 + sizeof(NimStrPayloadBase)
