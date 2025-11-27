66560840043d2ea8a96b4ce46ab55f0faed37349
Yuriy Glukhov <yglukhov@users.noreply.github.com>
2025-11-27 10:09:52 +0100

    Fixes #25261 (#25310)
    Returning or yielding from a closureiter must restore "external"
    exception, but `popCurrentException` from `blockLeaveActions` was
    getting in the way. So now `blockLeaveActions` doesn't emit
    `popCurrentException` for returns in closureiters. I'm not a fan of this
    "abstraction leakage", but don't see a better solution yet. Any input is
    much appreciated.
    
    ---------
    
    Co-authored-by: Andreas Rumpf <araq4k@proton.me>

--- compiler/ccgstmts.nim
+++ compiler/ccgstmts.nim
@@ -226,7 +226,7 @@ proc genState(p: BProc, n: PNode) =
   elif n0.kind == nkStrLit:
     p.s(cpsStmts).addLabel(n0.strVal)
 
-proc blockLeaveActions(p: BProc, howManyTrys, howManyExcepts: int) =
+proc blockLeaveActions(p: BProc, howManyTrys, howManyExcepts: int, isReturnStmt = false) =
   # Called by return and break stmts.
   # Deals with issues faced when jumping out of try/except/finally stmts.
 
@@ -258,7 +258,7 @@ proc blockLeaveActions(p: BProc, howManyTrys, howManyExcepts: int) =
 
   # Pop exceptions that was handled by the
   # except-blocks we are in
-  if noSafePoints notin p.flags:
+  if noSafePoints notin p.flags and not (isReturnStmt and isClosureIterator(p.prc.typ)):
     for i in countdown(howManyExcepts-1, 0):
       p.s(cpsStmts).addCallStmt(cgsymValue(p.module, "popCurrentException"))
 
@@ -557,7 +557,8 @@ proc genReturnStmt(p: BProc, t: PNode) =
   if (t[0].kind != nkEmpty): genStmts(p, t[0])
   blockLeaveActions(p,
     howManyTrys    = p.nestedTryStmts.len,
-    howManyExcepts = p.inExceptBlockLen)
+    howManyExcepts = p.inExceptBlockLen,
+    isReturnStmt = true)
   if (p.finallySafePoints.len > 0) and noSafePoints notin p.flags:
     # If we're in a finally block, and we came here by exception
     # consume it before we return.
