7e616077689298bba2c336e3765975f87d9412bf
araq <araq4k@proton.me>
2026-01-13 14:16:51 +0100

    prepare for NIF26 goodness

--- koch.nim
+++ koch.nim
@@ -16,7 +16,7 @@ const
   ChecksumsStableCommit = "0b8e46379c5bc1bf73d8b3011908389c60fb9b98" # 2.0.1
   SatStableCommit = "faf1617f44d7632ee9601ebc13887644925dcc01"
 
-  NimonyStableCommit = "fc8baa61b9911caf4666685a5f5ed41b9c04f6f8" # unversioned \
+  NimonyStableCommit = "e5acb82d29d5b6a82ec83a6690a6d9ea32cf8976" # unversioned \
     # Note that Nimony uses Nim as a git submodule but we don't want to install
     # Nimony's dependency to Nim as we are Nim. So a `git clone` without --recursive
     # is **required** here.
