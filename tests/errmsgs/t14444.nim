8e7886b818569ec57e2d25295b7b94a127ba0c6d
mar <gim.marcello@gmail.com>
2026-01-22 19:00:34 +0100

    update test cases for #25298

--- tests/errmsgs/t14444.nim
+++ tests/errmsgs/t14444.nim
@@ -3,7 +3,7 @@ discard """
   exitcode: "1"
   output: '''
 t14444.nim(13)           t14444
-fatal.nim(65)            sysFatal
+fatal.nim(62)            sysFatal
 Error: unhandled exception: index out of bounds, the container is empty [IndexDefect]
 '''
 """
