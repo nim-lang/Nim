9e57f4cafff9288bc66fbfbafecef9226e2f2bbe
ringabout <43030857+ringabout@users.noreply.github.com>
2026-02-05 18:42:37 +0800

    fixes #25464; gives a deprecated warning when  `=dup` is not provided while  there being a custom `=copy`

---
+++ tests/destructor/tdup_from_copy.nim
@@ -0,0 +1,40 @@
+discard """
+  matrix: "--warningaserror:deprecated"
+  errormsg: "'=dup' is not provided while a custom '=copy' is defined for type 'Foo'; this will become a compile time error in the future"
+"""
+
+type Foo = distinct int
+
+var counter = 0
+
+proc `=destroy`(pkt: var Foo) =
+  if cast[int](pkt) != 0:
+    echo cast[int](pkt)
+
+proc `=copy`(a: var Foo, b: Foo) =
+  if cast[int](a) == cast[int](b):
+    return
+
+  `=destroy`(a)
+  if cast[int](b) == 0:
+    zeroMem(addr a, sizeof(Foo))
+  else:
+    counter += 1
+    copyMem(addr a, addr counter, sizeof(Foo))
+    echo "copy!"
+
+proc makeFoo(): Foo =
+  counter += 1
+  cast[Foo](counter)
+
+
+type Bar = object
+  val: Foo
+
+
+proc consume(x: sink Bar) =
+  discard
+
+let x = Bar(val: makeFoo())
+consume(x)
+discard x
