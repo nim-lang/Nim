29125f0bc798252784ca2bdf537520c538670c04
ringabout <43030857+ringabout@users.noreply.github.com>
2026-02-09 18:50:45 +0800

    fixes #25494; [regression] Crash on enum ranges as default parameters in generic procs (#25496)
    fixes #25494;
    
    (cherry picked from commit ae5f864bff5b83799424f404955c8affe34267ae)

--- compiler/semexprs.nim
+++ compiler/semexprs.nim
@@ -813,7 +813,7 @@ proc semArrayConstr(c: PContext, n: PNode, flags: TExprFlags; expectedType: PTyp
       inc(lastIndex)
     if isGeneric:
       for i in 0..<result.len:
-        if isIntLit(result[i].typ):
+        if result[i].typ != nil and isIntLit(result[i].typ):
           # generic instantiation strips int lit type which makes conversions fail
           result[i].typ() = nil
       result.typ() = nil # current result.typ is invalid, index type is nil
@@ -2799,7 +2799,7 @@ proc semSetConstr(c: PContext, n: PNode, expectedType: PType = nil): PNode =
           expectedElementType = typ
     if isGeneric:
       for i in 0..<n.len:
-        if isIntLit(n[i].typ):
+        if n[i].typ != nil and isIntLit(n[i].typ):
           # generic instantiation strips int lit type which makes conversions fail
           n[i].typ() = nil
         result.add n[i]
@@ -2912,7 +2912,7 @@ proc semTupleFieldsConstr(c: PContext, n: PNode, flags: TExprFlags; expectedType
     result.add n[i]
   if isGeneric:
     for i in 0..<result.len:
-      if isIntLit(result[i][1].typ):
+      if result[i][1].typ != nil and isIntLit(result[i][1].typ):
         # generic instantiation strips int lit type which makes conversions fail
         result[i][1].typ() = nil
     result.typ() = makeTypeFromExpr(c, result.copyTree)
@@ -2953,7 +2953,7 @@ proc semTuplePositionsConstr(c: PContext, n: PNode, flags: TExprFlags; expectedT
     addSonSkipIntLit(typ, n[i].typ.skipTypes({tySink}), c.idgen)
   if isGeneric:
     for i in 0..<result.len:
-      if isIntLit(result[i].typ):
+      if result[i].typ != nil and isIntLit(result[i].typ):
         # generic instantiation strips int lit type which makes conversions fail
         result[i].typ() = nil
     result.typ() = makeTypeFromExpr(c, result.copyTree)
