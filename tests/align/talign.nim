12b1b45710ef5533c40a487d1c048d68a0c24c90
ringabout <43030857+ringabout@users.noreply.github.com>
2026-03-03 17:02:46 +0800

    tests

--- tests/align/talign.nim
+++ tests/align/talign.nim
@@ -168,13 +168,3 @@ for q in 0..100:
     new topArr[i]
     topArr[i].m.di.b = q
     doAssert(cast[uint](addr topArr[i].m.di) mod uint(alignof(DeepInner)) == 0)
-
-
-GC_fullCollect()
-
-block:
-  type U = object
-    d {.align: 16.}: int8
-  var e: seq[ref U]
-  for i in 0 ..< 10000: e.add(new U)
-  doAssert getTotalMem() <= 1052672 * 2
