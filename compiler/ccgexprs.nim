fa8e4e4e9b11dbb0111ff9c93748f97db00927df
araq <araq4k@proton.me>
2026-03-05 14:07:36 +0100

    ARC/ORC: specialize seq.add to eliminate more stores

--- compiler/ccgexprs.nim
+++ compiler/ccgexprs.nim
@@ -1587,6 +1587,42 @@ proc genSeqElemAppend(p: BProc, e: PNode, d: var TLoc) =
   genAssignment(p, dest, b, {needToCopy})
   gcUsage(p.config, e)
 
+proc genSeqElemAppendV2(p: BProc, e: PNode, d: var TLoc) =
+  # s.add(x) with optSeqDestructors (arc/orc), inlined for direct slot construction:
+  #   NI oldLen = s.len;
+  #   if (s.p == NIM_NIL || (s.p->cap & ~NIM_STRLIT_FLAG) < oldLen + 1)
+  #     s.p = (PayloadType*)prepareSeqAddUninit(oldLen, s.p, 1, sizeof(T), alignof(T));
+  #   s.len = oldLen + 1;
+  #   s.p->data[oldLen] = x;  // direct assignment, no function call overhead
+  let seqtype = skipTypes(e[1].typ, abstractVarRange)
+  var a = initLocExpr(p, e[1])
+  var b = initLocExpr(p, e[2])
+  let ra = rdLoc(a)
+  let et = getTypeDesc(p.module, seqtype.elementType)
+  let pt = getSeqPayloadType(p.module, seqtype)
+  var tmpL = getIntTemp(p)
+  p.s(cpsStmts).addAssignment(tmpL.snippet, dotField(ra, "len"))
+  let pField = dotField(ra, "p")
+  p.s(cpsStmts).addSingleIfStmt(
+      cOp(Or,
+        cOp(Equal, pField, NimNil),
+        cOp(LessThan,
+          cOp(BitAnd, NimInt, derefField(pField, "cap"), cOp(BitNot, NimInt, NimStrlitFlag)),
+          cOp(Add, NimInt, tmpL.snippet, cIntValue(1))))):
+    p.s(cpsStmts).addFieldAssignmentWithValue(ra, "p"):
+      p.s(cpsStmts).addCast(ptrType(pt)):
+        p.s(cpsStmts).addCall(cgsymValue(p.module, "prepareSeqAddUninit"),
+          tmpL.snippet,
+          pField,
+          cIntValue(1),
+          cSizeof(et),
+          cAlignof(et))
+  p.s(cpsStmts).addFieldAssignment(ra, "len",
+    cOp(Add, NimInt, tmpL.snippet, cIntValue(1)))
+  var dest = initLoc(locExpr, e[2], OnHeap)
+  dest.snippet = subscript(dataField(p, ra), tmpL.snippet)
+  genAssignment(p, dest, b, {})
+
 proc genDefault(p: BProc; n: PNode; d: var TLoc) =
   if d.k == locNone: d = getTemp(p, n.typ, needsInit=true)
   else: resetLoc(p, d)
@@ -2898,8 +2934,15 @@ proc genMagicExpr(p: BProc, e: PNode, d: var TLoc, op: TMagic) =
   of mAppendStrStr: genStrAppend(p, e, d)
   of mAppendSeqElem:
     if optSeqDestructors in p.config.globalOptions:
-      e[1] = makeAddr(e[1], p.module.idgen)
-      genCall(p, e, d)
+      if p.config.selectedGC in {gcArc, gcAtomicArc, gcOrc}:
+        # Inline growth + direct slot assignment: avoids the add() call overhead
+        # and lets the C compiler see the construction expression at its final
+        # destination, enabling in-place construction for nkObjConstr etc.
+        # gcYrc is excluded because its add() acquires a striped reader lock.
+        genSeqElemAppendV2(p, e, d)
+      else:
+        e[1] = makeAddr(e[1], p.module.idgen)
+        genCall(p, e, d)
     else:
       genSeqElemAppend(p, e, d)
   of mEqStr: genStrEquals(p, e, d)
