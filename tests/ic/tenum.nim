621f8816b2812a30dcba3569cd3e37827a096192
ringabout <43030857+ringabout@users.noreply.github.com>
2026-03-17 18:44:08 +0800

    adds a test case

---
+++ tests/ic/tenum.nim
@@ -0,0 +1,21 @@
+discard """
+  output: "42"
+"""
+
+# Object variant / case object
+type
+  NodeKind = enum
+    nkInt, nkStr, nkAdd
+
+  Node = object
+    case kind: NodeKind
+    of nkInt: intVal: int
+    of nkStr: strVal: string
+    of nkAdd: left, right: ref Node
+
+proc newInt(v: int): ref Node =
+  new(result)
+  result[] = Node(kind: nkInt, intVal: v)
+
+let n = newInt(42)
+echo n.intVal
