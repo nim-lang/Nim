39285aa7606d8234aacd50e26c6d2f5f6d42fe61
ringabout <43030857+ringabout@users.noreply.github.com>
2026-04-01 05:32:24 +0800

    fixes #25632; errors incompatibility between {.error.} and {.exportc} pragmas in semProcAux (#25639)
    fixes #25632
    fixes #25631
    fixes #25630
    
    This pull request introduces a compatibility check between the
    `{.error.}` and `{.exportc.}` pragmas in procedure declarations.
    Specifically, it prevents a procedure from being marked with both
    pragmas at the same time, as this combination is now considered invalid.
    
    Pragma compatibility enforcement:
    
    * Added a check in `semProcAux` (in `compiler/semstmts.nim`) to emit a
    local error if a procedure is declared with both `{.error.}` and
    `{.exportc.}` pragmas, preventing their incompatible usage.
    
    (cherry picked from commit fb31e86537dab922d91f37b8e4954e0bfa748932)

--- compiler/semstmts.nim
+++ compiler/semstmts.nim
@@ -2536,6 +2536,9 @@ proc semProcAux(c: PContext, n: PNode, kind: TSymKind,
   if not hasProto:
     implicitPragmas(c, s, n.info, validPragmas)
 
+  if {sfError, sfExportc} * s.flags == {sfError, sfExportc}:
+    localError(c.config, n.info, "{.error.} and {.exportc.} pragmas are incompatible")
+
   if n[pragmasPos].kind != nkEmpty and sfBorrow notin s.flags:
     setEffectsForProcType(c.graph, s.typ, n[pragmasPos], s)
   s.typ.flags.incl tfEffectSystemWorkaround
