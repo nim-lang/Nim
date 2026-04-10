dd9dd40c7911a0c30aeef4644ee1d238103f2d3a
metagn <metagngn@gmail.com>
2026-04-10 15:52:37 +0300

    update comment and add remaining hooks to test

--- tests/destructor/trecursivedestructor.nim
+++ tests/destructor/trecursivedestructor.nim
@@ -20,6 +20,12 @@ proc `=copy`(a: var ObjWithSeq, b: ObjWithSeq) {.nodestroy.} =
 proc `=sink`(a: var ObjWithSeq, b: ObjWithSeq) {.nodestroy.} =
   `=sink`(a.x, b.x)
 
+proc `=dup`(a: ObjWithSeq): ObjWithSeq {.nodestroy.} =
+  ObjWithSeq(x: a.x)
+
+proc `=trace`(a: var ObjWithSeq, env: pointer) {.nodestroy.} =
+  `=trace`(a.x, env)
+
 proc fooSeq() =
   let a = ObjWithSeq(x: @[ObjWithSeq()])
   let b = a
@@ -27,26 +33,31 @@ proc fooSeq() =
   let d = b
 fooSeq()
 
-when true:
-  type ObjWithRef = object
-    x: ref ObjWithRef
+type ObjWithRef = object
+  x: ref ObjWithRef
 
-  when not defined(gcDestructors):
-    proc `=destroy`(a: var ObjWithRef) {.nodestroy.} =
-      `=destroy`(a.x)
-  else:
-    proc `=destroy`(a: ObjWithRef) {.nodestroy.} =
-      `=destroy`(a.x)
+when not defined(gcDestructors):
+  proc `=destroy`(a: var ObjWithRef) {.nodestroy.} =
+    `=destroy`(a.x)
+else:
+  proc `=destroy`(a: ObjWithRef) {.nodestroy.} =
+    `=destroy`(a.x)
 
-  proc `=copy`(a: var ObjWithRef, b: ObjWithRef) {.nodestroy.} =
-    `=copy`(a.x, b.x)
-    
-  proc `=sink`(a: var ObjWithRef, b: ObjWithRef) {.nodestroy.} =
-    `=sink`(a.x, b.x)
+proc `=copy`(a: var ObjWithRef, b: ObjWithRef) {.nodestroy.} =
+  `=copy`(a.x, b.x)
+  
+proc `=sink`(a: var ObjWithRef, b: ObjWithRef) {.nodestroy.} =
+  `=sink`(a.x, b.x)
 
-  proc fooRef() =
-    let a = ObjWithRef(x: (ref ObjWithRef)())
-    let b = a
-    let c = a
-    let d = b
-  fooRef()
+proc `=dup`(a: ObjWithRef): ObjWithRef {.nodestroy.} =
+  ObjWithRef(x: a.x)
+
+proc `=trace`(a: var ObjWithRef, env: pointer) {.nodestroy.} =
+  `=trace`(a.x, env)
+
+proc fooRef() =
+  let a = ObjWithRef(x: (ref ObjWithRef)())
+  let b = a
+  let c = a
+  let d = b
+fooRef()
