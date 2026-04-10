316705bfa3b960d8affa40c42392855864c2f977
metagn <metagngn@gmail.com>
2026-04-10 15:40:11 +0300

    add ref test which doesnt work

--- tests/destructor/trecursivedestructor.nim
+++ tests/destructor/trecursivedestructor.nim
@@ -12,9 +12,29 @@ proc `=copy`(a: var ObjWithSeq, b: ObjWithSeq) {.nodestroy.} =
 proc `=sink`(a: var ObjWithSeq, b: ObjWithSeq) {.nodestroy.} =
   `=sink`(a.x, b.x)
 
-proc foo() =
+proc fooSeq() =
   let a = ObjWithSeq(x: @[ObjWithSeq()])
   let b = a
   let c = a
   let d = b
-foo()
+fooSeq()
+
+when false:
+  type ObjWithRef = object
+    x: ref ObjWithRef
+
+  proc `=destroy`(a: ObjWithRef) {.nodestroy.} =
+    `=destroy`(a.x)
+
+  proc `=copy`(a: var ObjWithRef, b: ObjWithRef) {.nodestroy.} =
+    `=copy`(a.x, b.x)
+    
+  proc `=sink`(a: var ObjWithRef, b: ObjWithRef) {.nodestroy.} =
+    `=sink`(a.x, b.x)
+
+  proc fooRef() =
+    let a = ObjWithRef(x: (ref ObjWithRef)())
+    let b = a
+    let c = a
+    let d = b
+  fooRef()
