242f761627c5b02581fc75b0c83920c2dca8702d
Ryan McConnell <rammcconnell@gmail.com>
2026-04-12 02:56:31 -0400

    RE: fix #25627 (#25736)
    Follow up PR to #25700
    
    @demotomohiro 
    
    This doesn't seem to mirror your suggested approach completely. I still
    went with a recursive walk. Could probably add some kind of "clean
    types" and "dirty types" cache through this to minimize the recursions,
    but that seems like a little much.

--- compiler/semdata.nim
+++ compiler/semdata.nim
@@ -183,6 +183,9 @@ type
     forwardTypeUpdates*: seq[(PType, PNode)]
       # types that need to be updated in a type section
       # due to containing forward types, and their corresponding nodes
+    forwardFieldUpdates*: seq[(PType, PNode, PType)]
+      # object/tuple field definitions whose default values mention forward
+      # types and need delayed const checking
     inTypeofContext*: int
 
     semAsgnOpr*: proc (c: PContext; n: PNode; k: TNodeKind): PNode {.nimcall.}
--- tests/objects/t25627.nim
+++ tests/objects/t25627.nim
@@ -20,3 +20,51 @@ let dir = DirStruct()
 doAssert dir.root.kind == fsoDir
 doAssert dir.root.dirName == "/"
 doAssert dir.root.files.len == 0
+
+block:
+  type
+    Opt[T] = object
+      when T is ref:
+        val: T
+        x: int
+      else:
+        val: T
+        x: string
+
+    DefaultOpt = ref object
+      files: Opt[DefaultOpt]
+
+    OptDirStruct = object
+      root = DefaultOpt()
+
+  let dir = OptDirStruct()
+  doAssert dir.root.files.x is int
+
+block:
+  type
+    Opt[T] = object
+      when T is ref:
+        x: int
+      else:
+        x: string
+
+    Foo[T] = object
+      x: Opt[T]
+
+    Nested = ref object
+      files: Foo[Nested]
+
+  let nested = Nested()
+  doAssert nested.files.x.x is int
+
+block:
+  type
+    Foo[T] = object
+      x = sizeof(T)
+
+    Sized = ref object
+      files: Foo[Sized]
+
+  let sized = Sized()
+  doAssert sized.files.x == sizeof(Sized)
+
