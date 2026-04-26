727fdcca1c7cbd552ce07e8be1d19293d48a50f1
demotomohiro <gpuppur@gmail.com>
2026-04-27 05:20:05 +0900

    fix compile error from custom pragma template

--- compiler/sigmatch.nim
+++ compiler/sigmatch.nim
@@ -3016,8 +3016,12 @@ proc matchesAux(c: PContext, n, nOrig: PNode, m: var TCandidate, marker: var Int
           m.typedescMatched = false
           var newlyTyped = false
           n[a] = prepareOperand(c, formal.typ, n[a], newlyTyped)
-          if (n[a].typ != nil and n[a].typ.kind == tyForward) or
-             (n[a].kind == nkSym and n[a].sym.typ != nil and n[a].sym.typ.kind == tyForward):
+          if ((n[a].typ != nil and n[a].typ.kind == tyForward) or
+              (n[a].kind == nkSym and n[a].sym.typ != nil and n[a].sym.typ.kind == tyForward)) and
+              not (m.calleeSym != nil and sfCustomPragma in m.calleeSym.flags):
+            # exclude custom pragma template (used as a pragma in a type section) to avoid generating compile errors from existing code
+            # maybe it should not be excluded if `formal.typ` is not generic.
+
             # set tyForward type to `m.call.typ` so that caller can easily see this call contains a tyForward type param.
             m.call.typ = if n[a].typ != nil and n[a].typ.kind == tyForward: n[a].typ else: n[a].sym.typ
             noMatch(true)
