85c708cb850375a63196f8b68107d779c330174a
ringabout <43030857+ringabout@users.noreply.github.com>
2026-05-29 23:07:23 +0800

    fixes #22122; raise effects for complex expressions

--- compiler/sempass2.nim
+++ compiler/sempass2.nim
@@ -497,6 +497,26 @@ proc addRaiseEffect(a: PEffects, e, comesFrom: PNode) =
     if not isDefectException(e.typ):
       throws(a.exc, e, comesFrom)
 
+proc addRaiseEffectsFromExpr(a: PEffects, e, comesFrom: PNode) =
+  if e.isNil:
+    return
+  let x = skipConvCastAndClosure(e)
+  case x.kind
+  of nkStmtList, nkStmtListExpr, nkBlockStmt, nkBlockExpr:
+    if x.len > 0:
+      addRaiseEffectsFromExpr(a, x.lastSon, comesFrom)
+  of nkIfExpr, nkIfStmt:
+    for branch in items(x):
+      if branch.len > 0:
+        addRaiseEffectsFromExpr(a, branch.lastSon, comesFrom)
+  of nkCaseStmt:
+    for i in 1..<x.len:
+      let branch = x[i]
+      if branch.len > 0:
+        addRaiseEffectsFromExpr(a, branch.lastSon, comesFrom)
+  else:
+    addRaiseEffect(a, x, x)
+
 proc addTag(a: PEffects, e, comesFrom: PNode) =
   var aa = a.tags
   for i in 0..<aa.len:
@@ -1324,7 +1344,7 @@ proc track(tracked: PEffects, n: PNode) =
     if n[0].kind != nkEmpty:
       n[0].info = n.info
       #throws(tracked.exc, n[0])
-      addRaiseEffect(tracked, n[0], n)
+      addRaiseEffectsFromExpr(tracked, n[0], n)
       for i in 0..<n.safeLen:
         track(tracked, n[i])
       createTypeBoundOps(tracked, n[0].typ, n.info)
