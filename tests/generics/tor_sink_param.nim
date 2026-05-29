6112648dc482baefb4016a743cb95d49e9964dd8
demotomohiro <gpuppur@gmail.com>
2026-05-29 20:35:31 +0900

    update test code

--- tests/generics/tor_sink_param.nim
+++ tests/generics/tor_sink_param.nim
@@ -1,12 +1,23 @@
 # issue #25726
 
-proc testOrSink(v: string | sink seq[int]) =
+proc testOrSink(v: sink (string | seq[int])) =
   discard move(v)
 
 proc testOrSink2(v: sink seq[int]) =
   testOrSink(v)
 
+proc testOrSink3(v: sink string | sink seq[int]) =
+  discard move(v)
+
+proc testOrSink4(v: sink string) =
+  testOrSink3(v)
+
 var v = newSeq[int]()
 testOrSink(v)
 var v2 = newSeq[int]()
 testOrSink2(v2)
+
+var v3 = "foo"
+testOrSink3(v3)
+var v4 = "bar"
+testOrSink4(v4)
