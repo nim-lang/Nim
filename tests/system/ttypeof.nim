22091f24d882a619444929eadaf2c7aeb02846b7
demotomohiro <gpuppur@gmail.com>
2026-05-29 11:48:43 +0900

    updates test code

--- tests/system/ttypeof.nim
+++ tests/system/ttypeof.nim
@@ -28,13 +28,27 @@ block:
   var a, f, i: int
   testVarParams1(a, 0, 0, 0, 0, f, 0, 0, i)
 
+# `typeOfModCompatible` and `typeOfModRemoveModifier` remove only top `var`, not `var` inside proc type
 proc testVarParams2(a: var proc(x: var int): var int;
                     b: typeof(a);
-                    c: typeof(a, modifierMode = typeOfModKeepModifier)) =
+                    c: typeof(a, modifierMode = typeOfModCompatible);
+                    d: typeof(a, modifierMode = typeOfModRemoveModifier);
+                    e: typeof(a, modifierMode = typeOfModKeepModifier)) =
   doAssert not isVar(b)
-  doAssert isVar(c)
+  doAssert not isVar(c)
+  doAssert not isVar(d)
+  doAssert isVar(e)
 
-static: doAssert testVarParams2 is proc (a: var proc(x: var int): var int; b: proc(x: var int): var int; c: var proc(x: var int): var int) {.nimcall.}
+static: doAssert testVarParams2 is proc (a: var proc(x: var int): var int;
+                                         b: proc(x: var int): var int;
+                                         c: proc(x: var int): var int;
+                                         d: proc(x: var int): var int;
+                                         e: var proc(x: var int): var int) {.nimcall.}
+
+block:
+  var a, e: proc(x: var int): var int = nil
+  let b, c, d: proc(x: var int): var int = nil
+  testVarParams2(a, b, c, d, e)
 
 proc testRet(a: var int): typeof(a) = 0
 static: doAssert testRet is proc (a: var int): int {.nimcall.}
@@ -42,26 +56,35 @@ proc testRet2(a: var int): typeof(a, modifierMode = typeOfModCompatible) = 0
 static: doAssert testRet2 is proc (a: var int): int {.nimcall.}
 proc testRet3(a: var int): typeof(a, modifierMode = typeOfModRemoveModifier) = 0
 static: doAssert testRet3 is proc (a: var int): int {.nimcall.}
-#proc testRet4(a: var int): typeof(a, modifierMode = typeOfModKeepModifier) = a
-#static: doAssert testRet4 is proc (a: var int): var int {.nimcall.}
 
 proc fooSink1(a: sink string;
               b: typeof(a);
-              c: typeof(a, modifierMode = typeOfModRemoveModifier);
-              d: typeof(a, modifierMode = typeOfModKeepModifier)) = discard
+              c: typeof(a, modifierMode = typeOfModCompatible);
+              d: typeof(a, modifierMode = typeOfModRemoveModifier);
+              e: typeof(a, modifierMode = typeOfModKeepModifier)) = discard
 
-static: doAssert fooSink1 is proc (a: sink string; b: sink string; c: string; d: sink string) {.nimcall.}
+static: doAssert fooSink1 is proc (a: sink string; b: sink string; c: sink string; d: string; e: sink string) {.nimcall.}
 
 proc fooLentRet(a: seq[string]): lent string = a[0]
 proc testLentRetComp(a: seq[string]): typeof(fooLentRet(a), modifierMode = typeOfModCompatible) = a[0]
 proc testLentRetRemo(a: seq[string]): typeof(fooLentRet(a), modifierMode = typeOfModRemoveModifier) = a[0]
 proc testLentRetKeep(a: seq[string]): typeof(fooLentRet(a), modifierMode = typeOfModKeepModifier) = a[0]
 
+# workaround # issue 25830
+proc dummyLentProc(a: seq[string]): lent string = a[0]
+
 static:
   doAssert testLentRetComp is proc (a: seq[string]): string {.nimcall.}
   doAssert testLentRetRemo is proc (a: seq[string]): string {.nimcall.}
-  #doAssert testLentRetKeep is proc (a: seq[string]): lent string {.nimcall.}
+  doAssert testLentRetKeep is typeof(dummyLentProc)
 
 proc voidProc() = discard
 static:
   doAssert typeof(voidProc()) is void
+
+type
+  Foo = typeof(Bar)
+  Bar = int
+
+static:
+  doAssert Foo is int
