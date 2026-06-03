70642d740854b69b4107fdc6d1592e3391b1a038
demotomohiro <gpuppur@gmail.com>
2026-06-03 12:02:23 +0900

    removes -std=c++11. Nim already switched to c++17

--- tests/misc/tsizeof.nim
+++ tests/misc/tsizeof.nim
@@ -8,11 +8,6 @@ macros api OK
 '''
 """
 
-# This is for Azure. The keyword ``alignof`` only exists in ``c++11``
-# and newer. On Azure gcc does not default to c++11 yet.
-when defined(cpp) and not defined(windows):
-  {.passC: "-std=c++11".}
-
 # Object offsets are different for inheritance objects when compiling
 # to c++.
 
