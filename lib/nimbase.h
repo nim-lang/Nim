2b6cf8c36bbba73237a6f61b1a1e5583da18a81e
ringabout <43030857+ringabout@users.noreply.github.com>
2026-07-06 22:34:21 +0800

    fixes #25968; ignores "-Wpsabi"

--- lib/nimbase.h
+++ lib/nimbase.h
@@ -65,6 +65,7 @@ __EMSCRIPTEN__
 #  pragma GCC diagnostic ignored "-Wincompatible-pointer-types-discards-qualifiers"
 #  pragma GCC diagnostic ignored "-Wpointer-bool-conversion"
 #  pragma GCC diagnostic ignored "-Wconstant-conversion"
+#  pragma GCC diagnostic ignored "-Wpsabi"
 #endif
 
 #if defined(_MSC_VER)
