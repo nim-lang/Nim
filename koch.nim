613e1c18d0b888fad557cd31a3f6f3b80963c061
Araq <araq4k@proton.me>
2026-07-09 20:31:22 +0200

    use newer Nimony

--- koch.nim
+++ koch.nim
@@ -16,11 +16,11 @@ const
   ChecksumsStableCommit = "5c132cd332cce5d64a0da9ac3e4c9664313dccb4" # 0.2.2
   SatStableCommit = "9d52513b3c68bfb929dbd687d4fb2836cfee6936"
 
-  NimonyStableCommit = "6f9ac6655dc6724ae4e5ccb93b8123c18d54391a" # unversioned \
+  NimonyStableCommit = "863a04c07277eaa0a6bb2a0059d731a59cbc09b4" # unversioned \
     # Note that Nimony uses Nim as a git submodule but we don't want to install
     # Nimony's dependency to Nim as we are Nim. So a `git clone` without --recursive
     # is **required** here.
-    # Commit from 2026-07-03 -- .bif files are memory mapped too
+    # Commit from 2026-07-09 -- .bif files are memory mapped too
 
   # examples of possible values for fusion: #head, #ea82b54, 1.2.3
   FusionStableHash = "#562467452b32cb7a97410ea177f083e6d8405734"
