46251bbfcc1ba83a21b0e300efa3bb3f1f64d758
Juan M Gómez <info@jmgomez.me>
2026-07-10 18:56:32 +0100

    Update NimbleStableCommit to test commit pre 0.24.0 (#25979)
    (cherry picked from commit 497a540d2091a801f0f67c28f394801bc365716c)

--- koch.nim
+++ koch.nim
@@ -11,7 +11,7 @@
 
 const
   # examples of possible values for repos: Head, ea82b54
-  NimbleStableCommit = "42ef70c2102a942c46f13eb76872326edd525cec"    # 0.22.3
+  NimbleStableCommit = "0d2504644fee2b2881a7b1f0c54ba0cb08c22978"    # 0.24.0
   AtlasStableCommit = "aa6fb162006f3015aa84c4305e15cb4d230f5ad6"     # 0.14.7
   ChecksumsStableCommit = "5c132cd332cce5d64a0da9ac3e4c9664313dccb4" # 0.2.2
   SatStableCommit = "9d52513b3c68bfb929dbd687d4fb2836cfee6936"
