21fe5f423f06f71fc18134ac87b88a20adf9ba04
ringabout <43030857+ringabout@users.noreply.github.com>
2026-08-03 18:02:29 +0800

    fixes #26023; incorrect sink requires a copy (#26028)
    fixes #26023
    
    (cherry picked from commit 23365deef0482fbcaa58cf6cc49f74b93aadd016)

--- compiler/modulegraphs.nim
+++ compiler/modulegraphs.nim
@@ -410,9 +410,15 @@ iterator methodsForGeneric*(g: ModuleGraph; t: PType): (int, PSym) =
 proc addMethodToGeneric*(g: ModuleGraph; module: int; t: PType; col: int; m: PSym) =
   g.methodsPerGenericType.mgetOrPut(t.itemId, @[]).add (col, LazySym(sym: m))
 
-proc hasDisabledAsgn*(g: ModuleGraph; t: PType): bool =
-  let op = getAttachedOp(g, t, attachedAsgn)
+proc hasDisabledOp(g: ModuleGraph; t: PType; kind: TTypeAttachedOp): bool =
+  let op = getAttachedOp(g, t, kind)
   result = op != nil and sfError in op.flags
+
+proc hasDisabledAsgn*(g: ModuleGraph; t: PType): bool =
+  result = hasDisabledOp(g, t, attachedAsgn)
+
+proc hasDisabledDup*(g: ModuleGraph; t: PType): bool =
+  result = hasDisabledOp(g, t, attachedDup)
 
 proc copyTypeProps*(g: ModuleGraph; module: int; dest, src: PType) =
   for k in low(TTypeAttachedOp)..high(TTypeAttachedOp):
