3dd52cfba99b75be74005323262699291119a8a4
ringabout <43030857+ringabout@users.noreply.github.com>
2026-08-07 22:26:36 +0800

    fixes #26088; StringStream.write regression

--- tests/js/tstreams.nim
+++ tests/js/tstreams.nim
@@ -20,3 +20,11 @@ doAssert s2.readAll == "abc"
 s2.write("def")
 doAssert s2.data == "abcdef"
 s2.close
+
+# bug #26088
+var s3 = newStringStream("0123456789ABCDEF")
+s3.setPosition(0)
+s3.write("XX")
+doAssert s3.data == "XX23456789ABCDEF"
+doAssert not s3.atEnd
+s3.close
