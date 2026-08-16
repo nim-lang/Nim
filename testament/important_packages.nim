c57494e588a23c2751db8aedf6611f9c00cfe4c3
narimiran <narimiran@disroot.org>
2026-08-16 20:58:27 +0200

    try with the slim branch

--- testament/important_packages.nim
+++ testament/important_packages.nim
@@ -176,7 +176,7 @@ pkg "unittest2"
 pkg "unpack"
 when not defined(arm64):
   pkg "weave", "nimble install -y cligen@#HEAD; nimble test_gc_arc", useHead = true
-pkg "web3", useHead = true
+pkg "web3", "nimble test_slim", "https://github.com/status-im/nim-web3/tree/tests/slim_tests", useHead = true
 pkg "websock", "nim c -d:chronicles_log_level=INFO tests/all_tests.nim"
 pkg "websocket", "nim c websocket.nim"
 pkg "with"
