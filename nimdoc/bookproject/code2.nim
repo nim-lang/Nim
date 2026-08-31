7da5c2f71813675479ab251c131bd94a5bb309f2
Constantine Molchanov <moigagoo@duck.com>
2026-08-31 15:51:01 +0400

    Add test for `nim book`.

---
+++ nimdoc/bookproject/code2.nim
@@ -0,0 +1,10 @@
+proc single*(x: int): int =
+  x * 1
+
+#doublestart
+proc double*(x: int): int =
+  x * 2
+#doubleend
+
+proc triple*(x: int): int =
+  x * 3
---
+++ nimdoc/bookproject/page1.md
@@ -0,0 +1,5 @@
+.. title:: Another page
+
+# Section 1
+
+Paragraph.
