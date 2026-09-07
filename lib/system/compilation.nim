8e8fbf60693418dc95bb0d762fd660231d08a583
narimiran <narimiran@disroot.org>
2026-09-07 21:41:07 +0200

    bump NimVersion to 2.2.12

--- lib/system/compilation.nim
+++ lib/system/compilation.nim
@@ -10,7 +10,7 @@ const
     ## is the minor number of Nim's version.
     ## Odd for devel, even for releases.
 
-  NimPatch* {.intdefine.}: int = 11
+  NimPatch* {.intdefine.}: int = 12
     ## is the patch number of Nim's version.
     ## Odd for devel, even for releases.
 
