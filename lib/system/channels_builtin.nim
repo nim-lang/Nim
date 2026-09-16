7340153b6903a53d567c7aaadb6a7409cb2d2222
Jacek Sieka <arnetheduck@gmail.com>
2026-09-16 11:31:32 +0200

    channel: make tryRecv block on the lock before checking for empty (#26233)
    Change `tryRecv` to match `trySend` and unconditoinally take the lock to
    perform its empty check - this prevents several common issues with the
    current design:
    
    * unbounded memory growth or message loss due to consumer starvation as
    producers take and retake the lock
    * "drain loops" are difficult to implement since they may get
    interruptted early
    * necessitates an external counter to acutally know if the queue is
    empty (`peek` is prone to races)
    
    trySend already blocks on the lock and returns false only when the
    channel is full. This makes tryRecv symmetric: block on the lock, then
    check for empty. false now reliably means 'no message is available'.
    
    While we're here, fix a few other things:
    
    * document staleness and deadlock potential in `peek`
    * document staleness potential in `ready`
    * document deadlock potential when closing with readers
    * document nim-lang/threading repo as the better option for orc/arc
    * simplify example and point out closing hazards
    * refactor implementation to more closely match that of the recently
    cleaned up std/deques
    
    (cherry picked from commit 8f1a8f6342642933c07be49b7250364cb8184ab6)

--- lib/system/channels_builtin.nim
+++ lib/system/channels_builtin.nim
@@ -7,13 +7,16 @@
 #    distribution, for details about the copyright.
 #
 
-## Channel support for threads.
+## Multi-producer, multi-consumer (MPMC) channel for refc.
+##
+## See https://github.com/nim-lang/threading/ for a modern replacement for
+## destructor-based memory managers (ARC/ORC).
 ##
 ## **Note**: This is part of the system module. Do not import it directly.
 ## To activate thread support compile with the `--threads:on` command line switch.
 ##
-## **Note:** Channels are designed for the `Thread` type. They are unstable when
-## used with `spawn`
+## **Note:** Channels are designed for the `Thread` type. They are not safe to
+## use with `spawn` which does not always create a separate thread.
 ##
 ## **Note:** The current implementation of message passing does
 ## not work with cyclic data structures.
@@ -104,15 +107,9 @@
 ##
 ## Passing Channels Safely
 ## -----------------------
-## Note that when passing objects to procedures on another thread by pointer
-## (for example through a thread's argument), objects created using the default
-## allocator will use thread-local, GC-managed memory. Thus it is generally
-## safer to store channel objects in global variables (as in the above example),
-## in which case they will use a process-wide (thread-safe) shared heap.
-##
-## However, it is possible to manually allocate shared memory for channels
-## using e.g. `system.allocShared0` and pass these pointers through thread
-## arguments:
+## To dynamically create channels at runtime, it is recommended to manually
+## allocate memory using e.g. `system.create` and pass the resulting pointer
+## through thread arguments:
 ##
 ##   ```Nim
 ##   proc worker(channel: ptr Channel[string]) =
@@ -120,20 +117,22 @@
 ##     echo greeting
 ##
 ##   proc localChannelExample() =
-##     # Use allocShared0 to allocate some shared-heap memory and zero it.
+##     # Use `create` to allocate manually managed memory for channel
 ##     # The usual warnings about dealing with raw pointers apply. Exercise caution.
-##     var channel = cast[ptr Channel[string]](
-##       allocShared0(sizeof(Channel[string]))
-##     )
+##     let channel = create(Channel[string])
 ##     channel[].open()
 ##     # Create a thread which will receive the channel as an argument.
 ##     var thread: Thread[ptr Channel[string]]
 ##     createThread(thread, worker, channel)
 ##     channel[].send("Hello from the main thread!")
-##     # Clean up resources.
+##     # Wait for the thread to finish reading before closing the channel
 ##     thread.joinThread()
+##     # Call close only once the channel is no longer being used
 ##     channel[].close()
-##     deallocShared(channel)
+##     # `dealloc` must be called from the same thread as `create` - use
+##     # `createshared`/`deallocShared` when deallocating the channel in another
+##     # thread.
+##     dealloc(channel)
 ##
 ##   localChannelExample() # "Hello from the main thread!"
 ##   ```
@@ -145,9 +144,15 @@ when not declared(ThisIsSystem):
 
 type
   pbytes = ptr UncheckedArray[byte]
-  RawChannel {.pure, final.} = object ## msg queue for a thread
-    rd, wr, count, mask, maxItems: int
-    data: pbytes
+  RawChannel {.pure, final.} = object
+    # Loosely based on std/deques - head / tail are masked on access - their
+    # difference is the current length of the queue. Items are written to the
+    # tail and read from the head.
+    head, tail, maxItems: uint
+
+    data: pbytes # circular buffer
+    cap: uint
+
     lock: SysLock
     notEmpty: SysCond ## receivers wait here
     notFull: SysCond  ## senders wait here (only used if `maxItems > 0`)
@@ -158,30 +163,35 @@ type
   Channel*[TMsg] {.gcsafe.} = RawChannel ## a channel for thread communication
 
 proc `=copy`(a: var RawChannel, b: RawChannel) {.error.}
-
-const ChannelDeadMask = -2
+const
+  ChannelDead = uint.high()
+  Unbounded = uint.high()
 
 proc initRawChannel(c: var RawChannel, maxItems: int) =
   # `close` frees `data` (and `region`) but leaves the stale fields behind;
   # reset everything so that a channel can be opened again after `close`.
+  assert c.data == nil, "Channel already open"
+
   zeroMem(addr c, sizeof(c))
   initSysLock(c.lock)
   initSysCond(c.notEmpty)
-  initSysCond(c.notFull)
-  c.mask = -1
-  c.maxItems = maxItems
+  if maxItems <= 0:
+    c.maxItems = Unbounded
+  else:
+    initSysCond(c.notFull)
+    c.maxItems = maxItems.uint
 
 proc deinitRawChannel(c: var RawChannel) =
-  # we need to grab the lock to be safe against sending threads!
-  acquireSys(c.lock)
-  c.mask = ChannelDeadMask
+  c.cap = ChannelDead
   when not usesDestructors:
     deallocOsPages(c.region)
   else:
     if c.data != nil: deallocShared(c.data)
+
   deinitSys(c.lock)
   deinitSysCond(c.notEmpty)
-  deinitSysCond(c.notFull)
+  if c.maxItems != Unbounded:
+    deinitSysCond(c.notFull)
 
 when not usesDestructors:
   proc storeAux(dest, src: pointer, mt: PNimType, t: var RawChannel,
@@ -240,7 +250,7 @@ when not usesDestructors:
       else:
         sysAssert(dest != nil, "dest == nil")
         when mode == mStore:
-          x[] = alloc0(t.region, align(GenericSeqSize, mt.base.align) +% seq.len *% mt.base.size)
+          x[] = alloc(t.region, align(GenericSeqSize, mt.base.align) +% seq.len *% mt.base.size)
         else:
           unsureAsgnRef(x, newSeq(mt, seq.len))
         let dst = cast[int](cast[PPointer](dest)[])
@@ -288,7 +298,7 @@ when not usesDestructors:
           let size = dyntype.base.size
           # we store the real dynamic 'ref type' at offset 0, so that
           # no information is lost
-          let a = alloc0(t.region, size+sizeof(pointer))
+          let a = alloc(t.region, size+sizeof(pointer))
           x[] = a
           cast[PPointer](a)[] = dyntype
           storeAux(a +! sizeof(pointer), s, dyntype.base, t, mode)
@@ -301,77 +311,106 @@ when not usesDestructors:
     else:
       copyMem(dest, src, mt.size) # copy raw bits
 
-proc rawSend(q: var RawChannel, data: pointer, typ: PNimType) =
-  ## Adds an `item` to the end of the queue `q`.
-  var cap = q.mask+1
-  if q.count >= cap:
-    # start with capacity for 2 entries in the queue:
-    if cap == 0: cap = 1
-    when not usesDestructors:
-      let n = cast[pbytes](alloc0(q.region, cap*2*typ.size))
-    else:
-      let n = cast[pbytes](allocShared0(cap*2*typ.size))
+template count(q: RawChannel): uint =
+  q.tail - q.head
+template mask(q: RawChannel): uint =
+  q.cap - 1
+template typeInfo(msg: typed): PNimType =
+  cast[PNimType](getTypeInfo(msg))
+template bytes(pos: uint, typSize: int): int =
+  cast[int](pos) *% typSize
 
-    let i = q.rd
-    let c = q.count
-    if c > 0:
-      let tail = cap - i
-      copyMem(addr n[0], addr q.data[i * typ.size], tail *% typ.size)
-      if tail < c:
-        copyMem(addr n[tail *% typ.size], addr q.data[0], (c - tail) *% typ.size)
+proc grow(q: var RawChannel, typSize: int) =
+  # See also deques.expandIfNeeded
+  # start with capacity for 2 entries in the queue:
+  let
+    cap = q.cap
+    newCap = max(1'u, cap)*2
+    newSize = newCap.bytes(typSize)
 
-    if q.data != nil:
-      when not usesDestructors:
-        dealloc(q.region, q.data)
-      else:
-        deallocShared(q.data)
-    q.data = n
-    q.mask = cap*2 - 1
-    q.wr = q.count
-    q.rd = 0
   when not usesDestructors:
-    storeAux(addr(q.data[q.wr * typ.size]), data, typ, q, mStore)
+    let n = cast[pbytes](alloc(q.region, newSize))
   else:
-    copyMem(addr(q.data[q.wr * typ.size]), data, typ.size)
-  inc q.count
-  q.wr = (q.wr + 1) and q.mask
+    let n = cast[pbytes](allocShared(newSize))
+
+  if cap > 0: # q.count == q.cap!
+    let
+      mask = cap - 1
+      head = q.head and mask
+      toCap = cap - head
+    copyMem(addr n[0], addr q.data[head.bytes(typSize)], toCap.bytes(typSize))
+    if head > 0:
+      copyMem(addr n[toCap.bytes(typSize)], addr q.data[0], head.bytes(typSize))
+
+    when not usesDestructors:
+      dealloc(q.region, q.data)
+    else:
+      deallocShared(q.data)
+  q.data = n
+  q.cap = newCap
+  q.tail = cap
+  q.head = 0
+
+proc rawSend(q: var RawChannel, data: pointer, typ: PNimType) =
+  # Adds an `item` to the end of the queue `q`.
+  if q.count >= q.cap:
+    q.grow(typ.size)
+
+  let tail = q.tail and q.mask
+  when not usesDestructors:
+    storeAux(addr q.data[tail.bytes(typ.size)], data, typ, q, mStore)
+  else:
+    copyMem(addr q.data[tail.bytes(typ.size)], data, typ.size)
+  inc q.tail
 
 proc rawRecv(q: var RawChannel, data: pointer, typ: PNimType) =
-  sysAssert q.count > 0, "rawRecv"
-  dec q.count
+  # Reads and removes an hitem from the beginning of the queue
+  let head = q.head and q.mask
   when not usesDestructors:
-    storeAux(data, addr(q.data[q.rd * typ.size]), typ, q, mLoad)
+    storeAux(data, addr q.data[head.bytes(typ.size)], typ, q, mLoad)
   else:
-    copyMem(data, addr(q.data[q.rd * typ.size]), typ.size)
-  q.rd = (q.rd + 1) and q.mask
+    copyMem(data, addr q.data[head.bytes(typ.size)], typ.size)
+  inc q.head
 
-template lockChannel(q, action): untyped =
+proc sendImpl(q: var RawChannel, typ: PNimType, msg: pointer, noBlock: static bool): bool =
+  if q.cap == ChannelDead: sysFatal(DeadThreadDefect, "channel closed")
   acquireSys(q.lock)
-  action
-  releaseSys(q.lock)
-
-proc sendImpl(q: var RawChannel, typ: PNimType, msg: pointer, noBlock: bool): bool =
-  if q.mask == ChannelDeadMask:
-    sysFatal(DeadThreadDefect, "cannot send message; thread died")
-  acquireSys(q.lock)
-  if q.maxItems > 0:
-    # Wait until count is less than maxItems
-    if noBlock and q.count >= q.maxItems:
+  when noBlock:
+    if q.count == q.maxItems:
       releaseSys(q.lock)
-      return
-
-    while q.count >= q.maxItems:
+      return false
+  else:
+    while q.count == q.maxItems:
       waitSysCond(q.notFull, q.lock)
 
   rawSend(q, msg, typ)
-  signalSysCond(q.notEmpty)
   releaseSys(q.lock)
-  result = true
+  signalSysCond(q.notEmpty)
+  return true
+
+proc recvImpl(q: var RawChannel, res: pointer, typ: PNimType, noBlock: static bool): bool =
+  if q.cap == ChannelDead: sysFatal(DeadThreadDefect, "channel closed")
+  acquireSys(q.lock)
+  when noBlock:
+    if q.count == 0:
+      releaseSys(q.lock)
+      return false
+  else:
+    inc q.waitingReceivers
+    while q.count == 0:
+      waitSysCond(q.notEmpty, q.lock)
+    dec q.waitingReceivers
+
+  rawRecv(q, res, typ)
+  releaseSys(q.lock)
+  if q.maxItems != Unbounded:
+    signalSysCond(q.notFull)
+  return true
 
 when defined(gcDestructors):
   proc send*[TMsg](c: var Channel[TMsg], msg: sink TMsg) {.inline.} =
     ## Sends a message to a thread.
-    discard sendImpl(c, cast[PNimType](getTypeInfo(msg)), unsafeAddr(msg), false)
+    discard sendImpl(c, typeInfo(msg), addr msg, false)
     wasMoved(msg)
 
   proc trySend*[TMsg](c: var Channel[TMsg], msg: sink TMsg): bool {.inline.} =
@@ -381,13 +420,13 @@ when defined(gcDestructors):
     ##
     ## Returns `false` if the message was not sent because number of pending items
     ## in the channel exceeded `maxItems`.
-    result = sendImpl(c, cast[PNimType](getTypeInfo(msg)), unsafeAddr(msg), true)
+    result = sendImpl(c, typeInfo(msg), addr msg, true)
     if result:
       wasMoved(msg)
 else:
   proc send*[TMsg](c: var Channel[TMsg], msg: TMsg) {.inline.} =
     ## Sends a message to a thread. `msg` is deeply copied.
-    discard sendImpl(c, cast[PNimType](getTypeInfo(msg)), unsafeAddr(msg), false)
+    discard sendImpl(c, typeInfo(msg), addr msg, false)
 
   proc trySend*[TMsg](c: var Channel[TMsg], msg: TMsg): bool {.inline.} =
     ## Tries to send a message to a thread.
@@ -396,52 +435,39 @@ else:
     ##
     ## Returns `false` if the message was not sent because number of pending items
     ## in the channel exceeded `maxItems`.
-    result = sendImpl(c, cast[PNimType](getTypeInfo(msg)), unsafeAddr(msg), true)
-
-proc llRecv(q: var RawChannel, res: pointer, typ: PNimType) =
-  inc q.waitingReceivers
-  while q.count <= 0:
-    waitSysCond(q.notEmpty, q.lock)
-  dec q.waitingReceivers
-  rawRecv(q, res, typ)
-  if q.maxItems > 0:
-    # A slot became free; wake up one sender that might be blocked in `send`.
-    signalSysCond(q.notFull)
+    result = sendImpl(c, typeInfo(msg), addr msg, true)
 
 proc recv*[TMsg](c: var Channel[TMsg]): TMsg =
   ## Receives a message from the channel `c`.
   ##
   ## This blocks until a message has arrived!
-  ## You may use `peek proc <#peek,Channel[TMsg]>`_ to avoid the blocking.
+  ## You may use `tryRecv proc <#tryRecv,Channel[TMsg]>`_ to avoid the blocking.
   result = default(TMsg)
-  lockChannel(c):
-    llRecv(c, addr(result), cast[PNimType](getTypeInfo(result)))
+  discard recvImpl(c, addr result, typeInfo(result), false)
 
 proc tryRecv*[TMsg](c: var Channel[TMsg]): tuple[dataAvailable: bool,
                                                   msg: TMsg] =
-  ## Tries to receive a message from the channel `c`, but this can fail
-  ## for all sort of reasons, including contention.
+  ## Tries to receive a message from the channel `c`.
   ##
-  ## If it fails, it returns `(false, default(msg))` otherwise it
-  ## returns `(true, msg)`.
+  ## If the queue is empty, `(false, default(msg))` is returned, otherwise
+  ## `(true, msg)`.
   result = default(tuple[dataAvailable: bool, msg: TMsg])
-  if c.mask != ChannelDeadMask:
-    if tryAcquireSys(c.lock):
-      if c.count > 0:
-        llRecv(c, addr(result.msg), cast[PNimType](getTypeInfo(result.msg)))
-        result.dataAvailable = true
-      releaseSys(c.lock)
+  result.dataAvailable = recvImpl(c, addr result.msg, typeInfo(result.msg), true)
 
 proc peek*[TMsg](c: var Channel[TMsg]): int =
-  ## Returns the current number of messages in the channel `c`.
+  ## Returns the number of messages in the channel `c` at the time of the call.
   ##
-  ## Returns -1 if the channel has been closed.
+  ## Returns -1 after channel has been closed but may deadlock if called during
+  ## closing - unsafe to use for testing if channel is closed.
   ##
-  ## **Note**: This is dangerous to use as it encourages races.
-  ## It's much better to use `tryRecv proc <#tryRecv,Channel[TMsg]>`_ instead.
-  if c.mask != ChannelDeadMask:
-    lockChannel(c):
-      result = c.count
+  ## By the time this function returns, the state might have changed.
+  ##
+  ## **Note**: See `tryRecv proc <#tryRecv,Channel[TMsg]>`_ for a non-blocking
+  ## recv.
+  if c.cap != ChannelDead:
+    acquireSys(c.lock)
+    result = int c.count
+    releaseSys(c.lock)
   else:
     result = -1
 
@@ -451,16 +477,21 @@ proc open*[TMsg](c: var Channel[TMsg], maxItems: int = 0) =
   ## The `send` operation will block until number of unprocessed items is
   ## less than `maxItems`.
   ##
-  ## For unlimited queue set `maxItems` to 0.
+  ## For unbounded queue set `maxItems` to 0.
   initRawChannel(c, maxItems)
 
 proc close*[TMsg](c: var Channel[TMsg]) =
   ## Closes a channel `c` and frees its associated resources.
+  ##
+  ## The caller must ensure that there are no threads accessing or waiting on
+  ## the channel before closing it.
   deinitRawChannel(c)
 
 proc ready*[TMsg](c: var Channel[TMsg]): bool =
-  ## Returns true if some thread is waiting on the channel `c` for
-  ## new messages.
+  ## Returns true if some thread was waiting on the channel `c` for
+  ## new messages at the time of the call.
+  ##
+  ## By the time this function returns, the state might have changed.
   c.waitingReceivers > 0
 
 {.pop.}
