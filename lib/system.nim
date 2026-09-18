cfef6428cdfe2372eae17bcf8dc455effcd5517d
Dylan Katz <dylanjaredkatz@gmail.com>
2026-09-18 16:37:11 -0700

    move -m64 flags into .cfg for bootstrap

--- lib/system.nim
+++ lib/system.nim
@@ -33,11 +33,6 @@ func zeroDefault*[T](_: typedesc[T]): T {.magic: "ZeroDefault".} =
 
 include "system/compilation"
 
-# TODO: remove this once csources has been updated
-when defined(solaris) and defined(amd64):
-  {.passC: "-m64".}
-  {.passL: "-m64".}
-
 {.push warning[GcMem]: off, warning[Uninit]: off.}
 # {.push hints: off.}
 
