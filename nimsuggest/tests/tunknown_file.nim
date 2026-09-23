8ff44e5ee84978598a8c185d0b810cea3ecb5e1f
nitely <ecastroborsani@gmail.com>
2026-09-23 20:00:42 -0300

    test

--- nimsuggest/tests/tunknown_file.nim
+++ nimsuggest/tests/tunknown_file.nim
@@ -1,9 +1,9 @@
 discard """
-$nimsuggest --tester $file
+$nimsuggest --tester --v4 $file
 >def $path/fixtures/munknown_file.nim:4:6
-def;;skProc;;munknown_file.greet;;*;;*fixtures/munknown_file.nim;;1;;5;;"";;100
+def;;skProc;;munknown_file.greet;;*;;*fixtures/munknown_file.nim;;1;;5;;""
 >outline $path/fixtures/munknown_file.nim
-outline;;skProc;;munknown_file.greet;;*;;*fixtures/munknown_file.nim;;1;;5;;"";;100
+outline;;skProc;;munknown_file.greet;;*;;*fixtures/munknown_file.nim;;1;;5;;""
 """
 
 proc unrelated(): int =
