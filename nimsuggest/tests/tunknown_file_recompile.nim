dc299091a5fb240b71351d2fad0234c66f8c8cab
nitely <ecastroborsani@gmail.com>
2026-09-23 22:27:03 -0300

    test

---
+++ nimsuggest/tests/tunknown_file_recompile.nim
@@ -0,0 +1,23 @@
+discard """
+$nimsuggest --tester --v4 $file
+>def $path/fixtures/munknown_file.nim:4:6
+def;;skProc;;munknown_file.greet;;*;;*fixtures/munknown_file.nim;;1;;5;;""
+>changed $path/fixtures/munknown_file.nim
+>chk $path/fixtures/munknown_file.nim
+*
+>def $path/fixtures/munknown_file.nim:4:6
+def;;skProc;;munknown_file.greet;;*;;*fixtures/munknown_file.nim;;1;;5;;""
+>sug $path/fixtures/munknown_file.nim:4:8
+*
+>chk $path/fixtures/munknown_file.nim
+*
+>def $path/fixtures/munknown_file.nim:4:6
+def;;skProc;;munknown_file.greet;;*;;*fixtures/munknown_file.nim;;1;;5;;""
+>outline $path/fixtures/munknown_file.nim
+outline;;skProc;;munknown_file.greet;;*;;*fixtures/munknown_file.nim;;1;;5;;""
+"""
+
+proc unrelated(): int =
+  42
+
+echo unrelated()
